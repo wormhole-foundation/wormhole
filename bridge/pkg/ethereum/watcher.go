9a4e1f396d62017b52b50ef8c788fe574b702429
Hendrik Hofstadt <hendrik@nexantic.com>
2020-08-06 19:00:16 +0200

    Track confirmations in watcher

--- bridge/pkg/ethereum/watcher.go
+++ bridge/pkg/ethereum/watcher.go
@@ -7,22 +7,37 @@ import (
 	"github.com/certusone/wormhole/bridge/pkg/ethereum/abi"
 	"github.com/certusone/wormhole/bridge/pkg/supervisor"
 	"github.com/certusone/wormhole/bridge/pkg/vaa"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	eth_common "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"go.uber.org/zap"
 	"sync"
+	"time"
 )
 
 type (
 	EthBridgeWatcher struct {
-		url    string
-		bridge eth_common.Address
+		url              string
+		bridge           eth_common.Address
+		minConfirmations uint64
+
+		pendingLocks      map[eth_common.Hash]*pendingLock
+		pendingLocksGuard sync.Mutex
 
 		evChan chan *common.ChainLock
 	}
+
+	pendingLock struct {
+		lock *common.ChainLock
+
+		txHash eth_common.Hash
+		height uint64
+	}
 )
 
-func NewEthBridgeWatcher(url string, bridge eth_common.Address, events chan *common.ChainLock) *EthBridgeWatcher {
-	return &EthBridgeWatcher{url: url, bridge: bridge, evChan: events}
+func NewEthBridgeWatcher(url string, bridge eth_common.Address, minConfirmations uint64, events chan *common.ChainLock) *EthBridgeWatcher {
+	return &EthBridgeWatcher{url: url, bridge: bridge, minConfirmations: minConfirmations, evChan: events, pendingLocks: map[eth_common.Hash]*pendingLock{}}
 }
 
 func (e *EthBridgeWatcher) Run(ctx context.Context) error {
@@ -36,13 +51,16 @@ func (e *EthBridgeWatcher) Run(ctx context.Context) error {
 		return fmt.Errorf("could not create wormhole bridge filter: %w", err)
 	}
 
-	sink := make(chan *abi.WormholeBridgeLogTokensLocked)
-	subscription, err := f.WatchLogTokensLocked(nil, sink, nil, nil)
+	sink := make(chan *abi.WormholeBridgeLogTokensLocked, 2)
+	eventSubscription, err := f.WatchLogTokensLocked(&bind.WatchOpts{
+		Context: ctx,
+	}, sink, nil, nil)
 	if err != nil {
 		return fmt.Errorf("failed to subscribe to eth events: %w", err)
 	}
-	defer subscription.Unsubscribe()
+	defer eventSubscription.Unsubscribe()
 
+	// We only add 1 to the wg so we stop when one of the routines stops/fails
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 
@@ -51,8 +69,10 @@ func (e *EthBridgeWatcher) Run(ctx context.Context) error {
 
 		for {
 			select {
-			case e := <-subscription.Err():
-				err = e
+			case e := <-eventSubscription.Err():
+				if err != nil {
+					err = e
+				}
 				return
 			case ev := <-sink:
 				lock := &common.ChainLock{
@@ -64,10 +84,68 @@ func (e *EthBridgeWatcher) Run(ctx context.Context) error {
 					TokenAddress:  ev.Token,
 					Amount:        ev.Amount,
 				}
-				e.evChan <- lock
+
+				supervisor.Logger(ctx).Info("found new lockup transaction", zap.Stringer("tx", ev.Raw.TxHash),
+					zap.Uint64("number", ev.Raw.BlockNumber))
+				e.pendingLocksGuard.Lock()
+				e.pendingLocks[ev.Raw.TxHash] = &pendingLock{
+					lock:   lock,
+					txHash: ev.Raw.TxHash,
+					height: ev.Raw.BlockNumber,
+				}
+				e.pendingLocksGuard.Unlock()
 			}
 		}
+	}()
 
+	// Watch headers
+	headSink := make(chan *types.Header, 2)
+	headerSubscription, err := c.SubscribeNewHead(ctx, headSink)
+	if err != nil {
+		return fmt.Errorf("failed to subscribe to header events: %w", err)
+	}
+	defer headerSubscription.Unsubscribe()
+
+	go func() {
+		defer wg.Done()
+
+		for {
+			select {
+			case e := <-headerSubscription.Err():
+				if err != nil {
+					err = e
+				}
+				return
+			case ev := <-headSink:
+				start := time.Now()
+				supervisor.Logger(ctx).Info("processing new header", zap.Stringer("number", ev.Number))
+				e.pendingLocksGuard.Lock()
+
+				blockNumberU := ev.Number.Uint64()
+				for hash, pLock := range e.pendingLocks {
+
+					// Transaction was dropped and never picked up again
+					if pLock.height+4*e.minConfirmations <= blockNumberU {
+						supervisor.Logger(ctx).Debug("lockup timed out", zap.Stringer("tx", pLock.txHash),
+							zap.Stringer("number", ev.Number))
+						delete(e.pendingLocks, hash)
+						continue
+					}
+
+					// Transaction is now ready
+					if pLock.height+e.minConfirmations <= ev.Number.Uint64() {
+						supervisor.Logger(ctx).Debug("lockup confirmed", zap.Stringer("tx", pLock.txHash),
+							zap.Stringer("number", ev.Number))
+						delete(e.pendingLocks, hash)
+						e.evChan <- pLock.lock
+					}
+				}
+
+				e.pendingLocksGuard.Unlock()
+				supervisor.Logger(ctx).Info("processed new header", zap.Stringer("number", ev.Number),
+					zap.Duration("took", time.Since(start)))
+			}
+		}
 	}()
 
 	supervisor.Signal(ctx, supervisor.SignalHealthy)
