4e6d1079f68de95523559977d37b815035db6afe
Leo <leo@certus.one>
2020-08-16 11:09:03 +0200

    generate.sh: compile buf

--- generate.sh
+++ generate.sh
@@ -3,6 +3,7 @@
 (
   cd tools/
   go build -o bin/protoc-gen-go google.golang.org/protobuf/cmd/protoc-gen-go
+  go build -o bin/buf github.com/bufbuild/buf/cmd/buf
 )
 
 tools/bin/buf protoc --go_out=bridge/pkg/ proto/**/**/** --plugin tools/bin/protoc-gen-go
