dc7a2147b797dd710a7515b2c70324bca54fc96a
Leo <leo@certus.one>
2020-08-17 11:59:52 +0200

    devnet: fix generation of deterministic contract addresses
    We previously had an extra contract that was deleted, which
    changed the contract addresses.
    
    As a workaround, deploy Wormhole twice to fix the tests.

--- ethereum/migrations/1_initial_migration.js
+++ ethereum/migrations/1_initial_migration.js
@@ -3,6 +3,8 @@ const Wormhole = artifacts.require("Wormhole");
 
 module.exports = async function (deployer) {
     await deployer.deploy(WrappedAsset);
+    // FIXME: this offsets the deleted Schnorr contract to keep the previous deterministic addresses
+    await deployer.deploy(WrappedAsset);
     await deployer.deploy(Wormhole, {
         keys: ["0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf"],
         expiration_time: 0
