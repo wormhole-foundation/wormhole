a9dee7da993b36489cdbecfb95358ef8c4327c97
Hendrik Hofstadt <hendrik@nexantic.com>
2020-08-28 08:34:53 +0200

    ethereum: use create2 for wrapped assets

--- ethereum/contracts/Wormhole.sol
+++ ethereum/contracts/Wormhole.sol
@@ -191,7 +191,7 @@ contract Wormhole is ReentrancyGuard {
             mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
             mstore(add(clone, 0x14), targetBytes)
             mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            asset := create(0, clone, 0x37)
+            asset := create2(0, clone, 0x37, seed)
         }
 
         // Call initializer
