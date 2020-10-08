bf83461f6d210b2e8bd53d232d8b8b9461480687
Leopold Schabel <leo@nexantic.com>
2020-10-08 17:24:09 +0200

    tools: actually build buf

--- generate-protos.sh
+++ generate-protos.sh
@@ -4,6 +4,7 @@
   cd tools/
   go build -o bin/protoc-gen-go google.golang.org/protobuf/cmd/protoc-gen-go
   go build -o bin/protoc-gen-go-grpc google.golang.org/grpc/cmd/protoc-gen-go-grpc
+  go build -o bin/buf github.com/bufbuild/buf/cmd/buf
 )
 
 tools/bin/buf protoc \
