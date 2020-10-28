819f89c064a6ae580172ed7b68a94a92b0707753
Leo <leo@certus.one>
2020-10-28 19:05:44 +0100

    node.proto stun and dependencies
    [ghstack-poisoned]

--- generate-protos.sh
+++ generate-protos.sh
@@ -5,10 +5,21 @@
   ./build.sh
 )
 
+(
+  cd third_party/
+  [[ ! -d googleapis ]] && git clone https://github.com/googleapis/googleapis
+  cd googleapis
+  git checkout 24fb9e5d1f37110bfa198189c34324aa3fdb0896
+)
+
 tools/bin/buf protoc \
+  -Iproto \
+  -Ithird_party/googleapis \
   --plugin tools/bin/protoc-gen-go \
   --go_out=bridge/pkg/ proto/**/**/**
 
 tools/bin/buf protoc \
-   --plugin tools/bin/protoc-gen-go-grpc \
-   --go-grpc_out=bridge/pkg/ proto/**/**/**
+  -Iproto \
+  -Ithird_party/googleapis \
+  --plugin tools/bin/protoc-gen-go-grpc \
+  --go-grpc_out=bridge/pkg/ proto/**/**/**
