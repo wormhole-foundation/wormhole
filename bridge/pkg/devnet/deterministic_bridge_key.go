bbf479871f47f2fa52f8d375203b475bcf6bf4ae
Leo <leo@certus.one>
2020-11-18 14:30:25 +0100

    pkg/devnet: split up deterministic key generation functions
    Only moved code, no functional changes.

--- bridge/pkg/devnet/deterministic_bridge_key.go
+++ bridge/pkg/devnet/deterministic_bridge_key.go
@@ -4,8 +4,6 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	mathrand "math/rand"
-
-	"github.com/libp2p/go-libp2p-core/crypto"
 )
 
 // DeterministicEcdsaKeyByIndex generates a deterministic ecdsa.PrivateKey from a given index.
@@ -19,15 +17,4 @@ func DeterministicEcdsaKeyByIndex(c elliptic.Curve, idx uint64) *ecdsa.PrivateKe
 	}
 
 	return key
-}
-
-// DeterministicP2PPrivKeyByIndex generates a deterministic libp2p crypto.PrivateKey from a given index.
-func DeterministicP2PPrivKeyByIndex(idx int64) crypto.PrivKey {
-	r := mathrand.New(mathrand.NewSource(int64(idx)))
-	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.Ed25519, -1, r)
-	if err != nil {
-		panic(err)
-	}
-
-	return priv
 }
---
+++ bridge/pkg/devnet/deterministic_p2p_key.go
@@ -0,0 +1,18 @@
+package devnet
+
+import (
+	mathrand "math/rand"
+
+	"github.com/libp2p/go-libp2p-core/crypto"
+)
+
+// DeterministicP2PPrivKeyByIndex generates a deterministic libp2p crypto.PrivateKey from a given index.
+func DeterministicP2PPrivKeyByIndex(idx int64) crypto.PrivKey {
+	r := mathrand.New(mathrand.NewSource(int64(idx)))
+	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.Ed25519, -1, r)
+	if err != nil {
+		panic(err)
+	}
+
+	return priv
+}
