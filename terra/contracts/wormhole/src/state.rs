436b35302b3b08c60928faae9d57a4091265713a
Yuriy Savchenko <yuriy.savchenko@gmail.com>
2021-02-10 15:34:30 +0200

    Added VAA verification query to the Terra smart contract, refactoring

--- terra/contracts/wormhole/src/state.rs
+++ terra/contracts/wormhole/src/state.rs
@@ -7,6 +7,11 @@ use cosmwasm_storage::{
     Singleton,
 };
 
+use crate::byte_utils::ByteUtils;
+use crate::error::ContractError;
+
+use sha3::{Digest, Keccak256};
+
 pub static CONFIG_KEY: &[u8] = b"config";
 pub static GUARDIAN_SET_KEY: &[u8] = b"guardian_set";
 pub static WRAPPED_ASSET_KEY: &[u8] = b"wrapped_asset";
@@ -29,6 +34,85 @@ pub struct ConfigInfo {
 
     // If true the contract is active and functioning
     pub is_active: bool,
+}
+
+// Validator Action Approval(VAA) data
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ParsedVAA {
+    pub version: u8,
+    pub guardian_set_index: u32,
+    pub len_signers: usize,
+    pub hash: Vec<u8>,
+    pub action: u8,
+    pub payload: Vec<u8>,
+}
+
+impl ParsedVAA {
+    /* VAA format:
+
+    header (length 6):
+    0   uint8   version (0x01)
+    1   uint32  guardian set index
+    5   uint8   len signatures
+
+    per signature (length 66):
+    0   uint8       index of the signer (in guardian keys)
+    1   [65]uint8   signature
+
+    body:
+    0   uint32  unix seconds
+    4   uint8   action
+    5   [payload_size]uint8 payload */
+
+    pub const HEADER_LEN: usize = 6;
+    pub const SIGNATURE_LEN: usize = 66;
+
+    pub const GUARDIAN_SET_INDEX_POS: usize = 1;
+    pub const LEN_SIGNER_POS: usize = 5;
+
+    pub const VAA_ACTION_POS: usize = 4;
+    pub const VAA_PAYLOAD_POS: usize = 5;
+
+    // Signature data offsets in the signature block
+    pub const SIG_DATA_POS: usize = 1;
+    // Signature length minus recovery id at the end
+    pub const SIG_DATA_LEN: usize = 64;
+    // Recovery byte is last after the main signature
+    pub const SIG_RECOVERY_POS: usize = Self::SIG_DATA_POS + Self::SIG_DATA_LEN;
+
+    pub fn deserialize(data: &[u8]) -> StdResult<Self> {
+        let version = data.get_u8(0);
+
+        // Load 4 bytes starting from index 1
+        let guardian_set_index: u32 = data.get_u32(Self::GUARDIAN_SET_INDEX_POS);
+        let len_signers = data.get_u8(Self::LEN_SIGNER_POS) as usize;
+        let body_offset: usize = Self::HEADER_LEN + Self::SIGNATURE_LEN * len_signers as usize;
+
+        // Hash the body
+        if body_offset >= data.len() {
+            return ContractError::InvalidVAA.std_err();
+        }
+        let body = &data[body_offset..];
+        let mut hasher = Keccak256::new();
+        hasher.update(body);
+        let hash = hasher.finalize().to_vec();
+
+        // Signatures valid, apply VAA
+        if body_offset + Self::VAA_PAYLOAD_POS > data.len() {
+            return ContractError::InvalidVAA.std_err();
+        }
+        let action = data.get_u8(body_offset + Self::VAA_ACTION_POS);
+        let payload = &data[body_offset + Self::VAA_PAYLOAD_POS..];
+
+        Ok(ParsedVAA {
+            version,
+            guardian_set_index,
+            len_signers,
+            hash,
+            action,
+            payload: payload.to_vec(),
+        })
+    }
 }
 
 // Guardian address
