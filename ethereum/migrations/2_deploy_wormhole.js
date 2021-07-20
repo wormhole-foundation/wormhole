58fd2bcbcbeeb16d29405386d4834edabd3383f7
valentin <valentin@nexantic.com>
2021-07-20 19:59:56 +0200

    clean up deployment & add etherscan verification
    Change-Id: Idd37f92c5ca61710bece77a473fad35af44ac8f9

--- ethereum/migrations/2_deploy_wormhole.js
+++ ethereum/migrations/2_deploy_wormhole.js
@@ -1,13 +1,13 @@
+require('dotenv').config({ path: "../.env" });
+
 const Implementation = artifacts.require("Implementation");
 const Wormhole = artifacts.require("Wormhole");
 
-const initialSigners = [
-    // testSigner 1 & 2
-    "0xbeFA429d57cD18b7F8A4d91A2da9AB4AF05d0FBe",
-]
-const chainId = "0x2";
-const governanceChainId = "0x3";
-const governanceContract = "0x0000000000000000000000000000000000000000000000000000000000000004"; // bytes32
+// CONFIG
+const initialSigners = JSON.parse(process.env.INIT_SIGNERS);
+const chainId = process.env.INIT_CHAIN_ID;
+const governanceChainId = process.env.INIT_GOV_CHAIN_ID;
+const governanceContract = process.env.INIT_GOV_CONTRACT; // bytes32
 
 module.exports = async function (deployer) {
     // deploy implementation
--- ethereum/truffle-config.js
+++ ethereum/truffle-config.js
@@ -1,88 +1,35 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
+require('dotenv').config({ path: ".env" });
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const infuraKey = "94aa97cf706648c49af56e080b0f1bc3";
-let mnemonic = "own great grid romance once pilot draw chuckle network desert race dream";
 
 module.exports = {
-    /**
-     * Networks define how you connect to your ethereum client and let you set the
-     * defaults web3 uses to send transactions. If you don't specify one truffle
-     * will spin up a development blockchain for you on port 9545 when you
-     * run `develop` or `test`. You can ask a truffle command to use a specific
-     * network from the command line, e.g
-     *
-     * $ truffle test --network <network-name>
-     */
-
     networks: {
-        // Useful for testing. The `development` name is special - truffle uses it by default
-        // if it's defined here and no other network is specified at the command line.
-        // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-        // tab if you use this network and you must also set the `host`, `port` and `network_id`
-        // options below to some value.
-        //
         development: {
-            host: "127.0.0.1",     // Localhost (default: none)
-            port: 8545,            // Standard Ethereum port (default: none)
-            network_id: "*",       // Any network (default: none)
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*",
         },
-        // Another network with more advanced options...
-        // advanced: {
-        // port: 8777,             // Custom port
-        // network_id: 1342,       // Custom network
-        // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-        // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-        // from: <address>,        // Account to send txs from (default: accounts[0])
-        // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-        // },
-        // Useful for deploying to a public network.
-        // NB: It's important to wrap the provider as a function.
         rinkeby: {
-            provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/94aa97cf706648c49af56e080b0f1bc3`),
-            network_id: 4,       // Ropsten's id
-            gas: 5500000,        // Ropsten has a lower block limit than mainnet
-            confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-            timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-            skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rinkeby.infura.io/v3/`+process.env.INFURA_KEY),
+            network_id: 4,
+            gas: 5500000,
+            confirmations: 2,
+            timeoutBlocks: 200,
+            skipDryRun: true
         },
-        // Useful for private networks
-        // private: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-        // network_id: 2111,   // This network is yours, in the cloud.
-        // production: true    // Treats this network as if it was a public net. (default: false)
-        // }
+        goerli: {
+            provider: () => {
+                return new HDWalletProvider(process.env.MNEMONIC, 'https://goerli.infura.io/v3/'+process.env.INFURA_KEY)
+            },
+            network_id: '5',
+            gas: 4465030,
+            gasPrice: 10000000000,
+        },
     },
 
-    // Set default mocha options here, use special reporters etc.
-    mocha: {
-        // timeout: 100000
-    },
-
-    // Configure your compilers
     compilers: {
         solc: {
-            version: "0.8.4",    // Fetch exact version from solc-bin (default: truffle's version)
-            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-            settings: {          // See the solidity docs for advice about optimization and evmVersion
+            version: "0.8.4",
+            settings: {
                  optimizer: {
                    enabled: true,
                    runs: 200
@@ -92,6 +39,11 @@ module.exports = {
     },
 
     plugins: [
-        "@chainsafe/truffle-plugin-abigen"
+        "@chainsafe/truffle-plugin-abigen",
+        "truffle-plugin-verify"
     ],
+
+    api_keys: {
+        etherscan: process.env.ETHERSCAN_KEY
+    },
 };
