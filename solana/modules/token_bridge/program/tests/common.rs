28c0c3c1955132a81e18733c7a8fc5ee508dbe68
Reisen <dparyente@jumptrading.com>
2021-07-23 14:13:47 +0000

    Add Token Bridge test for initialize
    Change-Id: Ia65c14b7a7bc6712e79db2b6f4244cb93f63093b

---
+++ solana/modules/token_bridge/program/tests/common.rs
@@ -0,0 +1,233 @@
+#![allow(warnings)]
+
+use solana_sdk::rent::Rent;
+use spl_token::state::Mint;
+use borsh::{
+    BorshDeserialize,
+    BorshSerialize,
+};
+use byteorder::{
+    BigEndian,
+    WriteBytesExt,
+};
+use hex_literal::hex;
+use secp256k1::{
+    Message as Secp256k1Message,
+    PublicKey,
+    SecretKey,
+};
+use sha3::Digest;
+use solana_client::{
+    client_error::ClientError,
+    rpc_client::RpcClient,
+    rpc_config::RpcSendTransactionConfig,
+};
+use solana_program::{
+    borsh::try_from_slice_unchecked,
+    hash,
+    instruction::{
+        AccountMeta,
+        Instruction,
+    },
+    program_pack::Pack,
+    pubkey::Pubkey,
+    system_instruction::{
+        self,
+        create_account,
+    },
+    system_program,
+    sysvar,
+};
+use solana_sdk::{
+    commitment_config::CommitmentConfig,
+    secp256k1_instruction::new_secp256k1_instruction,
+    signature::{
+        read_keypair_file,
+        Keypair,
+        Signature,
+        Signer,
+    },
+    transaction::Transaction,
+};
+use std::{
+    convert::TryInto,
+    env,
+    io::{
+        Cursor,
+        Write,
+    },
+    time::{
+        Duration,
+        SystemTime,
+    },
+};
+
+use token_bridge::{
+    accounts::*,
+    instruction,
+    instructions,
+    types::*,
+    Initialize,
+};
+
+use solitaire::{
+    processors::seeded::Seeded,
+    AccountState,
+};
+
+pub use helpers::*;
+
+/// Simple API wrapper for quickly preparing and sending transactions.
+pub fn execute(
+    client: &RpcClient,
+    payer: &Keypair,
+    signers: &[&Keypair],
+    instructions: &[Instruction],
+    commitment_level: CommitmentConfig,
+) -> Result<Signature, ClientError> {
+    let mut transaction = Transaction::new_with_payer(instructions, Some(&payer.pubkey()));
+    let recent_blockhash = client.get_recent_blockhash().unwrap().0;
+    transaction.sign(&signers.to_vec(), recent_blockhash);
+    client.send_and_confirm_transaction_with_spinner_and_config(
+        &transaction,
+        commitment_level,
+        RpcSendTransactionConfig {
+            skip_preflight: true,
+            preflight_commitment: None,
+            encoding: None,
+        },
+    )
+}
+
+mod helpers {
+    use token_bridge::{CompleteNativeData, TransferNativeData};
+
+    use super::*;
+
+    /// Initialize the test environment, spins up a solana-test-validator in the background so that
+    /// each test has a fresh environment to work within.
+    pub fn setup() -> (Keypair, RpcClient, Pubkey, Pubkey) {
+        let payer = env::var("BRIDGE_PAYER").unwrap_or("./payer.json".to_string());
+        let rpc_address = env::var("BRIDGE_RPC").unwrap_or("http://127.0.0.1:8899".to_string());
+        let payer = read_keypair_file(payer).unwrap();
+        let rpc = RpcClient::new(rpc_address);
+
+        let (program, token_program) = (
+            env::var("BRIDGE_PROGRAM")
+                .unwrap_or("Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o".to_string())
+                .parse::<Pubkey>()
+                .unwrap(),
+            env::var("TOKEN_BRIDGE_PROGRAM")
+                .unwrap_or("GatRhT513sQ6VcavtKVHv12S71pcpthjcgVFpmKkTs6H".to_string())
+                .parse::<Pubkey>()
+                .unwrap(),
+        );
+
+        (payer, rpc, program, token_program)
+    }
+
+    /// Wait for a single transaction to fully finalize, guaranteeing chain state has been
+    /// confirmed. Useful for consistently fetching data during state checks.
+    pub fn sync(client: &RpcClient, payer: &Keypair) {
+        execute(
+            client,
+            payer,
+            &[payer],
+            &[system_instruction::transfer(
+                &payer.pubkey(),
+                &payer.pubkey(),
+                1,
+            )],
+            CommitmentConfig::finalized(),
+        )
+        .unwrap();
+    }
+
+    /// Fetch account data, the loop is there to re-attempt until data is available.
+    pub fn get_account_data<T: BorshDeserialize>(client: &RpcClient, account: &Pubkey) -> T {
+        let account = client.get_account(account).unwrap();
+        T::try_from_slice(&account.data).unwrap()
+    }
+
+    pub fn initialize_bridge(
+        client: &RpcClient,
+        program: &Pubkey,
+        payer: &Keypair,
+    ) -> Result<Signature, ClientError> {
+        let initial_guardians = &[[1u8; 20]];
+        execute(
+            client,
+            payer,
+            &[payer],
+            &[bridge::instructions::initialize(
+                *program,
+                payer.pubkey(),
+                50,
+                2_000_000_000,
+                initial_guardians,
+            )
+            .unwrap()],
+            CommitmentConfig::processed(),
+        )
+    }
+
+    pub fn create_mint(
+        client: &RpcClient,
+        payer: &Keypair,
+        mint_authority: &Pubkey,
+        mint: &Keypair,
+    ) -> Result<Signature, ClientError> {
+        execute(
+            client,
+            payer,
+            &[payer, mint],
+            &[
+                solana_sdk::system_instruction::create_account(
+                    &payer.pubkey(),
+                    &mint.pubkey(),
+                    Rent::default().minimum_balance(spl_token::state::Mint::LEN),
+                    spl_token::state::Mint::LEN as u64,
+                    &spl_token::id(),
+                ),
+                spl_token::instruction::initialize_mint(
+                    &spl_token::id(),
+                    &mint.pubkey(),
+                    mint_authority,
+                    None,
+                    8,
+                ).unwrap(),
+            ],
+            CommitmentConfig::processed(),
+        )
+    }
+
+    pub fn create_token_account(
+        client: &RpcClient,
+        payer: &Keypair,
+        token_acc: &Keypair,
+        token_authority: Pubkey,
+        mint: Pubkey,
+    ) -> Result<Signature, ClientError> {
+        execute(
+            client,
+            payer,
+            &[payer, token_acc],
+            &[
+                solana_sdk::system_instruction::create_account(
+                    &payer.pubkey(),
+                    &token_acc.pubkey(),
+                    Rent::default().minimum_balance(spl_token::state::Account::LEN),
+                    spl_token::state::Account::LEN as u64,
+                    &spl_token::id(),
+                ),
+                spl_token::instruction::initialize_account(
+                    &spl_token::id(),
+                    &token_acc.pubkey(),
+                    &mint,
+                    &token_authority,
+                ).unwrap(),
+            ],
+            CommitmentConfig::processed(),
+        )
+    }
+}
---
+++ solana/modules/token_bridge/program/tests/integration.rs
@@ -0,0 +1,213 @@
+#![allow(warnings)]
+
+use borsh::BorshSerialize;
+use byteorder::{
+    BigEndian,
+    WriteBytesExt,
+};
+use hex_literal::hex;
+use rand::Rng;
+use secp256k1::{
+    Message as Secp256k1Message,
+    PublicKey,
+    SecretKey,
+};
+use sha3::Digest;
+use solana_client::rpc_client::RpcClient;
+use solana_program::{
+    borsh::try_from_slice_unchecked,
+    hash,
+    instruction::{
+        AccountMeta,
+        Instruction,
+    },
+    program_pack::Pack,
+    pubkey::Pubkey,
+    system_instruction::{
+        self,
+        create_account,
+    },
+    system_program,
+    sysvar,
+};
+use solana_sdk::{
+    signature::{
+        read_keypair_file,
+        Keypair,
+        Signer,
+    },
+    transaction::Transaction,
+};
+use solitaire::{
+    processors::seeded::Seeded,
+    AccountState,
+};
+use std::{
+    convert::TryInto,
+    io::{
+        Cursor,
+        Write,
+    },
+    time::{
+        Duration,
+        SystemTime,
+    },
+};
+
+use bridge::{
+    accounts::{
+        Bridge,
+        FeeCollector,
+        GuardianSet,
+        GuardianSetDerivationData,
+        Message,
+        MessageDerivationData,
+        SignatureSet,
+        SignatureSetDerivationData,
+    },
+    instruction,
+    types::{
+        BridgeConfig,
+        BridgeData,
+        GovernancePayloadGuardianSetChange,
+        GovernancePayloadSetMessageFee,
+        GovernancePayloadTransferFees,
+        GuardianSetData,
+        PostedMessage,
+        PostedMessageData,
+        SequenceTracker,
+        SignatureSet as SignatureSetData,
+    },
+    Initialize,
+    PostVAA,
+    PostVAAData,
+    SerializePayload,
+    Signature,
+};
+use primitive_types::U256;
+use std::collections::HashMap;
+use std::str::FromStr;
+use std::time::UNIX_EPOCH;
+
+
+mod common;
+
+struct Context {
+    /// Address of the core bridge contract.
+    bridge: Pubkey,
+
+    /// Shared RPC client for tests to make transactions with.
+    client: RpcClient,
+
+    /// Payer key with a ton of lamports to ease testing with.
+    payer: Keypair,
+
+    /// Track nonces throughout the tests.
+    seq: Sequencer,
+
+    /// Address of the token bridge itself that we wish to test.
+    token_bridge: Pubkey,
+
+    /// Keypairs for mint information, required in multiple tests.
+    mint_authority: Keypair,
+    mint: Keypair,
+    mint_meta: Keypair,
+
+    /// Keypairs for test token accounts.
+    token_authority: Keypair,
+    token_account: Keypair,
+}
+
+/// Small helper to track and provide sequences during tests. This is in particular needed for
+/// guardian operations that require them for derivations.
+struct Sequencer {
+    sequences: HashMap<[u8; 32], u64>,
+}
+
+impl Sequencer {
+    fn next(&mut self, emitter: [u8; 32]) -> u64 {
+        let entry = self.sequences.entry(emitter).or_insert(0);
+        *entry += 1;
+        *entry - 1
+    }
+
+    fn peek(&mut self, emitter: [u8; 32]) -> u64 {
+        *self.sequences.entry(emitter).or_insert(0)
+    }
+}
+
+#[test]
+fn run_integration_tests() {
+    let (payer, client, bridge, token_bridge) = common::setup();
+
+    // Setup a Bridge to test against.
+    println!("Bridge: {}", bridge);
+    common::initialize_bridge(&client, &bridge, &payer);
+
+    // Context for test environment.
+    let mut context = Context {
+        seq: Sequencer { 
+            sequences: HashMap::new(),
+        },
+        bridge,
+        client,
+        payer,
+        token_bridge,
+        mint_authority: Keypair::new(),
+        mint: Keypair::new(),
+        mint_meta: Keypair::new(),
+        token_account: Keypair::new(),
+        token_authority: Keypair::new(),
+    };
+
+    // Create a mint for use within tests.
+    common::create_mint(
+        &context.client,
+        &context.payer,
+        &context.mint_authority.pubkey(),
+        &context.mint
+    ).unwrap();
+
+    // Create Token accounts for use within tests.
+    common::create_token_account(
+        &context.client,
+        &context.payer,
+        &context.token_account,
+        context.token_authority.pubkey(),
+        context.mint.pubkey(),
+    ).unwrap();
+
+    common::sync(&context.client, &context.payer);
+
+    // Initialize the bridge and verify the bridges state.
+    test_initialize(&mut context);
+}
+
+fn test_initialize(context: &mut Context) {
+    println!("Initialize");
+    use token_bridge::{
+        accounts::ConfigAccount,
+        types::{
+            Config,
+            FeeStructure,
+        },
+    };
+
+    let Context {
+        ref payer,
+        ref client,
+        ref bridge,
+        ref token_bridge,
+        ..
+    } = context;
+
+    common::initialize(client, token_bridge, payer, &bridge).unwrap();
+    common::sync(client, payer);
+
+    // Verify Token Bridge State
+    let config_key = ConfigAccount::<'_, { AccountState::Uninitialized }>::key(None, &token_bridge);
+    let config: Config = common::get_account_data(client, &config_key);
+    assert_eq!(config.wormhole_bridge, *bridge);
+    assert_eq!(config.fees.usd_ephemeral, 0);
+    assert_eq!(config.fees.usd_persistent, 0);
+}
