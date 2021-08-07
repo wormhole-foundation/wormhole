b035ebc438bf70ea2a533bd8815355884ece6ce0
Evan Gray <56235822+evan-gray@users.noreply.github.com>
2021-08-07 00:26:23 -0400

    bridge_ui/client: fix wasm targets
    Change-Id: Icab8f0acc0fbe1097d2b10f43378c539e55e165f

--- generate-wasm.sh
+++ generate-wasm.sh
@@ -11,18 +11,19 @@ set -euo pipefail
     -e EMITTER_ADDRESS=11111111111111111111111111111115 \
     localhost/certusone/wormhole-wasmpack:latest \
     /usr/local/cargo/bin/wasm-pack build --target bundler -- --features wasm
-  docker run --rm -it --workdir /usr/src/bridge/bridge/program \
-    -v $(pwd)/../clients/solana/pkg:/usr/src/bridge/bridge/program/pkg \
-    -e EMITTER_ADDRESS=11111111111111111111111111111115 \
-    localhost/certusone/wormhole-wasmpack:latest \
-    /usr/local/cargo/bin/wasm-pack build --target nodejs -- --features wasm
   docker run --rm -it --workdir /usr/src/bridge/modules/token_bridge/program \
     -v $(pwd)/../bridge_ui/rust_modules/token:/usr/src/bridge/modules/token_bridge/program/pkg \
     -e EMITTER_ADDRESS=11111111111111111111111111111115 \
     localhost/certusone/wormhole-wasmpack:latest \
     /usr/local/cargo/bin/wasm-pack build --target bundler -- --features wasm
+  docker run --rm -it --workdir /usr/src/bridge/bridge/program \
+    -v $(pwd)/../clients/solana/pkg:/usr/src/bridge/bridge/program/pkg \
+    -e EMITTER_ADDRESS=11111111111111111111111111111115 \
+    localhost/certusone/wormhole-wasmpack:latest \
+    /usr/local/cargo/bin/wasm-pack build --target nodejs -- --features wasm
+  cp $(pwd)/../clients/solana/pkg/. $(pwd)/../clients/token_bridge/pkg/core -R
   docker run --rm -it --workdir /usr/src/bridge/modules/token_bridge/program \
-    -v $(pwd)/../clients/token_bridge/pkg:/usr/src/bridge/modules/token_bridge/program/pkg \
+    -v $(pwd)/../clients/token_bridge/pkg/token:/usr/src/bridge/modules/token_bridge/program/pkg \
     -e EMITTER_ADDRESS=11111111111111111111111111111115 \
     localhost/certusone/wormhole-wasmpack:latest \
     /usr/local/cargo/bin/wasm-pack build --target nodejs -- --features wasm
