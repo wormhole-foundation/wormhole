f53d180753b478b38b204faf039e2a8f5dfaca5a
Evan Gray <56235822+evan-gray@users.noreply.github.com>
2021-08-10 19:46:13 -0400

    scripts: try catch truffle execs
    Change-Id: Id9b27812cd393056ef373d8ec2569ba75f0caea8

--- ethereum/scripts/register_solana_chain.js
+++ ethereum/scripts/register_solana_chain.js
@@ -6,15 +6,20 @@ const TokenImplementation = artifacts.require("TokenImplementation");
 const BridgeImplementationFullABI = jsonfile.readFileSync("../build/contracts/BridgeImplementation.json").abi
 
 module.exports = async function (callback) {
-    const accounts = await web3.eth.getAccounts();
-    const initialized = new web3.eth.Contract(BridgeImplementationFullABI, TokenBridge.address);
+    try {
+        const accounts = await web3.eth.getAccounts();
+        const initialized = new web3.eth.Contract(BridgeImplementationFullABI, TokenBridge.address);
 
-    // Register the Solana endpoint
-    await initialized.methods.registerChain("0x01000000000100c9f4230109e378f7efc0605fb40f0e1869f2d82fda5b1dfad8a5a2dafee85e033d155c18641165a77a2db6a7afbf2745b458616cb59347e89ae0c7aa3e7cc2d400000000010000000100010000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000546f6b656e4272696467650100000001c69a1b1a65dd336bf1df6a77afb501fc25db7fc0938cb08595a9ef473265cb4f").send({
-        value: 0,
-        from: accounts[0],
-        gasLimit: 2000000
-    });
+        // Register the Solana endpoint
+        await initialized.methods.registerChain("0x01000000000100c9f4230109e378f7efc0605fb40f0e1869f2d82fda5b1dfad8a5a2dafee85e033d155c18641165a77a2db6a7afbf2745b458616cb59347e89ae0c7aa3e7cc2d400000000010000000100010000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000546f6b656e4272696467650100000001c69a1b1a65dd336bf1df6a77afb501fc25db7fc0938cb08595a9ef473265cb4f").send({
+            value: 0,
+            from: accounts[0],
+            gasLimit: 2000000
+        });
 
-    callback();
+        callback();
+    }
+    catch (e) {
+        callback(e);
+    }
 }
