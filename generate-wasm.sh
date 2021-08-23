fdceea95159613e5b8dbcdae2392e32969843fa8
Hendrik Hofstadt <hendrik@nexantic.com>
2021-08-23 20:02:55 +0200

    Fix wasm generation scripts
    Change-Id: I23aa186d7cb0b805b6a6cc30063e8be3147fd7d8

--- generate-wasm.sh
+++ generate-wasm.sh
@@ -10,22 +10,26 @@ set -euo pipefail
   docker run --rm -it --workdir /usr/src/bridge/bridge/program \
     -v $(pwd)/../sdk/js/src/solana/core:/usr/src/bridge/bridge/program/pkg \
     -e EMITTER_ADDRESS=11111111111111111111111111111115 \
+    -e BRIDGE_ADDRESS=11111111111111111111111111111115 \
     localhost/certusone/wormhole-wasmpack:latest \
     /usr/local/cargo/bin/wasm-pack build --target bundler -- --features wasm
   docker run --rm -it --workdir /usr/src/bridge/modules/token_bridge/program \
     -v $(pwd)/../sdk/js/src/solana/token:/usr/src/bridge/modules/token_bridge/program/pkg \
     -e EMITTER_ADDRESS=11111111111111111111111111111115 \
+    -e BRIDGE_ADDRESS=11111111111111111111111111111115 \
     localhost/certusone/wormhole-wasmpack:latest \
     /usr/local/cargo/bin/wasm-pack build --target bundler -- --features wasm
   docker run --rm -it --workdir /usr/src/bridge/bridge/program \
     -v $(pwd)/../clients/solana/pkg:/usr/src/bridge/bridge/program/pkg \
     -e EMITTER_ADDRESS=11111111111111111111111111111115 \
+    -e BRIDGE_ADDRESS=11111111111111111111111111111115 \
     localhost/certusone/wormhole-wasmpack:latest \
     /usr/local/cargo/bin/wasm-pack build --target nodejs -- --features wasm
   cp $(pwd)/../clients/solana/pkg/. $(pwd)/../clients/token_bridge/pkg/core -R
   docker run --rm -it --workdir /usr/src/bridge/modules/token_bridge/program \
     -v $(pwd)/../clients/token_bridge/pkg/token:/usr/src/bridge/modules/token_bridge/program/pkg \
     -e EMITTER_ADDRESS=11111111111111111111111111111115 \
+    -e BRIDGE_ADDRESS=11111111111111111111111111111115 \
     localhost/certusone/wormhole-wasmpack:latest \
     /usr/local/cargo/bin/wasm-pack build --target nodejs -- --features wasm
 )
