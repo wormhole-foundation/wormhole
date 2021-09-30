b01ad107e7eed0aaaf592804c45339ae0f98575c
Hendrik Hofstadt <hendrik@nexantic.com>
2021-09-30 18:13:37 +0200

    Add governance VAA initializer
    Change-Id: I4c5a050e5b17ed8b278f91e26bf61adacc45ecc5

---
+++ node/pkg/vaa/governance.go
@@ -0,0 +1,25 @@
+package vaa
+
+import (
+	"time"
+)
+
+var governanceEmitter = Address{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4}
+var governanceChain = ChainIDSolana
+
+func CreateGovernanceVAA(nonce uint32, sequence uint64, guardianSetIndex uint32, payload []byte) *VAA {
+	vaa := &VAA{
+		Version:          1,
+		GuardianSetIndex: guardianSetIndex,
+		Signatures:       nil,
+		Timestamp:        time.Unix(0, 0),
+		Nonce:            nonce,
+		Sequence:         sequence,
+		ConsistencyLevel: 32,
+		EmitterChain:     governanceChain,
+		EmitterAddress:   governanceEmitter,
+		Payload:          payload,
+	}
+
+	return vaa
+}
