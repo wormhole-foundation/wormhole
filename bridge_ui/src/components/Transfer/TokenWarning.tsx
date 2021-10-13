75c818d2bf6ef97cab8dd7285124a1fd51ce5d50
Chase Moran <chasemoran45@gmail.com>
2021-10-13 11:53:48 -0400

    bridge_ui:market warning for wbnb and wsol
    Change-Id: I7da1b079a49258ddebb0bd9c7a52d335ee4afe04

--- bridge_ui/src/components/Transfer/TokenWarning.tsx
+++ bridge_ui/src/components/Transfer/TokenWarning.tsx
@@ -1,7 +1,9 @@
 import {
   ChainId,
+  CHAIN_ID_BSC,
   CHAIN_ID_ETH,
   CHAIN_ID_SOLANA,
+  WSOL_ADDRESS,
 } from "@certusone/wormhole-sdk";
 import { getAddress } from "@ethersproject/address";
 import { Alert } from "@material-ui/lab";
@@ -10,6 +12,7 @@ import {
   ETH_TOKENS_THAT_CAN_BE_SWAPPED_ON_SOLANA,
   ETH_TOKENS_THAT_EXIST_ELSEWHERE,
   SOLANA_TOKENS_THAT_EXIST_ELSEWHERE,
+  WBNB_ADDRESS,
 } from "../../utils/consts";
 
 export default function TokenWarning({
@@ -36,9 +39,29 @@ export default function TokenWarning({
         : undefined,
     [sourceChain, tokenAddress, symbol]
   );
+  const marketsWarning = useMemo(() => {
+    let show = false;
+    if (sourceChain === CHAIN_ID_SOLANA && tokenAddress === WSOL_ADDRESS) {
+      show = true;
+    } else if (sourceChain === CHAIN_ID_BSC && tokenAddress === WBNB_ADDRESS) {
+      show = true;
+    }
+    if (show) {
+      return `As of 10/13/2021, markets have not been established for ${
+        symbol ? "Wormhole-wrapped " + symbol : "this token"
+      }. Please verify this token will be useful on the target chain.`;
+    } else {
+      return null;
+    }
+  }, [sourceChain, tokenAddress, symbol]);
+
   return tokenConflictingNativeWarning ? (
     <Alert severity="warning" variant="outlined">
       {tokenConflictingNativeWarning}
+    </Alert>
+  ) : marketsWarning ? (
+    <Alert severity="warning" variant="outlined">
+      {marketsWarning}
     </Alert>
   ) : sourceChain === CHAIN_ID_ETH &&
     tokenAddress &&
