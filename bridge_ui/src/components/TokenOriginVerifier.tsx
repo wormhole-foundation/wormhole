0d7e145c63cdcb378507e9c39342bbf9ae09ed8d
Evan Gray <battledingo@gmail.com>
2021-11-10 11:46:46 -0500

    bridge_ui: add tokenName to TokenOriginVerifier
    Change-Id: Id7cdd87b35ab649f664c50b319dd63e5a0847a37

--- bridge_ui/src/components/TokenOriginVerifier.tsx
+++ bridge_ui/src/components/TokenOriginVerifier.tsx
@@ -91,6 +91,7 @@ function PrimaryAssetInfomation({
               address={originAsset}
               chainId={originChain}
               symbol={metadata.data?.get(originAsset)?.symbol}
+              tokenName={metadata.data?.get(originAsset)?.tokenName}
             />
           </Typography>
         </div>
@@ -132,6 +133,10 @@ function SecondaryAssetInformation({
             metadata.data?.get(originAssetInfo.originAddress || "")?.symbol ||
             undefined
           }
+          tokenName={
+            metadata.data?.get(originAssetInfo.originAddress || "")
+              ?.tokenName || undefined
+          }
         />
       </div>
     </div>
@@ -158,6 +163,10 @@ function SecondaryAssetInformation({
           address={foreignAssetInfo.address || undefined}
           symbol={
             metadata.data?.get(foreignAssetInfo.address || "")?.symbol ||
+            undefined
+          }
+          tokenName={
+            metadata.data?.get(foreignAssetInfo.address || "")?.tokenName ||
             undefined
           }
         />
