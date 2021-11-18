06c103d0e50a30587790942751cdf3927a9e88fb
Stan Drozd <stan@nexantic.com>
2021-11-18 17:15:50 +0100

    p2w: configurability and sane envs
    commit-id:f10fd90e

--- third_party/pyth/p2w-relay/src/index.ts
+++ third_party/pyth/p2w-relay/src/index.ts
@@ -10,9 +10,10 @@ import {ethers} from "ethers";
 
 import {getSignedAttestation, parseAttestation, p2w_core, sol_addr2buf} from "@certusone/p2w-sdk";
 
-interface AttestationState {
+interface NewAttestationsResponse {
     pendingSeqnos: Array<number>,
 }
+
 
 async function readinessProbeRoutine(port: number) {
     let srv = net.createServer();
@@ -22,25 +23,32 @@ async function readinessProbeRoutine(port: number) {
 
 (async () => {
 
+    // p2w-attest exposes an HTTP endpoint that shares the currently pending sequence numbers
     const P2W_ATTESTATIONS_HOST = process.env.P2W_ATTESTATIONS_HOST || "p2w-attest";
     const P2W_ATTESTATIONS_PORT = Number(process.env.P2W_ATTESTATIONS_PORT || "4343");
+    const P2W_ATTESTATIONS_POLL_INTERVAL_MS = Number(process.env.P2W_ATTESTATIONS_POLL_INTERVAL_MS || "5000");
+
     const P2W_SOL_ADDRESS = process.env.P2W_SOL_ADDRESS || "P2WH424242424242424242424242424242424242424";
 
     const READINESS_PROBE_PORT = Number(process.env.READINESS_PROBE_PORT || "2000");
-    const POLL_INTERVAL_MS = 5000;
 
+    const P2W_RELAY_RETRY_COUNT = Number(process.env.P2W_RELAY_RETRY_COUNT || "3");
+
+    // ETH node connection details; Currently, we expect to read BIP44
+    // wallet recovery mnemonics from a text file.
     const ETH_NODE_URL = process.env.ETH_NODE_URL || "ws://eth-devnet:8545";
     const ETH_P2W_CONTRACT = process.env.ETH_P2W_CONTRACT || "0xA94B7f0465E98609391C623d0560C5720a3f2D33";
     const ETH_MNEMONIC_FILE = process.env.ETH_MNEMONIC_FILE || "../../../ethereum/devnet_mnemonic.txt";
     const ETH_HD_WALLET_PATH = process.env.ETH_HD_WALLET_PATH || "m/44'/60'/0'/0/0";
 
+    // Public RPC address for use with signed attestation queries
     const GUARDIAN_RPC_HOST_PORT = process.env.GUARDIAN_RPC_HOST_PORT || "http://guardian:7071";
 
     let readinessProbe = null;
 
-    let seqnoPool: Set<number> = new Set(); // Seqnos we are yet to process
+    let seqnoPool: Map<number, number> = new Map();
 
-    console.log(`Polling attestations endpoint every ${POLL_INTERVAL_MS / 1000} seconds`);
+    console.log(`Polling attestations endpoint every ${P2W_ATTESTATIONS_POLL_INTERVAL_MS / 1000} seconds`);
 
     const wormhole = await import("@certusone/wormhole-sdk/lib/solana/core/bridge");
 
@@ -50,10 +58,8 @@ async function readinessProbeRoutine(port: number) {
     try {
 	let provider = new ethers.providers.WebSocketProvider(ETH_NODE_URL);
 	let mnemonic: string = fs.readFileSync(ETH_MNEMONIC_FILE).toString("utf-8").trim();
-	console.log(`Using ETH devnet mnemonic: ${mnemonic}`);
 	let wallet = ethers.Wallet.fromMnemonic(mnemonic, ETH_HD_WALLET_PATH);
 	console.log(`Using ETH wallet pubkey: ${wallet.publicKey}`);
-	console.log(`Using ETH wallet privkey: ${wallet.privateKey}`);
 	let signer = new ethers.Wallet(wallet.privateKey, provider);
 	let balance = await signer.getBalance();
 	console.log(`Account balance is ${balance}`);
@@ -61,7 +67,7 @@ async function readinessProbeRoutine(port: number) {
 	p2w_eth = factory.attach(ETH_P2W_CONTRACT);
     }
     catch(e) {
-	console.log(`Error: Could not instantiate ETH contract:`, e);
+	console.error(`Error: Could not instantiate ETH contract:`, e);
 	throw e;
     }
 
@@ -73,7 +79,7 @@ async function readinessProbeRoutine(port: number) {
 	    agent: false
 	}, (res) => {
 	    if (res.statusCode != 200) {
-		console.log("Could not reach attestations endpoint", res);
+		console.error("Could not reach attestations endpoint", res);
 	    } else {
 		let chunks: string[] = [];
 		res.setEncoding("utf-8");
@@ -85,12 +91,12 @@ async function readinessProbeRoutine(port: number) {
 		res.on('end', () => {
 		    let body = chunks.join('');
 
-		    let state: AttestationState = JSON.parse(body);
+		    let response: NewAttestationsResponse = JSON.parse(body);
 
-		    console.log(`Got ${state.pendingSeqnos.length} new seqnos: ${state.pendingSeqnos}`);
+		    console.log(`Got ${response.pendingSeqnos.length} new seqnos: ${response.pendingSeqnos}`);
 
-		    for (let seqno of state.pendingSeqnos) {
-			seqnoPool.add(seqno);
+		    for (let seqno of response.pendingSeqnos) {
+			seqnoPool.set(seqno, 0);
 		    }
 		});
 	    }
@@ -99,43 +105,51 @@ async function readinessProbeRoutine(port: number) {
 	});
 
 	console.log("Processing seqnos:", seqnoPool);
-	for (let seqno of seqnoPool) {
+	for (let poolEntry of seqnoPool) {
+
+	    if (poolEntry[1] >= P2W_RELAY_RETRY_COUNT) {
+		console.warn(`[seqno ${poolEntry}] Exceeded retry count, removing from list`);
+		seqnoPool.delete(poolEntry[0]);
+		continue;
+	    }
 
 	    let vaaResponse;
 	    try {
 		vaaResponse = await getSignedAttestation(
 		    GUARDIAN_RPC_HOST_PORT,
 		    P2W_SOL_ADDRESS,
-		    seqno,
+		    poolEntry[0],
 		    {
 			transport: NodeHttpTransport()
 		    }
 		);
 	    }
 	    catch(e) {
-		console.log(`[seqno ${seqno}] Error: Could not call getSignedAttestation:`, e);
+		console.error(`[seqno ${poolEntry}] Error: Could not call getSignedAttestation:`, e);
+
+		seqnoPool.set(poolEntry[0], poolEntry[1] + 1);
 
 		continue;
 	    }
 
-	    console.log(`[seqno ${seqno}] Price attestation VAA bytes:\n`, vaaResponse.vaaBytes);
-
-	    seqnoPool.delete(seqno); // We don't care to retry beyond this point
+	    console.log(`[seqno ${poolEntry}] Price attestation VAA bytes:\n`, vaaResponse.vaaBytes);
 
 	    let parsedVaa = wormhole.parse_vaa(vaaResponse.vaaBytes);
 
-	    console.log(`[seqno ${seqno}] Parsed VAA:\n`, parsedVaa);
+	    console.log(`[seqno ${poolEntry}] Parsed VAA:\n`, parsedVaa);
 
 	    let parsedAttestation = await parseAttestation(parsedVaa.payload);
 
-	    console.log(`[seqno ${seqno}] Parsed price attestation:\n`, parsedAttestation);
+	    console.log(`[seqno ${poolEntry}] Parsed price attestation:\n`, parsedAttestation);
 
 	    try {
 		let tx = await p2w_eth.attestPrice(vaaResponse.vaaBytes, {gasLimit: 1000000});
 		let retval = await tx.wait();
-		console.log(`[seqno ${seqno}] attestPrice() output:\n`, retval);
+		console.log(`[seqno ${poolEntry}] attestPrice() output:\n`, retval);
 	    } catch(e) {
-		console.log(`[seqno ${seqno}] Error: Could not call attestPrice() on ETH:`, e);
+		console.error(`[seqno ${poolEntry}] Error: Could not call attestPrice() on ETH:`, e);
+
+		seqnoPool.set(poolEntry[0], poolEntry[1] + 1);
 
 		continue;
 	    }
@@ -152,18 +166,23 @@ async function readinessProbeRoutine(port: number) {
 
 		latest_attestation = await p2w_eth.latestAttestation(product_id, price_type);
 	    } catch(e) {
-		console.log(`[seqno ${seqno}] Error: Could not call latestAttestation() on ETH:`, e);
+		console.error(`[seqno ${poolEntry}] Error: Could not call latestAttestation() on ETH:`, e);
+
+		seqnoPool.set(poolEntry[0], poolEntry[1] + 1);
+
 		continue;
 	    }
 
-	    console.log(`[seqno ${seqno}] Latest price type ${price_type} attestation of ${product_id} is ${latest_attestation}`);
+	    console.log(`[seqno ${poolEntry}] Latest price type ${price_type} attestation of ${product_id} is ${latest_attestation}`);
 	    if (!readinessProbe) {
-		console.log(`[seqno ${seqno}] Attestation successful. Starting readiness probe.`);
+		console.log(`[seqno ${poolEntry}] Attestation successful. Starting readiness probe.`);
 		readinessProbe = readinessProbeRoutine(READINESS_PROBE_PORT);
 	    }
+
+	    seqnoPool.delete(poolEntry[0]); // Everything went well, seqno no longer pending.
 	}
 
-	await new Promise(f => {setTimeout(f, POLL_INTERVAL_MS);});
+	await new Promise(f => {setTimeout(f, P2W_ATTESTATIONS_POLL_INTERVAL_MS);});
     }
 
 })();
