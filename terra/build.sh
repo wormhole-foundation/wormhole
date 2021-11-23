0c747199ab2f12400d0395f8ee788660c7c5000c
Hendrik Hofstadt <hendrik@nexantic.com>
2021-11-23 11:24:47 +0100

    Implement pyth2wormhole for Terra
    Change-Id: I3c206cf9850818c1fc012a593ad057e07b5dfa3e

--- terra/build.sh
+++ terra/build.sh
@@ -3,4 +3,4 @@
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.10.7
+  cosmwasm/workspace-optimizer:0.12.1
---
+++ terra/contracts/pyth-bridge/src/state.rs
@@ -0,0 +1,67 @@
+use schemars::JsonSchema;
+use serde::{
+    Deserialize,
+    Serialize,
+};
+
+use cosmwasm_std::{
+    StdResult,
+    Storage,
+};
+use cosmwasm_storage::{
+    bucket,
+    bucket_read,
+    singleton,
+    singleton_read,
+    Bucket,
+    ReadonlyBucket,
+    ReadonlySingleton,
+    Singleton,
+};
+
+use wormhole::byte_utils::ByteUtils;
+
+type HumanAddr = String;
+
+pub static CONFIG_KEY: &[u8] = b"config";
+pub static PRICE_INFO_KEY: &[u8] = b"price_info";
+
+// Guardian set information
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ConfigInfo {
+    // governance contract details
+    pub gov_chain: u16,
+    pub gov_address: Vec<u8>,
+
+    pub wormhole_contract: HumanAddr,
+    pub pyth_emitter: Vec<u8>,
+}
+
+pub fn config(storage: &mut dyn Storage) -> Singleton<ConfigInfo> {
+    singleton(storage, CONFIG_KEY)
+}
+
+pub fn config_read(storage: &dyn Storage) -> ReadonlySingleton<ConfigInfo> {
+    singleton_read(storage, CONFIG_KEY)
+}
+
+pub fn price_info(storage: &mut dyn Storage) -> Bucket<Vec<u8>> {
+    bucket(storage, PRICE_INFO_KEY)
+}
+
+pub fn price_info_read(storage: &dyn Storage) -> ReadonlyBucket<Vec<u8>> {
+    bucket_read(storage, PRICE_INFO_KEY)
+}
+
+
+pub struct UpgradeContract {
+    pub new_contract: u64,
+}
+
+impl UpgradeContract {
+    pub fn deserialize(data: &Vec<u8>) -> StdResult<Self> {
+        let data = data.as_slice();
+        let new_contract = data.get_u64(24);
+        Ok(UpgradeContract { new_contract })
+    }
+}
