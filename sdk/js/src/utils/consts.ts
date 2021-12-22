f022a3d4cea66d5b2f03036a06477d01dd9c4d69
Josh Siegel <jsiegel@jumptrading.com>
2021-12-22 13:21:49 +0000

    pr/jsiegel/algorand-v1: fix miss-merge @ gusc1a-ossdev-jsl1

--- sdk/js/src/utils/consts.ts
+++ sdk/js/src/utils/consts.ts
@@ -4,6 +4,7 @@ export const CHAIN_ID_ETH: ChainId = 2;
 export const CHAIN_ID_TERRA: ChainId = 3;
 export const CHAIN_ID_BSC: ChainId = 4;
 export const CHAIN_ID_POLYGON: ChainId = 5;
+export const CHAIN_ID_AVAX: ChainId = 6;
 export const CHAIN_ID_OASIS: ChainId = 7;
 export const CHAIN_ID_ALGORAND: ChainId = 8;
 export const CHAIN_ID_ETHEREUM_ROPSTEN: ChainId = 10001;
