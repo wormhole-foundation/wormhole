7adac5d6592625e167d60f76fd3e19c32416086f
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2021-12-28 11:39:48 +0100

    terra/nft_bridge: vendor cw721 and cw721-base
    The most recent released versions of these libraries use a different
    version of the cosmwasm than the rest of the wormhole projects, which
    leads to a linker error. So we vendor these libraries and downgrade
    their cosmwasm-std dependency to match the rest.
    
    commit-id:a1a5c20b

---
+++ terra/packages/cw721/src/msg.rs
@@ -0,0 +1,36 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::Binary;
+use cw0::Expiration;
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum Cw721ExecuteMsg {
+    /// Transfer is a base message to move a token to another account without triggering actions
+    TransferNft { recipient: String, token_id: String },
+    /// Send is a base message to transfer a token to a contract and trigger an action
+    /// on the receiving contract.
+    SendNft {
+        contract: String,
+        token_id: String,
+        msg: Binary,
+    },
+    /// Allows operator to transfer / send the token from the owner's account.
+    /// If expiration is set, then this allowance has a time/height limit
+    Approve {
+        spender: String,
+        token_id: String,
+        expires: Option<Expiration>,
+    },
+    /// Remove previously granted Approval
+    Revoke { spender: String, token_id: String },
+    /// Allows operator to transfer / send any token from the owner's account.
+    /// If expiration is set, then this allowance has a time/height limit
+    ApproveAll {
+        operator: String,
+        expires: Option<Expiration>,
+    },
+    /// Remove previously granted ApproveAll permission
+    RevokeAll { operator: String },
+}
