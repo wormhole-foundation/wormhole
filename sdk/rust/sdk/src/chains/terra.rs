e561d6de02f89da1d9df323c7bf13fa866435c71
Reisen <reisen@morphism.org>
2022-01-06 15:08:09 +0000

    sdk: fixes to types and builds
    Change-Id: I10b753450445cc021dbeb9f28ddb0384070e9635

--- sdk/rust/sdk/src/chains/terra.rs
+++ sdk/rust/sdk/src/chains/terra.rs
@@ -25,18 +25,24 @@ pub fn id() -> Addr {
 }
 
 /// Export Core Devnet Contract Address
+#[cfg(feature = "testnet")]
+pub fn id() -> Addr {
+    Addr::unchecked("terra1pd65m0q9tl3v8znnz5f5ltsfegyzah7g42cx5v")
+}
+
+/// Export Core Devnet Contract Address
 #[cfg(feature = "devnet")]
 pub fn id() -> Addr {
     Addr::unchecked("terra1pd65m0q9tl3v8znnz5f5ltsfegyzah7g42cx5v")
 }
 
-pub fn post_message<T>(wormhole: Addr, nonce: u32, message: &T) -> StdResult<CosmosMsg>
+pub fn post_message<T>(nonce: u32, message: &T) -> StdResult<CosmosMsg>
 where
     T: Serialize,
     T: ?Sized,
 {
     Ok(CosmosMsg::Wasm(WasmMsg::Execute {
-        contract_addr: wormhole.to_string(),
+        contract_addr: id().to_string(),
         funds:         vec![],
         msg:           to_binary(&ExecuteMsg::PostMessage {
             message: to_binary(message)?,
@@ -47,13 +53,12 @@ where
 
 /// Parse a VAA using the Wormhole contract Query interface.
 pub fn parse_vaa(
-    wormhole: Addr,
     deps: DepsMut,
     env: Env,
     data: &Binary,
 ) -> StdResult<ParsedVAA> {
     let vaa: ParsedVAA = deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
-        contract_addr: wormhole.to_string(),
+        contract_addr: id().to_string(),
         msg:           to_binary(&QueryMsg::VerifyVAA {
             vaa: data.clone(),
             block_time: env.block.time.seconds(),
