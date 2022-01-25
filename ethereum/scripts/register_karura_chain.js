4d8822d218b5bf0fb3fa32a6d40c3e0eba45787d
zjb0807 <zjb0807@qq.com>
2022-01-25 22:47:44 +0800

    update truffle/hdwallet-provider v1.7.0

--- ethereum/scripts/register_karura_chain.js
+++ ethereum/scripts/register_karura_chain.js
@@ -10,7 +10,7 @@ module.exports = async function (callback) {
         const accounts = await web3.eth.getAccounts();
         const initialized = new web3.eth.Contract(BridgeImplementationFullABI, TokenBridge.address);
 
-        // Register the Acala endpoint
+        // Register the Karura endpoint
         await initialized.methods.registerChain("0x01000000000100719b4ada436f614489dbf87593c38ba9aea35aa7b997387f8ae09f819806f5654c8d45b6b751faa0e809ccbc294794885efa205bd8a046669464c7cbfb03d183010000000100000001000100000000000000000000000000000000000000000000000000000000000000040000000002c8bb0600000000000000000000000000000000000000000000546f6b656e42726964676501000000040000000000000000000000000290fb167208af455bb137780163b7b7a9a10c16").send({
             value: 0,
             from: accounts[0],
