5a5051ff579a13d90801927f45c961d9373124fa
Stan Drozd <stan@nexantic.com>
2022-01-26 16:49:35 +0100

    p2w: configurability and sane envs
    commit-id:f10fd90e

--- third_party/pyth/p2w_autoattest.py
+++ third_party/pyth/p2w_autoattest.py
@@ -1,33 +1,46 @@
 #!/usr/bin/env python3
 
 # This script sets up a simple loop for periodical attestation of Pyth data
-from pyth_utils import *
-
-from http.client import HTTPConnection
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
 import json
+import logging
 import os
 import re
-import subprocess
-import time
+import sys
 import threading
+import time
+from http.client import HTTPConnection
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
+from pyth_utils import *
 
-P2W_ADDRESS = "P2WH424242424242424242424242424242424242424"
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s | %(module)s | %(levelname)s | %(message)s"
+)
+
+P2W_SOL_ADDRESS = os.environ.get(
+    "P2W_SOL_ADDRESS", "P2WH424242424242424242424242424242424242424"
+)
 P2W_ATTEST_INTERVAL = float(os.environ.get("P2W_ATTEST_INTERVAL", 5))
 P2W_OWNER_KEYPAIR = os.environ.get(
-    "P2W_OWNER_KEYPAIR", f"/usr/src/solana/keys/p2w_owner.json")
+    "P2W_OWNER_KEYPAIR", "/usr/src/solana/keys/p2w_owner.json"
+)
 P2W_ATTESTATIONS_PORT = int(os.environ.get("P2W_ATTESTATIONS_PORT", 4343))
+P2W_INITIALIZE_SOL_CONTRACT = os.environ.get("P2W_INITIALIZE_SOL_CONTRACT", None)
+
+PYTH_PRICE_ACCOUNT = os.environ.get("PYTH_PRICE_ACCOUNT", None)
+PYTH_PRODUCT_ACCOUNT = os.environ.get("PYTH_PRODUCT_ACCOUNT", None)
 
 PYTH_ACCOUNTS_HOST = "pyth"
 PYTH_ACCOUNTS_PORT = 4242
 
-WORMHOLE_ADDRESS = "Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o"
+WORMHOLE_ADDRESS = os.environ.get(
+    "WORMHOLE_ADDRESS", "Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o"
+)
 
 ATTESTATIONS = {
     "pendingSeqnos": [],
 }
+
 
 class P2WAutoattestStatusEndpoint(BaseHTTPRequestHandler):
     """
@@ -35,10 +48,10 @@ class P2WAutoattestStatusEndpoint(BaseHTTPRequestHandler):
     """
 
     def do_GET(self):
-        print(f"Got path {self.path}")
+        logging.info(f"Got path {self.path}")
         sys.stdout.flush()
         data = json.dumps(ATTESTATIONS).encode("utf-8")
-        print(f"Sending:\n{data}")
+        logging.debug(f"Sending: {data}")
 
         ATTESTATIONS["pendingSeqnos"] = []
 
@@ -49,91 +62,121 @@ class P2WAutoattestStatusEndpoint(BaseHTTPRequestHandler):
         self.wfile.write(data)
         self.wfile.flush()
 
+
 def serve_attestations():
     """
     Run a barebones HTTP server to share Pyth2wormhole attestation history
     """
-    server_address = ('', P2W_ATTESTATIONS_PORT)
+    server_address = ("", P2W_ATTESTATIONS_PORT)
     httpd = HTTPServer(server_address, P2WAutoattestStatusEndpoint)
     httpd.serve_forever()
 
 
-# Get actor pubkeys
-P2W_OWNER_ADDRESS = sol_run_or_die(
-    "address", ["--keypair", P2W_OWNER_KEYPAIR], capture_output=True).stdout.strip()
-PYTH_OWNER_ADDRESS = sol_run_or_die(
-    "address", ["--keypair", PYTH_PROGRAM_KEYPAIR], capture_output=True).stdout.strip()
+if P2W_INITIALIZE_SOL_CONTRACT is not None:
+    # Get actor pubkeys
+    P2W_OWNER_ADDRESS = sol_run_or_die(
+        "address", ["--keypair", P2W_OWNER_KEYPAIR], capture_output=True
+    ).stdout.strip()
+    PYTH_OWNER_ADDRESS = sol_run_or_die(
+        "address", ["--keypair", PYTH_PROGRAM_KEYPAIR], capture_output=True
+    ).stdout.strip()
 
+    init_result = run_or_die(
+        [
+            "pyth2wormhole-client",
+            "--log-level",
+            "4",
+            "--p2w-addr",
+            P2W_SOL_ADDRESS,
+            "--rpc-url",
+            SOL_RPC_URL,
+            "--payer",
+            P2W_OWNER_KEYPAIR,
+            "init",
+            "--wh-prog",
+            WORMHOLE_ADDRESS,
+            "--owner",
+            P2W_OWNER_ADDRESS,
+            "--pyth-owner",
+            PYTH_OWNER_ADDRESS,
+        ],
+        capture_output=True,
+        die=False,
+    )
 
-# Top up pyth2wormhole owner
-sol_run_or_die("airdrop", [
-    str(SOL_AIRDROP_AMT),
-    "--keypair",  P2W_OWNER_KEYPAIR,
-    "--commitment", "finalized",
-], capture_output=True)
+    if init_result.returncode != 0:
+        logging.error(
+            "NOTE: pyth2wormhole-client init failed, retrying with set_config"
+        )
+        run_or_die(
+            [
+                "pyth2wormhole-client",
+                "--log-level",
+                "4",
+                "--p2w-addr",
+                P2W_SOL_ADDRESS,
+                "--rpc-url",
+                SOL_RPC_URL,
+                "--payer",
+                P2W_OWNER_KEYPAIR,
+                "set-config",
+                "--owner",
+                P2W_OWNER_KEYPAIR,
+                "--new-owner",
+                P2W_OWNER_ADDRESS,
+                "--new-wh-prog",
+                WORMHOLE_ADDRESS,
+                "--new-pyth-owner",
+                PYTH_OWNER_ADDRESS,
+            ],
+            capture_output=True,
+        )
 
-# Initialize pyth2wormhole
-init_result = run_or_die([
-    "pyth2wormhole-client",
-    "--log-level", "4",
-    "--p2w-addr", P2W_ADDRESS,
-    "--rpc-url", SOL_RPC_URL,
-    "--payer", P2W_OWNER_KEYPAIR,
-    "init",
-    "--wh-prog", WORMHOLE_ADDRESS,
-    "--owner", P2W_OWNER_ADDRESS,
-    "--pyth-owner", PYTH_OWNER_ADDRESS,
-], capture_output=True, die=False)
+# Retrieve current price/product pubkeys from the pyth publisher if not provided in envs
+if PYTH_PRICE_ACCOUNT is None or PYTH_PRODUCT_ACCOUNT is None:
+    conn = HTTPConnection(PYTH_ACCOUNTS_HOST, PYTH_ACCOUNTS_PORT)
 
-if init_result.returncode != 0:
-    print("NOTE: pyth2wormhole-client init failed, retrying with set_config")
-    run_or_die([
-        "pyth2wormhole-client",
-        "--log-level", "4",
-        "--p2w-addr", P2W_ADDRESS,
-        "--rpc-url", SOL_RPC_URL,
-        "--payer", P2W_OWNER_KEYPAIR,
-        "set-config",
-        "--owner", P2W_OWNER_KEYPAIR,
-        "--new-owner", P2W_OWNER_ADDRESS,
-        "--new-wh-prog", WORMHOLE_ADDRESS,
-        "--new-pyth-owner", PYTH_OWNER_ADDRESS,
-    ], capture_output=True)
+    conn.request("GET", "/")
 
-# Retrieve current price/product pubkeys from the pyth publisher
-conn = HTTPConnection(PYTH_ACCOUNTS_HOST, PYTH_ACCOUNTS_PORT)
+    res = conn.getresponse()
 
-conn.request("GET", "/")
+    pyth_accounts = None
 
-res = conn.getresponse()
+    if res.getheader("Content-Type") == "application/json":
+        pyth_accounts = json.load(res)
+    else:
+        logging.error("Bad Content type")
+        sys.exit(1)
 
-pyth_accounts = None
-
-if res.getheader("Content-Type") == "application/json":
-    pyth_accounts = json.load(res)
-else:
-    print(f"Bad Content type {res.getheader('Content-Type')}", file=sys.stderr)
-    sys.exit(1)
-
-price_addr = pyth_accounts["price"]
-product_addr = pyth_accounts["product"]
+    PYTH_PRICE_ACCOUNT = pyth_accounts["price"]
+    PYTH_PRODUCT_ACCOUNT = pyth_accounts["product"]
+    logging.info(f"Retrieved Pyth accounts from endpoint: {pyth_accounts}")
 
 nonce = 0
-attest_result = run_or_die([
-    "pyth2wormhole-client",
-    "--log-level", "4",
-    "--p2w-addr", P2W_ADDRESS,
-    "--rpc-url", SOL_RPC_URL,
-    "--payer", P2W_OWNER_KEYPAIR,
-    "attest",
-    "--price", price_addr,
-    "--product", product_addr,
-    "--nonce", str(nonce),
-], capture_output=True)
+attest_result = run_or_die(
+    [
+        "pyth2wormhole-client",
+        "--log-level",
+        "4",
+        "--p2w-addr",
+        P2W_SOL_ADDRESS,
+        "--rpc-url",
+        SOL_RPC_URL,
+        "--payer",
+        P2W_OWNER_KEYPAIR,
+        "attest",
+        "--price",
+        PYTH_PRICE_ACCOUNT,
+        "--product",
+        PYTH_PRODUCT_ACCOUNT,
+        "--nonce",
+        str(nonce),
+    ],
+    capture_output=True,
+)
 
-print("p2w_autoattest ready to roll.")
-print(f"ACCOUNTS: {pyth_accounts}")
-print(f"Attest Interval: {P2W_ATTEST_INTERVAL}")
+logging.info("p2w_autoattest ready to roll!")
+logging.info(f"Attest Interval: {P2W_ATTEST_INTERVAL}")
 
 # Serve p2w endpoint
 endpoint_thread = threading.Thread(target=serve_attestations, daemon=True)
@@ -147,29 +190,37 @@ seqno_regex = re.compile(r"^Sequence number: (\d+)")
 
 nonce = 1
 while True:
-    attest_result = run_or_die([
-        "pyth2wormhole-client",
-        "--log-level", "4",
-        "--p2w-addr", P2W_ADDRESS,
-        "--rpc-url", SOL_RPC_URL,
-        "--payer", P2W_OWNER_KEYPAIR,
-        "attest",
-        "--price", price_addr,
-        "--product", product_addr,
-        "--nonce", str(nonce),
-    ], capture_output=True)
+    attest_result = run_or_die(
+        [
+            "pyth2wormhole-client",
+            "--log-level",
+            "4",
+            "--p2w-addr",
+            P2W_SOL_ADDRESS,
+            "--rpc-url",
+            SOL_RPC_URL,
+            "--payer",
+            P2W_OWNER_KEYPAIR,
+            "attest",
+            "--price",
+            PYTH_PRICE_ACCOUNT,
+            "--product",
+            PYTH_PRODUCT_ACCOUNT,
+            "--nonce",
+            str(nonce),
+        ],
+        capture_output=True,
+    )
     matches = seqno_regex.match(attest_result.stdout)
 
     if matches is not None:
         seqno = int(matches.group(1))
-        print(f"Got seqno {seqno}")
+        logging.info(f"Got seqno: {seqno}")
 
         ATTESTATIONS["pendingSeqnos"].append(seqno)
 
     else:
-        print(f"Warning: Could not get sequence number")
+        logging.warn("Warning: Could not get sequence number")
 
     time.sleep(P2W_ATTEST_INTERVAL)
     nonce += 1
-
-readiness_thread.join()
--- third_party/pyth/pyth_utils.py
+++ third_party/pyth/pyth_utils.py
@@ -1,22 +1,31 @@
 import os
 import socketserver
-import sys
 import subprocess
+import sys
 
+# Settings specific to local devnet Pyth instance
 PYTH = os.environ.get("PYTH", "./pyth")
 PYTH_KEY_STORE = os.environ.get("PYTH_KEY_STORE", "/home/pyth/.pythd")
 PYTH_PROGRAM_KEYPAIR = os.environ.get(
-    "PYTH_PROGRAM_KEYPAIR", f"{PYTH_KEY_STORE}/publish_key_pair.json")
+    "PYTH_PROGRAM_KEYPAIR", f"{PYTH_KEY_STORE}/publish_key_pair.json"
+)
 PYTH_PROGRAM_SO_PATH = os.environ.get("PYTH_PROGRAM_SO", "../target/oracle.so")
 PYTH_PUBLISHER_KEYPAIR = os.environ.get(
-    "PYTH_PUBLISHER_KEYPAIR", f"{PYTH_KEY_STORE}/publish_key_pair.json")
+    "PYTH_PUBLISHER_KEYPAIR", f"{PYTH_KEY_STORE}/publish_key_pair.json"
+)
 PYTH_PUBLISHER_INTERVAL = float(os.environ.get("PYTH_PUBLISHER_INTERVAL", "5"))
 
-SOL_AIRDROP_AMT = 100
-SOL_RPC_HOST = "solana-devnet"
-SOL_RPC_PORT = 8899
-SOL_RPC_URL = f"http://{SOL_RPC_HOST}:{str(SOL_RPC_PORT)}"
+# 0 setting disables airdropping
+SOL_AIRDROP_AMT = int(os.environ.get("SOL_AIRDROP_AMT", 0))
 
+# SOL RPC settings
+SOL_RPC_HOST = os.environ.get("SOL_RPC_HOST", "solana-devnet")
+SOL_RPC_PORT = int(os.environ.get("SOL_RPC_PORT", 8899))
+SOL_RPC_URL = os.environ.get(
+    "SOL_RPC_URL", "http://{0}:{1}".format(SOL_RPC_HOST, SOL_RPC_PORT)
+)
+
+# A TCP port we open when a service is ready
 READINESS_PORT = int(os.environ.get("READINESS_PORT", "2000"))
 
 
@@ -24,12 +33,12 @@ def run_or_die(args, die=True, **kwargs):
     """
     Opinionated subprocess.run() call with fancy logging
     """
-    args_readable = ' '.join(args)
+    args_readable = " ".join(args)
     print(f"CMD RUN\t{args_readable}", file=sys.stderr)
     sys.stderr.flush()
     ret = subprocess.run(args, text=True, **kwargs)
 
-    if ret.returncode is not 0:
+    if ret.returncode != 0:
         print(f"CMD FAIL {ret.returncode}\t{args_readable}", file=sys.stderr)
 
         out = ret.stdout if ret.stdout is not None else "<not captured>"
@@ -41,7 +50,7 @@ def run_or_die(args, die=True, **kwargs):
         if die:
             sys.exit(ret.returncode)
         else:
-            print(f"CMD DIE FALSE", file=sys.stderr)
+            print(f'{"CMD DIE FALSE"}', file=sys.stderr)
 
     else:
         print(f"CMD OK\t{args_readable}", file=sys.stderr)
@@ -54,23 +63,21 @@ def pyth_run_or_die(subcommand, args=[], debug=False, confirm=True, **kwargs):
     Pyth boilerplate in front of run_or_die
     """
     return run_or_die(
-        [PYTH, subcommand]
-        + args
-        + (["-d"] if debug else [])
+        [PYTH, subcommand] + args + (["-d"] if debug else [])
         # Note: not all pyth subcommands accept -n
         + ([] if confirm else ["-n"])
         + ["-k", PYTH_KEY_STORE]
         + ["-r", SOL_RPC_HOST]
-        + ["-c", "finalized"], **kwargs)
+        + ["-c", "finalized"],
+        **kwargs,
+    )
 
 
 def sol_run_or_die(subcommand, args=[], **kwargs):
     """
     Solana boilerplate in front of run_or_die
     """
-    return run_or_die(["solana", subcommand]
-                      + args
-                      + ["--url", SOL_RPC_URL], **kwargs)
+    return run_or_die(["solana", subcommand] + args + ["--url", SOL_RPC_URL], **kwargs)
 
 
 class ReadinessTCPHandler(socketserver.StreamRequestHandler):
@@ -83,6 +90,7 @@ def readiness():
     """
     Accept connections from readiness probe
     """
-    with socketserver.TCPServer(("0.0.0.0", READINESS_PORT), ReadinessTCPHandler) as srv:
+    with socketserver.TCPServer(
+        ("0.0.0.0", READINESS_PORT), ReadinessTCPHandler
+    ) as srv:
         srv.serve_forever()
-    # run_or_die(["nc", "-k", "-l", "-p", READINESS_PORT])
