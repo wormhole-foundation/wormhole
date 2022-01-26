3a24a26a7d102ad3a39e23c7b6b09a89edee474c
Csongor Kiss <ckiss@jumptrading.com>
2022-01-26 16:15:10 +0000

    eth deploy updates

---
+++ ethereum/migrations/6_deploy_bridge_implementation_only.js
@@ -0,0 +1,12 @@
+// run with:
+// npm run deploy-bridge-implementation-only
+// e.g. Ethereum Mainnet
+// INFURA_KEY="" MNEMONIC="" npm run deploy-bridge-implementation-only -- --network mainnet
+// e.g. BSC
+// MNEMONIC="" npm run deploy-bridge-implementation-only -- --network binance
+// e.g. Polygon
+// MNEMONIC="" npm run deploy-bridge-implementation-only -- --network polygon
+const BridgeImplementation = artifacts.require("BridgeImplementation");
+module.exports = async function(deployer) {
+  await deployer.deploy(BridgeImplementation);
+};
