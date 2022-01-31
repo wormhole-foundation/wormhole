0f43f5fec15cc9bc5208ee129abbd163f0520c28
Csongor Kiss <ckiss@jumptrading.com>
2022-01-31 20:55:34 +0000

    sdk/js: Add Fantom & Aurora to SDK

--- sdk/js/src/utils/consts.ts
+++ sdk/js/src/utils/consts.ts
@@ -1,4 +1,4 @@
-export type ChainId = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 10001;
+export type ChainId = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 10001;
 export const CHAIN_ID_SOLANA: ChainId = 1;
 export const CHAIN_ID_ETH: ChainId = 2;
 export const CHAIN_ID_TERRA: ChainId = 3;
@@ -7,6 +7,8 @@ export const CHAIN_ID_POLYGON: ChainId = 5;
 export const CHAIN_ID_AVAX: ChainId = 6;
 export const CHAIN_ID_OASIS: ChainId = 7;
 export const CHAIN_ID_ALGORAND: ChainId = 8;
+export const CHAIN_ID_AURORA: ChainId = 9;
+export const CHAIN_ID_FANTOM: ChainId = 10;
 export const CHAIN_ID_ETHEREUM_ROPSTEN: ChainId = 10001;
 
 export const WSOL_ADDRESS = "So11111111111111111111111111111111111111112";
