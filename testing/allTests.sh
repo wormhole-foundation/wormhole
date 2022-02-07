fd53dffa01f2201bc1200cb025e45c8a17521c48
Chase Moran <chasemoran45@gmail.com>
2022-02-07 13:01:24 -0500

    tests use kube proxy when in ci

--- testing/allTests.sh
+++ testing/allTests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-exec npm --prefix ../sdk/js run test-ci ; echo "JS test results from sdk: $?"
-exec npm --prefix ../spydk/js run test-ci ; echo "JS test results from spydk: $?" 
-exec npm --prefix ../bridge_ui run test; echo "JS test results from bridge_ui: $?" 
+npm --prefix ../sdk/js run test-ci
+npm --prefix ../spydk/js run test-ci 
+npm --prefix ../bridge_ui run test 
