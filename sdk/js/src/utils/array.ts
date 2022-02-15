d5571fd498419e401d4b6b4347bdddd8af5d6d7e
Shunji Zhan <shunhahaha@live.com>
2022-02-15 13:09:18 +0800

    fix

--- sdk/js/src/utils/array.ts
+++ sdk/js/src/utils/array.ts
@@ -8,6 +8,8 @@ import {
   CHAIN_ID_ETHEREUM_ROPSTEN,
   CHAIN_ID_AVAX,
   CHAIN_ID_OASIS,
+  CHAIN_ID_AURORA,
+  CHAIN_ID_FANTOM,
   CHAIN_ID_KARURA,
   CHAIN_ID_ACALA,
 } from "./consts";
@@ -24,8 +26,10 @@ export const isEVMChain = (chainId: ChainId) => {
     chainId === CHAIN_ID_AVAX ||
     chainId === CHAIN_ID_POLYGON ||
     chainId === CHAIN_ID_OASIS ||
+    chainId === CHAIN_ID_AURORA ||
+    chainId === CHAIN_ID_FANTOM ||
     chainId === CHAIN_ID_KARURA ||
-    chainId === CHAIN_ID_ACALA ||
+    chainId === CHAIN_ID_ACALA
   );
 };
 
