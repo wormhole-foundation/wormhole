a31d680c777ae80e90e775d02abf0fdce9a3b69f
claudijd <jclaudius@jumptrading.com>
2022-02-21 16:50:08 -0500

    Use structs for testing governance VAA

--- node/pkg/vaa/governance_test.go
+++ node/pkg/vaa/governance_test.go
@@ -10,19 +10,22 @@ func TestCreateGovernanceVAA(t *testing.T) {
 	var sequence uint64 = 1
 	var guardianSetIndex uint32 = 1
 	var payload = []byte{97, 97, 97, 97, 97, 97}
+	var governanceEmitter = Address{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4}
 
-	vaa := CreateGovernanceVAA(nonce, sequence, guardianSetIndex, payload)
+	got_vaa := CreateGovernanceVAA(nonce, sequence, guardianSetIndex, payload)
+	
+	want_vaa := &VAA{
+		Version:          uint8(1),
+		GuardianSetIndex: uint32(1),
+		Signatures:       nil,
+		Timestamp:        time.Unix(0, 0),
+		Nonce:            uint32(1),
+		Sequence:         uint64(1),
+		ConsistencyLevel: uint8(32),
+		EmitterChain:     ChainIDSolana,
+		EmitterAddress:   governanceEmitter,
+		Payload:          payload,
+	}
 
-	assert.Equal(t, vaa.Version, uint8(1))
-	assert.Equal(t, vaa.GuardianSetIndex, uint32(1))
-	assert.Nil(t, vaa.Signatures)
-	assert.Equal(t, vaa.Timestamp, time.Unix(0, 0))
-	assert.Equal(t, vaa.Timestamp, time.Unix(0, 0))
-	assert.Equal(t, vaa.Nonce, uint32(1))
-	assert.Equal(t, vaa.Sequence, uint64(1))
-	assert.Equal(t, vaa.ConsistencyLevel, uint8(32))
-	assert.Equal(t, vaa.ConsistencyLevel, uint8(32))
-	assert.Equal(t, vaa.EmitterChain, ChainIDSolana)
-	assert.Equal(t, vaa.EmitterAddress, Address{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4})
-	assert.Equal(t, string(vaa.Payload), "aaaaaa")
+	assert.Equal(t, got_vaa, want_vaa)
 }
