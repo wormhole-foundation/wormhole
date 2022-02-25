cd6ee640832e1accaced1076d249e7b1c6dc013a
chase-45 <chasemoran45@gmail.com>
2022-02-25 17:56:48 -0500

    js testing tooling

--- wormhole_chain/vue/src/store/generated/certusone/wormhole-chain/certusone.wormholechain.tokenbridge/module/index.ts
+++ wormhole_chain/vue/src/store/generated/certusone/wormhole-chain/certusone.wormholechain.tokenbridge/module/index.ts
@@ -5,16 +5,16 @@ import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
 import { MsgExecuteGovernanceVAA } from "./types/tokenbridge/tx";
-import { MsgAttestToken } from "./types/tokenbridge/tx";
 import { MsgExecuteVAA } from "./types/tokenbridge/tx";
 import { MsgTransfer } from "./types/tokenbridge/tx";
+import { MsgAttestToken } from "./types/tokenbridge/tx";
 
 
 const types = [
   ["/certusone.wormholechain.tokenbridge.MsgExecuteGovernanceVAA", MsgExecuteGovernanceVAA],
-  ["/certusone.wormholechain.tokenbridge.MsgAttestToken", MsgAttestToken],
   ["/certusone.wormholechain.tokenbridge.MsgExecuteVAA", MsgExecuteVAA],
   ["/certusone.wormholechain.tokenbridge.MsgTransfer", MsgTransfer],
+  ["/certusone.wormholechain.tokenbridge.MsgAttestToken", MsgAttestToken],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -48,9 +48,9 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
     msgExecuteGovernanceVAA: (data: MsgExecuteGovernanceVAA): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgExecuteGovernanceVAA", value: MsgExecuteGovernanceVAA.fromPartial( data ) }),
-    msgAttestToken: (data: MsgAttestToken): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgAttestToken", value: MsgAttestToken.fromPartial( data ) }),
     msgExecuteVAA: (data: MsgExecuteVAA): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgExecuteVAA", value: MsgExecuteVAA.fromPartial( data ) }),
     msgTransfer: (data: MsgTransfer): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgTransfer", value: MsgTransfer.fromPartial( data ) }),
+    msgAttestToken: (data: MsgAttestToken): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgAttestToken", value: MsgAttestToken.fromPartial( data ) }),
     
   };
 };
