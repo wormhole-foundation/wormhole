0a6b3a012d2fa99c8383ed26cd49007c7acf6f6f
Evan Gray <battledingo@gmail.com>
2022-03-01 21:34:51 -0500

    bridge_ui: add fantom beta

--- bridge_ui/src/components/NFTOriginVerifier.tsx
+++ bridge_ui/src/components/NFTOriginVerifier.tsx
@@ -2,9 +2,10 @@ import {
   CHAIN_ID_AVAX,
   CHAIN_ID_BSC,
   CHAIN_ID_ETH,
+  CHAIN_ID_FANTOM,
+  CHAIN_ID_OASIS,
   CHAIN_ID_POLYGON,
   CHAIN_ID_SOLANA,
-  CHAIN_ID_OASIS,
   hexToNativeString,
   isEVMChain,
   uint8ArrayToHex,
@@ -340,6 +341,17 @@ export default function NFTOriginVerifier() {
                     variant="outlined"
                   >
                     View on Snowtrace
+                  </Button>
+                ) : originInfo.chainId === CHAIN_ID_FANTOM ? (
+                  <Button
+                    href={`https://ftmscan.com/token/${readableAddress}?a=${originInfo.tokenId}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    startIcon={<Launch />}
+                    className={classes.viewButton}
+                    variant="outlined"
+                  >
+                    View on FTMScan
                   </Button>
                 ) : originInfo.chainId === CHAIN_ID_OASIS ? null : (
                   <Button
