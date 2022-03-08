e46d58c0efbf42ad89740f4a2382e280ec9b4bd6
Josh Siegel <jsiegel@jumptrading.com>
2022-03-08 17:06:37 +0000

    algo/integration: start work on the algo guardian watcher @ gusc1a-ossdev-jsl1

--- node/pkg/algorand/watcher.go
+++ node/pkg/algorand/watcher.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"github.com/certusone/wormhole/node/pkg/common"
 	"github.com/certusone/wormhole/node/pkg/readiness"
+
+	"encoding/json"
+	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
+	"github.com/algorand/go-algorand-sdk/client/v2/indexer"
 )
 
 type (
@@ -23,8 +27,66 @@ func NewWatcher(urlRPC string, urlToken string, contract string, lockEvents chan
 	return &Watcher{urlRPC: urlRPC, urlToken: urlToken, contract: contract, msgChan: lockEvents, setChan: setEvents}
 }
 
+func lookAtTxn(e *Watcher, t models.Transaction) {
+	var at = t.ApplicationTransaction
+	if len(at.ApplicationArgs) == 0 {
+		return
+	}
+
+	JSON, err := json.Marshal(t)
+	_ = err
+	fmt.Printf(string(JSON))
+
+	fmt.Printf("%d\n", at.ApplicationId)
+	if string(at.ApplicationArgs[0]) == "publishMessage" { // The note filter is effectively the same thing
+		var vaa = at.ApplicationArgs[1]
+		fmt.Printf(t.Sender + " -> " + string(vaa) + "\n")
+	}
+}
+
 func (e *Watcher) Run(ctx context.Context) error {
 	readiness.SetReady(common.ReadinessAlgorandSyncing)
 
-	select {}
+	go func() {
+		indexerClient, err := indexer.MakeClient(indexerAddress, indexerToken)
+		_ = err
+
+		// Parameters
+		var notePrefix = "publishMessage"
+		var next_round uint64 = 0
+
+		for true {
+			var nextToken = ""
+			for true {
+				result, err := indexerClient.SearchForTransactions().NotePrefix([]byte(notePrefix)).MinRound(next_round).NextToken(nextToken).Do(context.Background())
+				_ = err
+
+				for i := 0; i < len(result.Transactions); i++ {
+					var t = result.Transactions[i]
+					if len(t.InnerTxns) > 0 {
+						for q := 0; q < len(t.InnerTxns); q++ {
+							lookAtTxn(t.InnerTxns[q])
+						}
+					} else {
+						lookAtTxn(t)
+					}
+				}
+
+				if result.NextToken != "" {
+					nextToken = result.NextToken
+				} else {
+					next_round = result.CurrentRound + 1
+					break
+				}
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-errC:
+		return err
+	}
 }
