30d4ece521f571dc046017383da4cf6dc8dff401
Josh Siegel <jsiegel@jumptrading.com>
2022-03-08 18:27:26 +0000

    algo/integration: make this look more like the other watchers @ gusc1a-ossdev-jsl1

--- node/pkg/algorand/watcher.go
+++ node/pkg/algorand/watcher.go
@@ -2,15 +2,15 @@ package algorand
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
+	"github.com/algorand/go-algorand-sdk/client/v2/indexer"
 	"github.com/certusone/wormhole/node/pkg/common"
 	"github.com/certusone/wormhole/node/pkg/readiness"
 	"github.com/certusone/wormhole/node/pkg/supervisor"
-        "time"
-        "fmt"
-	"encoding/json"
-	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
-	"github.com/algorand/go-algorand-sdk/client/v2/indexer"
 	"go.uber.org/zap"
+	"time"
 )
 
 type (
@@ -55,9 +55,12 @@ func (e *Watcher) Run(ctx context.Context) error {
 	logger := supervisor.Logger(ctx)
 	errC := make(chan error)
 
-        logger.Info("Algorand watcher connecting", zap.String("url", e.indexerRPC))
+	logger.Info("Algorand watcher connecting", zap.String("url", e.indexerRPC))
 
 	go func() {
+		timer := time.NewTicker(time.Second * 1)
+		defer timer.Stop()
+
 		indexerClient, err := indexer.MakeClient(e.indexerRPC, e.indexerToken)
 		_ = err
 
@@ -65,31 +68,35 @@ func (e *Watcher) Run(ctx context.Context) error {
 		var notePrefix = "publishMessage"
 		var next_round uint64 = 0
 
-		for true {
-			var nextToken = ""
-			for true {
-				result, err := indexerClient.SearchForTransactions().NotePrefix([]byte(notePrefix)).MinRound(next_round).NextToken(nextToken).Do(context.Background())
-				_ = err
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-timer.C:
+				var nextToken = ""
+				for true {
+					result, err := indexerClient.SearchForTransactions().NotePrefix([]byte(notePrefix)).MinRound(next_round).NextToken(nextToken).Do(context.Background())
+					_ = err
 
-				for i := 0; i < len(result.Transactions); i++ {
-					var t = result.Transactions[i]
-					if len(t.InnerTxns) > 0 {
-						for q := 0; q < len(t.InnerTxns); q++ {
-							lookAtTxn(e, t.InnerTxns[q])
+					for i := 0; i < len(result.Transactions); i++ {
+						var t = result.Transactions[i]
+						if len(t.InnerTxns) > 0 {
+							for q := 0; q < len(t.InnerTxns); q++ {
+								lookAtTxn(e, t.InnerTxns[q])
+							}
+						} else {
+							lookAtTxn(e, t)
 						}
+					}
+
+					if result.NextToken != "" {
+						nextToken = result.NextToken
 					} else {
-						lookAtTxn(e, t)
+						next_round = result.CurrentRound + 1
+						break
 					}
 				}
-
-				if result.NextToken != "" {
-					nextToken = result.NextToken
-				} else {
-					next_round = result.CurrentRound + 1
-					break
-				}
 			}
-			time.Sleep(1 * time.Second)
 		}
 	}()
 
