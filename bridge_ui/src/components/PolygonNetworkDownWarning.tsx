2b64e5a10c76b48d7175c4bf1ab872858950c874
Leopold Schabel <leo@certus.one>
2022-03-11 23:52:29 +0100

    bridge_ui: be more specific about Polygon downtime (#994)
    commit-id:12c98920

--- bridge_ui/src/components/PolygonNetworkDownWarning.tsx
+++ bridge_ui/src/components/PolygonNetworkDownWarning.tsx
@@ -13,16 +13,16 @@ export default function PolygonNetworkDownWarning() {
 
   return (
     <Alert variant="outlined" severity="warning" className={classes.alert}>
-      Polygon is experiencing downtime. As a precautionary measure, Wormhole
-      network and portal have temporarily paused Polygon support until the issue
-      is resolved.
+      Polygon is currently experiencing partial downtime.
+      As a precautionary measure, Wormhole Network and Portal have paused Polygon
+      support until the network has been fully restored.
       <Typography component="div">
         <Link
-          href="https://twitter.com/0xPolygonDevs/status/1501944974933303305"
+          href="https://twitter.com/0xPolygonDevs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Link to @0xPolygonDevs tweet
+          Follow @0xPolygonDevs for updates
         </Link>
       </Typography>
     </Alert>
