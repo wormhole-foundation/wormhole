18a9f2ed21f8b867884d8f51c2805e0a8d465ee5
chase-45 <chasemoran45@gmail.com>
2022-03-16 17:54:32 -0400

    ignore ts-sdk lib directory

--- wormhole_chain/ts-sdk/lib/modules/certusone.wormholechain.tokenbridge/types/google/api/annotations.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.protobufPackage = void 0;
-//@ts-nocheck
-/* eslint-disable */
-exports.protobufPackage = "google.api";
