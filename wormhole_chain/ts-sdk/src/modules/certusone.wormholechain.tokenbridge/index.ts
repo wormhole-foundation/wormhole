3db7b1bbbae0489372fc37320b8ee6a1a1f7ef2a
chase-45 <chasemoran45@gmail.com>
2022-03-16 18:32:23 -0400

    failing test for register guardian validator

--- wormhole_chain/ts-sdk/src/modules/certusone.wormholechain.tokenbridge/index.ts
+++ wormhole_chain/ts-sdk/src/modules/certusone.wormholechain.tokenbridge/index.ts
@@ -5,17 +5,17 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgTransfer } from "./types/tokenbridge/tx";
 import { MsgAttestToken } from "./types/tokenbridge/tx";
-import { MsgExecuteVAA } from "./types/tokenbridge/tx";
 import { MsgExecuteGovernanceVAA } from "./types/tokenbridge/tx";
+import { MsgTransfer } from "./types/tokenbridge/tx";
+import { MsgExecuteVAA } from "./types/tokenbridge/tx";
 
 
 const types = [
-  ["/certusone.wormholechain.tokenbridge.MsgTransfer", MsgTransfer],
   ["/certusone.wormholechain.tokenbridge.MsgAttestToken", MsgAttestToken],
-  ["/certusone.wormholechain.tokenbridge.MsgExecuteVAA", MsgExecuteVAA],
   ["/certusone.wormholechain.tokenbridge.MsgExecuteGovernanceVAA", MsgExecuteGovernanceVAA],
+  ["/certusone.wormholechain.tokenbridge.MsgTransfer", MsgTransfer],
+  ["/certusone.wormholechain.tokenbridge.MsgExecuteVAA", MsgExecuteVAA],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -48,10 +48,10 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
-    msgTransfer: (data: MsgTransfer): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgTransfer", value: MsgTransfer.fromPartial( data ) }),
     msgAttestToken: (data: MsgAttestToken): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgAttestToken", value: MsgAttestToken.fromPartial( data ) }),
-    msgExecuteVAA: (data: MsgExecuteVAA): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgExecuteVAA", value: MsgExecuteVAA.fromPartial( data ) }),
     msgExecuteGovernanceVAA: (data: MsgExecuteGovernanceVAA): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgExecuteGovernanceVAA", value: MsgExecuteGovernanceVAA.fromPartial( data ) }),
+    msgTransfer: (data: MsgTransfer): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgTransfer", value: MsgTransfer.fromPartial( data ) }),
+    msgExecuteVAA: (data: MsgExecuteVAA): EncodeObject => ({ typeUrl: "/certusone.wormholechain.tokenbridge.MsgExecuteVAA", value: MsgExecuteVAA.fromPartial( data ) }),
     
   };
 };
