9dbd358c31dd46c6e44a37ffd3c7639fbf3f0a2a
Josh Siegel <jsiegel@jumptrading.com>
2022-03-21 13:25:51 +0000

    algo/integration: verify functions @ gusc1a-ossdev-jsl1

--- sdk/js/src/token_bridge/Algorand.ts
+++ sdk/js/src/token_bridge/Algorand.ts
@@ -61,6 +61,9 @@ export const TESTNET_ACCOUNT_ADDRESS =
 export const TESTNET_ACCOUNT_MN =
     "enforce sail meat library retreat rain praise run floor drastic flat end true olympic boy dune dust regular feed allow top universe borrow able ginger";
 
+const ALGO_VERIFY_HASH = "5PKAA6VERF6XZQAOJVKST262JKLKIYP3EGFO54ZD7YPUV7TRQOZ2BN6SPA"
+const ALGO_VERIFY = new Uint8Array([6, 32, 4, 1, 0, 32, 20, 38, 1, 0, 49, 32, 50, 3, 18, 68, 49, 16, 129, 6, 18, 68, 54, 26, 1, 54, 26, 3, 54, 26, 2, 136, 0, 3, 68, 34, 67, 53, 2, 53, 1, 53, 0, 40, 53, 240, 40, 53, 241, 52, 0, 21, 53, 5, 35, 53, 3, 35, 53, 4, 52, 3, 52, 5, 12, 65, 0, 68, 52, 1, 52, 0, 52, 3, 129, 65, 8, 34, 88, 23, 52, 0, 52, 3, 34, 8, 36, 88, 52, 0, 52, 3, 129, 33, 8, 36, 88, 7, 0, 53, 241, 53, 240, 52, 2, 52, 4, 37, 88, 52, 240, 52, 241, 80, 2, 87, 12, 20, 18, 68, 52, 3, 129, 66, 8, 53, 3, 52, 4, 37, 8, 53, 4, 66, 255, 180, 34, 137, ])
+
 export function getKmdClient(): algosdk.Kmd {
     const kmdClient: algosdk.Kmd = new algosdk.Kmd(
         ALGO_TOKEN,
