7f158c32f324b2f93fd6ee1c8054b932fd4d01b2
Josh Siegel <jsiegel@jumptrading.com>
2022-03-24 13:02:21 +0000

    algo/integration: cleanup @ gusc1a-ossdev-jsl1

--- algorand/test/foo2.ts
+++ algorand/test/foo2.ts
@@ -37,13 +37,6 @@ async function firstTransaction() {
         const algodPort = 4001;
         let algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort);
 
-//        const filePath = path.join(__dirname, '../teal/vaa_verify.teal');
-//        const data = fs.readFileSync(filePath);
-//
-//        // Compile teal
-//        const results = await algodClient.compile(data).do();
-//        console.log(results);
-
         //Check your balance
         let accountInfo = await algodClient.accountInformation(myAccount.addr).do();
         console.log("Account balance: %d microAlgos", accountInfo.amount);
@@ -54,51 +47,6 @@ async function firstTransaction() {
 
         let sstate = await submitVAAHdr(evaa, algodClient, myAccount, 4);
         console.log(await simpleSignVAA(evaa, algodClient, myAccount, 4, sstate.txns));
-        
-//        console.log(await submitVAA(new Uint8Array(Buffer.from(vaa, "hex")), algodClient, myAccount, 4))
-//console.log(parseVAA(vaa))
-//        console.log(await submitVAA(vaa, algodClient, myAccount, 4))
-
-//        // Construct the transaction
-//        let params = await algodClient.getTransactionParams().do();
-//        // comment out the next two lines to use suggested fee
-//        params.fee = algosdk.ALGORAND_MIN_TX_FEE;
-//        params.flatFee = true;
-//
-//        // receiver defined as TestNet faucet address 
-//        const receiver = "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA";
-//        const enc = new TextEncoder();
-//        const note = enc.encode("Hello World");
-//        let amount = 1000000;
-//        let sender = myAccount.addr;
-//        let txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
-//            from: sender, 
-//            to: receiver, 
-//            amount: amount, 
-//            note: note, 
-//            suggestedParams: params
-//        });
-//
-//
-//        // Sign the transaction
-//        let signedTxn = txn.signTxn(myAccount.sk);
-//        let txId = txn.txID().toString();
-//        console.log("Signed transaction with txID: %s", txId);
-//
-//        // Submit the transaction
-//        await algodClient.sendRawTransaction(signedTxn).do();
-//
-//        // Wait for confirmation
-//        let confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 4);
-//        //Get the completed Transaction
-//        console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
-//        let string = new TextDecoder().decode(confirmedTxn.txn.txn.note);
-//        console.log("Note field: ", string);
-//        accountInfo = await algodClient.accountInformation(myAccount.addr).do();
-//        console.log("Transaction Amount: %d microAlgos", confirmedTxn.txn.txn.amt);        
-//        console.log("Transaction Fee: %d microAlgos", confirmedTxn.txn.txn.fee);
-//
-//        console.log("Account balance: %d microAlgos", accountInfo.amount);
     }
     catch (err) {
         console.log("err", err);
