c3ce2a2f17c35912170217e19d3090d22d2a4f05
justinschuldt <justinschuldt@gmail.com>
2022-04-01 10:51:46 -0500

    eth-devnet init with multiple guardians

--- ethereum/scripts/register_solana_chain.js
+++ ethereum/scripts/register_solana_chain.js
@@ -5,6 +5,8 @@ const TokenBridge = artifacts.require("TokenBridge");
 const NFTBridge = artifacts.require("NFTBridgeEntrypoint");
 const TokenImplementation = artifacts.require("TokenImplementation");
 const BridgeImplementationFullABI = jsonfile.readFileSync("../build/contracts/BridgeImplementation.json").abi
+const solTokenBridgeVAA = process.env.REGISTER_SOL_TOKEN_BRIDGE_VAA
+const solNFTBridgeVAA = process.env.REGISTER_SOL_NFT_BRIDGE_VAA
 
 module.exports = async function (callback) {
     try {
@@ -13,14 +15,14 @@ module.exports = async function (callback) {
         const nftBridge = new web3.eth.Contract(BridgeImplementationFullABI, NFTBridge.address);
 
         // Register the Solana token bridge endpoint
-        await tokenBridge.methods.registerChain("0x01000000000100c9f4230109e378f7efc0605fb40f0e1869f2d82fda5b1dfad8a5a2dafee85e033d155c18641165a77a2db6a7afbf2745b458616cb59347e89ae0c7aa3e7cc2d400000000010000000100010000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000546f6b656e4272696467650100000001c69a1b1a65dd336bf1df6a77afb501fc25db7fc0938cb08595a9ef473265cb4f").send({
+        await tokenBridge.methods.registerChain("0x" + solTokenBridgeVAA).send({
             value: 0,
             from: accounts[0],
             gasLimit: 2000000
         });
 
         // Register the Solana NFT bridge endpoint
-        await nftBridge.methods.registerChain("0x010000000001007985ba742002ae745c19722fea4d82102e68526c7c9d94d0e5d0a809071c98451c9693b230b3390f4ca9555a3ba9a9abbe87cf6f9e400682213e4fbbe1dabb9e0100000001000000010001000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000004e4654427269646765010000000196ee982293251b48729804c8e8b24b553eb6b887867024948d2236fd37a577ab").send({
+        await nftBridge.methods.registerChain("0x" + solNFTBridgeVAA).send({
             value: 0,
             from: accounts[0],
             gasLimit: 2000000
--- ethereum/scripts/register_terra_chain.js
+++ ethereum/scripts/register_terra_chain.js
@@ -5,6 +5,8 @@ const TokenBridge = artifacts.require("TokenBridge");
 const NFTBridge = artifacts.require("NFTBridgeEntrypoint");
 const TokenImplementation = artifacts.require("TokenImplementation");
 const BridgeImplementationFullABI = jsonfile.readFileSync("../build/contracts/BridgeImplementation.json").abi
+const terraTokenBridgeVAA = process.env.REGISTER_TERRA_TOKEN_BRIDGE_VAA
+const terraNFTBridgeVAA = process.env.REGISTER_TERRA_NFT_BRIDGE_VAA
 
 module.exports = async function (callback) {
     try {
@@ -13,14 +15,14 @@ module.exports = async function (callback) {
         const nftBridge = new web3.eth.Contract(BridgeImplementationFullABI, NFTBridge.address);
 
         // Register the terra token bridge endpoint
-        await tokenBridge.methods.registerChain("0x010000000001009a895e8b42444fdf60a71666121d7e84b3a49579ba3b84fff1dbdf9ec93360390c05a88f66c90df2034cb38427ba9b01632e780ce7b84df559a1bf44c316447d01000000010000000100010000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000546f6b656e4272696467650100000003000000000000000000000000784999135aaa8a3ca5914468852fdddbddd8789d").send({
+        await tokenBridge.methods.registerChain("0x" + terraTokenBridgeVAA).send({
             value: 0,
             from: accounts[0],
             gasLimit: 2000000
         });
 
         // Register the terra NFT bridge endpoint
-        await nftBridge.methods.registerChain("0x01000000000100880ab34efd145fa1c9c4f2f7b233f3bc99cb730ab2d2138babf989a2555e2e8f737b759f91970d300c4364249b0740b134dbefb943fb55fd76483261ce68c17b0100000001000000010001000000000000000000000000000000000000000000000000000000000000000400000000023c2cdd0000000000000000000000000000000000000000000000004e465442726964676501000000030000000000000000000000000fe5c51f539a651152ae461086d733777a54a134").send({
+        await nftBridge.methods.registerChain("0x" + terraNFTBridgeVAA).send({
             value: 0,
             from: accounts[0],
             gasLimit: 2000000
