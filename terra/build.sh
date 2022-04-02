e1f4b8e10ba6c114a39ad822e5293928d5999cd2
Csongor Kiss <ckiss@jumptrading.com>
2022-04-02 13:30:40 +0000

    Add scripts and readme to deploy and verify terra contracts

--- terra/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm -v "$(pwd)":/code \
-  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.12.1
