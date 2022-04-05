8be25b3aec4ecad2acbf602b056f83246301d77b
Josh Siegel <jsiegel@jumptrading.com>
2022-04-05 13:39:47 +0000

    algo/integration: update words @ gusc1a-ossdev-jsl1

--- algorand/test/foo2.ts
+++ algorand/test/foo2.ts
@@ -28,7 +28,7 @@ const PYTH_PAYLOAD = '0x50325748000101230abfe0ec3b460bd55fc4fb363567163299151454
 async function firstTransaction() {
     try {
         // This is a funded account... 
-        let myAccount = algosdk.mnemonicToSecretKey("total prepare learn month artefact friend smile ranch humble peasant trend indoor smile seat short identify vague truly barrel hockey toss boss voyage abandon salt")
+        let myAccount = algosdk.mnemonicToSecretKey("intact frozen tooth wealth syrup elevator list book property census imitate attend draft silly fortune afford injury poem section wait main bench feel absent giraffe")
 
         console.log(myAccount)
 
