e4e518ec7c6fa1e0ac0053388f2eccc8e7a13af0
Jonathan Claudius <jclaudius@jumptrading.com>
2022-04-07 20:21:27 +0000

    Add multi-signer same key cases

--- node/pkg/processor/observation_test.go
+++ node/pkg/processor/observation_test.go
@@ -77,6 +77,10 @@ func TestHandleInboundSignedVAAWithQuorum(t *testing.T) {
 			err: "received SignedVAAWithQuorum message with no VAA signatures"},
 		{label: "VAAInvalidSignatures", keyOrder: []*ecdsa.PrivateKey{badPrivateKey1}, indexOrder: []uint8{0}, addrs: []ethcommon.Address{goodAddr1},
 			err: "received SignedVAAWithQuorum message with invalid VAA signatures"},
+		{label: "DuplicateGoodSignaturesNonMonotonic", keyOrder: []*ecdsa.PrivateKey{goodPrivateKey1, goodPrivateKey1, goodPrivateKey1, goodPrivateKey1}, indexOrder: []uint8{0, 0, 0, 0}, addrs: []ethcommon.Address{goodAddr1},
+			err: "received SignedVAAWithQuorum message with invalid VAA signatures"},
+		{label: "DuplicateGoodSignaturesMonotonic", keyOrder: []*ecdsa.PrivateKey{goodPrivateKey1, goodPrivateKey1, goodPrivateKey1, goodPrivateKey1}, indexOrder: []uint8{0, 1, 2, 3}, addrs: []ethcommon.Address{goodAddr1},
+			err: "received SignedVAAWithQuorum message with invalid VAA signatures"},
 	}
 
 	for _, tc := range tests {
