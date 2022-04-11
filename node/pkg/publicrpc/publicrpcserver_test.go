b18a6c8c2f478e5a21b270c10abf6f4922e936d3
Jonathan Claudius <jclaudius@jumptrading.com>
2022-04-11 19:16:59 -0400

    Fix segfault from empty GetSignedVAARequest (#1069)

---
+++ node/pkg/publicrpc/publicrpcserver_test.go
@@ -0,0 +1,64 @@
+package publicrpc
+
+import (
+	"context"
+	publicrpcv1 "github.com/certusone/wormhole/node/pkg/proto/publicrpc/v1"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"testing"
+)
+
+func TestGetSignedVAANoMessage(t *testing.T) {
+	msg := publicrpcv1.GetSignedVAARequest{}
+	ctx := context.Background()
+
+	logger, _ := zap.NewProduction()
+	server := &PublicrpcServer{logger: logger}
+
+	resp, err := server.GetSignedVAA(ctx, &msg)
+	assert.Nil(t, resp)
+
+	expected_err := status.Error(codes.InvalidArgument, "no message ID specified")
+	assert.Equal(t, expected_err, err)
+}
+
+func TestGetSignedVAANoAddress(t *testing.T) {
+	msg := publicrpcv1.GetSignedVAARequest{MessageId: &publicrpcv1.MessageID{}}
+	ctx := context.Background()
+
+	logger, _ := zap.NewProduction()
+	server := &PublicrpcServer{logger: logger}
+
+	resp, err := server.GetSignedVAA(ctx, &msg)
+	assert.Nil(t, resp)
+
+	expected_err := status.Error(codes.InvalidArgument, "address must be 32 bytes")
+	assert.Equal(t, expected_err, err)
+}
+
+func TestGetSignedVAABadAddress(t *testing.T) {
+	chainID := uint32(1)
+	emitterAddr := "AAAA"
+	seq := uint64(1)
+
+	msg := publicrpcv1.GetSignedVAARequest{
+		MessageId: &publicrpcv1.MessageID{
+			EmitterChain:   publicrpcv1.ChainID(chainID),
+			EmitterAddress: emitterAddr,
+			Sequence:       seq,
+		},
+	}
+
+	ctx := context.Background()
+
+	logger, _ := zap.NewProduction()
+	server := &PublicrpcServer{logger: logger}
+
+	resp, err := server.GetSignedVAA(ctx, &msg)
+	assert.Nil(t, resp)
+
+	expected_err := status.Error(codes.InvalidArgument, "address must be 32 bytes")
+	assert.Equal(t, expected_err, err)
+}
