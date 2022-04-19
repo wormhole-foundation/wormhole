0f6c1e2d9813355c2e14b6f596b0a9434a33dff5
Csongor Kiss <ckiss@jumptrading.com>
2022-04-19 16:59:00 +0100

    Allow specifying the timestamp in governance inject
    The 'timestamp' field is now allowed in a governance prototxt, which
    applies to all the messages in the struct. If the timestamp is not
    specified, then it defaults to 0, which is identical to the existing
    behaviour.

--- node/pkg/vaa/governance.go
+++ node/pkg/vaa/governance.go
@@ -7,12 +7,12 @@ import (
 var GovernanceEmitter = Address{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4}
 var GovernanceChain = ChainIDSolana
 
-func CreateGovernanceVAA(nonce uint32, sequence uint64, guardianSetIndex uint32, payload []byte) *VAA {
+func CreateGovernanceVAA(timestamp time.Time, nonce uint32, sequence uint64, guardianSetIndex uint32, payload []byte) *VAA {
 	vaa := &VAA{
 		Version:          SupportedVAAVersion,
 		GuardianSetIndex: guardianSetIndex,
 		Signatures:       nil,
-		Timestamp:        time.Unix(0, 0),
+		Timestamp:        timestamp,
 		Nonce:            nonce,
 		Sequence:         sequence,
 		ConsistencyLevel: 32,
