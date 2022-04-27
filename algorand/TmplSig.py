b83024a2e9a377b3f49e94bd3d21156495eebc52
Josh Siegel <jsiegel@jumptrading.com>
2022-04-27 18:17:07 +0000

    algo/contracts2: more cleanup @ gusc1a-ossdev-jsl5

--- algorand/TmplSig.py
+++ algorand/TmplSig.py
@@ -40,7 +40,12 @@ class TmplSig:
             "name":"lsig.teal",
             "version":6,"source":"",
             "bytecode":"BiABAYEASIAASIEANQCAADUBMRkiEkAADTEWIgk1AjEWNQNCAAoxFjUCMRYiCDUDNAI4EIEGEkQ0AjgZIhJENAI4GDQAEkQ0AjggMgMSRDQDOBAiEkQ0AzgIgQASRDQDOCA0ARJENAM4CTIDEkQiQw==",
-            "template_labels":{"TMPL_ADDR_IDX":{"source_line":3,"position":5,"bytes":False},"TMPL_APP_ADDRESS":{"source_line":9,"position":15,"bytes":True},"TMPL_APP_ID":{"source_line":7,"position":11,"bytes":False},"TMPL_EMITTER_ID":{"source_line":5,"position":8,"bytes":True}},
+            "template_labels":{
+                "TMPL_ADDR_IDX":{"source_line":3,"position":5,"bytes":False},
+                "TMPL_EMITTER_ID":{"source_line":5,"position":8,"bytes":True},
+                "TMPL_APP_ID":{"source_line":7,"position":11,"bytes":False},
+                "TMPL_APP_ADDRESS":{"source_line":9,"position":15,"bytes":True}
+            },
             "label_map":{"main_l2":21,"main_l3":34},
             "line_map":[0,1,4,6,7,9,10,12,14,16,18,20,21,22,25,27,28,29,31,33,35,0,38,40,42,44,45,46,0,48,50,52,54,55,56,58,60,61,62,63,65,67,69,70,71,73,75,77,78,79,81,83,84,85,86,88,90,92,93,94,96,98,100,101,102,104,106,108,109,110,111]
         }
