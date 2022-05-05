a0b66a1a5fb2fd860536350bc4cbf1ca72fd941a
Paul Noel <panoel007@gmail.com>
2022-05-05 14:50:01 +0000

    algo/make: add Makefile and test script

---
+++ algorand/runPythonUnitTests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+if [ ! -d _sandbox ]; then
+  echo We need to create it...
+  git clone https://github.com/algorand/sandbox.git _sandbox
+fi
+if [ "`grep enable-all-parameters _sandbox/images/indexer/start.sh | wc -l`" == "0" ]; then
+  echo the indexer is incorrectly configured
+  sed -i -e 's/dev-mode/dev-mode --enable-all-parameters/'  _sandbox/images/indexer/start.sh
+  echo delete all the existing docker images
+  ./sandbox clean
+fi
+./sandbox up dev
+echo "run the tests"
+cd test
+python3 test.py
+echo "bring the sandbox down"
+cd ..
+./sandbox down
