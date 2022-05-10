ca0433a5ade917d7879f72f93987e443910d8e8a
Paul Noel <panoel007@gmail.com>
2022-05-10 20:56:41 +0000

    bridge_ui: changed formatting for token picker

---
+++ bridge_ui/src/utils/balancePretty.ts
@@ -0,0 +1,17 @@
+export const balancePretty = (uiString: string) => {
+  const numberString = uiString.split(".")[0];
+  const nsLen = numberString.length;
+  if (nsLen > 9) {
+    // Billion case
+    const num = numberString.substring(0, nsLen - 9);
+    const fract = numberString.substring(nsLen - 9, nsLen - 9 + 2);
+    return num + "." + fract + " B";
+  } else if (nsLen > 6) {
+    // Million case
+    const num = numberString.substring(0, nsLen - 6);
+    const fract = numberString.substring(nsLen - 6, nsLen - 6 + 2);
+    return num + "." + fract + " M";
+  } else {
+    return uiString;
+  }
+};
