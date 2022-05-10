1cb2a739b04acce593afc4b69d990b2421d74657
claudijd <jclaudius@jumptrading.com>
2022-05-10 09:31:23 -0400

    Neaten up the quorum conditional

--- ethereum/contracts/Messages.sol
+++ ethereum/contracts/Messages.sol
@@ -51,8 +51,7 @@ contract Messages is Getters {
         *   if making any changes to this, obtain additional peer review. If guardianSet key length is 0 and 
         *   vm.signatures length is 0, this could compromise the integrity of both vm and signature verification.
         */
-        (bool quorum_result) = quorum(vm.signatures, guardianSet);
-        if !quorum_result{
+        if !quorum(vm.signatures, guardianSet){
             return (false, "no quorum");
         }
 
