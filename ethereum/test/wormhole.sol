c0eac087e40760857dbd3cba45070d47da215620
claudijd <jclaudius@jumptrading.com>
2022-05-10 09:54:03 -0400

    Another iteration on figuring out the test logic

--- ethereum/test/wormhole.sol
+++ ethereum/test/wormhole.sol
@@ -4,12 +4,27 @@ import "truffle/Assert.sol";
 import "../contracts/Messages.sol";
 
 contract TestMessages {
-  function testQuorum() {
-    // TODO: Setup cases and loop through them with assert expectations
-    // setup test signatures
-    // setup test guardianSet
+  function testQuorum() { 
+    // An array to store our test cases
+    type[] testCases;
 
-    Assert.equal(quorum(signatures, guardianSet), true, "We should have quorum");
-    Assert.equal(quorum(signatures, guardianSet), true, "We should not have quorum");
+    // A struct to define our test cases
+	struct TestCase {
+		Signature[] signatures;
+        GuardianSet guardianSet;
+        bool result;
+	}
+
+    // All our test cases to the array
+    // TODO: figure out how to build up these test cases in Solidity
+    testCases.push(TestCase(signatures1, guardianSet, true));
+    testCases.push(TestCase(signatures2, guardianSet, false));
+
+    // Loop through our test cases and make sure they meet our expectations
+    uint testCaseLength = testCases.length;
+    for (uint i=0; i<testCaseLength; i++) {
+        testCase = testCases[i]
+        Assert.equal(quorum(testCase.signatures, testCase.guardianSet), testCase.result, "We should return the right quorum expectation");
+    }
   }
 }
