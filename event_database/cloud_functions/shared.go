e545fdd095fea509103f7efb5842722d409003d4
ceun <ceun@jumptrading.com>
2022-05-16 19:32:39 +0000

    Added fake btc on terra to tokensToSkip list

--- event_database/cloud_functions/shared.go
+++ event_database/cloud_functions/shared.go
@@ -41,33 +41,33 @@ var releaseDay = time.Date(2021, 9, 13, 0, 0, 0, 0, time.UTC)
 
 // token addresses blacklisted from TVL calculation
 var tokensToSkip = map[string]bool{
-	"0x04132bf45511d03a58afd4f1d36a29d229ccc574": true,
-	"0xa79bd679ce21a2418be9e6f88b2186c9986bbe7d": true,
-	"0x931c3987040c90b6db09981c7c91ba155d3fa31f": true,
-	"0x8fb1a59ca2d57b51e5971a85277efe72c4492983": true,
-	"0xd52d9ba6fcbadb1fe1e3aca52cbb72c4d9bbb4ec": true,
-	"0x1353c55fd2beebd976d7acc4a7083b0618d94689": true,
-	"0xf0fbdb8a402ec0fc626db974b8d019c902deb486": true,
-	"0x1fd4a95f4335cf36cac85730289579c104544328": true,
-	"0x358aa13c52544eccef6b0add0f801012adad5ee3": true,
-	"0xbe32b7acd03bcc62f25ebabd169a35e69ef17601": true,
-	"0x7ffb3d637014488b63fb9858e279385685afc1e2": true,
-	"0x337dc89ebcc33a337307d58a51888af92cfdc81b": true,
-	"0x5Cb89Ac06F34f73B1A6b8000CEb0AfBc97d58B6b": true,
-	"0xd9F0446AedadCf16A12692E02FA26C617FA4D217": true,
-	"0xD7b41531456b636641F7e867eC77120441D1E1E8": true,
-	"0x9f607027b69f6e123bc3bd56a686b735fa75f30a": true,
-	"0x2a35965bbad6fd3964ef815d011c51ab1c546e67": true,
-	"0x053c070f0923a5b770cc59d7bf74ecff991cd0b8": true,
-	"0xA18036c8ecb3235087d990c886c242546D1E560f": true,
-	"0x6B3105826942071E7B6346cbE9867d37Ed7f98Eb": true,
-	"0x0749902ae8ed9c6a508271bad18f185dba7185d4": true, // fake WETH on poly
-	"0x4411146b7714f5dc7aa4445fcb44e3ca120c8a1e": true, // testWETH on poly
-	"0xE389Ac691BD2b0228DAFFfF548fbcE38470373E8": true, // fake WMATIC on poly
-	"0x7e347498dfef39a88099e3e343140ae17cde260e": true, // fake wAVAX on bsc
-	"0x685629e5e99e3959254c4d23cd9097fbaef01fb2": true, // amWeth
-	
-	
+	"0x04132bf45511d03a58afd4f1d36a29d229ccc574":   true,
+	"0xa79bd679ce21a2418be9e6f88b2186c9986bbe7d":   true,
+	"0x931c3987040c90b6db09981c7c91ba155d3fa31f":   true,
+	"0x8fb1a59ca2d57b51e5971a85277efe72c4492983":   true,
+	"0xd52d9ba6fcbadb1fe1e3aca52cbb72c4d9bbb4ec":   true,
+	"0x1353c55fd2beebd976d7acc4a7083b0618d94689":   true,
+	"0xf0fbdb8a402ec0fc626db974b8d019c902deb486":   true,
+	"0x1fd4a95f4335cf36cac85730289579c104544328":   true,
+	"0x358aa13c52544eccef6b0add0f801012adad5ee3":   true,
+	"0xbe32b7acd03bcc62f25ebabd169a35e69ef17601":   true,
+	"0x7ffb3d637014488b63fb9858e279385685afc1e2":   true,
+	"0x337dc89ebcc33a337307d58a51888af92cfdc81b":   true,
+	"0x5Cb89Ac06F34f73B1A6b8000CEb0AfBc97d58B6b":   true,
+	"0xd9F0446AedadCf16A12692E02FA26C617FA4D217":   true,
+	"0xD7b41531456b636641F7e867eC77120441D1E1E8":   true,
+	"0x9f607027b69f6e123bc3bd56a686b735fa75f30a":   true,
+	"0x2a35965bbad6fd3964ef815d011c51ab1c546e67":   true,
+	"0x053c070f0923a5b770cc59d7bf74ecff991cd0b8":   true,
+	"0xA18036c8ecb3235087d990c886c242546D1E560f":   true,
+	"0x6B3105826942071E7B6346cbE9867d37Ed7f98Eb":   true,
+	"0x0749902ae8ed9c6a508271bad18f185dba7185d4":   true, // fake WETH on poly
+	"0x4411146b7714f5dc7aa4445fcb44e3ca120c8a1e":   true, // testWETH on poly
+	"0xE389Ac691BD2b0228DAFFfF548fbcE38470373E8":   true, // fake WMATIC on poly
+	"0x7e347498dfef39a88099e3e343140ae17cde260e":   true, // fake wAVAX on bsc
+	"0x685629e5e99e3959254c4d23cd9097fbaef01fb2":   true, // amWeth
+	"terra1vpehfldr2u2m2gw38zaryp4tfw7fe2kw2lryjf": true, //fake btc on terra
+
 }
 
 // init runs during cloud function initialization. So, this will only run during an
