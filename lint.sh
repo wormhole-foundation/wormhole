fbd9b9144d4312fc8446c6d9c8db30045a4007e9
tbjump <>
2022-05-23 13:49:31 +0000

    ignore status code of grep

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 set -o pipefail
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.lint . 2>&1  | while read line; do echo $line; echo $line >&2; done | grep "::" | cut -f3- -d " "
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.lint . 2>&1  | while read line; do echo $line; echo $line >&2; done | (grep "::" || true) | cut -f3- -d " "
