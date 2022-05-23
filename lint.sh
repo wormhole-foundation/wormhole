3c6862db8eae59134180fe2353ac373469af8eda
tbjump <103955289+tbjump@users.noreply.github.com>
2022-05-23 16:21:10 +0200

    enable additional linters, including gosec (#1215)
    enable additional linters, including gosec

--- lint.sh
+++ lint.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.lint .
+# fail if any command fails
+set -e
+set -o pipefail
+
+# we duplicate stderr to stdout and then filter and parse stdout to only include errors that are readable as github annotations
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.lint . 2>&1  | while read line; do echo $line; echo $line >&2; done | (grep "::" || true) | cut -f3- -d " "
