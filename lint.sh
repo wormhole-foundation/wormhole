3e76c0a5b19c35d04fcea5a072ae823daea840b5
tbjump <>
2022-05-23 11:56:30 +0000

    test

--- lint.sh
+++ lint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo "LS"
-ls 
-echo "LS PWD"
-ls $(pwd)
+ls -al $(pwd)/node
 
 docker run --mount=type=bind,target=/app,source=$(pwd)/node --workdir /app $(DOCKER_BUILDKIT=1 docker build -q -f Dockerfile.lint .) sh -c 'GOGC=off /home/lint/golangci-lint run --skip-dirs pkg/supervisor --timeout=10m  --out-format=github-actions ./...'
