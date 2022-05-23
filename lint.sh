f5e397f393bc79a7985168f665c44a2c8f0ec21f
tbjump <>
2022-05-23 12:36:51 +0000

    lint

--- lint.sh
+++ lint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.lint .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.lint . 2>&1  | while read line; do echo $line; echo $line >&2; done | grep "::" | cut -f3- -d " "
