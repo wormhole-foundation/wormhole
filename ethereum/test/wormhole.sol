5a23c9cf8239478dda3b3508ce49fd3b24d4e9fc
claudijd <jclaudius@jumptrading.com>
2022-05-23 08:57:14 -0400

    Fix variable name

--- ethereum/test/wormhole.sol
+++ ethereum/test/wormhole.sol
@@ -6,7 +6,7 @@ import "../contracts/Messages.sol";
 contract TestMessages {
   function testQuorum() public { 
     // An array to hold our test cases
-    uint[][] balance = [[0, 1],[1, 1],[2, 2],[3, 3],[4, 3],[5, 4],[6, 5],[7, 5],[8, 6],[9, 7],[10, 7],[11, 8],[12, 9],[20, 14]];
+    uint[][] testCases = [[0, 1],[1, 1],[2, 2],[3, 3],[4, 3],[5, 4],[6, 5],[7, 5],[8, 6],[9, 7],[10, 7],[11, 8],[12, 9],[20, 14]];
 
     // Loop through our testCases array and assert that our expectations are met
     for (uint i=0; i<testCases.length; i++) {
