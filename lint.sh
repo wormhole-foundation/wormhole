5fea994c3315a38b2c773e657ee8b034f15b4ddb
tbjump <>
2022-05-23 10:39:12 +0000

    make linter output parsable by github

--- lint.sh
+++ lint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.lint .
+docker run --mount=type=bind,target=/app,source=$(pwd)/node --workdir /app $(DOCKER_BUILDKIT=1 docker build -q -f Dockerfile.lint .) sh -c 'GOGC=off /home/lint/golangci-lint run --skip-dirs pkg/supervisor --timeout=10m  --out-format=github-actions ./...'
