8b7291848b52f8dd60c53f1caf84c957ec014ae8
tbjump <>
2022-05-23 11:54:51 +0000

    test

--- lint.sh
+++ lint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-ls /opt/gha/_work/wormhole/*
+echo "LS"
+ls 
+echo "LS PWD"
+ls $(pwd)
+
 docker run --mount=type=bind,target=/app,source=$(pwd)/node --workdir /app $(DOCKER_BUILDKIT=1 docker build -q -f Dockerfile.lint .) sh -c 'GOGC=off /home/lint/golangci-lint run --skip-dirs pkg/supervisor --timeout=10m  --out-format=github-actions ./...'
