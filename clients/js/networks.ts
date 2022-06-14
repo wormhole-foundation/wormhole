fd8f8bc775d4f610735d15f5706da08c9ee09ce9
Csongor Kiss <ckiss@jumptrading.com>
2022-06-14 18:35:28 +0100

    use ankr

--- clients/js/networks.ts
+++ clients/js/networks.ts
@@ -31,7 +31,7 @@ const MAINNET = {
     key: get_env_var("TERRA_MNEMONIC"),
   },
   ethereum: {
-    rpc: `https://mainnet.infura.io/v3/${get_env_var("INFURA_KEY")}`,
+    rpc: `https://rpc.ankr.com/eth`,
     key: get_env_var("ETH_KEY"),
   },
   bsc: {
@@ -39,11 +39,11 @@ const MAINNET = {
     key: get_env_var("ETH_KEY"),
   },
   polygon: {
-    rpc: "https://polygon-rpc.com",
+    rpc: "https://rpc.ankr.com/polygon",
     key: get_env_var("ETH_KEY"),
   },
   avalanche: {
-    rpc: "https://api.avax.network/ext/bc/C/rpc",
+    rpc: "https://rpc.ankr.com/avalanche",
     key: get_env_var("ETH_KEY"),
   },
   algorand: {
@@ -87,7 +87,7 @@ const MAINNET = {
     key: undefined,
   },  
   ropsten: {
-    rpc: `https://ropsten.infura.io/v3/${get_env_var("INFURA_KEY")}`,
+    rpc: `https://rpc.ankr.com/eth_ropsten`,
     key: get_env_var("ETH_KEY"),
   },
 };
@@ -107,7 +107,7 @@ const TESTNET = {
     key: get_env_var("TERRA_MNEMONIC"),
   },
   ethereum: {
-    rpc: `https://goerli.infura.io/v3/${get_env_var("INFURA_KEY")}`,
+    rpc: `https://rpc.ankr.com/eth_goerli`,
     key: get_env_var("ETH_KEY"),
   },
   bsc: {
@@ -115,11 +115,11 @@ const TESTNET = {
     key: get_env_var("ETH_KEY"),
   },
   polygon: {
-    rpc: `https://polygon-mumbai.infura.io/v3/${get_env_var("INFURA_KEY")}`,
+    rpc: `https://rpc.ankr.com/polygon_mumbai`,
     key: get_env_var("ETH_KEY"),
   },
   avalanche: {
-    rpc: "https://api.avax-test.network/ext/bc/C/rpc",
+    rpc: "https://rpc.ankr.com/avalanche_fuji",
     key: get_env_var("ETH_KEY"),
   },
   oasis: {
@@ -163,7 +163,7 @@ const TESTNET = {
     key: get_env_var("ETH_KEY"),
   },  
   ropsten: {
-    rpc: `https://ropsten.infura.io/v3/${get_env_var("INFURA_KEY")}`,
+    rpc: `https://rpc.ankr.com/eth_ropsten`,
     key: get_env_var("ETH_KEY"),
   },
 };
