4f3aa52dc13c6c379cf9a6c1273163dabc4b455b
Csongor Kiss <ckiss@jumptrading.com>
2022-07-01 00:02:16 +0100

    clients/js: Dump payload in case of parse failure

--- clients/js/main.ts
+++ clients/js/main.ts
@@ -452,8 +452,8 @@ yargs(hideBin(process.argv))
       const buf = Buffer.from(vaa_hex, "hex");
       const parsed_vaa = vaa.parse(buf);
 
-      if (!vaa.hasPayload(parsed_vaa)) {
-        throw Error("Couldn't parse VAA payload");
+      if (parsed_vaa.payload.type === "Other") {
+        throw Error(`Couldn't parse VAA payload: ${parsed_vaa.payload.hex}`);
       }
 
       console.log(parsed_vaa.payload);
