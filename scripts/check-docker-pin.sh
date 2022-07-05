02221421a5b7b47deca2f9cda4f7d649e0d1416b
claudijd <jclaudius@jumptrading.com>
2022-07-05 15:20:44 -0400

    Clean up find command

--- scripts/check-docker-pin.sh
+++ scripts/check-docker-pin.sh
@@ -4,7 +4,7 @@
 #   - https://nickjanetakis.com/blog/docker-tip-18-please-pin-your-docker-image-versions
 #   - https://snyk.io/blog/10-docker-image-security-best-practices/ (Specifically: USE FIXED TAGS FOR IMMUTABILITY)
 #
-find ~+ -type f -name "Docker*" -print0 | xargs -0 grep -s "FROM" {} | grep -v scratch | egrep -v 'scratch|sha256|solana AS builder|solana AS ci_tests|node_module'
+find . -name 'Dockerfile*' -print0  -type f | xargs -0 grep -s "FROM" {} | grep -v scratch | egrep -v 'scratch|sha256|solana AS builder|solana AS ci_tests|node_module'
 if [ $? -eq 0 ]; then
    echo "[!] Unpinned docker files" >&2
    exit 1
