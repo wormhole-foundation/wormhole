bb13d6a5ad7cce16507f12a72ef5224ca1879753
claudijd <jclaudius@jumptrading.com>
2022-07-05 16:01:30 -0400

    Switch from find to git ls-files and add justification for ignore choices

--- scripts/check-docker-pin.sh
+++ scripts/check-docker-pin.sh
@@ -6,7 +6,12 @@
 #   - https://nickjanetakis.com/blog/docker-tip-18-please-pin-your-docker-image-versions
 #   - https://snyk.io/blog/10-docker-image-security-best-practices/ (Specifically: USE FIXED TAGS FOR IMMUTABILITY)
 #
-find . -name 'Dockerfile*' -print0  -type f | xargs -0 grep -s "FROM" {} | egrep -v 'scratch|sha256|solana AS (builder|ci_tests)|node_module'
+# Explaination of regex ignore choices
+#   - We ignore sha256 because it suggests that the image dep is pinned
+#   - We ignore scratch because it's literally the docker base image
+#   - We ignore solana AS (builder|ci_tests) because it's a relative reference to another FROM call
+#
+git ls-files | grep "Dockerfile*" | xargs grep -s "FROM" | egrep -v 'sha256|scratch|solana AS (builder|ci_tests)'
 if [ $? -eq 0 ]; then
    echo "[!] Unpinned docker files" >&2
    exit 1
