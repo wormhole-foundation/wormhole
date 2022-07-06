0919f29dc37bfe10d799d490a88a1208295a7f36
Jonathan Claudius <jclaudius@jumptrading.com>
2022-07-06 15:05:30 -0400

    Docker FROM image pinning (#1337)
    * Add script to check docker pinning project wide
    
    * Add workflow to run check-docker-pin
    
    * Pin Docker image refs which were unpinned
    
    * Add exceptions to the pin checking logic
    
    * Fixes to check-docker-pin
    
    * Clean up find command
    
    * Bash optimizations
    
    * Switch to env shebang
    
    * Switch from find to git ls-files and add justification for ignore choices

---
+++ scripts/check-docker-pin.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This script is checks to that all our Docker images are pinned to a specific SHA256 hash
+#
+# References as to why...
+#   - https://nickjanetakis.com/blog/docker-tip-18-please-pin-your-docker-image-versions
+#   - https://snyk.io/blog/10-docker-image-security-best-practices/ (Specifically: USE FIXED TAGS FOR IMMUTABILITY)
+#
+# Explaination of regex ignore choices
+#   - We ignore sha256 because it suggests that the image dep is pinned
+#   - We ignore scratch because it's literally the docker base image
+#   - We ignore solana AS (builder|ci_tests) because it's a relative reference to another FROM call
+#
+git ls-files | grep "Dockerfile*" | xargs grep -s "FROM" | egrep -v 'sha256|scratch|solana AS (builder|ci_tests)'
+if [ $? -eq 0 ]; then
+   echo "[!] Unpinned docker files" >&2
+   exit 1
+else
+   echo "[+] No unpinned docker files"
+fi
