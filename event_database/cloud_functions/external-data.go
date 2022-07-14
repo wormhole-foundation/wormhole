cbc92a957146cf2d62bba60a7d3988fe42fc8099
kev1n-peters <96065607+kev1n-peters@users.noreply.github.com>
2022-07-14 12:27:12 -0500

    cloud_functions: terra2 support (#1349)

--- event_database/cloud_functions/external-data.go
+++ event_database/cloud_functions/external-data.go
@@ -107,6 +107,8 @@ func chainIdToCoinGeckoPlatform(chain vaa.ChainID) string {
 		return "klay-token"
 	case vaa.ChainIDCelo:
 		return "celo"
+	case vaa.ChainIDTerra2:
+		return "" // TODO
 	case vaa.ChainIDEthereumRopsten:
 		return "ethereum"
 	}
