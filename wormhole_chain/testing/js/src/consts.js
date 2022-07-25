94206b91c2fc8d0d58fdb699da445816bfd3614b
Chirantan Ekbote <cekbote@jumptrading.com>
2022-07-25 17:57:15 +0900

    wormchain: Fix ports in tests
    The current ports are already taken by terra2.

--- wormhole_chain/testing/js/src/consts.js
+++ wormhole_chain/testing/js/src/consts.js
@@ -25,8 +25,8 @@ export const GUARDIAN_VALIDATOR_VALADDR =
 export const GUARDIAN_VALIDATOR_BASE64_VALADDR = "wQggmD8zRWznvrOgRvWoP6NPAn0=";
 
 //This is a VAA in hex which is for guardian set 2, where Guardian 2 is the only active guardian.
-export const NODE_URL = "http://0.0.0.0:1318";
-export const TENDERMINT_URL = "http://localhost:26658";
+export const NODE_URL = "http://0.0.0.0:1319";
+export const TENDERMINT_URL = "http://localhost:26659";
 //export const FAUCET_URL = "http://localhost:4500";
 
 export const WORM_DENOM = "uworm";
