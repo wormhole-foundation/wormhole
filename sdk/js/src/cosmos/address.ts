033fbfa425b49d7681ee3d9d6437ec8b1eccb0c1
Chirantan Ekbote <cekbote@jumptrading.com>
2022-07-28 18:15:39 +0900

    sdk/js: Move address functions to cosmos package
    `canonicalAddress` and `humanAddress` are useful functions for all
    cosmos chains so move it into a new cosmos package.  This also means we
    need to stop hardcoding "terra" in `humanAddress` and take the human
    readable part as a parameter instead.

---
+++ sdk/js/src/cosmos/address.ts
@@ -0,0 +1,8 @@
+import { bech32 } from "bech32";
+
+export function canonicalAddress(humanAddress: string) {
+    return new Uint8Array(bech32.fromWords(bech32.decode(humanAddress).words));
+}
+export function humanAddress(hrp: string, canonicalAddress: Uint8Array) {
+    return bech32.encode(hrp, bech32.toWords(canonicalAddress));
+}
