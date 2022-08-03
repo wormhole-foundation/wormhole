3a5fc14a934b5d886ba24ba6aab044d9cd1ed77a
Josh Siegel <jsiegel@jumptrading.com>
2022-08-03 16:54:05 +0000

    aptos/integration: Fix the builds @ gusc1a-ossdev-jsl5

--- aptos/node_builder.sh
+++ aptos/node_builder.sh
@@ -6,5 +6,7 @@ git clone https://github.com/aptos-labs/aptos-core.git
 cd aptos-core
 ./scripts/dev_setup.sh -b
 
-CARGO_NET_GIT_FETCH_WITH_CLI=true cargo build -p aptos-node
-CARGO_NET_GIT_FETCH_WITH_CLI=true cargo build -p aptos-faucet
+cd aptos-node
+CARGO_NET_GIT_FETCH_WITH_CLI=true cargo build
+cd ../crates/aptos-faucet
+CARGO_NET_GIT_FETCH_WITH_CLI=true cargo build
