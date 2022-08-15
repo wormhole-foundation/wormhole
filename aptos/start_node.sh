b161447475930d3265756bea1b7357dee434e7a1
Josh Siegel <jsiegel@jumptrading.com>
2022-08-15 19:51:27 +0000

    aptos/integration: Faucet at 8000

--- aptos/start_node.sh
+++ aptos/start_node.sh
@@ -3,4 +3,6 @@
 set -x
 
 cd aptos-core/
-target/debug/aptos-node --test
+target/debug/aptos-node --test --test-dir "/tmp/foo"&
+sleep 5
+target/debug/aptos-faucet --chain-id TESTING  --mint-key-file-path "/tmp/foo/mint.key" --address 0.0.0.0 --port 8000 --server-url http://127.0.0.1:8080
