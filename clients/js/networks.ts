f033195868e810c5d31b3dc80a405675a3c00057
Paul Noel <panoel007@gmail.com>
2022-08-19 15:47:44 +0000

    clients/js: fix merge issue

--- clients/js/networks.ts
+++ clients/js/networks.ts
@@ -354,14 +354,6 @@ const DEVNET = {
     rpc: undefined,
     key: undefined,
   },
-  pythnet: {
-    rpc: undefined,
-    key: undefined,
-  },
-  wormholechain: {
-    rpc: undefined,
-    key: undefined,
-  },
   moonbeam: {
     rpc: undefined,
     key: "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d",
