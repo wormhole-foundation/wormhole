bb1f70c5330e5ebf00e8bfbe4b29514e71f9705b
Chirantan Ekbote <cekbote@jumptrading.com>
2022-08-23 17:24:48 +0900

    wormchain: client: Delete unused variable

--- wormhole_chain/x/tokenbridge/client/cli/tx.go
+++ wormhole_chain/x/tokenbridge/client/cli/tx.go
@@ -2,17 +2,12 @@ package cli
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	// "github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/certusone/wormhole-chain/x/tokenbridge/types"
-)
-
-var (
-	DefaultRelativePacketTimeoutTimestamp = uint64((time.Duration(10) * time.Minute).Nanoseconds())
 )
 
 // GetTxCmd returns the transaction commands for this module
