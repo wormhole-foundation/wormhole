e8327921546f15103a4dabb8e71c6c53f5a9bed0
Chirantan Ekbote <cekbote@jumptrading.com>
2022-08-24 18:55:33 +0900

    node: Deduplicate re-observation requests
    Commit `1753bb3: Send re-observation request when re-broadcasting local
    observations` changed the processor code to automatically send
    re-observation requests.  If multiple guardians do this around the same
    time that would lead to multiple re-observation requests for the same
    transaction.
    
    Deduplicate re-observation requests by keeping a cache of the requests
    that were received in the last 11 minutes.  Any request that's already
    in the cache will not be forwarded to the chain-specific observation
    code.  The 11 minute value was chosen because the guardians will send
    these requests approximately every 5 minutes.
    
    Also move the re-observation code into a standalone function so that it
    can be more easily tested.

---
+++ node/cmd/guardiand/reobserve.go
@@ -0,0 +1,71 @@
+package guardiand
+
+import (
+	"context"
+	"encoding/hex"
+	"time"
+
+	"github.com/benbjohnson/clock"
+	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
+	"github.com/certusone/wormhole/node/pkg/vaa"
+	"go.uber.org/zap"
+)
+
+// Multiplex observation requests to the appropriate chain
+func handleReobservationRequests(
+	ctx context.Context,
+	clock clock.Clock,
+	logger *zap.Logger,
+	obsvReqC <-chan *gossipv1.ObservationRequest,
+	chainObsvReqC map[vaa.ChainID]chan *gossipv1.ObservationRequest,
+) {
+	// Due to the automatic re-observation requests sent out by the processor we may end
+	// up getting multiple requests to re-observe the same tx. Keep a cache of the
+	// requests received in the last 11 minutes so that we don't end up repeatedly
+	// re-observing the same transactions.
+	type cachedRequest struct {
+		chainId vaa.ChainID
+		txHash  string
+	}
+
+	cache := make(map[cachedRequest]time.Time)
+	ticker := clock.Ticker(7 * time.Minute)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			now := clock.Now()
+			for r, t := range cache {
+				if now.Sub(t) > 11*time.Minute {
+					delete(cache, r)
+				}
+			}
+		case req := <-obsvReqC:
+			r := cachedRequest{
+				chainId: vaa.ChainID(req.ChainId),
+				txHash:  hex.EncodeToString(req.TxHash),
+			}
+
+			if _, ok := cache[r]; ok {
+				// We've recently seen a re-observation request for this tx
+				// so skip this one.
+				logger.Info("skipping duplicate re-observation request",
+					zap.Stringer("chain", r.chainId),
+					zap.String("tx_hash", r.txHash),
+				)
+				continue
+			}
+
+			cache[r] = clock.Now()
+
+			if channel, ok := chainObsvReqC[r.chainId]; ok {
+				channel <- req
+			} else {
+				logger.Error("unknown chain ID for reobservation request",
+					zap.Uint16("chain_id", uint16(r.chainId)),
+					zap.String("tx_hash", r.txHash))
+			}
+		}
+	}
+}
