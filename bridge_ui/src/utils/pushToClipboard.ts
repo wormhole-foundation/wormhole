2d2a4b63ac222489b337b215bb120e1f970a3342
Christine Eun <ceun@jumptrading.com>
2022-08-25 20:21:50 +0000

    bridge_ui: remove

--- bridge_ui/src/utils/pushToClipboard.ts
+++
@@ -1,7 +0,0 @@
-export default function pushToClipboard(content: any) {
-  if (!navigator.clipboard) {
-    // Clipboard API not available
-    return;
-  }
-  return navigator.clipboard.writeText(content);
-}
