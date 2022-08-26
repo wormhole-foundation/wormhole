a84dbd4b0443e11727e37616e8bd60d7c63754aa
Josh Siegel <jsiegel@jumptrading.com>
2022-08-26 09:56:43 -0500

    aptos/faucet-fix: Fix faucet

--- aptos/start_node.sh
+++ aptos/start_node.sh
@@ -5,4 +5,4 @@ set -x
 cd aptos-core/
 target/debug/aptos-node --test --test-dir "/tmp/foo"&
 sleep 5
-target/debug/aptos-faucet --chain-id TESTING  --mint-key-file-path "/tmp/foo/mint.key" --address 0.0.0.0 --port 8000 --server-url http://127.0.0.1:8080
+target/debug/aptos-faucet --chain-id TESTING  --mint-key-file-path "/tmp/foo/mint.key" --address 0.0.0.0 --port 8081 --server-url http://127.0.0.1:8080
