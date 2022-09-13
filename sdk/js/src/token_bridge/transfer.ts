e0843bf2bc2cfd8d33be39fe2262620e2e3271a7
Josh Siegel <jsiegel@jumptrading.com>
2022-09-13 15:35:06 -0500

    near/watcher_fix: improve metrics

--- sdk/js/src/token_bridge/transfer.ts
+++ sdk/js/src/token_bridge/transfer.ts
@@ -771,11 +771,11 @@ export async function transferNearFromNear(
     args: {
       receiver: uint8ArrayToHex(receiver),
       chain: chain,
-      fee: fee.toString(),
+      fee: fee.toString(10),
       payload: payload,
       message_fee: message_fee,
     },
-    attachedDeposit: (new BN(qty.toString()).add(new BN(message_fee))),
+    attachedDeposit: (new BN(qty.toString(10)).add(new BN(message_fee))),
     gas: new BN("100000000000000"),
   });
 
