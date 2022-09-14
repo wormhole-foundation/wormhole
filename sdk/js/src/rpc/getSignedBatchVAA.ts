bac6ab1faf354967a07546d4667a52ec59a2362c
justinschuldt <justinschuldt@gmail.com>
2022-09-14 05:31:18 -0500

    sdk/js - add getSignedBatchVAA and bump version

---
+++ sdk/js/src/rpc/getSignedBatchVAA.ts
@@ -0,0 +1,19 @@
+import { ChainId, ChainName, coalesceChainId } from "../utils/consts";
+import { publicrpc } from "@certusone/wormhole-sdk-proto-web";
+const { GrpcWebImpl, PublicRPCServiceClientImpl } = publicrpc;
+
+export async function getSignedBatchVAA(
+  host: string,
+  emitterChain: ChainId | ChainName,
+  transactionId: string,
+  extraGrpcOpts = {}
+) {
+  const rpc = new GrpcWebImpl(host, extraGrpcOpts);
+  const api = new PublicRPCServiceClientImpl(rpc);
+  return await api.GetSignedBatchVAA({
+    batchId: {
+      emitterChain: coalesceChainId(emitterChain),
+      txId: transactionId,
+    },
+  });
+}
---
+++ sdk/js/src/rpc/getSignedBatchVAAWithRetry.ts
@@ -0,0 +1,35 @@
+import { ChainId, ChainName, getSignedBatchVAA } from "..";
+import { coalesceChainId } from "../utils";
+
+export async function getSignedBatchVAAWithRetry(
+  hosts: string[],
+  emitterChain: ChainId | ChainName,
+  transactionId: string,
+  extraGrpcOpts = {},
+  retryTimeout = 1000,
+  retryAttempts?: number
+) {
+  let currentWormholeRpcHost = -1;
+  const getNextRpcHost = () => ++currentWormholeRpcHost % hosts.length;
+  let result;
+  let attempts = 0;
+  while (!result) {
+    attempts++;
+    await new Promise((resolve) => setTimeout(resolve, retryTimeout));
+    try {
+      result = await getSignedBatchVAA(
+        hosts[getNextRpcHost()],
+        coalesceChainId(emitterChain),
+        transactionId,
+        extraGrpcOpts
+      );
+    } catch (e) {
+      if (retryAttempts !== undefined && attempts > retryAttempts) {
+        throw e;
+      }
+    }
+  }
+  return result;
+}
+
+export default getSignedBatchVAAWithRetry;
