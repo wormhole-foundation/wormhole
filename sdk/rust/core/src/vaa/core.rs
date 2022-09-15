19e747f51574d4fef2f6af6bf79914d795336bed
Reisen <reisen@morphism.org>
2022-09-15 22:02:36 +0200

    sdk/rust: make actions enumerations

--- sdk/rust/core/src/vaa/core.rs
+++ sdk/rust/core/src/vaa/core.rs
@@ -8,9 +8,11 @@
 use {
     crate::vaa::{
         parse_fixed,
-        GovernanceAction,
+        Action,
+        GovHeader,
     },
     nom::{
+        error::ErrorKind,
         multi::{
             count,
             fill,
@@ -27,29 +29,51 @@ use {
     primitive_types::U256,
 };
 
-pub struct GovernanceContractUpgrade {
+pub enum CoreAction {
+    ContractUpgrade(ContractUpgrade),
+    GuardianSetChange(GuardianSetChange),
+    SetMessageFee(SetMessageFee),
+    TransferFees(TransferFees),
+}
+
+impl Action for CoreAction {
+    // Module: 000..Core
+    const MODULE: [u8; 32] =
+        hex_literal::hex!("00000000000000000000000000000000000000000000000000000000436f7265");
+
+    #[inline]
+    fn parse<'a, 'b>(i: &'a [u8], header: &'b GovHeader) -> IResult<&'a [u8], Self> {
+        match header.action {
+            1 => ContractUpgrade::parse(i).map(|(i, r)| (i, Self::ContractUpgrade(r))),
+            2 => GuardianSetChange::parse(i).map(|(i, r)| (i, Self::GuardianSetChange(r))),
+            3 => SetMessageFee::parse(i).map(|(i, r)| (i, Self::SetMessageFee(r))),
+            4 => TransferFees::parse(i).map(|(i, r)| (i, Self::TransferFees(r))),
+            _ => Err(nom::Err::Error(nom::error_position!(i, ErrorKind::NoneOf))),
+        }
+    }
+}
+
+pub struct ContractUpgrade {
     pub new_contract: [u8; 32],
 }
 
-impl GovernanceAction for GovernanceContractUpgrade {
-    const MODULE: &'static [u8] = b"Core";
-    const ACTION: u8 = 1;
-    fn parse(input: &[u8]) -> IResult<&[u8], Self> {
-        let (i, new_contract) = parse_fixed(input)?;
+impl ContractUpgrade {
+    #[inline]
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
+        let (i, new_contract) = parse_fixed(i)?;
         Ok((i, Self { new_contract }))
     }
 }
 
-pub struct GovernanceGuardianSetChange {
+pub struct GuardianSetChange {
     pub new_guardian_set_index: u32,
     pub new_guardian_set:       Vec<[u8; 20]>,
 }
 
-impl GovernanceAction for GovernanceGuardianSetChange {
-    const MODULE: &'static [u8] = b"Core";
-    const ACTION: u8 = 2;
-    fn parse(input: &[u8]) -> IResult<&[u8], Self> {
-        let (i, new_guardian_set_index) = u32(Endianness::Big)(input)?;
+impl GuardianSetChange {
+    #[inline]
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
+        let (i, new_guardian_set_index) = u32(Endianness::Big)(i)?;
         let (i, guardian_count) = u8(i)?;
         let (i, new_guardian_set) = count(parse_fixed, guardian_count.into())(i)?;
         Ok((
@@ -62,16 +86,15 @@ impl GovernanceAction for GovernanceGuardianSetChange {
     }
 }
 
-pub struct GovernanceSetMessageFee {
+pub struct SetMessageFee {
     pub fee: U256,
 }
 
-impl GovernanceAction for GovernanceSetMessageFee {
-    const MODULE: &'static [u8] = b"Core";
-    const ACTION: u8 = 3;
-    fn parse(input: &[u8]) -> IResult<&[u8], Self> {
+impl SetMessageFee {
+    #[inline]
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
         let mut fee = [0u8; 32];
-        let (i, _) = fill(u8, &mut fee)(input)?;
+        let (i, _) = fill(u8, &mut fee)(i)?;
         Ok((
             i,
             Self {
@@ -81,17 +104,16 @@ impl GovernanceAction for GovernanceSetMessageFee {
     }
 }
 
-pub struct GovernanceTransferFees {
+pub struct TransferFees {
     pub amount: U256,
     pub to:     [u8; 32],
 }
 
-impl GovernanceAction for GovernanceTransferFees {
-    const MODULE: &'static [u8] = b"Core";
-    const ACTION: u8 = 4;
-    fn parse(input: &[u8]) -> IResult<&[u8], Self> {
+impl TransferFees {
+    #[inline]
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
         let mut amount = [0u8; 32];
-        let (i, _) = fill(u8, &mut amount)(input)?;
+        let (i, _) = fill(u8, &mut amount)(i)?;
         let (i, to) = parse_fixed(i)?;
         Ok((
             i,
