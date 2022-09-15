d69ad85994765642584374a129e79ecb65a53834
Justin Schuldt <justinschuldt@gmail.com>
2022-09-15 11:03:26 -0500

    sdk/js - add getSignedBatchVAA and bump version (#1594)

--- sdk/js/src/rpc/index.ts
+++ sdk/js/src/rpc/index.ts
@@ -1,4 +1,6 @@
 export * from "./getSignedVAA";
 export { getSignedVAAWithRetry } from "./getSignedVAAWithRetry";
+export * from "./getSignedBatchVAA";
+export { getSignedBatchVAAWithRetry } from "./getSignedBatchVAAWithRetry";
 export * from "./getGovernorIsVAAEnqueued";
 export * from "./getGovernorIsVAAEnqueuedWithRetry";
