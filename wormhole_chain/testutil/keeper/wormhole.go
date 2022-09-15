b3d4539bad340939e4d1f4a2a6984241ee4cb7f8
Conor Patrick <conorpp94@gmail.com>
2022-09-15 19:52:26 +0000

    wormchain: add tests around wasmd operations and guardian set upgrades

--- wormhole_chain/testutil/keeper/wormhole.go
+++ wormhole_chain/testutil/keeper/wormhole.go
@@ -1,40 +1,133 @@
 package keeper
 
 import (
+	"io/ioutil"
 	"testing"
+	"time"
 
-	"github.com/cosmos/cosmos-sdk/codec"
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/CosmWasm/wasmd/x/wasm"
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/store"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	capabilitykeeper "github.com/cosmos/cosmos-sdk/x/capability/keeper"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+	"github.com/wormhole-foundation/wormhole-chain/app"
+	"github.com/wormhole-foundation/wormhole-chain/x/wormhole/keeper"
+	"github.com/wormhole-foundation/wormhole-chain/x/wormhole/types"
+
+	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/spm/cosmoscmd"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
-	"github.com/wormhole-foundation/wormhole-chain/x/wormhole/keeper"
-	"github.com/wormhole-foundation/wormhole-chain/x/wormhole/types"
 )
 
 func WormholeKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
-	storeKey := sdk.NewKVStoreKey(types.StoreKey)
-	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
+	keys := sdk.NewKVStoreKeys(
+		authtypes.StoreKey,
+		paramstypes.StoreKey,
+		capabilitytypes.StoreKey,
+		types.StoreKey,
+		wasmtypes.StoreKey,
+	)
+	tkeys := sdk.NewTransientStoreKeys(paramstypes.TStoreKey)
+	memKeys := sdk.NewMemoryStoreKeys(capabilitytypes.MemStoreKey, types.MemStoreKey)
+	maccPerms := map[string][]string{}
 
 	db := tmdb.NewMemDB()
 	stateStore := store.NewCommitMultiStore(db)
-	stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
-	stateStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
+	stateStore.MountStoreWithDB(keys[authtypes.StoreKey], sdk.StoreTypeIAVL, db)
+	stateStore.MountStoreWithDB(keys[paramstypes.StoreKey], sdk.StoreTypeIAVL, db)
+	stateStore.MountStoreWithDB(keys[capabilitytypes.StoreKey], sdk.StoreTypeIAVL, db)
+	stateStore.MountStoreWithDB(keys[types.StoreKey], sdk.StoreTypeIAVL, db)
+	stateStore.MountStoreWithDB(keys[wasmtypes.StoreKey], sdk.StoreTypeIAVL, db)
+	stateStore.MountStoreWithDB(memKeys[types.MemStoreKey], sdk.StoreTypeMemory, nil)
+	stateStore.MountStoreWithDB(tkeys[paramstypes.TStoreKey], sdk.StoreTypeTransient, nil)
 	require.NoError(t, stateStore.LoadLatestVersion())
 
-	registry := codectypes.NewInterfaceRegistry()
+	encodingConfig := cosmoscmd.MakeEncodingConfig(app.ModuleBasics)
+	appCodec := encodingConfig.Marshaler
+	amino := encodingConfig.Amino
+	// txDecoder := encodingConfig.TxConfig.TxDecoder()
+
+	paramsKeeper := paramskeeper.NewKeeper(appCodec, amino, keys[paramstypes.StoreKey], tkeys[paramstypes.TStoreKey])
+	paramsKeeper.Subspace(types.ModuleName)
+	paramsKeeper.Subspace(wasm.ModuleName)
+
+	paramsKeeper.Subspace(authtypes.ModuleName)
+	subspace_auth, _ := paramsKeeper.GetSubspace(authtypes.ModuleName)
+	accountKeeper := authkeeper.NewAccountKeeper(
+		appCodec, keys[authtypes.StoreKey], subspace_auth, authtypes.ProtoBaseAccount, maccPerms,
+	)
+	// this line is used by starport scaffolding # stargate/app/paramSubspace
+
+	subspaceWasmd, _ := paramsKeeper.GetSubspace(wasmtypes.ModuleName)
+
+	bApp := baseapp.NewBaseApp("wormchain", log.NewNopLogger(), db, encodingConfig.TxConfig.TxDecoder())
+	// bApp.SetCommitMultiStoreTracer(traceStore)
+	bApp.SetVersion(version.Version)
+	bApp.SetInterfaceRegistry(encodingConfig.InterfaceRegistry)
+
+	appapp := &app.App{
+		BaseApp: bApp,
+	}
+
 	k := keeper.NewKeeper(
-		codec.NewProtoCodec(registry),
-		storeKey,
-		memStoreKey,
-		nil,
+		appCodec,
+		keys[types.StoreKey],
+		memKeys[types.MemStoreKey],
+		accountKeeper,
 		nil,
 	)
 
-	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
+	supportedFeatures := "iterator,staking,stargate"
+	appapp.WormholeKeeper = *k
+
+	appapp.CapabilityKeeper = capabilitykeeper.NewKeeper(appCodec, keys[capabilitytypes.StoreKey], memKeys[capabilitytypes.MemStoreKey])
+	scopedWasmKeeper := appapp.CapabilityKeeper.ScopeToModule(wasm.ModuleName)
+
+	wasmDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		panic(err)
+	}
+	wasmKeeper := wasm.NewKeeper(
+		appCodec,
+		keys[wasmtypes.StoreKey],
+		subspaceWasmd,
+		&app.AccountKeeperHandler{AccountKeeper: accountKeeper},
+		&app.BankKeeperHandler{},
+		&app.StakingKeeperHandler{},
+		&app.DistributionKeeperHandler{},
+		&app.ChannelKeeperHandler{},
+		&app.PortKeeperHandler{},
+		scopedWasmKeeper,
+		&app.ICS20TransferPortSourceHandler{},
+		appapp.MsgServiceRouter(),
+		appapp.GRPCQueryRouter(),
+		wasmDir,
+		wasm.DefaultWasmConfig(),
+		// wasmConfig.ToWasmConfig(),
+		supportedFeatures,
+	)
+	ctx := sdk.NewContext(stateStore, tmproto.Header{
+		Time: time.Now(),
+	}, false, log.NewNopLogger())
+	appapp.MountKVStores(keys)
+	appapp.MountTransientStores(tkeys)
+	appapp.MountMemoryStores(memKeys)
+
+	wasmGenState := wasmtypes.GenesisState{}
+	wasmGenState.Params.CodeUploadAccess = wasmtypes.DefaultUploadAccess
+	wasmGenState.Params.InstantiateDefaultPermission = wasmtypes.AccessTypeEverybody
+	wasmKeeper.SetParams(ctx, wasmGenState.Params)
+	appapp.WormholeKeeper.SetWasmdKeeper(wasmKeeper)
+	k.SetWasmdKeeper(wasmKeeper)
+
 	return k, ctx
 }
---
+++ wormhole_chain/x/wormhole/keeper/msg_server_execute_governance_vaa_test.go
@@ -0,0 +1,113 @@
+package keeper_test
+
+import (
+	"crypto/ecdsa"
+	"encoding/binary"
+	"testing"
+
+	keepertest "github.com/certusone/wormhole-chain/testutil/keeper"
+	"github.com/certusone/wormhole-chain/x/wormhole/keeper"
+	"github.com/certusone/wormhole-chain/x/wormhole/types"
+	"github.com/certusone/wormhole/node/pkg/vaa"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func createExecuteGovernanceVaaPayload(k *keeper.Keeper, ctx sdk.Context, num_guardians byte) ([]byte, []*ecdsa.PrivateKey) {
+	guardians, privateKeys := createNGuardianValidator(k, ctx, int(num_guardians))
+	next_index := k.GetGuardianSetCount(ctx)
+	set_update := make([]byte, 4)
+	binary.BigEndian.PutUint32(set_update, next_index)
+	set_update = append(set_update, num_guardians)
+	// Add keys to set_update
+	for _, guardian := range guardians {
+		set_update = append(set_update, guardian.GuardianKey...)
+	}
+	// governance message with sha3 of wasmBytes as the payload
+	payload := []byte{}
+	payload = append(payload, vaa.CoreModule[:]...)
+	payload = append(payload, byte(keeper.ActionGuardianSetUpdate))
+	chain_bz := [2]byte{}
+	binary.BigEndian.PutUint16(chain_bz[:], uint16(WH_CHAIN_ID))
+	payload = append(payload, chain_bz[:]...)
+	// set update payload
+	payload = append(payload, set_update...)
+	return payload, privateKeys
+}
+
+func TestExecuteGovernanceVAA(t *testing.T) {
+	k, ctx := keepertest.WormholeKeeper(t)
+	guardians, privateKeys := createNGuardianValidator(k, ctx, 10)
+	_ = privateKeys
+	k.SetConfig(ctx, types.Config{
+		GovernanceEmitter:     GOVERNANCE_EMITTER[:],
+		GovernanceChain:       uint32(GOVERNANCE_CHAIN),
+		ChainId:               uint32(WH_CHAIN_ID),
+		GuardianSetExpiration: 86400,
+	})
+	signer_bz := [20]byte{}
+	signer := sdk.AccAddress(signer_bz[:])
+
+	set := createNewGuardianSet(k, ctx, guardians)
+	k.SetConsensusGuardianSetIndex(ctx, types.ConsensusGuardianSetIndex{Index: set.Index})
+
+	context := sdk.WrapSDKContext(ctx)
+	msgServer := keeper.NewMsgServerImpl(*k)
+
+	// create governance to update guardian set with extra guardian
+	payload, newPrivateKeys := createExecuteGovernanceVaaPayload(k, ctx, 11)
+	v := generateVaa(set.Index, privateKeys, vaa.ChainID(GOVERNANCE_CHAIN), payload)
+	vBz, _ := v.Marshal()
+	_, err := msgServer.ExecuteGovernanceVAA(context, &types.MsgExecuteGovernanceVAA{
+		Signer: signer.String(),
+		Vaa:    vBz,
+	})
+	assert.NoError(t, err)
+
+	// we should have a new set with 11 guardians now
+	new_index := k.GetLatestGuardianSetIndex(ctx)
+	assert.Equal(t, set.Index+1, new_index)
+	new_set, _ := k.GetGuardianSet(ctx, new_index)
+	assert.Len(t, new_set.Keys, 11)
+
+	// Submitting another change with the old set doesn't work
+	v = generateVaa(set.Index, privateKeys, vaa.ChainID(GOVERNANCE_CHAIN), payload)
+	vBz, _ = v.Marshal()
+	_, err = msgServer.ExecuteGovernanceVAA(context, &types.MsgExecuteGovernanceVAA{
+		Signer: signer.String(),
+		Vaa:    vBz,
+	})
+	assert.Equal(t, types.ErrGuardianSetNotSequential, err)
+
+	// Invalid length
+	v = generateVaa(set.Index, privateKeys, vaa.ChainID(GOVERNANCE_CHAIN), payload[:len(payload)-1])
+	vBz, _ = v.Marshal()
+	_, err = msgServer.ExecuteGovernanceVAA(context, &types.MsgExecuteGovernanceVAA{
+		Signer: signer.String(),
+		Vaa:    vBz,
+	})
+	assert.Equal(t, types.ErrInvalidGovernancePayloadLength, err)
+
+	// Include a guardian address twice in an update
+	payload_bad, _ := createExecuteGovernanceVaaPayload(k, ctx, 11)
+	copy(payload_bad[len(payload_bad)-20:], payload_bad[len(payload_bad)-40:len(payload_bad)-20])
+	v = generateVaa(set.Index, privateKeys, vaa.ChainID(GOVERNANCE_CHAIN), payload_bad)
+	vBz, _ = v.Marshal()
+	_, err = msgServer.ExecuteGovernanceVAA(context, &types.MsgExecuteGovernanceVAA{
+		Signer: signer.String(),
+		Vaa:    vBz,
+	})
+	assert.Equal(t, types.ErrDuplicateGuardianAddress, err)
+
+	// Change set again with new set update
+	payload, _ = createExecuteGovernanceVaaPayload(k, ctx, 12)
+	v = generateVaa(new_set.Index, newPrivateKeys, vaa.ChainID(GOVERNANCE_CHAIN), payload)
+	vBz, _ = v.Marshal()
+	_, err = msgServer.ExecuteGovernanceVAA(context, &types.MsgExecuteGovernanceVAA{
+		Signer: signer.String(),
+		Vaa:    vBz,
+	})
+	assert.NoError(t, err)
+	new_index2 := k.GetLatestGuardianSetIndex(ctx)
+	assert.Equal(t, new_set.Index+1, new_index2)
+}
