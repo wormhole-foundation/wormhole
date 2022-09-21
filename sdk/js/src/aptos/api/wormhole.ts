1ffcb81a04f9bb831fe92fbcdba7fcdbcc753043
aki <akshath@live.com>
2022-09-21 06:28:07 +0000

    sdk/aptos: add wormhole sdk

--- sdk/js/src/aptos/api/wormhole.ts
+++ sdk/js/src/aptos/api/wormhole.ts
@@ -1,9 +1,73 @@
-import { AptosClientWrapper } from "../client";
+import { AptosAccount } from 'aptos';
+import { AptosClientWrapper } from '../client';
 
 export class AptosWormholeApi {
-  client: AptosClientWrapper;
-  
-  constructor(client: AptosClientWrapper) {
+  private client: AptosClientWrapper;
+  private address: string;
+
+  constructor(client: AptosClientWrapper, network: string) {
     this.client = client;
+    this.address = '';
   }
+
+  // Contract upgrade
+
+  authorizeUpgrade = (sender: AptosAccount, vaa: Uint8Array) => {
+    const payload = {
+      function: `${this.address}::contract_upgrade::submit_vaa`,
+      type_arguments: [],
+      arguments: [vaa],
+    };
+    return this.client.executeEntryFunction(sender, payload);
+  };
+
+  upgradeContract = (
+    sender: AptosAccount,
+    metadataSerialized: Uint8Array,
+    code: Array<Uint8Array>,
+  ) => {
+    const payload = {
+      function: `${this.address}::contract_upgrade::upgrade`,
+      type_arguments: [],
+      arguments: [metadataSerialized, code],
+    };
+    return this.client.executeEntryFunction(sender, payload);
+  };
+
+  migrateContract = (sender: AptosAccount) => {
+    const payload = {
+      function: `${this.address}::contract_upgrade::migrate`,
+      type_arguments: [],
+      arguments: [],
+    };
+    return this.client.executeEntryFunction(sender, payload);
+  };
+
+  // Guardian set upgrade
+
+  upgradeGuardianSet = (sender: AptosAccount, vaa: Uint8Array) => {
+    const payload = {
+      function: `${this.address}::guardian_set_upgrade::submit_vaa`,
+      type_arguments: [],
+      arguments: [vaa],
+    };
+    return this.client.executeEntryFunction(sender, payload);
+  };
+
+  // Init WH
+
+  initWormhole = (
+    sender: AptosAccount,
+    chainId: number,
+    governanceChainId: number,
+    governanceContract: Uint8Array,
+    initialGuardian: Uint8Array,
+  ) => {
+    const payload = {
+      function: `${this.address}::wormhole::init`,
+      type_arguments: [],
+      arguments: [chainId, governanceChainId, governanceContract, initialGuardian],
+    };
+    return this.client.executeEntryFunction(sender, payload);
+  };
 }
