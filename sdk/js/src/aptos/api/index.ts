33e6fb0ce16ab834076a27e0c5478022b843a05a
aki <akshath@live.com>
2022-09-21 06:19:58 +0000

    sdk/aptos: added tokenbridge sdk

--- sdk/js/src/aptos/api/index.ts
+++ sdk/js/src/aptos/api/index.ts
@@ -1,16 +1,15 @@
 import { AptosClientWrapper } from "../client";
 import { AptosTokenBridgeApi } from "./tokenBridge";
-import { AptosWormholeApi } from "./wormhole";
 
 export class AptosApi {
-  wormhole: AptosWormholeApi;
+  // wormhole: AptosWormholeApi;
   tokenBridge: AptosTokenBridgeApi;
 
   private client: AptosClientWrapper;
 
   constructor(client: AptosClientWrapper) {
     this.client = client;
-    this.wormhole = new AptosWormholeApi(this.client);
-    this.tokenBridge = new AptosTokenBridgeApi(this.client);
+    // this.wormhole = new AptosWormholeApi(this.client, "");
+    this.tokenBridge = new AptosTokenBridgeApi(this.client, "");
   }
 }
