f3ecc5b93b7ab4ab9bdcfe491cd638c79254698d
aki <akshath@live.com>
2022-09-21 07:31:10 +0000

    sdk/aptos: formatting

--- sdk/js/src/aptos/utils.ts
+++ sdk/js/src/aptos/utils.ts
@@ -5,23 +5,15 @@ import { ChainId } from "../utils";
 export const deriveWrappedAssetAddress = (
   tokenBridgeAddress: string, // 32 bytes
   originChain: ChainId,
-  originAddress: string // 32 bytes
+  originAddress: string, // 32 bytes
 ): string => {
   let chain: Buffer = Buffer.alloc(2);
   chain.writeUInt16BE(originChain);
   return sha3_256(
-    Buffer.concat([
-      hex(tokenBridgeAddress),
-      chain,
-      Buffer.from("::", "ascii"),
-      hex(originAddress),
-    ])
+    Buffer.concat([hex(tokenBridgeAddress), chain, Buffer.from("::", "ascii"), hex(originAddress)]),
   );
 };
 
 export const hex = (x: string): Buffer => {
-  return Buffer.from(
-    ethers.utils.hexlify(x, { allowMissingPrefix: true }).substring(2),
-    "hex"
-  );
+  return Buffer.from(ethers.utils.hexlify(x, { allowMissingPrefix: true }).substring(2), "hex");
 };
