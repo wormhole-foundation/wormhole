8bfff2a644c6c0e41ec50a07f192cd7db829461c
aki <akshath@live.com>
2022-09-22 21:13:49 +0000

    sdk/aptos: support native assets

--- sdk/js/src/utils/aptos.ts
+++ sdk/js/src/utils/aptos.ts
@@ -1,11 +1,17 @@
 import { sha3_256 } from "js-sha3";
-import { ChainId, hex } from "../utils";
+import { ChainId, CHAIN_ID_APTOS, hex } from "../utils";
 
 export const deriveWrappedAssetAddress = (
   tokenBridgeAddress: string, // 32 bytes
   originChain: ChainId,
   originAddress: string, // 32 bytes
 ): string => {
+  // native asset
+  if (originChain === CHAIN_ID_APTOS) {
+    return originAddress;
+  }
+  
+  // non-native asset, derive unique address
   let chain: Buffer = Buffer.alloc(2);
   chain.writeUInt16BE(originChain);
   return sha3_256(
