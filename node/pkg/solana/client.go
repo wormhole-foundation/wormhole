b92c1075851362a7574772981825919d6b96bade
Jeff Schroeder <jeffschroeder@computer.org>
2022-09-23 14:39:08 -0400

    Remove solana and pythnet websocket flags
    They're both unused since the solana watcher uses http and not a
    websocket for getting info.

--- node/pkg/solana/client.go
+++ node/pkg/solana/client.go
@@ -25,7 +25,6 @@ import (
 
 type SolanaWatcher struct {
 	contract     solana.PublicKey
-	wsUrl        string
 	rpcUrl       string
 	commitment   rpc.CommitmentType
 	messageEvent chan *common.MessagePublication
@@ -107,7 +106,7 @@ type PostMessageData struct {
 }
 
 func NewSolanaWatcher(
-	wsUrl, rpcUrl string,
+	rpcUrl string,
 	contractAddress solana.PublicKey,
 	messageEvents chan *common.MessagePublication,
 	obsvReqC chan *gossipv1.ObservationRequest,
@@ -115,8 +114,8 @@ func NewSolanaWatcher(
 	readiness readiness.Component,
 	chainID vaa.ChainID) *SolanaWatcher {
 	return &SolanaWatcher{
-		contract: contractAddress,
-		wsUrl:    wsUrl, rpcUrl: rpcUrl,
+		rpcUrl:       rpcUrl,
+		contract:     contractAddress,
 		messageEvent: messageEvents,
 		obsvReqC:     obsvReqC,
 		commitment:   commitment,
