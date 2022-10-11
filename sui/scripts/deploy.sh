f8733092a30b30b3d3eaf5b285d547762e2114d6
Josh Siegel <jsiegel@jumptrading.com>
2022-10-11 16:29:14 +0000

    sui/tilt2: basic deploy working

---
+++ sui/scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -f
+
+set -x
+
+cd wormhole
+sed -i -e 's/wormhole = .*/wormhole = "0x0"/' Move.toml
+make build
+sui client publish --gas-budget 10000 | tee publish.log
+wa="`grep "The newly published package" publish.log | sed -e 's/^.*: //'`"
+sed -i -e "s/wormhole = .*/wormhole = \"$wa\"/" Move.toml
+state="`grep "Move Object" publish.log | sed -e 's/^.*(\(.*\)\[.*$/\1/'`"
+echo "WORM_ACCOUNT=\"$wa\"" > ../env.sh
+echo "WORM_STATE=\"$state\"" >> ../env.sh
+
