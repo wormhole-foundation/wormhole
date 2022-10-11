fc1804020bd0f6d361f615b9a7b754b0c379f0f4
Josh Siegel <jsiegel@jumptrading.com>
2022-10-11 17:14:33 +0000

    sui/tilt2: better names

--- sui/scripts/publish_message.sh
+++ sui/scripts/publish_message.sh
@@ -2,4 +2,4 @@
 
 . env.sh
 
-sui client call --function publish_message --module wormhole --package $WORM_ACCOUNT --gas-budget 20000 --args \"$WORM_STATE\" 400 [2] \"0x29651a048dda2a8e07e793cc0f4274b256293436\"
+sui client call --function publish_message --module wormhole --package $WORM_PACKAGE --gas-budget 20000 --args \"$WORM_STATE\" 400 [2] \"0x29651a048dda2a8e07e793cc0f4274b256293436\"
