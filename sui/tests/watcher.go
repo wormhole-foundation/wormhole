1aa80bfd63169be3a370d451c81dfb540a020fcd
Josh Siegel <jsiegel@jumptrading.com>
2022-10-13 13:48:27 +0000

    sui/tilt2: save this

--- sui/tests/watcher.go
+++ sui/tests/watcher.go
@@ -29,7 +29,7 @@ func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/sui_subscribeEvent"}
 	log.Printf("connecting to %s", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
