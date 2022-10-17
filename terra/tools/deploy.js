4a2fbd923f57d280e6ee10f14589deb18bb83c79
Hendrik Hofstadt <hendrik@nexantic.com>
2022-10-17 18:13:19 +0200

    tilt: shorten terra block time (#1744)
    Change-Id: I35cce03ae6e82b522482f2c1c40ccb6819eb51f6

--- terra/tools/deploy.js
+++ terra/tools/deploy.js
@@ -15,13 +15,7 @@ function sleep(ms) {
 }
 
 async function broadcastAndWait(terra, tx) {
-  const response = await terra.tx.broadcast(tx);
-  let currentHeight = (await terra.tendermint.blockInfo()).block.header.height;
-  while (currentHeight <= response.height) {
-    await sleep(100);
-    currentHeight = (await terra.tendermint.blockInfo()).block.header.height;
-  }
-  return response;
+  return await terra.tx.broadcast(tx);
 }
 
 /*
