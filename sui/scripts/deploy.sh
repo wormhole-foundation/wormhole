0043d769af95048041b883dbebc6bd8c27a959c7
Josh Siegel <jsiegel@jumptrading.com>
2022-10-19 14:59:41 +0000

    sui/tilt2: fix scripts

--- sui/scripts/deploy.sh
+++ sui/scripts/deploy.sh
@@ -6,9 +6,9 @@ cd wormhole
 sed -i -e 's/wormhole = .*/wormhole = "0x0"/' Move.toml
 make build
 sui client publish --gas-budget 10000 | tee publish.log
-#wa="`grep "The newly published package" publish.log | sed -e 's/^.*: //'`"
-#sed -i -e "s/wormhole = .*/wormhole = \"$wa\"/" Move.toml
-#state="`grep "Move Object" publish.log | sed -e 's/^.*(\(.*\)\[.*$/\1/'`"
-#echo "WORM_PACKAGE=\"$wa\"" > ../env.sh
-#echo "WORM_STATE=\"$state\"" >> ../env.sh
-#
+wa="`grep "The newly published package" publish.log | sed -e 's/^.*: //'`"
+sed -i -e "s/wormhole = .*/wormhole = \"$wa\"/" Move.toml
+state="`grep "Move Object" publish.log | sed -e 's/^.*(\(.*\)\[.*$/\1/'`"
+echo "WORM_PACKAGE=\"$wa\"" > ../env.sh
+echo "WORM_STATE=\"$state\"" >> ../env.sh
+
