f0c53e137950efce801b0892ec8c3428db455029
tbjump <>
2022-10-20 23:03:43 +0000

    robustness improvements

--- scripts/check-npm-package-scopes.sh
+++ scripts/check-npm-package-scopes.sh
@@ -2,7 +2,7 @@
 
 # This script checks to ensure that all our NPM packages have an appropriate scope.
 #
-git ls-files | grep "package.json" | xargs grep -s "\"name\":" | egrep -v '@certusone/|@wormhole-foundation/'
+find . -type f -name 'package.json' -print0 | xargs -n1 -r -0 /bin/bash -c 'printf "[$@]"; jq  ".name" "$@";' '' | egrep -v "^\[.*\]null$" | egrep -v '^\[.*\]"(@certusone/|@wormhole-foundation/)'
 if [ $? -eq 0 ]; then
    echo "[!] Unscoped npm packages" >&2
    exit 1
