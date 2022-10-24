0aedc1132c86fcabe0510c771f473d3b8da31df8
jumpsiegel <83408952+jumpsiegel@users.noreply.github.com>
2022-10-24 14:04:54 -0500

    sui/upgrade: update to new sui version (#1785)

--- sui/tests/ws.py
+++ sui/tests/ws.py
@@ -14,10 +14,11 @@ def on_close(ws, close_status_code, close_msg):
 
 def on_open(ws):
     print("Opened connection")
+    ws.send("{\"jsonrpc\":\"2.0\", \"id\": 1, \"method\": \"sui_subscribeEvent\", \"params\": [{\"SenderAddress\": \"0xf4e306649ca37370e61c27b1fc684dae62234b78\"}]}")
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://localhost:5001/sui_subscribeEvent?id=1",
+    ws = websocket.WebSocketApp("ws://localhost:9001",
                               on_open=on_open,
                               on_message=on_message,
                               on_error=on_error,
