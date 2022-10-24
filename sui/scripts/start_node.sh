f89974584f4234aec76456d5afa02c50aa0f73a1
Josh Siegel <jsiegel@jumptrading.com>
2022-10-24 17:09:26 +0000

    sui/upgrade: update to new sui version

--- sui/scripts/start_node.sh
+++ sui/scripts/start_node.sh
@@ -9,6 +9,6 @@ sleep 5
 sui-faucet --host-ip 0.0.0.0&
 sleep 2
 curl -X POST -d '{"FixedAmountRequest":{"recipient": "'"0x2acab6bb0e4722e528291bc6ca4f097e18ce9331"'"}}' -H 'Content-Type: application/json' http://127.0.0.1:5003/gas
-rpc-server --host 0.0.0.0
+sui-node --config-path ~/.sui/sui_config/fullnode.yaml
 
 #sleep infinity
