508643f9452214d31ee9d9fc066c7df219f5b0fd
njayakumar <njayakumar@jumptrading.com>
2022-10-24 15:00:01 +0000

    adding unit test

--- ethereum/forge-test/Messages.t.sol
+++ ethereum/forge-test/Messages.t.sol
@@ -213,21 +213,15 @@ contract TestMessages is Messages, Test {
 
     // Manipulate one of the observations of the batch
     bytes memory observation = abi.encodePacked(
-        parsedValidVm2.observations[0].timestamp,
-        parsedValidVm2.observations[0].nonce,
-        parsedValidVm2.observations[0].emitterChainId,
-        parsedValidVm2.observations[0].emitterAddress,
-        parsedValidVm2.observations[0].sequence,
-        parsedValidVm2.observations[0].consistencyLevel,
-        "malicious payload"
+        parsedValidVm2.observations[0],
+        "malicious bytes in payload"
     );
 
-    parsedValidVm2.observations[0] = abi.encodePacked(
-      uint8(3),
-      observation
-    );
+    parsedValidVm2.observations[0] = observation;
 
-    parseValidVm2.hashes[0] = doubleKeccak256(observation);
+    bytes32 originalHash = parsedValidVm2.hashes[0];
+
+    parsedValidVm2.hashes[0] = doubleKeccak256(observation.slice(1, observation.length - 1));
 
     // Create the malicious VM
     Structs.VM2 memory invalidVM2 = Structs.VM2({
@@ -241,14 +235,18 @@ contract TestMessages is Messages, Test {
 
     (valid, reason) = this.verifyBatchVM(invalidVM2, true);
 
+    assertEq(verifiedHashCached(originalHash), false);
+    assertEq(verifiedHashCached(parsedValidVm2.hashes[0]), true);
+    assertEq(verifiedHashCached(parsedValidVm2.hashes[1]), true);
+    assertEq(verifiedHashCached(parsedValidVm2.hashes[2]), true);
+
     bytes memory invalidVM3 = abi.encodePacked(
-      uint8(3),
       observation
     );
 
-    assertEq(verifiedHashCached(parseValidVm2.hashes[0]), true);
-
-    (Structs.VM2 memory parsedInvalidVm3, bool isValid, string memory invalidReason) = this.parseAndVerifyVM(valinvalidVM3);
+    (Structs.VM memory parsedInvalidVm3, bool isValid, string memory invalidReason) = this.parseAndVerifyVM(invalidVM3);
+    assertEq(isValid, true);
+    assertEq(invalidReason, "");
   }
 
   struct Observation {
@@ -294,16 +292,6 @@ contract TestMessages is Messages, Test {
     vm.expectRevert("invalid observation");
     this.verifyBatchVM(parsedValidVm2, true);
   }
-
-  struct Observation {
-		uint32 timestamp;
-		uint32 nonce;
-		uint16 emitterChainId;
-		bytes32 emitterAddress;
-		uint64 sequence;
-		uint8 consistencyLevel;
-		bytes payload;
-	}
 
     // This test confirms that verifyVM reverts verifying a batchVM with observations
   // that have been manipulated.
