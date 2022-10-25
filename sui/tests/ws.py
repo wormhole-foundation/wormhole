a441701b02534fabf0235ac7739d57dcb01a7251
Josh Siegel <jsiegel@jumptrading.com>
2022-10-25 13:51:15 +0000

    sui/watcher: updates to python example

--- sui/tests/ws.py
+++ sui/tests/ws.py
@@ -2,9 +2,71 @@ import websocket
 import _thread
 import time
 import rel
+import os
+import pprint
+import json
+import base64
+
+# https://github.com/MystenLabs/sui/pull/5113
+
+# {
+#     "jsonrpc": "2.0",
+#     "method": "sui_subscribeEvent",
+#     "params": {
+#         "subscription": 1805876586195140,
+#         "result": {
+#             "timestamp": 1666704112752,
+#             "txDigest": "ckB13AaG+OHrO0Ha3I8IK3ERanYHmHAI0jSXnqk9R+I=",
+#             "event": {
+#                 "moveEvent": {
+#                     "packageId": "0xbd99019f3c8f9d08b5498fedcc97e1c24cddff88",
+#                     "transactionModule": "wormhole",
+#                     "sender": "0xdc2f7334400a353c6a9303235b578477202809c6",
+#                     "type": "0xbd99019f3c8f9d08b5498fedcc97e1c24cddff88::state::WormholeMessage",
+#                     "fields": {
+#                         "consistency_level": 0,
+#                         "nonce": 400,
+#                         "payload": "Ag==",
+#                         "sender": "0xdc2f7334400a353c6a9303235b578477202809c6",
+#                         "sequence": 19,
+#                         "timestamp": 0
+#                     },
+#                     "bcs": "3C9zNEAKNTxqkwMjW1eEdyAoCcYTAAAAAAAAAJABAAAAAAAAAQIAAAAAAAAAAAA="
+#                 }
+#             }
+#         }
+#     }
+# }
+
+# curl -s -X POST -d '{"jsonrpc":"2.0", "id": 1, "method": "sui_getEventsByTransaction", "params": ["KgsiF8pCF61N02zX2oMFYLWQdrbxkOD1ypBxND752No=", 2]}' -H 'Content-Type: application/json' http://127.0.0.1:9000 | jq
+
+# {
+#   "jsonrpc": "2.0",
+#   "result": [
+#     {
+#       "timestamp": 1666704112752,
+#       "txDigest": "ckB13AaG+OHrO0Ha3I8IK3ERanYHmHAI0jSXnqk9R+I=",
+#       "event": {
+#         "moveEvent": {
+#           "packageId": "0xbd99019f3c8f9d08b5498fedcc97e1c24cddff88",
+#           "transactionModule": "wormhole",
+#           "sender": "0xdc2f7334400a353c6a9303235b578477202809c6",
+#           "type": "0xbd99019f3c8f9d08b5498fedcc97e1c24cddff88::state::WormholeMessage",
+#           "bcs": "3C9zNEAKNTxqkwMjW1eEdyAoCcYTAAAAAAAAAJABAAAAAAAAAQIAAAAAAAAAAAA="
+#         }
+#       }
+#     }
+#   ],
+#   "id": 1
+# }
 
 def on_message(ws, message):
-    print(message)
+    v = json.loads(message)
+    if "params" in v:
+        tx = v["params"]["result"]["txDigest"]
+        tx = len(base64.standard_b64decode(tx))
+        print(tx.hex())
+    print(json.dumps(v, indent=4))
 
 def on_error(ws, error):
     print(error)
@@ -14,10 +76,9 @@ def on_close(ws, close_status_code, close_msg):
 
 def on_open(ws):
     print("Opened connection")
-    ws.send("{\"jsonrpc\":\"2.0\", \"id\": 1, \"method\": \"sui_subscribeEvent\", \"params\": [{\"SenderAddress\": \"0xf4e306649ca37370e61c27b1fc684dae62234b78\"}]}")
+    ws.send("{\"jsonrpc\":\"2.0\", \"id\": 1, \"method\": \"sui_subscribeEvent\", \"params\": [{\"SenderAddress\": \"" + os.getenv("WORM_OWNER") + "\"}]}")
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
     ws = websocket.WebSocketApp("ws://localhost:9001",
                               on_open=on_open,
                               on_message=on_message,
