16dfd94b52f81a639c13264379f1996dab5325a4
jumpsiegel <83408952+jumpsiegel@users.noreply.github.com>
2022-10-25 11:07:32 -0500

    Sui/watcher (#1791)
    * sui/watcher: updates to python example
    
    * sui/watcher: simple go example

--- sui/scripts/deploy.sh
+++ sui/scripts/deploy.sh
@@ -9,6 +9,8 @@ sui client publish --gas-budget 10000 | tee publish.log
 wa="`grep "The newly published package" publish.log | sed -e 's/^.*: //'`"
 sed -i -e "s/wormhole = .*/wormhole = \"$wa\"/" Move.toml
 state="`grep "Move Object" publish.log | sed -e 's/^.*(\(.*\)\[.*$/\1/'`"
+owner="`grep '^Owner: Account Address' publish.log  | sed -e 's/^.* ( \(.*\) )/\1/'`"
 echo "WORM_PACKAGE=\"$wa\"" > ../env.sh
 echo "WORM_STATE=\"$state\"" >> ../env.sh
+echo "WORM_OWNER=\"$owner\"" >> ../env.sh
 
