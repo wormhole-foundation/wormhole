b4680f4002a19ca172f9da4309d8abf3f71d5a3a
jumpsiegel <83408952+jumpsiegel@users.noreply.github.com>
2022-10-25 11:07:32 -0500

    Sui/watcher (#1791)
    * sui/watcher: updates to python example
    
    * sui/watcher: simple go example

--- sui/tests/watcher.go
+++ sui/tests/watcher.go
@@ -1,85 +1,35 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-//go:build ignore
-// +build ignore
-
-// go get github.com/gorilla/websocket
-
 package main
 
 import (
-	"flag"
+	"fmt"
+	"golang.org/x/net/websocket"
 	"log"
-	"net/url"
-	"os"
-	"os/signal"
 	"time"
-
-	"github.com/gorilla/websocket"
+	"os"
 )
 
-var addr = flag.String("addr", "localhost:5001", "http service address")
-
 func main() {
-	flag.Parse()
-	log.SetFlags(0)
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/sui_subscribeEvent"}
-	log.Printf("connecting to %s", u.String())
-
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	origin := "http://localhost/"
+	url := "ws://localhost:9001"
+	ws, err := websocket.Dial(url, "", origin)
 	if err != nil {
-		log.Fatal("dial:", err)
+		log.Fatal(err)
 	}
-	defer c.Close()
 
-	done := make(chan struct{})
+	s := fmt.Sprintf(`{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"SenderAddress": "%s"}]}`, os.Getenv("WORM_OWNER"))
+	fmt.Printf("Sending: %s.\n", s)
 
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				return
-			}
-			log.Printf("recv: %s", message)
-		}
-	}()
-
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
+	if _, err := ws.Write([]byte(s)); err != nil {
+		log.Fatal(err)
+	}
 	for {
-		select {
-		case <-done:
-			return
-		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-			if err != nil {
-				log.Println("write:", err)
-				return
-			}
-		case <-interrupt:
-			log.Println("interrupt")
-
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
-				return
-			}
-			select {
-			case <-done:
-			case <-time.After(time.Second):
-			}
-			return
+		var msg = make([]byte, 512)
+		var n int
+		ws.SetReadDeadline(time.Now().Local().Add(1_000_000_000));
+		if n, err = ws.Read(msg); err != nil {
+			fmt.Printf("err");
+		} else {
+			fmt.Printf("Received: %s.\n", msg[:n])
 		}
 	}
 }
