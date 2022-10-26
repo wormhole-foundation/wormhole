12679b0c559e6e2b2cac81732fae752e9877db56
Evan Gray <battledingo@gmail.com>
2022-10-26 16:41:19 -0400

    ci: sdk tests wait for guardian ready

--- testing/sdk.sh
+++ testing/sdk.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' guardian:6060/readyz)" != "200" ]]; do sleep 5; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' spy:6060/metrics)" != "200" ]]; do sleep 5; done
 CI=true npm --prefix ../sdk/js run test-ci
