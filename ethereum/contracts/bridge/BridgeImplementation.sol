2f0cb19a0a8016e4168ce2a346f9473a6eef7232
Csongor Kiss <ckiss@jumptrading.com>
2022-11-16 18:46:42 +0000

    ethereum: Remove initialisation code
    These migrations have been performed, so the migration code can
    now safely be removed.

--- ethereum/contracts/bridge/BridgeImplementation.sol
+++ ethereum/contracts/bridge/BridgeImplementation.sol
@@ -17,28 +17,6 @@ contract BridgeImplementation is Bridge {
 
     function initialize() initializer public virtual {
         // this function needs to be exposed for an upgrade to pass
-        address tokenContract;
-        uint256 chain = chainId();
-
-        // A previous upgrade
-        // (https://github.com/wormhole-foundation/wormhole/commit/cb161db5e01970cc92d80fc679e506db054f8368)
-        // set the token contract implementations for a number of chains,
-        // excluding arbitrum and optimism, both of which were deployed with
-        // that `initialize` contract, meaning there's a potential for a
-        // temporary DoS.
-        //
-        // Performing this upgrade closes that DoS window.
-        //
-        // *insert rant about implicit variable initialisation*
-        if        (chain == 23) { // arbitrum
-            tokenContract = 0x53B56de645B9de6e5a40acE047D1c74E8B42Eccb;
-        } else if (chain == 24) { // optimism
-            tokenContract = 0xb91e3638F82A1fACb28690b37e3aAE45d2c33808;
-        } else {
-            revert("Chain not handled.");
-        }
-
-        setTokenImplementation(tokenContract);
     }
 
     modifier initializer() {
