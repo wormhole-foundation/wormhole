9e20c4c83d346076f1b1e119f81e338c8885c092
optke3 <optke3@gmail.com>
2022-11-17 18:28:41 +0000

    sui: init_and_share_state for core and token bridge, plus register emitter, in deploy.sh

--- sui/scripts/deploy.sh
+++ sui/scripts/deploy.sh
@@ -37,3 +37,8 @@ echo "export TOKEN_PACKAGE=\"$wa\"" >> ../env.sh
 echo "export TOKEN_STATE=\"$state\"" >> ../env.sh
 echo "export TOKEN_OWNER=\"$owner\"" >> ../env.sh
 
+. ../env.sh
+sui client call --function init_and_share_state --module state --package $WORM_PACKAGE  --gas-budget 20000 --args \"$WORM_STATE\" 0 0 [190,250,66,157,87,205,24,183,248,164,217,26,45,169,171,74,240,93,15,190] [[190,250,66,157,87,205,24,183,248,164,217,26,45,169,171,74,240,93,15,190]]
+sui client call --function get_new_emitter --module wormhole --package $WORM_PACKAGE --gas-budget 20000 --args \"$WORM_STATE\" | tee emitter.log
+emitter=`grep ID: emitter.log | head -1 | sed -e 's/^.*: \(.*\) ,.*/\1/'`
+sui client call --function init_and_share_state --module bridge_state --package $TOKEN_PACKAGE  --gas-budget 20000 --args \"$TOKEN_STATE\" \"$emitter\"
