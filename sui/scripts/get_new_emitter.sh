d4d748ebc5d659df6bd1519125ce764e1a0da3ad
optke3 <optke3@gmail.com>
2022-11-17 16:25:27 +0000

    sui: update scripts to use variables from deploy.sh, set address to 0x0

--- sui/scripts/get_new_emitter.sh
+++ sui/scripts/get_new_emitter.sh
@@ -2,4 +2,4 @@
 
 . env.sh
 
-sui client call --function get_new_emitter --module wormhole --package 0xae77cb8d8dd113d539123ee9bccc18a05e1d16d1  --gas-budget 20000 --args 0xf4c56900b4d4e41e519cbcc5ee4a750369446adc
+sui client call --function get_new_emitter --module wormhole --package $WORM_PACKAGE  --gas-budget 20000 --args \"$WORM_STATE\"
--- sui/scripts/init_tokenbridge.sh
+++ sui/scripts/init_tokenbridge.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -f
 
-sui client call --function init_and_share_state --module bridge_state --package 0x975030c700e4afe389b04a0b0627bc1797729189 --gas-budget 20000 --args 0x22f2c58912bc6674cd2ee4eeb41bf655024af623 0x0a398059600906accfb8a9b30c393b930cab360a
+. env.sh
+
+sui client call --function init_and_share_state --module bridge_state --package $TOKEN_PACKAGE --gas-budget 20000 --args \"$TOKEN_STATE\" \"$EMITTER_CAP\"
