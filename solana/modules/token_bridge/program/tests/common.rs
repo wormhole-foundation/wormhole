2df1cd8072fb7ac5bf804939530228145117819e
Bruce Riley <briley@jumptrading.com>
2022-11-22 15:03:12 +0000

    Okay, that didn't work

--- solana/modules/token_bridge/program/tests/common.rs
+++ solana/modules/token_bridge/program/tests/common.rs
@@ -130,7 +130,7 @@ mod helpers {
                 .unwrap(),
         );
 
-        let mut builder = ProgramTest::new("bridge4tb", program, processor!(bridge::solitaire));
+        let mut builder = ProgramTest::new("bridge", program, processor!(bridge::solitaire));
         builder.add_program("spl_token_metadata", spl_token_metadata::id(), None);
         builder.add_program(
             "token_bridge",
