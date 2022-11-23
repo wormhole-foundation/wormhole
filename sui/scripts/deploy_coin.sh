d11744f85e228db0ff7ccb8a9fac34f5eeb3ed4c
Csongor Kiss <ckiss@jumptrading.com>
2022-11-23 20:26:57 +0000

    sui: update deployment script
    now it handles regsiter_wrapped_coin

--- sui/scripts/deploy_coin.sh
+++ sui/scripts/deploy_coin.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -f
 
+set -euo pipefail
+
+cd "$(dirname "$0")"/..
+
 . env.sh
 
-cd coin
-sui client publish --gas-budget 20000 | tee publish.log
+sui client publish --gas-budget 20000 --path coin | tee publish.log
 grep ID: publish.log  | head -2 > ids.log
-treasury_cap="`grep "Account Address" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/'`"
-coin_package="`grep "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/'`"
-echo "export TREASURY_CAP=\"$treasury_cap\"" >> ../env.sh
-echo "export COIN_PACKAGE=\"$coin_package\"" >> ../env.sh
-cd ../
+NEW_WRAPPED_COIN=$(grep "Account Address" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/')
+COIN_PACKAGE=$(grep "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/')
+echo "export NEW_WRAPPED_COIN=$NEW_WRAPPED_COIN" >> env.sh
+echo "export COIN_PACKAGE=$COIN_PACKAGE" >> env.sh
