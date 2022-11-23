feaa7b70f49246f031af65a9a7616c47afbee07e
Csongor Kiss <ckiss@jumptrading.com>
2022-11-23 20:26:57 +0000

    sui: update deployment script
    now it handles regsiter_wrapped_coin

--- sui/scripts/deploy.sh
+++ sui/scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -f
 
-set -x
+set -euo pipefail
+
+cd "$(dirname "$0")"/..
 
 #Transaction Kind : Publish
 #----- Transaction Effects ----
@@ -15,13 +17,10 @@ make build
 sui client publish --gas-budget 10000 | tee publish.log
 grep ID: publish.log  | head -2 > ids.log
 
-wa="`grep "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/'`"
-sed -i -e "s/wormhole = .*/wormhole = \"$wa\"/" Move.toml
-deployer_cap="`grep -v "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/'`"
-owner="`grep -v "Immutable" ids.log | sed -e 's/^.*( \(.*\) )/\1/'`"
-echo "export WORM_PACKAGE=\"$wa\"" > ../env.sh
-echo "export WORM_DEPLOYER_CAPABILITY=\"$deployer_cap\"" >> ../env.sh
-echo "export WORM_OWNER=\"$owner\"" >> ../env.sh
+WORM_PACKAGE=$(grep "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/')
+sed -i -e "s/wormhole = .*/wormhole = \"$WORM_PACKAGE\"/" Move.toml
+WORM_DEPLOYER_CAPABILITY=$(grep -v "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/')
+WORM_OWNER=$(grep -v "Immutable" ids.log | sed -e 's/^.*( \(.*\) )/\1/')
 
 cd ../token_bridge
 sed -i -e 's/token_bridge = .*/token_bridge = "0x0"/' Move.toml
@@ -29,25 +28,27 @@ make build
 sui client publish --gas-budget 10000 | tee publish.log
 grep ID: publish.log  | head -2 > ids.log
 
-wa="`grep "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/'`"
-sed -i -e "s/token_bridge = .*/token_bridge = \"$wa\"/" Move.toml
-deployer_cap="`grep -v "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/'`"
-owner="`grep -v "Immutable" ids.log | sed -e 's/^.*( \(.*\) )/\1/'`"
-echo "export TOKEN_PACKAGE=\"$wa\"" >> ../env.sh
-echo "export TOKEN_DEPLOYER_CAPABILITY=\"$deployer_cap\"" >> ../env.sh
-echo "export TOKEN_OWNER=\"$owner\"" >> ../env.sh
+TOKEN_PACKAGE=$(grep "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/')
+sed -i -e "s/token_bridge = .*/token_bridge = \"$TOKEN_PACKAGE\"/" Move.toml
+TOKEN_DEPLOYER_CAPABILITY=$(grep -v "Immutable" ids.log  | sed -e 's/^.*: \(.*\) ,.*/\1/')
+TOKEN_OWNER=$(grep -v "Immutable" ids.log | sed -e 's/^.*( \(.*\) )/\1/')
 
-. ../env.sh
-sui client call --function init_and_share_state --module state --package $WORM_PACKAGE  --gas-budget 20000 --args \"$WORM_DEPLOYER_CAPABILITY\" 0 0 [190,250,66,157,87,205,24,183,248,164,217,26,45,169,171,74,240,93,15,190] [[190,250,66,157,87,205,24,183,248,164,217,26,45,169,171,74,240,93,15,190]] | tee wormhole.log
-wormhole=`grep Shared wormhole.log | head -1 | sed -e 's/^.*: \(.*\) ,.*/\1/'`
-echo "export WORM_STATE=\"$wormhole\"" >> ../env.sh
+sui client call --function init_and_share_state --module state --package $WORM_PACKAGE  --gas-budget 20000 --args \"$WORM_DEPLOYER_CAPABILITY\" 0 0 "[190,250,66,157,87,205,24,183,248,164,217,26,45,169,171,74,240,93,15,190]" "[[190,250,66,157,87,205,24,183,248,164,217,26,45,169,171,74,240,93,15,190]]" | tee wormhole.log
+WORM_STATE=$(grep Shared wormhole.log | head -1 | sed -e 's/^.*: \(.*\) ,.*/\1/')
 
-. ../env.sh
 sui client call --function get_new_emitter --module wormhole --package $WORM_PACKAGE --gas-budget 20000 --args \"$WORM_STATE\" | tee emitter.log
-emitter=`grep ID: emitter.log | head -1 | sed -e 's/^.*: \(.*\) ,.*/\1/'`
-echo "export TOKEN_EMITTER_CAPABILITY=\"$emitter\"" >> ../env.sh
+TOKEN_EMITTER_CAPABILITY=$(grep ID: emitter.log | head -1 | sed -e 's/^.*: \(.*\) ,.*/\1/')
 
-. ../env.sh
-sui client call --function init_and_share_state --module bridge_state --package $TOKEN_PACKAGE  --gas-budget 20000 --args \"$TOKEN_DEPLOYER_CAPABILITY\" \"$TOKEN_EMITTER_CAPABILITY\" | tee token.log
-token_bridge=`grep Shared token.log | head -1 | sed -e 's/^.*: \(.*\) ,.*/\1/'`
-echo "export TOKEN_STATE=\"$token_bridge\"" >> ../env.sh
+sui client call --function init_and_share_state --module bridge_state --package $TOKEN_PACKAGE  --gas-budget 20000 --args "$TOKEN_DEPLOYER_CAPABILITY" "$TOKEN_EMITTER_CAPABILITY" | tee token.log
+TOKEN_STATE=$(grep Shared token.log | head -1 | sed -e 's/^.*: \(.*\) ,.*/\1/')
+
+{ echo "export WORM_PACKAGE=$WORM_PACKAGE";
+  echo "export WORM_DEPLOYER_CAPABILITY=$WORM_DEPLOYER_CAPABILITY";
+  echo "export WORM_OWNER=$WORM_OWNER";
+  echo "export TOKEN_PACKAGE=$TOKEN_PACKAGE";
+  echo "export TOKEN_DEPLOYER_CAPABILITY=$TOKEN_DEPLOYER_CAPABILITY";
+  echo "export TOKEN_OWNER=$TOKEN_OWNER";
+  echo "export WORM_STATE=$WORM_STATE";
+  echo "export TOKEN_EMITTER_CAPABILITY=$TOKEN_EMITTER_CAPABILITY";
+  echo "export TOKEN_STATE=$TOKEN_STATE";
+} > ../env.sh
