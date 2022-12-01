f8dfa678dc85179522dc1fc7a28cede20f1ec4c3
Nikhil Suri <nikhilsuri@comcast.net>
2022-11-30 17:01:33 -0800

    fix msg_server_wasmd_test

--- wormchain/x/wormhole/keeper/msg_server_wasmd_test.go
+++ wormchain/x/wormhole/keeper/msg_server_wasmd_test.go
@@ -31,12 +31,7 @@ func createWasmInstantiatePayload(code_id uint64, label string, json_msg string)
 	// - code_id (big endian)
 	// - label
 	// - json_msg
-	var expected_hash [32]byte
-	keccak := sha3.NewLegacyKeccak256()
-	binary.Write(keccak, binary.BigEndian, code_id)
-	keccak.Write([]byte(label))
-	keccak.Write([]byte(json_msg))
-	keccak.Sum(expected_hash[:0])
+	expected_hash := vaa.CreateInstatiateCosmwasmContractHash(code_id, label, []byte(json_msg))
 
 	var payload bytes.Buffer
 	payload.Write(keeper.WasmdModule[:])
