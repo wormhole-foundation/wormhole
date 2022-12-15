2b9b2505d06f4e76b1d6842304a77750df2b665d
Chirantan Ekbote <cekbote@jumptrading.com>
2022-12-15 19:33:44 +0900

    cosmwasm: accounting: Drop dependency on the tokenbridge contract
    Now that the accounting contract can handle chain registrations on
    its own, there's no need to query the tokenbridge contract.  Remove
    references to it from `InstantiateMsg` and the internal state.

--- cosmwasm/contracts/wormchain-accounting/src/state.rs
+++ cosmwasm/contracts/wormchain-accounting/src/state.rs
@@ -1,14 +1,12 @@
 use accounting::state::transfer;
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::Addr;
-use cw_storage_plus::{Item, Map};
+use cw_storage_plus::Map;
 use thiserror::Error;
 use tinyvec::TinyVec;
 use wormhole::{vaa::Signature, Address};
 
 use crate::msg::Observation;
 
-pub const TOKENBRIDGE_ADDR: Item<Addr> = Item::new("tokenbride_addr");
 pub const PENDING_TRANSFERS: Map<transfer::Key, TinyVec<[Data; 2]>> = Map::new("pending_transfers");
 pub const CHAIN_REGISTRATIONS: Map<u16, Address> = Map::new("chain_registrations");
 pub const GOVERNANCE_VAAS: Map<Vec<u8>, ()> = Map::new("governance_vaas");
