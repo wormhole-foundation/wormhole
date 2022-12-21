43035fcc44f32effded0d7827fb3d55bf23c6410
guibescos <59208140+guibescos@users.noreply.github.com>
2022-12-21 12:35:37 +0100

    sdk/js: Add instruction decoder to solana (#2095)
    * Bump package
    
    * Refactor coder add decoder
    
    * Refactor
    
    * Fix
    
    * Fix u64
    
    * Simplify encode
    
    * Add comment
    
    * Fix verify signatures
    
    * Fix package lock
    
    * Revert "Fix package lock"
    
    This reverts commit 9f219bc6fc21d2d0f8bf33fedafd6c204a00e659.
    
    * Fix deps
    
    * Fix import for bs58

--- sdk/js/src/solana/wormhole/coder/instruction.ts
+++ sdk/js/src/solana/wormhole/coder/instruction.ts
@@ -1,40 +1,87 @@
-import { Idl, InstructionCoder } from "@project-serum/anchor";
-import { ETHEREUM_KEY_LENGTH } from "../../utils";
+import { Idl, Instruction, InstructionCoder } from "@project-serum/anchor";
+import bs58 from "bs58";
+import { Layout } from "buffer-layout";
+import { camelCase, upperFirst } from "lodash";
+import { IdlField, IdlStateMethod } from "../../anchor";
+import * as borsh from "@coral-xyz/borsh";
+import { IdlCoder } from "./idl";
 
+// Inspired by  coral-xyz/anchor
+//
+// https://github.com/coral-xyz/anchor/blob/master/ts/packages/anchor/src/coder/borsh/instruction.ts
 export class WormholeInstructionCoder implements InstructionCoder {
-  constructor(_: Idl) {}
+  private ixLayout: Map<string, Layout>;
+
+  constructor(idl: Idl) {
+    this.ixLayout = WormholeInstructionCoder.parseIxLayout(idl);
+  }
+
+  private static parseIxLayout(idl: Idl): Map<string, Layout> {
+    const stateMethods = idl.state ? idl.state.methods : [];
+
+    const ixLayouts = stateMethods
+      .map((m: IdlStateMethod): [string, Layout<unknown>] => {
+        let fieldLayouts = m.args.map((arg: IdlField) => {
+          return IdlCoder.fieldLayout(
+            arg,
+            Array.from([...(idl.accounts ?? []), ...(idl.types ?? [])])
+          );
+        });
+        const name = camelCase(m.name);
+        return [name, borsh.struct(fieldLayouts, name)];
+      })
+      .concat(
+        idl.instructions.map((ix) => {
+          let fieldLayouts = ix.args.map((arg: IdlField) =>
+            IdlCoder.fieldLayout(
+              arg,
+              Array.from([...(idl.accounts ?? []), ...(idl.types ?? [])])
+            )
+          );
+          const name = camelCase(ix.name);
+          return [name, borsh.struct(fieldLayouts, name)];
+        })
+      );
+    return new Map(ixLayouts);
+  }
 
   encode(ixName: string, ix: any): Buffer {
-    switch (ixName) {
-      case "initialize": {
-        return encodeInitialize(ix);
-      }
-      case "postVaa": {
-        return encodePostVaa(ix);
-      }
-      case "setFees": {
-        return encodeSetFees(ix);
-      }
-      case "transferFees": {
-        return encodeTransferFees(ix);
-      }
-      case "upgradeContract": {
-        return encodeUpgradeContract(ix);
-      }
-      case "upgradeGuardianSet": {
-        return encodeUpgradeGuardianSet(ix);
-      }
-      case "verifySignatures": {
-        return encodeVerifySignatures(ix);
-      }
-      default: {
-        throw new Error(`Invalid instruction: ${ixName}`);
-      }
+    const buffer = Buffer.alloc(1000); // TODO: use a tighter buffer.
+    const methodName = camelCase(ixName);
+    const layout = this.ixLayout.get(methodName);
+    if (!layout) {
+      throw new Error(`Unknown method: ${methodName}`);
     }
+    const len = layout.encode(ix, buffer);
+    const data = buffer.slice(0, len);
+
+    return encodeWormholeInstructionData(
+      (WormholeInstruction as any)[upperFirst(methodName)],
+      data
+    );
   }
 
   encodeState(_ixName: string, _ix: any): Buffer {
     throw new Error("Wormhole program does not have state");
+  }
+
+  public decode(
+    ix: Buffer | string,
+    encoding: "hex" | "base58" = "hex"
+  ): Instruction | null {
+    if (typeof ix === "string") {
+      ix = encoding === "hex" ? Buffer.from(ix, "hex") : bs58.decode(ix);
+    }
+    let discriminator = ix.slice(0, 1).readInt8();
+    let data = ix.slice(1);
+
+    let name = camelCase(WormholeInstruction[discriminator]);
+    let layout = this.ixLayout.get(name);
+
+    if (!layout) {
+      return null;
+    }
+    return { data: this.ixLayout.get(name)?.decode(data), name };
   }
 }
 
@@ -45,7 +92,7 @@ export class WormholeInstructionCoder implements InstructionCoder {
 export enum WormholeInstruction {
   Initialize,
   PostMessage,
-  PostVAA,
+  PostVaa,
   SetFees,
   TransferFees,
   UpgradeContract,
@@ -54,101 +101,16 @@ export enum WormholeInstruction {
   PostMessageUnreliable, // sounds useful
 }
 
-function encodeInitialize({
-  guardianSetExpirationTime,
-  fee,
-  initialGuardians,
-}: any): Buffer {
-  if (typeof fee != "bigint") {
-    fee = BigInt(fee);
-  }
-  const initialGuardiansLen = initialGuardians.length;
-  const serialized = Buffer.alloc(
-    16 + initialGuardiansLen * ETHEREUM_KEY_LENGTH
-  );
-  serialized.writeUInt32LE(guardianSetExpirationTime, 0);
-  serialized.writeBigUInt64LE(fee, 4);
-  serialized.writeUInt32LE(initialGuardiansLen, 12);
-  for (let i = 0; i < initialGuardiansLen; ++i) {
-    const key = initialGuardians.at(i)!;
-    if (!Buffer.isBuffer(key)) {
-      throw new Error("key must be Buffer");
-    }
-    serialized.write(key.toString("hex"), 16 + i * ETHEREUM_KEY_LENGTH, "hex");
-  }
-  return encodeWormholeInstructionData(
-    WormholeInstruction.Initialize,
-    serialized
-  );
-}
-
 function encodeWormholeInstructionData(
-  instructionType: WormholeInstruction,
+  discriminator: number,
   data?: Buffer
 ): Buffer {
   const instructionData = Buffer.alloc(
     1 + (data === undefined ? 0 : data.length)
   );
-  instructionData.writeUInt8(instructionType, 0);
+  instructionData.writeUInt8(discriminator, 0);
   if (data !== undefined) {
     instructionData.write(data.toString("hex"), 1, "hex");
   }
   return instructionData;
-}
-
-function encodePostVaa({
-  version,
-  guardianSetIndex,
-  timestamp,
-  nonce,
-  emitterChain,
-  emitterAddress,
-  sequence,
-  consistencyLevel,
-  payload,
-}: any) {
-  if (!Buffer.isBuffer(emitterAddress)) {
-    throw new Error("emitterAddress must be Buffer");
-  }
-  if (!Buffer.isBuffer(payload)) {
-    throw new Error("payload must be Buffer");
-  }
-  if (typeof sequence != "bigint") {
-    sequence = BigInt(sequence);
-  }
-  const serialized = Buffer.alloc(60 + payload.length);
-  serialized.writeUInt8(version, 0);
-  serialized.writeUInt32LE(guardianSetIndex, 1);
-  serialized.writeUInt32LE(timestamp, 5);
-  serialized.writeUInt32LE(nonce, 9);
-  serialized.writeUInt16LE(emitterChain, 13);
-  serialized.write(emitterAddress.toString("hex"), 15, "hex");
-  serialized.writeBigUInt64LE(sequence, 47);
-  serialized.writeUInt8(consistencyLevel, 55);
-  serialized.writeUInt32LE(payload.length, 56);
-  serialized.write(payload.toString("hex"), 60, "hex");
-  return encodeWormholeInstructionData(WormholeInstruction.PostVAA, serialized);
-}
-
-function encodeSetFees({}: any) {
-  return encodeWormholeInstructionData(WormholeInstruction.SetFees);
-}
-
-function encodeTransferFees({}: any) {
-  return encodeWormholeInstructionData(WormholeInstruction.TransferFees);
-}
-
-function encodeUpgradeContract({}: any) {
-  return encodeWormholeInstructionData(WormholeInstruction.UpgradeContract);
-}
-
-function encodeUpgradeGuardianSet({}: any) {
-  return encodeWormholeInstructionData(WormholeInstruction.UpgradeGuardianSet);
-}
-
-function encodeVerifySignatures({ signatureStatus }: any) {
-  return encodeWormholeInstructionData(
-    WormholeInstruction.VerifySignatures,
-    Buffer.from(signatureStatus, "hex")
-  );
 }
