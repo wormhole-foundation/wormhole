f36392fe8aa5741bf7394bffbc0479717641630e
Josh Siegel <jsiegel@jumptrading.com>
2023-01-12 19:48:52 +0000

    sui: tilt updates

--- sui/scripts/start_node.sh
+++ sui/scripts/start_node.sh
@@ -5,7 +5,6 @@ set -x
 sui start &
 sleep 10
 #sleep infinity
-sui client object --id 0x5
 #sui-faucet --host-ip 0.0.0.0&
 #sleep 2
 #curl -X POST -d '{"FixedAmountRequest":{"recipient": "'"0x2acab6bb0e4722e528291bc6ca4f097e18ce9331"'"}}' -H 'Content-Type: application/json' http://127.0.0.1:5003/gas
