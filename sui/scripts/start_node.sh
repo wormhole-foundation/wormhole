da5234d29413e2ab587de85d092cde503c1a924a
Josh Siegel <jsiegel@jumptrading.com>
2023-01-18 12:56:24 +0000

    sui/runtime_update: corrections for sui updates

--- sui/scripts/start_node.sh
+++ sui/scripts/start_node.sh
@@ -2,13 +2,13 @@
 
 set -x
 
-sui start &
-sleep 10
+sui start
+#sleep 10
 #sleep infinity
 #sui-faucet --host-ip 0.0.0.0&
 #sleep 2
 #curl -X POST -d '{"FixedAmountRequest":{"recipient": "'"0x2acab6bb0e4722e528291bc6ca4f097e18ce9331"'"}}' -H 'Content-Type: application/json' http://127.0.0.1:5003/gas
-sed -i -e 's/:9000/:9002/' ~/.sui/sui_config/fullnode.yaml
-sui-node --config-path ~/.sui/sui_config/fullnode.yaml
+#sed -i -e 's/:9000/:9002/' ~/.sui/sui_config/fullnode.yaml
+#sui-node --config-path ~/.sui/sui_config/fullnode.yaml
 
 #sleep infinity
