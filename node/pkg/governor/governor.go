b9a2b19d6132082f873e889bcaf7bc74d75621d3
Bruce Riley <briley@jumptrading.com>
2023-02-08 17:09:56 +0000

    Node/Gov: Don't use named return values
    Change-Id: Ia4e04e496b0b1525a6cf31fca82d3851e0978cdb

--- node/pkg/governor/governor.go
+++ node/pkg/governor/governor.go
@@ -450,44 +450,43 @@ func (gov *ChainGovernor) IsGovernedMsg(msg *common.MessagePublication) (msgIsGo
 }
 
 // parseMsgAlreadyLocked determines if the message applies to the governor and also returns data useful to the governor. It assumes the caller holds the lock.
-func (gov *ChainGovernor) parseMsgAlreadyLocked(msg *common.MessagePublication) (msgIsGoverned bool, ce *chainEntry, token *tokenEntry, payload *vaa.TransferPayloadHdr, err error) {
+func (gov *ChainGovernor) parseMsgAlreadyLocked(msg *common.MessagePublication) (bool, *chainEntry, *tokenEntry, *vaa.TransferPayloadHdr, error) {
 	// If we don't care about this chain, the VAA can be published.
 	ce, exists := gov.chains[msg.EmitterChain]
 	if !exists {
 		if msg.EmitterChain != vaa.ChainIDPythNet {
 			gov.logger.Info("cgov: ignoring vaa because the emitter chain is not configured", zap.String("msgID", msg.MessageIDString()))
 		}
-		return
+		return false, nil, nil, nil, nil
 	}
 
 	// If we don't care about this emitter, the VAA can be published.
 	if msg.EmitterAddress != ce.emitterAddr {
 		gov.logger.Info("cgov: ignoring vaa because the emitter address is not configured", zap.String("msgID", msg.MessageIDString()))
-		return
+		return false, nil, nil, nil, nil
 	}
 
 	// We only care about transfers.
 	if !vaa.IsTransfer(msg.Payload) {
 		gov.logger.Info("cgov: ignoring vaa because it is not a transfer", zap.String("msgID", msg.MessageIDString()))
-		return
+		return false, nil, nil, nil, nil
 	}
 
-	payload, err = vaa.DecodeTransferPayloadHdr(msg.Payload)
+	payload, err := vaa.DecodeTransferPayloadHdr(msg.Payload)
 	if err != nil {
 		gov.logger.Error("cgov: failed to decode vaa", zap.String("msgID", msg.MessageIDString()), zap.Error(err))
-		return
+		return false, nil, nil, nil, err
 	}
 
 	// If we don't care about this token, the VAA can be published.
 	tk := tokenKey{chain: payload.OriginChain, addr: payload.OriginAddress}
-	token, exists = gov.tokens[tk]
+	token, exists := gov.tokens[tk]
 	if !exists {
 		gov.logger.Info("cgov: ignoring vaa because the token is not in the list", zap.String("msgID", msg.MessageIDString()))
-		return
+		return false, nil, nil, nil, nil
 	}
 
-	msgIsGoverned = true
-	return
+	return true, ce, token, payload, nil
 }
 
 func (gov *ChainGovernor) CheckPending() ([]*common.MessagePublication, error) {
