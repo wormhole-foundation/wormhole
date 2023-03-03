d6eca32eabc5d6cf23723777a9f4bf4adfa39516
heyitaki <akshath@live.com>
2023-03-03 21:19:14 +0000

    sui: import default account to ensure deterministic key

--- sui/scripts/fund_account.sh
+++ sui/scripts/fund_account.sh
@@ -1,3 +1,6 @@
-#!/bin/bash -f
+#!/usr/bin/env bash
+
+# This dev script funds the given account. It defaults to the account that is 
+# created in `import_default_account.sh`.
 
 sui client transfer-sui --to 0x13b3cb89cf3226d3b860294fc75dc6c91f0c5ecf --sui-coin-object-id `sui client objects | grep sui::SUI | tail -1 | sed -e 's/|.*//'` --gas-budget 10000
