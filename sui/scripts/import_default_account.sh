133a459190a775a596cd80dbe5aa08551e525671
heyitaki <akshath@live.com>
2023-03-03 21:19:14 +0000

    sui: import default account to ensure deterministic key

--- sui/scripts/import_default_account.sh
+++ sui/scripts/import_default_account.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# This dev script imports and funds an account, following the steps in 
-# `sui/NOTES.md`. It also deploys the core/token bridge contracts.
+# This dev script imports an account, following the steps in `sui/NOTES.md`. 
+# It also deploys the core/token bridge contracts.
 
 # Remove directory for idempotency
 rm -rf $HOME/.sui
@@ -15,10 +15,3 @@ dev
 0
 EOF
 sed -i -e 's/active_address.*/active_address: "0x13b3cb89cf3226d3b860294fc75dc6c91f0c5ecf"/' ~/.sui/sui_config/client.yaml
-
-# Fund account
-kubectl exec -it sui-0 -c sui-node -- /tmp/funder.sh
-
-# Deploy contracts
-DIR_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-$DIR_PATH/deploy.sh
