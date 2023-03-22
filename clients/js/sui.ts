275366357337d0aa0feb10ae27719426c09be54a
heyitaki <akshath@live.com>
2023-03-22 22:06:56 +0000

    clients/js: re-org sui helpers alphabetically

--- clients/js/sui.ts
+++ clients/js/sui.ts
@@ -10,6 +10,68 @@ import { resolve } from "path";
 import { NETWORKS } from "./networks";
 import { Network } from "./utils";
 
+export async function executeEntry(
+  provider: JsonRpcProvider,
+  network: Network,
+  packageObjectId: string,
+  moduleName: string,
+  functionName: string,
+  typeArgs: string[],
+  args: any[]
+) {
+  const signer = getSigner(provider, network);
+  const moveCallTxn = await signer.executeMoveCall({
+    packageObjectId,
+    module: moduleName,
+    function: functionName,
+    typeArguments: typeArgs,
+    arguments: args,
+    gasBudget: 50000,
+  });
+
+  console.log(
+    "Transaction digest: ",
+    moveCallTxn["certificate"]["transactionDigest"]
+  );
+  console.log(
+    "Sender:             ",
+    moveCallTxn["certificate"]["data"]["sender"]
+  );
+
+  // Let caller handle parsing and logging effects
+  return moveCallTxn["effects"]["effects"];
+}
+
+export const getProvider = (
+  network?: Network,
+  rpc?: string
+): JsonRpcProvider => {
+  if (!network && !rpc) {
+    throw new Error("Must provide network or RPC to initialize provider");
+  }
+
+  rpc = rpc || NETWORKS[network]["sui"].rpc;
+  if (!rpc) {
+    throw new Error(`No default RPC found for Sui ${network}`);
+  }
+
+  return new JsonRpcProvider(new Connection({ fullnode: rpc }));
+};
+
+export const getSigner = (
+  provider: JsonRpcProvider,
+  network: Network
+): RawSigner => {
+  const privateKey: string | undefined = NETWORKS[network]["sui"].key;
+  if (!privateKey) {
+    throw new Error(`No private key found for Sui ${network}`);
+  }
+
+  const bytes = new Uint8Array(Buffer.from(privateKey, "base64"));
+  const keypair = Ed25519Keypair.fromSecretKey(bytes.slice(1));
+  return new RawSigner(keypair, provider);
+};
+
 export const publishPackage = async (
   provider: JsonRpcProvider,
   network: Network,
@@ -62,67 +124,59 @@ export const publishPackage = async (
   }
 };
 
-export const getProvider = (
-  network?: Network,
-  rpc?: string
-): JsonRpcProvider => {
-  if (!network && !rpc) {
-    throw new Error("Must provide network or RPC to initialize provider");
-  }
+const cleanupToml = (packagePath: string): void => {
+  const defaultTomlPath = getDefaultTomlPath(packagePath);
+  const tempTomlPath = getTempTomlPath(packagePath);
+  if (fs.existsSync(tempTomlPath)) {
+    // Clean up Move.toml for dependencies
+    const dependencyPaths = getAllPackageDependencyPaths(packagePath);
+    for (const path of dependencyPaths) {
+      cleanupToml(path);
+    }
 
-  rpc = rpc || NETWORKS[network]["sui"].rpc;
-  if (!rpc) {
-    throw new Error(`No default RPC found for Sui ${network}`);
+    fs.renameSync(tempTomlPath, defaultTomlPath);
   }
-
-  return new JsonRpcProvider(new Connection({ fullnode: rpc }));
 };
 
-export const getSigner = (
-  provider: JsonRpcProvider,
-  network: Network
-): RawSigner => {
-  const privateKey: string | undefined = NETWORKS[network]["sui"].key;
-  if (!privateKey) {
-    throw new Error(`No private key found for Sui ${network}`);
+/**
+ * Get Move.toml dependencies by looking for all lines of form 'local = ".*"'.
+ * This works because network-specific Move.toml files should not contain
+ * dev addresses, so the only lines that match this regex are the dependencies
+ * that need to be replaced.
+ * @param packagePath
+ * @returns
+ */
+const getAllPackageDependencyPaths = (packagePath: string): string[] => {
+  const tomlPath = getDefaultTomlPath(packagePath);
+  const tomlStr = fs.readFileSync(tomlPath, "utf8").toString();
+
+  // Sanity check that Move.toml does not contain dev info since this currently
+  // breaks building and publishing packages
+  if (
+    /\[dev\-dependencies\]/.test(tomlStr) ||
+    /\[dev\-addresses\]/.test(tomlStr)
+  ) {
+    throw new Error(
+      "Network-specific Move.toml should not contain dev-dependencies or dev-addresses."
+    );
   }
 
-  const bytes = new Uint8Array(Buffer.from(privateKey, "base64"));
-  const keypair = Ed25519Keypair.fromSecretKey(bytes.slice(1));
-  return new RawSigner(keypair, provider);
+  return [...tomlStr.matchAll(/local = "(.*)"/g)].map((match) =>
+    resolve(packagePath, match[1])
+  );
 };
 
-export async function executeEntry(
-  provider: JsonRpcProvider,
-  network: Network,
-  packageObjectId: string,
-  moduleName: string,
-  functionName: string,
-  typeArgs: string[],
-  args: any[]
-) {
-  const signer = getSigner(provider, network);
-  const moveCallTxn = await signer.executeMoveCall({
-    packageObjectId,
-    module: moduleName,
-    function: functionName,
-    typeArguments: typeArgs,
-    arguments: args,
-    gasBudget: 50000,
-  });
+const getDefaultTomlPath = (packagePath: string): string =>
+  `${packagePath}/Move.toml`;
 
-  console.log(
-    "Transaction digest: ",
-    moveCallTxn["certificate"]["transactionDigest"]
-  );
-  console.log(
-    "Sender:             ",
-    moveCallTxn["certificate"]["data"]["sender"]
-  );
+const getTempTomlPath = (packagePath: string): string =>
+  `${packagePath}/Move.temp.toml`;
 
-  // Let caller handle parsing and logging effects
-  return moveCallTxn["effects"]["effects"];
-}
+const getTomlPathByNetwork = (packagePath: string, network: Network): string =>
+  `${packagePath}/Move.${network.toLowerCase()}.toml`;
+
+const getPackageNameFromPath = (packagePath: string): string =>
+  packagePath.split("/").pop() || "";
 
 const setupToml = (
   packagePath: string,
@@ -183,58 +237,4 @@ const setupToml = (
   for (const path of dependencyPaths) {
     setupToml(path, network, namedAddresses, true);
   }
-};
-
-const cleanupToml = (packagePath: string): void => {
-  const defaultTomlPath = getDefaultTomlPath(packagePath);
-  const tempTomlPath = getTempTomlPath(packagePath);
-  if (fs.existsSync(tempTomlPath)) {
-    // Clean up Move.toml for dependencies
-    const dependencyPaths = getAllPackageDependencyPaths(packagePath);
-    for (const path of dependencyPaths) {
-      cleanupToml(path);
-    }
-
-    fs.renameSync(tempTomlPath, defaultTomlPath);
-  }
-};
-
-const getDefaultTomlPath = (packagePath: string): string =>
-  `${packagePath}/Move.toml`;
-
-const getTempTomlPath = (packagePath: string): string =>
-  `${packagePath}/Move.temp.toml`;
-
-const getTomlPathByNetwork = (packagePath: string, network: Network): string =>
-  `${packagePath}/Move.${network.toLowerCase()}.toml`;
-
-const getPackageNameFromPath = (packagePath: string): string =>
-  packagePath.split("/").pop() || "";
-
-/**
- * Get Move.toml dependencies by looking for all lines of form 'local = ".*"'.
- * This works because network-specific Move.toml files should not contain
- * dev addresses, so the only lines that match this regex are the dependencies
- * that need to be replaced.
- * @param packagePath
- * @returns
- */
-const getAllPackageDependencyPaths = (packagePath: string): string[] => {
-  const tomlPath = getDefaultTomlPath(packagePath);
-  const tomlStr = fs.readFileSync(tomlPath, "utf8").toString();
-
-  // Sanity check that Move.toml does not contain dev info since this currently
-  // breaks building and publishing packages
-  if (
-    /\[dev\-dependencies\]/.test(tomlStr) ||
-    /\[dev\-addresses\]/.test(tomlStr)
-  ) {
-    throw new Error(
-      "Network-specific Move.toml should not contain dev-dependencies or dev-addresses."
-    );
-  }
-
-  return [...tomlStr.matchAll(/local = "(.*)"/g)].map((match) =>
-    resolve(packagePath, match[1])
-  );
 };
