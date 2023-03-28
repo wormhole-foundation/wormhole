f39fec82552a662e58a470b51a7c66e3488d5ba5
Sebastián Nale <sebinale@gmail.com>
2023-03-28 17:10:26 -0300

    Uses batch update to configure `RelayProvider` contract.

--- ethereum/ts-scripts/relayer/shell/deployConfigureTest.sh
+++ ethereum/ts-scripts/relayer/shell/deployConfigureTest.sh
@@ -1,7 +1,6 @@
 ts-node ./ts-scripts/relayer/config/checkNetworks.ts \
   && ts-node ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
   && ts-node ./ts-scripts/relayer/coreRelayer/deployCoreRelayer.ts \
-  && ts-node ./ts-scripts/relayer/relayProvider/registerChainsRelayProvider.ts \
   && ts-node ./ts-scripts/relayer/coreRelayer/registerChainsCoreRelayerSelfSign.ts \
   && ts-node ./ts-scripts/relayer/relayProvider/configureRelayProvider.ts \
   && ts-node ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
