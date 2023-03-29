f13b7da43d1f27b936e128e7d49ed593a25eb8d8
A5 Pickle <a5-pickle@users.noreply.github.com>
2023-03-29 22:56:31 +0000

    sui: uptick to 0.29.1

--- sui/scripts/node_builder.sh
+++ sui/scripts/node_builder.sh
@@ -4,7 +4,7 @@ source $HOME/.cargo/env
 
 git clone https://github.com/MystenLabs/sui.git --branch devnet
 cd sui
-git reset --hard 82c9c80c11488858f1d3930f47ec9f335a566683
+git reset --hard 9ea7599fe5ca95454e43038ef41884753cee753c
 
 cargo --locked install --path crates/sui
 cargo --locked install --path crates/sui-faucet
