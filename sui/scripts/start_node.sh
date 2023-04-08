73e71063cd222818a9d0c2fac4f9b79f01a0beb9
heyitaki <akshath@live.com>
2023-04-08 23:40:44 +0000

    sui: auto deploy contracts on tilt up

--- sui/scripts/start_node.sh
+++ sui/scripts/start_node.sh
@@ -2,11 +2,6 @@
 
 set -x
 
-sui start&
+sui start >/dev/null 2>&1 &
 sleep 10
 sui-faucet --write-ahead-log faucet.log
-
-#curl -X POST -d '{"FixedAmountRequest":{"recipient": "'"0x2acab6bb0e4722e528291bc6ca4f097e18ce9331"'"}}' -H 'Content-Type: application/json' http://127.0.0.1:5003/gas
-#sed -i -e 's/:9000/:9002/' ~/.sui/sui_config/fullnode.yaml
-#sui-node --config-path ~/.sui/sui_config/fullnode.yaml
-#sleep infinity
