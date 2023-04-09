d911269c72d271aa856c2616b0c74030bab31d9b
chase-45 <chasemoran45@gmail.com>
2023-04-09 11:51:46 -0400

    relayer engine sdk temporarily imports out of ethereum folder

--- relayer/generic_relayer/relayer-engine-v2/pkgs/sdk/src/index.ts
+++ relayer/generic_relayer/relayer-engine-v2/pkgs/sdk/src/index.ts
@@ -1,16 +1,17 @@
-export type { CoreRelayer } from "./ethers-contracts/CoreRelayer";
-export { CoreRelayer__factory } from "./ethers-contracts/factories/CoreRelayer__factory";
-export type { MockRelayerIntegration } from "./ethers-contracts/MockRelayerIntegration";
-export { MockRelayerIntegration__factory } from "./ethers-contracts/factories/MockRelayerIntegration__factory";
+//TODO deprecate this in favor of the real SDK
+export type { CoreRelayer } from "../../../../../../ethereum/ethers-contracts/CoreRelayer";
+export { CoreRelayer__factory } from "../../../../../../ethereum/ethers-contracts/factories/CoreRelayer__factory";
+export type { MockRelayerIntegration } from "../../../../../../ethereum/ethers-contracts/MockRelayerIntegration";
+export { MockRelayerIntegration__factory } from "../../../../../../ethereum/ethers-contracts/factories/MockRelayerIntegration__factory";
 export type {
   IWormhole,
   LogMessagePublishedEvent,
-} from "./ethers-contracts/IWormhole";
-export { IWormhole__factory } from "./ethers-contracts/factories/IWormhole__factory";
-export type { RelayProvider } from "./ethers-contracts/RelayProvider";
-export { RelayProvider__factory } from "./ethers-contracts/factories/RelayProvider__factory";
-export type { IDelivery } from "./ethers-contracts/IDelivery";
-export type { IWormholeRelayer } from "./ethers-contracts/IWormholeRelayer";
+} from "../../../../../../ethereum/ethers-contracts/IWormhole";
+export { IWormhole__factory } from "../../../../../../ethereum/ethers-contracts/factories/IWormhole__factory";
+export type { RelayProvider } from "../../../../../../ethereum/ethers-contracts/RelayProvider";
+export { RelayProvider__factory } from "../../../../../../ethereum/ethers-contracts/factories/RelayProvider__factory";
+export type { IDelivery } from "../../../../../../ethereum/ethers-contracts/IDelivery";
+export type { IWormholeRelayer } from "../../../../../../ethereum/ethers-contracts/IWormholeRelayer";
 export * from "./structs";
 export * from "./consts";
 export * from "./main/index";
