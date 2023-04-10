46e6168152e8eac61025791e7e8ff2a79bfe1c4f
Sebastián Claudio Nale <sebinale@gmail.com>
2023-04-10 20:15:14 -0300

    Fixes relayer contracts deployment in containers.

--- ethereum/ts-scripts/relayer/shell/deployInContainer.sh
+++ ethereum/ts-scripts/relayer/shell/deployInContainer.sh
@@ -2,6 +2,5 @@
   npx ts-node ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
   && npx ts-node ./ts-scripts/relayer/coreRelayer/deployCoreRelayer.ts \
   && npx ts-node ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
-  && npx ts-node ./ts-scripts/relayer/relayProvider/registerChainsRelayProvider.ts \
   && npx ts-node ./ts-scripts/relayer/coreRelayer/registerChainsCoreRelayerSelfSign.ts \
   && npx ts-node ./ts-scripts/relayer/relayProvider/configureRelayProvider.ts \
