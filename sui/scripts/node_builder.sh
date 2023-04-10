f6b79971196b9fb1163cca793ef825dd09ec5a62
heyitaki <akshath@live.com>
2023-04-10 16:02:39 +0000

    sui: bump rev to 0.30.0

--- sui/scripts/node_builder.sh
+++ sui/scripts/node_builder.sh
@@ -4,7 +4,7 @@ source $HOME/.cargo/env
 
 git clone https://github.com/MystenLabs/sui.git --branch devnet
 cd sui
-git reset --hard 81dbcf2b6cab07d623a1012bf31daf658963c765
+git reset --hard ddfc3fa0768a38286787319603a5458a9ff91cc1
 
 cargo --locked install --path crates/sui
 cargo --locked install --path crates/sui-faucet
