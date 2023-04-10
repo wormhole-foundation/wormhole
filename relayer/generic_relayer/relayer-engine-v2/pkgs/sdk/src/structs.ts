04957fe2db136172e7944936aec5084b7542e6d2
chase-45 <chasemoran45@gmail.com>
2023-04-10 08:41:54 -0400

    parser and other relayer engine fixes

--- relayer/generic_relayer/relayer-engine-v2/pkgs/sdk/src/structs.ts
+++ relayer/generic_relayer/relayer-engine-v2/pkgs/sdk/src/structs.ts
@@ -1,5 +1,6 @@
-import { BigNumber, ethers } from "ethers"
-import { arrayify } from "ethers/lib/utils"
+import { parseNFTPayload } from "@certusone/wormhole-sdk";
+import { BigNumber, ethers } from "ethers";
+import { arrayify } from "ethers/lib/utils";
 
 export enum RelayerPayloadId {
   Delivery = 1,
@@ -19,31 +20,34 @@ export enum DeliveryStatus {
 }
 
 export interface DeliveryInstructionsContainer {
-  payloadId: number // 1
-  messages: MessageInfo[]
-  instructions: DeliveryInstruction[]
+  payloadId: number; // 1
+  senderAddress: Buffer;
+  messages: MessageInfo[];
+  instructions: DeliveryInstruction[];
 }
 
 export interface MessageInfo {
-  payloadType: MessageInfoType
-  emitterAddress?: Buffer
-  sequence?: BigNumber
-  vaaHash?: Buffer
+  payloadType: MessageInfoType;
+  emitterAddress?: Buffer;
+  sequence?: BigNumber;
+  vaaHash?: Buffer;
 }
 
 export interface DeliveryInstruction {
-  targetChain: number
-  targetAddress: Buffer
-  refundAddress: Buffer
-  maximumRefundTarget: BigNumber
-  receiverValueTarget: BigNumber
-  executionParameters: ExecutionParameters
+  targetChain: number;
+  targetAddress: Buffer;
+  refundAddress: Buffer;
+  refundChain: number;
+  maximumRefundTarget: BigNumber;
+  receiverValueTarget: BigNumber;
+  targetRelayProvider: Buffer;
+  executionParameters: ExecutionParameters;
+  payload: Buffer;
 }
 
 export interface ExecutionParameters {
-  version: number
-  gasLimit: number
-  providerDeliveryAddress: Buffer
+  version: number;
+  gasLimit: number;
 }
 
 export enum MessageInfoType {
@@ -55,85 +59,122 @@ export function parseWormholeRelayerPayloadType(
   stringPayload: string | Buffer | Uint8Array
 ): RelayerPayloadId {
   const payload =
-    typeof stringPayload === "string" ? arrayify(stringPayload) : stringPayload
+    typeof stringPayload === "string" ? arrayify(stringPayload) : stringPayload;
   if (payload[0] == 0 || payload[0] >= 3) {
-    throw new Error("Unrecogned payload type " + payload[0])
+    throw new Error("Unrecogned payload type " + payload[0]);
   }
-  return payload[0]
+  return payload[0];
 }
 
-export function parseWormholeRelayerSend(bytes: Buffer): DeliveryInstructionsContainer {
-  let idx = 0
-  const payloadId = bytes.readUInt8(idx)
+export function parseWormholeRelayerSend(
+  bytes: Buffer
+): DeliveryInstructionsContainer {
+  let idx = 0;
+  const payloadId = bytes.readUInt8(idx);
   if (payloadId !== RelayerPayloadId.Delivery) {
     throw new Error(
       `Expected Delivery payload type (${RelayerPayloadId.Delivery}), found: ${payloadId}`
-    )
+    );
   }
-  idx += 1
+  idx += 1;
 
-  const numMessages = bytes.readUInt8(idx)
-  idx += 1
+  dbg(payloadId, "payloadId");
 
-  let messages = [] as MessageInfo[]
+  const senderAddress = bytes.slice(idx, idx + 32);
+  idx += 32;
+
+  dbg(senderAddress, "senderAddress");
+
+  const numMessages = bytes.readUInt8(idx);
+  idx += 1;
+
+  dbg(numMessages, "numMessages");
+
+  const numInstructions = bytes.readUInt8(idx);
+  idx += 1;
+
+  dbg(numInstructions, "numInstructions");
+
+  let messages = [] as MessageInfo[];
   for (let i = 0; i < numMessages; ++i) {
-    const res = parseMessageInfo(bytes, idx)
-    idx = res[1]
-    messages.push(res[0])
+    const res = parseMessageInfo(bytes, idx);
+    idx = res[1];
+    messages.push(res[0]);
   }
 
-  const numInstructions = bytes.readUInt8(idx)
-  idx += 1
-
-  let instructions = [] as DeliveryInstruction[]
+  let instructions = [] as DeliveryInstruction[];
   for (let i = 0; i < numInstructions; ++i) {
-    const targetChain = bytes.readUInt16BE(idx)
-    idx += 2
-    const targetAddress = bytes.slice(idx, idx + 32)
-    idx += 32
-    const refundAddress = bytes.slice(idx, idx + 32)
-    idx += 32
+    const targetChain = bytes.readUInt16BE(idx);
+    idx += 2;
+    const targetAddress = bytes.slice(idx, idx + 32);
+    idx += 32;
+    const refundAddress = bytes.slice(idx, idx + 32);
+    idx += 32;
+    const refundChain = bytes.readUInt16BE(idx);
+    idx += 2;
     const maximumRefundTarget = ethers.BigNumber.from(
       Uint8Array.prototype.subarray.call(bytes, idx, idx + 32)
-    )
-    idx += 32
+    );
+    idx += 32;
     const receiverValueTarget = ethers.BigNumber.from(
       Uint8Array.prototype.subarray.call(bytes, idx, idx + 32)
-    )
-    idx += 32
-    let res = parseWormholeRelayerExecutionParameters(bytes, idx)
-    const executionParameters = res[0]
-    idx = res[1]
+    );
+    idx += 32;
+    const targetRelayProvider = bytes.slice(idx, idx + 32);
+    idx += 32;
+    let res = parseWormholeRelayerExecutionParameters(bytes, idx);
+    const executionParameters = res[0];
+    idx = res[1];
+    let payload: Buffer;
+    [payload, idx] = parsePayload(bytes, idx);
     instructions.push(
       // dumb typechain format
       {
         targetChain,
         targetAddress,
         refundAddress,
+        refundChain,
         maximumRefundTarget,
         receiverValueTarget,
+        targetRelayProvider,
         executionParameters,
+        payload,
       }
-    )
+    );
   }
 
   return {
     payloadId,
+    senderAddress,
     messages,
     instructions,
-  }
+  };
+}
+
+function parsePayload(bytes: Buffer, idx: number): [Buffer, number] {
+  const length = bytes.readUInt32BE(idx);
+  idx += 4;
+  const payload = bytes.slice(idx, idx + length);
+  idx += length;
+  return [payload, idx];
 }
 
 function parseMessageInfo(bytes: Buffer, idx: number): [MessageInfo, number] {
-  const payloadType = bytes.readUint8(idx) as MessageInfoType
-  idx += 1
+  const version = bytes.readUint8(idx);
+  idx += 1;
+
+  const payloadType = bytes.readUint8(idx) as MessageInfoType;
+  idx += 1;
+
+  dbg(payloadType, "payloadType");
   if (payloadType == MessageInfoType.EMITTER_SEQUENCE) {
-    const emitterAddress = bytes.slice(idx, idx + 32)
-    idx += 32
+    dbg(null, "parsingEmitterSequence");
+    const emitterAddress = bytes.slice(idx, idx + 32);
+    idx += 32;
     const sequence = ethers.BigNumber.from(
       Uint8Array.prototype.subarray.call(bytes, idx, idx + 8)
-    )
-    idx += 8
+    );
+    idx += 8;
     return [
       {
         payloadType,
@@ -141,19 +182,19 @@ function parseMessageInfo(bytes: Buffer, idx: number): [MessageInfo, number] {
         sequence,
       },
       idx,
-    ]
+    ];
   } else if (payloadType == MessageInfoType.VAAHASH) {
-    const vaaHash = bytes.slice(idx, idx + 32)
-    idx += 32
+    const vaaHash = bytes.slice(idx, idx + 32);
+    idx += 32;
     return [
       {
         payloadType,
         vaaHash,
       },
       idx,
-    ]
+    ];
   } else {
-    throw new Error("Unexpected MessageInfo payload type")
+    throw new Error("Unexpected MessageInfo payload type");
   }
 }
 
@@ -161,13 +202,11 @@ function parseWormholeRelayerExecutionParameters(
   bytes: Buffer,
   idx: number
 ): [ExecutionParameters, number] {
-  const version = bytes.readUInt8(idx)
-  idx += 1
-  const gasLimit = bytes.readUint32BE(idx)
-  idx += 4
-  const providerDeliveryAddress = bytes.slice(idx, idx + 32)
-  idx += 32
-  return [{ version, gasLimit, providerDeliveryAddress }, idx]
+  const version = bytes.readUInt8(idx);
+  idx += 1;
+  const gasLimit = bytes.readUint32BE(idx);
+  idx += 4;
+  return [{ version, gasLimit }, idx];
 }
 
 /*
@@ -176,8 +215,8 @@ function parseWormholeRelayerExecutionParameters(
 
 export function dbg<T>(x: T, msg?: string): T {
   if (msg) {
-    console.log("[DEBUG] " + msg)
+    console.log("[DEBUG] " + msg);
   }
-  console.log(x)
-  return x
+  console.log(x);
+  return x;
 }
