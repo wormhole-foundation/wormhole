ac31cd8b822b91a0df228761b1814d0beae646b7
Alber Erre <arblugo@gmail.com>
2023-04-13 00:55:36 +0100

    feat: add submit tests

---
+++ clients/js/__tests__/submit.test.ts
@@ -0,0 +1,26 @@
+import yargs from "yargs";
+import { describe, expect, it } from "@jest/globals";
+
+describe("worm submit", () => {
+  describe("check arguments", () => {
+    const FIRST_POSITIONAL_ARGUMENT = "<vaa>";
+    const REQUIRED_FIRST_FLAG = "--network";
+    const REQUIRED_SECOND_FLAG = "--chain";
+
+    it(`should have correct positional arguments`, async () => {
+      const command = await yargs.command(require("../cmds/submit")).help();
+
+      // Run the command module with --help as argument
+      const output = await new Promise((resolve) => {
+        command.parse(["submit", "--help"], (_err, _argv, output) => {
+          console.log("output", output);
+          resolve(output);
+        });
+      });
+
+      expect(output).toContain(FIRST_POSITIONAL_ARGUMENT);
+      expect(output).toContain(REQUIRED_FIRST_FLAG);
+      expect(output).toContain(REQUIRED_SECOND_FLAG);
+    });
+  });
+});
