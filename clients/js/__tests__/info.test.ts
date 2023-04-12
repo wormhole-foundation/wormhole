bd72550b76774ec204c253cca93f43894d7df98b
Alber Erre <arblugo@gmail.com>
2023-04-13 00:05:26 +0100

    feat: add info namespace tests

---
+++ clients/js/__tests__/info.test.ts
@@ -0,0 +1,26 @@
+import yargs from "yargs";
+import { describe, expect, it } from "@jest/globals";
+
+describe("worm info", () => {
+  describe("check commands", () => {
+    const FIRST_COMMAND = "info chain-id";
+    const SECOND_COMMAND = "info rpc";
+    const THIRD_COMMAND = "info contract";
+
+    it(`should has correct commands in namespace`, async () => {
+      const command = await yargs.command(require("../cmds/info")).help();
+
+      // Run the command module with --help as argument
+      const output = await new Promise((resolve) => {
+        command.parse(["info", "--help"], (_err, _argv, output) => {
+          console.log("output", output);
+          resolve(output);
+        });
+      });
+
+      expect(output).toContain(FIRST_COMMAND);
+      expect(output).toContain(SECOND_COMMAND);
+      expect(output).toContain(THIRD_COMMAND);
+    });
+  });
+});
