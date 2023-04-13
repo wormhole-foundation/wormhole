f163f965b10eaf2467922eb19c80e6665029303f
Joe Howarth <jhowarth@jumptrading.com>
2023-04-13 17:31:42 +0000

    deployment fixes

--- ethereum/ts-scripts/relayer/shell/deployConfigureTest.sh
+++ ethereum/ts-scripts/relayer/shell/deployConfigureTest.sh
@@ -1,8 +1,8 @@
-ts-node ./ts-scripts/relayer/config/checkNetworks.ts \
-  && ts-node ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
-  && ts-node ./ts-scripts/relayer/coreRelayer/deployCoreRelayer.ts \
-  && ts-node ./ts-scripts/relayer/coreRelayer/registerChainsCoreRelayerSelfSign.ts \
-  && ts-node ./ts-scripts/relayer/relayProvider/configureRelayProvider.ts \
-  && ts-node ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
-  && ts-node ./ts-scripts/relayer/mockIntegration/messageTest.ts \
-  && ts-node ./ts-scripts/relayer/config/syncContractsJson.ts
+npx ts-node ./ts-scripts/relayer/config/checkNetworks.ts --last-run=true \
+  && npx ts-node ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
+  && npx ts-node ./ts-scripts/relayer/coreRelayer/deployCoreRelayer.ts \
+  && npx ts-node ./ts-scripts/relayer/coreRelayer/registerChainsCoreRelayerSelfSign.ts \
+  && npx ts-node ./ts-scripts/relayer/relayProvider/configureRelayProvider.ts \
+  && npx ts-node ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
+  && npx ts-node ./ts-scripts/relayer/mockIntegration/messageTest.ts \
+  && npx ts-node ./ts-scripts/relayer/config/syncContractsJson.ts
