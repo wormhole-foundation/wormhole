edde738572d6ba91f220b33cd00c9ba324ff4c15
chase-45 <chasemoran45@gmail.com>
2023-04-17 15:57:14 -0400

    fixing sdk build errors

--- sdk/js/scripts/copyContracts.js
+++ sdk/js/scripts/copyContracts.js
@@ -1,2 +1,4 @@
 const copydir = require("copy-dir");
+const fs = require("fs");
+fs.rmSync("src/ethers-contracts", { recursive: true, force: true });
 copydir.sync("../../ethereum/ethers-contracts", "src/ethers-contracts");
