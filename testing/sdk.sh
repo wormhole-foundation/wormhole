9dd4b7c67e997c07340f6cc81c0bdd8ec903582f
scnale <sebinale@gmail.com>
2023-04-21 18:49:39 -0300

    relayer: fix several ci issues (#2745)
    * Installs forge dependencies in `eth-node` image.
    
    * changes sdk.sh to use bash
    
    * removing relayer tests from SDK image
    
    * relayer: deployment script can self-sign with multiple signers
    
    * relayer: update dockerignore
    
    ---------
    
    Co-authored-by: chase-45 <chasemoran45@gmail.com>
    Co-authored-by: Evan Gray <battledingo@gmail.com>
    Co-authored-by: Joe Howarth <jhowarth@jumptrading.com>

--- testing/sdk.sh
+++ testing/sdk.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' guardian:6060/readyz)" != "200" ]]; do sleep 5; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' spy:6060/metrics)" != "200" ]]; do sleep 5; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' ibc-relayer:7597/debug/pprof/)" != "200" ]]; do sleep 5; done
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' relayer-engine:3000/metrics)" != "200" ]]; do sleep 5; done
 CI=true npm --prefix ../sdk/js run test-ci
-CI=true npm --prefix ../ethereum run relayer-ci-test
