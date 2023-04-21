861c5c2e3ca9807642d2e750e3ae1172b4bada1f
Joe Howarth <jhowarth@jumptrading.com>
2023-04-21 17:23:12 +0000

    forge tests work

--- ethereum/contracts/relayer/coreRelayer/CoreRelayerProxy.sol
+++ ethereum/contracts/relayer/coreRelayer/CoreRelayerProxy.sol
@@ -6,7 +6,5 @@ import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "forge-std/console.sol";
 
 contract CoreRelayerProxy is ERC1967Proxy {
-    constructor(address implementation) ERC1967Proxy(implementation, new bytes(0)) {
-        console.log(implementation);
-    }
+    constructor(address implementation) ERC1967Proxy(implementation, new bytes(0)) {}
 }
