3f8d7903add0e9397ac4d8f47514848a72d7c1f6
chase-45 <chasemoran45@gmail.com>
2023-04-21 07:57:31 -0400

    removing relayer tests from SDK image

--- testing/sdk.sh
+++ testing/sdk.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -ex
+set -e
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' guardian:6060/readyz)" != "200" ]]; do sleep 5; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' spy:6060/metrics)" != "200" ]]; do sleep 5; done
 echo "guardian and spy are ready, running tests"
 CI=true npm --prefix ../sdk/js run test-ci
-CI=true npm --prefix ../ethereum run relayer-ci-test
