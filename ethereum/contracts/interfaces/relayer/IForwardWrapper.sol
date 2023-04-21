5dc042f232d47441dacfe7b7bade332011cef405
chase-45 <chasemoran45@gmail.com>
2023-04-21 17:04:38 -0400

    relayer: other file changes for code moving

--- ethereum/contracts/interfaces/relayer/IForwardWrapper.sol
+++ ethereum/contracts/interfaces/relayer/IForwardWrapper.sol
@@ -18,41 +18,4 @@ interface IForwardWrapper {
         view
         returns (bool isSupported);
 
-    function parseUpgrade(bytes memory encodedUpgrade, bytes32 module)
-        external
-        pure
-        returns (ContractUpgrade memory cu);
-
-    function parseRegisterChain(bytes memory encodedRegistration, bytes32 module)
-        external
-        pure
-        returns (RegisterChain memory registerChain);
-
-    function parseUpdateDefaultProvider(bytes memory encodedDefaultProvider, bytes32 module)
-        external
-        pure
-        returns (UpdateDefaultProvider memory defaultProvider);
-
-    struct ContractUpgrade {
-        bytes32 module;
-        uint8 action;
-        uint16 chain;
-        address newContract;
-    }
-
-    struct RegisterChain {
-        bytes32 module;
-        uint8 action;
-        uint16 chain; //TODO Why is this on this object?
-        uint16 emitterChain;
-        bytes32 emitterAddress;
-    }
-
-    //This could potentially be combined with ContractUpgrade
-    struct UpdateDefaultProvider {
-        bytes32 module;
-        uint8 action;
-        uint16 chain;
-        address newProvider;
-    }
 }
