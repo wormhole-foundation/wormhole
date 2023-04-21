4a097e123569c6c358ddff21df74d4ed08db4652
Joe Howarth <jhowarth@jumptrading.com>
2023-04-21 18:32:38 +0000

    fix salt

--- ethereum/ts-scripts/relayer/shell/deployConfigureTest.sh
+++ ethereum/ts-scripts/relayer/shell/deployConfigureTest.sh
@@ -1,7 +1,8 @@
-npx ts-node ./ts-scripts/relayer/config/checkNetworks.ts --last-run=true \
-  && npx ts-node ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
+npx ts-node ./ts-scripts/relayer/config/checkNetworks.ts --set-last-run \
+  && npx ts-node ./ts-scripts/relayer/create2Factory/deployCreate2Factory.ts \
   && npx ts-node ./ts-scripts/relayer/coreRelayer/deployCoreRelayer.ts \
   && npx ts-node ./ts-scripts/relayer/coreRelayer/registerChainsCoreRelayerSelfSign.ts \
+  && npx ts-node ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
   && npx ts-node ./ts-scripts/relayer/relayProvider/configureRelayProvider.ts \
   && npx ts-node ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
   && npx ts-node ./ts-scripts/relayer/mockIntegration/messageTest.ts \
--- ethereum/ts-scripts/relayer/shell/deployInContainer.sh
+++ ethereum/ts-scripts/relayer/shell/deployInContainer.sh
@@ -1,5 +1,7 @@
  echo "deploying generic relayer contracts" \ 
-  npx ts-node ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
+  npx ts-node ./ts-scripts/relayer/config/checkNetworks.ts --set-last-run \
+  && npx ts-node ./ts-scripts/relayer/create2Factory/deployCreate2Factory.ts \
+  && npx ts-node ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
   && npx ts-node ./ts-scripts/relayer/coreRelayer/deployCoreRelayer.ts \
   && npx ts-node ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
   && npx ts-node ./ts-scripts/relayer/coreRelayer/registerChainsCoreRelayerSelfSign.ts \
