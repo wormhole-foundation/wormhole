731c3f195243a47eb7179f07fdb4b1aa8028288e
heyitaki <akshath@live.com>
2023-04-29 01:36:47 +0000

    sdk/js: revert createWrapped sui changes

---
+++ sdk/js/src/sui/publish.ts
@@ -0,0 +1,59 @@
+import {
+  fromB64,
+  normalizeSuiAddress,
+  normalizeSuiObjectId,
+  TransactionBlock,
+} from "@mysten/sui.js";
+import { SuiBuildOutput } from "./types";
+
+export const publishCoin = async (
+  coreBridgePackageId: string,
+  tokenBridgePackageId: string,
+  vaa: string,
+  signerAddress: string
+) => {
+  const build = getCoinBuildOutput(
+    coreBridgePackageId,
+    tokenBridgePackageId,
+    vaa
+  );
+  return publishPackage(build, signerAddress);
+};
+
+export const getCoinBuildOutput = (
+  coreBridgePackageId: string,
+  tokenBridgePackageId: string,
+  vaa: string
+): SuiBuildOutput => {
+  const bytecode =
+    "oRzrCwYAAAAKAQAIAggOAxYWBCwEBTAoB1iGAQjeAWAGvgLlAQqjBAUMqAQWAAMBCgELAgQAAAIAAgECAAMCDAEAAQAGAAEAAQgIAQEMAgkFBgADBwMEAQIDAgEHAggABwgBAAEIAAMJAAoCBwgBAQsCAQkAAQYIAQEFAQsCAQgAAgkABQRDT0lOCVR4Q29udGV4dBFXcmFwcGVkQXNzZXRTZXR1cARjb2luDmNyZWF0ZV93cmFwcGVkC2R1bW15X2ZpZWxkBGluaXQUcHJlcGFyZV9yZWdpc3RyYXRpb24PcHVibGljX3RyYW5zZmVyBnNlbmRlcgh0cmFuc2Zlcgp0eF9jb250ZXh0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAjVFfai/LOUUAUvqTWa3Rh8p5xhe/wYJfCRRBlwG9pGyCgLhAd8B" +
+    Buffer.from(vaa, "hex").toString("base64").replace(/\=+$/, "") +
+    "ACAQUBAAAAAAEJCwAHAAoBOAALAS4RAjgBAgA=";
+  return {
+    modules: [bytecode],
+    dependencies: [
+      normalizeSuiAddress("0x1"),
+      normalizeSuiAddress("0x2"),
+      tokenBridgePackageId,
+      coreBridgePackageId,
+    ],
+  };
+};
+
+export const publishPackage = async (
+  buildOutput: SuiBuildOutput,
+  signerAddress: string
+): Promise<TransactionBlock> => {
+  // Publish contracts
+  const tx = new TransactionBlock();
+  const [upgradeCap] = tx.publish({
+    modules: buildOutput.modules.map((m: string) => Array.from(fromB64(m))),
+    dependencies: buildOutput.dependencies.map((d: string) =>
+      normalizeSuiObjectId(d)
+    ),
+  });
+
+  // Transfer upgrade capability to recipient
+  tx.transferObjects([upgradeCap], tx.pure(signerAddress));
+  return tx;
+};
--- sdk/js/src/sui/types.ts
+++ sdk/js/src/sui/types.ts
@@ -1,3 +1,27 @@
+export type GithubTreeResponse = {
+  sha: string;
+  url: string;
+  tree: {
+    path: string;
+    mode: string;
+    type: string;
+    sha: string;
+    size: number;
+    url: string;
+  }[];
+  truncated: boolean;
+};
+
+export type ParsedMoveToml = {
+  name: string;
+  rows: { key: string; value: string }[];
+}[];
+
+export type SuiBuildOutput = {
+  modules: string[];
+  dependencies: string[];
+};
+
 export type SuiError = {
   code: number;
   message: string;
