398652ea77a541a5c10502b50437e26266ae295c
Joe Howarth <jhowarth@jumptrading.com>
2023-05-05 14:48:38 -0400

    relayer: use tsx instead of ts-node for faster script start time (#2880)

--- ethereum/ts-scripts/relayer/shell/deployInContainer.sh
+++ ethereum/ts-scripts/relayer/shell/deployInContainer.sh
@@ -1,7 +1,7 @@
  echo "deploying generic relayer contracts" \ 
-  npx ts-node ./ts-scripts/relayer/create2Factory/deployCreate2Factory.ts \
-  && npx ts-node ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
-  && npx ts-node ./ts-scripts/relayer/coreRelayer/deployCoreRelayer.ts \
-  && npx ts-node ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
-  && npx ts-node ./ts-scripts/relayer/coreRelayer/registerChainsCoreRelayerSelfSign.ts \
-  && npx ts-node ./ts-scripts/relayer/relayProvider/configureRelayProvider.ts \
+  npx tsx ./ts-scripts/relayer/create2Factory/deployCreate2Factory.ts \
+  && npx tsx ./ts-scripts/relayer/relayProvider/deployRelayProvider.ts \
+  && npx tsx ./ts-scripts/relayer/coreRelayer/deployCoreRelayer.ts \
+  && npx tsx ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
+  && npx tsx ./ts-scripts/relayer/coreRelayer/registerChainsCoreRelayerSelfSign.ts \
+  && npx tsx ./ts-scripts/relayer/relayProvider/configureRelayProvider.ts \
