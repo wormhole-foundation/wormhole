b841b3b7571adf58e856e0259a9f162dab6bde02
Evan Gray <battledingo@gmail.com>
2023-05-05 13:19:39 -0400

    clients/js: suppress lib warnings

--- clients/js/src/main.ts
+++ clients/js/src/main.ts
@@ -1,24 +1,9 @@
 #!/usr/bin/env node
-
-// <sigh>
-// when the native secp256k1 is missing, the eccrypto library decides TO PRINT A MESSAGE TO STDOUT:
-// https://github.com/bitchan/eccrypto/blob/a4f4a5f85ef5aa1776dfa1b7801cad808264a19c/index.js#L23
-//
-// do you use a CLI tool that depends on that library and try to pipe the output
-// of the tool into another? tough luck
-//
-// for lack of a better way to stop this, we patch the console.info function to
-// drop that particular message...
-// </sigh>
-const infoTemp = console.info;
-console.info = function (x: string) {
-  if (x != "secp256k1 unavailable, reverting to browser version") {
-    infoTemp(x);
-  }
-};
-
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+// Quiet is here so that it can trigger before the afflicted libraries' on-import warnings can be emitted.
+// It is also imported so that it can side-effect without being tree-shaken.
+import "./quiet";
 // https://github.com/yargs/yargs/blob/main/docs/advanced.md#example-command-hierarchy-using-indexmjs
 import * as aptos from "./cmds/aptos";
 import * as chainId from "./cmds/chainId";
