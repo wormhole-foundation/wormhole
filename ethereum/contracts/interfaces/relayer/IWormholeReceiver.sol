ad753fe48b5133ab01f5d92c387e36255c140f15
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-05-09 16:03:33 +0000

    gRelayer: nits

--- ethereum/contracts/interfaces/relayer/IWormholeReceiver.sol
+++ ethereum/contracts/interfaces/relayer/IWormholeReceiver.sol
@@ -1,20 +1,18 @@
 // SPDX-License-Identifier: Apache 2
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.0;
 
 /**
  * @notice DeliveryData is the struct that the relay provider passes into 'deliver' containing an
  *     array of the signed wormhole messages that are to be relayed
  *
  * @custom:member sourceAddress - the (wormhole format) address on the sending chain which requested
- *     this delivery. Any address is able to initiate a delivery to anywhere else.
+ *     this delivery.
  * @custom:member sourceChainId - the wormhole chain ID where this delivery was requested.
  * @custom:member maximumRefund - the maximum refund that can possibly be awarded at the end of this
  *     delivery, assuming no gas is used by receiveWormholeMessages.
- * @custom:member deliveryHash - the VAA hash of the deliveryVAA. If you do not want to potentially
- *     process this delivery multiple times, you should store this hash in state for replay
- *     protection.
- * @custom:member payload - an optional arbitrary message which was included in the delivery by the
+ * @custom:member deliveryHash - the VAA hash of the deliveryVAA. 
+ * @custom:member payload - an arbitrary message which was included in the delivery by the
  *     requester.
  */
 struct DeliveryData {
@@ -26,40 +24,27 @@ struct DeliveryData {
 }
 
 interface IWormholeReceiver {
-  /**
-   * @notice When a 'send' or 'forward' is performed with this contract as the target, this function
-   *           will be invoked by the WormholeDelivery contract.
-   *
-   * NOTE: This function should be restricted such that only getDeliveryAddress() can call it.
-   *
-   * Deliveries should be expected to be performed *at least once*, and potentially multiple times.
-   * NOTE: This function should also only be callable by the WormholeDelivery address for the chain
-   *     it's deployed on, otherwise callers could potentially bypass the delivery logic, which
-   *     would prevent the refund and forwarding mechanisms from functioning.
-   *
-   * The invocation of this function corresponding to the 'send' request will have msg.value equal
-   *   to the receiverValue specified in the send request.
-   *
-   * If the invocation of this function reverts or exceeds the gasLimit (maxTransactionFee)
-   *   specified by the send requester, this delivery will result in a ReceiverFailure.
-   * //TODO AMO: duplicated documentation below
-   * @param deliveryData - This struct contains information about the delivery which is being
-   *      performed.
-   * - sourceAddress - the (wormhole format) address on the sending chain which requested this
-   *     delivery. Any address is able to initiate a delivery to anywhere else.
-   * - sourceChainId - the wormhole chain ID where this delivery was requested.
-   * - maximumRefund - the maximum refund that can possibly be awarded at the end of this delivery,
-   *     assuming no gas is used by receiveWormholeMessages.
-   * - deliveryHash - the VAA hash of the deliveryVAA. If you do not want to potentially process
-   *     this delivery multiple times, you should store this hash in state for replay protection.
-   * - payload - an optional arbitrary message which was included in the delivery by the requester.
-   * @param signedVaas - Additional VAAs which were requested to be included in this delivery. They
-   *     are guaranteed to all be included and in the same order as was specified in the delivery
-   *     request.
-   * NOTE: These signedVaas are NOT verified by the Wormhole core contract prior to being provided
-   *     to this call. Always make sure parseAndVerify is called on the Wormhole core contract
-   *     before trusting the content of a raw VAA, otherwise the VAA may be invalid or malicious.
-   */
+   /**
+     * @notice When a 'send' is performed with this contract as the target, this function will be invoked.
+     * To get the address that will invoke this contract, call the 'getDeliveryAddress()' function on this chain (the target chain)'s WormholeRelayer contract
+     *
+     * NOTE: This function should be restricted such that only getDeliveryAddress() can call it.
+     *
+     * We also recommend that this function:
+     *      - Stores all received 'deliveryData.deliveryHash's in a mapping (bytes32 => bool), and on every call, checks that deliveryData.deliveryHash has not already been stored in the map
+     *        (This is to prevent other users maliciously trying to relay the same message)
+     *      - Checks that 'deliveryData.sourceChain' and 'deliveryData.sourceAddress' are indeed who you expect to have requested the calling of 'send' or 'forward' on the source chain
+     *
+     * The invocation of this function corresponding to the 'send' request will have msg.value equal to the receiverValue specified in the send request.
+     *
+     * If the invocation of this function reverts or exceeds the gasLimit (maxTransactionFee) specified by the send requester, this delivery will result in a ReceiverFailure.
+     *
+     * @param deliveryData - This struct contains information about the delivery which is being performed
+     * @param signedVaas - Additional VAAs which were requested to be included in this delivery. They are guaranteed to all be included and in the same order as was specified in the delivery request.
+     * NOTE: These signedVaas are NOT verified by the Wormhole core contract prior to being provided to this call.
+     * Always make sure parseAndVerify is called on the Wormhole core contract before trusting the content of a raw VAA,
+     * otherwise the VAA may be invalid or malicious.
+     */
   function receiveWormholeMessages(
     DeliveryData memory deliveryData,
     bytes[] memory signedVaas
