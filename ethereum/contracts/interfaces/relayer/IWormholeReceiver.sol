06dfb32cfd509b8e7bed5fe9326454aff448736d
nonergodic <nonergodic@gmail.com>
2023-05-16 00:13:56 -0700

    improve comment uniformity

--- ethereum/contracts/interfaces/relayer/IWormholeReceiver.sol
+++ ethereum/contracts/interfaces/relayer/IWormholeReceiver.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 /**
- * @notice DeliveryData is the struct that the relay provider passes into 'deliver' containing an
+ * @notice DeliveryData is the struct that the relay provider passes into `deliver()` containing an
  *     array of the signed wormhole messages that are to be relayed
  *
  * @custom:member sourceAddress - the (wormhole format) address on the sending chain which requested
@@ -25,25 +25,34 @@ struct DeliveryData {
 
 interface IWormholeReceiver {
    /**
-     * @notice When a 'send' is performed with this contract as the target, this function will be invoked.
-     * To get the address that will invoke this contract, call the 'getDeliveryAddress()' function on this chain (the target chain)'s WormholeRelayer contract
+     * @notice When a `send` is performed with this contract as the target, this function will be
+     *     invoked.
+     *   To get the address that will invoke this contract, call the `getDeliveryAddress()` function
+     *     on this chain (the target chain)'s WormholeRelayer contract
      *
-     * NOTE: This function should be restricted such that only getDeliveryAddress() can call it.
+     * NOTE: This function should be restricted such that only `getDeliveryAddress()` can call it.
      *
      * We also recommend that this function:
-     *      - Stores all received 'deliveryData.deliveryHash's in a mapping (bytes32 => bool), and on every call, checks that deliveryData.deliveryHash has not already been stored in the map
-     *        (This is to prevent other users maliciously trying to relay the same message)
-     *      - Checks that 'deliveryData.sourceChain' and 'deliveryData.sourceAddress' are indeed who you expect to have requested the calling of 'send' or 'forward' on the source chain
+     *   - Stores all received `deliveryData.deliveryHash`s in a mapping `(bytes32 => bool)`, and
+     *       on every call, checks that deliveryData.deliveryHash has not already been stored in the
+     *       map (This is to prevent other users maliciously trying to relay the same message)
+     *   - Checks that `deliveryData.sourceChain` and `deliveryData.sourceAddress` are indeed who
+     *       you expect to have requested the calling of `send` or `forward` on the source chain
      *
-     * The invocation of this function corresponding to the 'send' request will have msg.value equal to the receiverValue specified in the send request.
+     * The invocation of this function corresponding to the `send` request will have msg.value equal
+     *   to the receiverValue specified in the send request.
      *
-     * If the invocation of this function reverts or exceeds the gasLimit (maxTransactionFee) specified by the send requester, this delivery will result in a ReceiverFailure.
+     * If the invocation of this function reverts or exceeds the gas limit (`maxTransactionFee`)
+     *   specified by the send requester, this delivery will result in a `ReceiverFailure`.
      *
-     * @param deliveryData - This struct contains information about the delivery which is being performed
-     * @param signedVaas - Additional VAAs which were requested to be included in this delivery. They are guaranteed to all be included and in the same order as was specified in the delivery request.
-     * NOTE: These signedVaas are NOT verified by the Wormhole core contract prior to being provided to this call.
-     * Always make sure parseAndVerify is called on the Wormhole core contract before trusting the content of a raw VAA,
-     * otherwise the VAA may be invalid or malicious.
+     * @param deliveryData - This struct contains information about the delivery which is being
+     *     performed
+     * @param signedVaas - Additional VAAs which were requested to be included in this delivery.
+     *   They are guaranteed to all be included and in the same order as was specified in the
+     *     delivery request.
+     * NOTE: These signedVaas are NOT verified by the Wormhole core contract prior to being provided
+     *     to this call. Always make sure `parseAndVerify()` is called on the Wormhole core contract
+     *     before trusting the content of a raw VAA, otherwise the VAA may be invalid or malicious.
      */
   function receiveWormholeMessages(
     DeliveryData memory deliveryData,
