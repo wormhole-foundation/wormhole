8f133806c635999a32efa6bd2e5c7be862997cb8
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-05-17 14:37:18 +0000

    Wait until relayer engine ready to run SDK tests

--- testing/sdk.sh
+++ testing/sdk.sh
@@ -2,5 +2,6 @@
 set -e
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' guardian:6060/readyz)" != "200" ]]; do sleep 5; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' spy:6060/metrics)" != "200" ]]; do sleep 5; done
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' relayer-engine:3000/metrics)" != "200" ]]; do sleep 5; done
 echo "guardian and spy are ready, running tests"
 CI=true npm --prefix ../sdk/js run test-ci
