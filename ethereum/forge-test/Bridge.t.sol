ca2f8ce283f440375eab7bcaf8fccb273e15dc81
Jeff Schroeder <jeffschroeder@computer.org>
2023-05-17 13:58:59 +0000

    ethereum: change inheritance for BridgeTest
    The BridgeTest contract inherited from both Bridge and forge-std's
    Test contracts. This was fine with the ancient version of
    forge-std it was using, but newer forge-std has an isFork() from
    foundry-rs/forge-std#236. This makes upgrading forge-std to take
    advantage of new features like improved invariant testing or smart
    fuzzing impossibru.
    
    This makes it a bit cleaner and also fixes some of the logic in the
    testTruncate to not be undefined.
    
    Thanks @scnale for the assist on this one!

--- ethereum/forge-test/Bridge.t.sol
+++ ethereum/forge-test/Bridge.t.sol
@@ -5,32 +5,57 @@ pragma solidity ^0.8.0;
 import "../contracts/bridge/Bridge.sol";
 import "forge-std/Test.sol";
 
-contract TestBridge is Bridge, Test {
+// @dev ensure some internal methods are public for testing
+contract ExportedBridge is Bridge {
+    function _truncateAddressPub(bytes32 b) public pure returns (address) {
+        return super._truncateAddress(b);
+    }
+
+    function setChainIdPub(uint16 chainId) public {
+        return super.setChainId(chainId);
+    }
+
+    function setEvmChainIdPub(uint256 evmChainId) public {
+        return super.setEvmChainId(evmChainId);
+    }
+}
+
+contract TestBridge is Test {
+    ExportedBridge bridge;
+
+    function setUp() public {
+        bridge = new ExportedBridge();
+    }
+
     function testTruncate(bytes32 b) public {
-        if (bytes12(b) != 0) {
+        bool invalidAddress = bytes12(b) != 0;
+        if (invalidAddress) {
             vm.expectRevert( "invalid EVM address");
         }
-        bytes32 converted = bytes32(uint256(uint160(bytes20(_truncateAddress(b)))));
-        require(converted == b, "truncate does not roundrip");
+        bytes32 converted = bytes32(uint256(uint160(bytes20(bridge._truncateAddressPub(b)))));
+
+        if (!invalidAddress) {
+            require(converted == b, "truncate does not roundrip");
+        }
     }
 
     function testEvmChainId() public {
         vm.chainId(1);
-        setChainId(1);
-        setEvmChainId(1);
-        assertEq(chainId(), 1);
-        assertEq(evmChainId(), 1);
+        bridge.setChainIdPub(1);
+        bridge.setEvmChainIdPub(1);
+        assertEq(bridge.chainId(), 1);
+        assertEq(bridge.evmChainId(), 1);
 
         // fork occurs, block.chainid changes
         vm.chainId(10001);
 
-        setEvmChainId(10001);
-        assertEq(chainId(), 1);
-        assertEq(evmChainId(), 10001);
+        bridge.setEvmChainIdPub(10001);
+        assertEq(bridge.chainId(), 1);
+        assertEq(bridge.evmChainId(), 10001);
 
         // evmChainId must equal block.chainid
         vm.expectRevert("invalid evmChainId");
-        setEvmChainId(1337);
+        bridge.setEvmChainIdPub(1337);
 
     }
 }
