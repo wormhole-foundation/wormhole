d7a25f43e97cd4026759d199f0d43f77aa91e465
Evan Gray <battledingo@gmail.com>
2023-05-21 21:42:33 +0000

    WIP: CCQ add block to call

--- node/cmd/guardiand/node.go
+++ node/cmd/guardiand/node.go
@@ -412,6 +412,7 @@ var Build = "prod"
 
 // observationRequestBufferSize is the buffer size of the per-network reobservation channel
 const observationRequestBufferSize = 25
+
 // queryRequestBufferSize is the buffer size of the per-network reobservation channel
 const queryRequestBufferSize = 25
 
@@ -1549,7 +1550,7 @@ func runNode(cmd *cobra.Command, args []string) {
 		}
 
 		go handleReobservationRequests(rootCtx, clock.New(), logger, obsvReqReadC, chainObsvReqC)
-		go handleQueryRequests(rootCtx, clock.New(), logger, signedQueryReqReadC, chainQueryReqC)
+		go handleQueryRequests(rootCtx, logger, signedQueryReqReadC, chainQueryReqC)
 
 		if acct != nil {
 			if err := acct.Start(ctx); err != nil {
