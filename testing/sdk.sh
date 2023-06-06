5c37e92fa19bbdbefc79c8ee0dbceeb127c53373
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-06-06 14:19:42 +0000

    bash -> sh

--- testing/sdk.sh
+++ testing/sdk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' guardian:6060/readyz)" != "200" ]]; do sleep 5; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' spy:6060/metrics)" != "200" ]]; do sleep 5; done
