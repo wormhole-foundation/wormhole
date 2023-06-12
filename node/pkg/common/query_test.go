3afdb625e81d1d77a1aa9cf4802ed03a15119ff0
bruce-riley <96066700+bruce-riley@users.noreply.github.com>
2023-06-12 13:14:45 -0500

    CCQ: Batch queries (#3023)
    * CCQ: Add marshalling tests
    
    * More marshalling changes
    
    * Start of support for multiple calls in a batch
    
    * Multiple responses to go with multiple requests
    
    * Support multiple EVM queries in a batch
    
    * Multiple queries in a batch
    
    * Start of handler tests
    
    * More handler tests
    
    * More handler tests
    
    * node: add multi batch query tests
    
    * Added comments to tests
    
    * Per chain query time out not getting retried
    
    * node: chang rand package
    
    * Tweak serialization of per call request
    
    ---------
    
    Co-authored-by: Paul Noel <panoel007@gmail.com>

---
+++ node/pkg/common/query_test.go
@@ -0,0 +1,181 @@
+package common
+
+import (
+	"encoding/hex"
+	"math/big"
+	"strings"
+	"testing"
+	"time"
+
+	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	ethCommon "github.com/ethereum/go-ethereum/common"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func createQueryRequestForTesting() *gossipv1.QueryRequest {
+	// Create a query request.
+	wethAbi, err := abi.JSON(strings.NewReader("[{\"constant\":true,\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"))
+	if err != nil {
+		panic(err)
+	}
+
+	data1, err := wethAbi.Pack("name")
+	if err != nil {
+		panic(err)
+	}
+	data2, err := wethAbi.Pack("totalSupply")
+	if err != nil {
+		panic(err)
+	}
+
+	to, _ := hex.DecodeString("0d500b1d8e8ef31e21c99d1db9a6444d3adf1270")
+	block := "0x28d9630"
+	callData := []*gossipv1.EthCallQueryRequest_EthCallData{
+		{
+			To:   to,
+			Data: data1,
+		},
+		{
+			To:   to,
+			Data: data2,
+		},
+	}
+	callRequest := &gossipv1.EthCallQueryRequest{
+		Block:    block,
+		CallData: callData,
+	}
+
+	perChainQuery := &gossipv1.PerChainQueryRequest{
+		ChainId: 5,
+		Message: &gossipv1.PerChainQueryRequest_EthCallQueryRequest{
+			EthCallQueryRequest: callRequest,
+		},
+	}
+
+	queryRequest := &gossipv1.QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*gossipv1.PerChainQueryRequest{perChainQuery},
+	}
+
+	return queryRequest
+}
+
+// A timestamp has nanos, but we only marshal down to micros, so trim our time to micros for testing purposes.
+func timeForTest(t time.Time) time.Time {
+	return time.UnixMicro(t.UnixMicro())
+}
+
+func TestQueryRequestProtoMarshalUnMarshal(t *testing.T) {
+	queryRequest := createQueryRequestForTesting()
+	queryRequestBytes, err := proto.Marshal(queryRequest)
+	require.NoError(t, err)
+
+	var queryRequest2 gossipv1.QueryRequest
+	err = proto.Unmarshal(queryRequestBytes, &queryRequest2)
+	require.NoError(t, err)
+
+	assert.True(t, QueryRequestEqual(queryRequest, &queryRequest2))
+}
+
+func TestQueryRequestMarshalUnMarshal(t *testing.T) {
+	queryRequest := createQueryRequestForTesting()
+	queryRequestBytes, err := MarshalQueryRequest(queryRequest)
+	require.NoError(t, err)
+
+	queryRequest2, err := UnmarshalQueryRequest(queryRequestBytes)
+	require.NoError(t, err)
+
+	assert.True(t, QueryRequestEqual(queryRequest, queryRequest2))
+}
+
+func TestQueryResponseMarshalUnMarshal(t *testing.T) {
+	queryRequest := createQueryRequestForTesting()
+	queryRequestBytes, err := proto.Marshal(queryRequest)
+	require.NoError(t, err)
+
+	sig := [65]byte{}
+	signedQueryRequest := &gossipv1.SignedQueryRequest{
+		QueryRequest: queryRequestBytes,
+		Signature:    sig[:],
+	}
+
+	results, err := hex.DecodeString("010203040506070809")
+	require.NoError(t, err)
+
+	respPub := &QueryResponsePublication{
+		Request: signedQueryRequest,
+		PerChainResponses: []PerChainQueryResponse{
+			{
+				ChainID: 5,
+				Responses: []EthCallQueryResponse{
+					{
+						Number: big.NewInt(42),
+						Hash:   ethCommon.HexToHash("0x9999bac44d09a7f69ee7941819b0a19c59ccb1969640cc513be09ef95ed2d8e2"),
+						Time:   timeForTest(time.Now()),
+						Result: results,
+					},
+					{
+						Number: big.NewInt(43),
+						Hash:   ethCommon.HexToHash("0x9999bac44d09a7f69ee7941819b0a19c59ccb1969640cc513be09ef9deadbeef"),
+						Time:   timeForTest(time.Now()),
+						Result: results,
+					},
+				},
+			},
+			{
+				ChainID: 11,
+				Responses: []EthCallQueryResponse{
+					{
+						Number: big.NewInt(44),
+						Hash:   ethCommon.HexToHash("0x9999bac44d09a7f69ee7941819b0a19c59ccb1969640cc513be09ef95ed2d8e3"),
+						Time:   timeForTest(time.Now()),
+						Result: results,
+					},
+				},
+			},
+		},
+	}
+
+	respPubBytes, err := MarshalQueryResponsePublication(respPub)
+	require.NoError(t, err)
+
+	respPub2, err := UnmarshalQueryResponsePublication(respPubBytes)
+	require.NoError(t, err)
+	require.NotNil(t, respPub2)
+
+	assert.True(t, respPub.Equal(respPub2))
+}
+
+/*
+func TesMarshalUnMarshalQueryResponseWithNoResults(t *testing.T) {
+	queryRequest := createQueryRequestForTesting()
+	queryRequestBytes, err := proto.Marshal(queryRequest)
+	require.NoError(t, err)
+
+	sig := [65]byte{}
+	signedQueryRequest := &gossipv1.SignedQueryRequest{
+		QueryRequest: queryRequestBytes,
+		Signature:    sig[:],
+	}
+
+	respPub := &QueryResponsePublication{
+		Request:   signedQueryRequest,
+		Responses: nil,
+	}
+
+	respPubBytes, err := MarshalQueryResponsePublication(respPub)
+	require.NoError(t, err)
+
+	respPub2, err := UnmarshalQueryResponsePublication(respPubBytes)
+	require.NoError(t, err)
+	require.NotNil(t, respPub2)
+
+	assert.True(t, respPub.Equal(respPub2))
+}
+*/
