6f0341953e412c55b3f191f473d957ef87b3a7ef
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-06-13 14:43:06 -0400

    add hardcoded block json payload for testing, remove dep on external api

--- node/pkg/watchers/algorand/watcher_test.go
+++ node/pkg/watchers/algorand/watcher_test.go
@@ -1,83 +1,62 @@
 package algorand
 
 import (
-	"context"
-	"sync"
+	"encoding/base64"
+	"encoding/json"
+	"os"
 	"testing"
 
-	"github.com/algorand/go-algorand-sdk/client/v2/algod"
+	"github.com/algorand/go-algorand-sdk/types"
 	"github.com/certusone/wormhole/node/pkg/common"
 	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
 	"go.uber.org/zap"
 )
 
-// TODO: grab these from const file somewhere?
-const (
-	IDX_URL   = "https://testnet-idx.algonode.cloud"
-	ALGOD_URL = "https://testnet-api.algonode.cloud"
+const APP_ID = 86525623
 
-	APP_ID = 86525623
-)
+// Tests for nested inner transactions calling the core bridge
+func TestLookAtTxnInnerTxn(t *testing.T) {
+	// Setup a watcher
+	msgC := make(chan *common.MessagePublication)
+	obsvReqC := make(chan *gossipv1.ObservationRequest, 50)
+	w := NewWatcher("", "", "", "", APP_ID, msgC, obsvReqC)
 
-type testStruct struct {
-	block uint64
-	seq   uint64
-}
+	var expectedSequence uint64 = 993
 
-var testCases = []testStruct{
-	{block: 30453935, seq: 993},
-}
+	// read in test block for inner transactions
+	b, err := os.ReadFile("test_nested_inner.block.json")
+	if err != nil {
+		t.Fatalf("failed to read block file: %s", err)
+	}
 
-func TestLookAtTxn(t *testing.T) {
+	txn := types.SignedTxnInBlock{}
+	err = json.Unmarshal(b, &txn)
+	if err != nil {
+		t.Fatalf("failed to unmarshal block: %s", err)
+	}
 
+	// Because we are using a json blob and the type of logs array is []string
+	// and because go json package will refuse to properly encode/decode
+	// invalid utf8 characters, the json blob has the relevant log encoded as base64
+	// and we base64 decode it and convert it to a string _manually_ so we can
+	// make sure we got the right sequence number
+	b64Data := txn.EvalDelta.InnerTxns[2].EvalDelta.InnerTxns[0].EvalDelta.Logs[0]
+	bb, err := base64.StdEncoding.DecodeString(b64Data)
+	if err != nil {
+		t.Fatalf("Cant decode: %s", err)
+	}
+	txn.EvalDelta.InnerTxns[2].EvalDelta.InnerTxns[0].EvalDelta.Logs[0] = string(bb)
+
+	// for each tx in the block, check to see if its a valid
+	// wh emitted message
 	logger, _ := zap.NewProduction()
-	for _, tc := range testCases {
-		// Setup a watcher
-		msgC := make(chan *common.MessagePublication)
-		obsvReqC := make(chan *gossipv1.ObservationRequest, 50)
-		w := NewWatcher(IDX_URL, "", ALGOD_URL, "", APP_ID, msgC, obsvReqC)
+	observations := gatherObservations(w, txn.SignedTxnWithAD, 0, logger)
 
-		// grab client
-		algodClient, err := algod.MakeClient(w.algodRPC, w.algodToken)
-		if err != nil {
-			t.Fatalf("Failed to create client: %s", err)
-		}
+	if len(observations) != 1 {
+		t.Fatalf("expected 1 observation, got %d", len(observations))
+	}
 
-		// spin up goroutine to receive msgpub
-		var foundMsg *common.MessagePublication
-		wg := &sync.WaitGroup{}
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for x := range msgC {
-				foundMsg = x
-			}
-		}()
-
-		// grab the block
-		block, err := algodClient.Block(tc.block).Do(context.Background())
-		if err != nil {
-			t.Fatalf("Failed to get block: %s", err)
-		}
-
-		// for each tx in the block, check to see if its a valid
-		// wh emitted message
-		for _, element := range block.Payset {
-			lookAtTxn(w, element, block, logger)
-		}
-
-		// close the channel so the goroutine returns
-		close(msgC)
-		// wait for the goroutine to finish
-		wg.Wait()
-
-		// assertions
-		if foundMsg == nil {
-			t.Fatal("no message found")
-		}
-
-		if foundMsg.Sequence != tc.seq {
-			t.Fatalf("sequence did not match: %d vs %d", foundMsg.Sequence, tc.seq)
-		}
+	if observations[0].sequence != expectedSequence {
+		t.Fatalf("expected sequence observed to be %d, got %d", expectedSequence, observations[0].sequence)
 	}
 }
