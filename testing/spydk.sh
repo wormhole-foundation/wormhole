f0c8351e6f3fc1038c68a6b2c52f1cb47e15986a
Evan Gray <battledingo@gmail.com>
2023-06-16 08:59:36 -0400

    ci: pass num guardians to ci tests

--- testing/spydk.sh
+++ testing/spydk.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' guardian:6060/readyz)" != "200" ]]; do sleep 5; done
+num=${NUM_GUARDIANS:-1} # default value for NUM_GUARDIANS = 1
+for ((i=0; i<num; i++)); do
+    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' guardian-$i.guardian:6060/readyz)" != "200" ]]; do sleep 5; done
+done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' spy:6060/metrics)" != "200" ]]; do sleep 5; done
 CI=true npm --prefix ../spydk/js run test-ci
