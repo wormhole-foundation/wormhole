bff3d6b4f2b2a2efe061e27f5b9989c84ac26950
Sebastián Claudio Nale <sebinale@gmail.com>
2023-06-16 13:55:41 -0300

    gRelayer: separates out create2 factory deploy step.
    This allows using a separate account easily.

--- ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
+++ ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
@@ -1,7 +1,6 @@
 
 # This pipeline doesn't attempt to sign governance VAAs since those need to be signed by the guardians.
 npx tsx ./ts-scripts/relayer/config/checkNetworks.ts --set-last-run \
-  && npx tsx ./ts-scripts/relayer/create2Factory/deployCreate2Factory.ts \
   && npx tsx ./ts-scripts/relayer/deliveryProvider/deployDeliveryProvider.ts \
   && npx tsx ./ts-scripts/relayer/wormholeRelayer/deployWormholeRelayer.ts \
   && npx tsx ./ts-scripts/relayer/deliveryProvider/configureDeliveryProvider.ts \
