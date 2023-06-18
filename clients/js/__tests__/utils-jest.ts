d8e275ac63c0f6b1ef2d247a491b8dac8a64530e
Aber <arblugo@gmail.com>
2023-06-18 09:12:42 +0100

    feat: extract positional arguments checker function

--- clients/js/__tests__/utils-jest.ts
+++ clients/js/__tests__/utils-jest.ts
@@ -1,4 +1,5 @@
 import { execSync } from "child_process";
+import { expect, it } from "@jest/globals";
 
 export const get_WORM_CLI_PATH = () => {
   //This functions returns the machine path targeting to the generated `worm` command
@@ -12,4 +13,24 @@ export const run_worm_command = (commandArgs: string) => {
 
 export const run_worm_help_command = (commandArgs: string) => {
   return run_worm_command(`${commandArgs} --help`);
+};
+
+export const test_command_positional_args = (
+  command: string,
+  args: string[]
+) => {
+  //NOTE: Guard condition to avoid passing infered `worm` keyword from command input
+  if (command.includes("worm")) {
+    throw new Error(
+      "initial 'worm' keyword must be excluded from command params, pass only worm specific commands."
+    );
+  }
+
+  it(`should have correct positional arguments`, async () => {
+    // Run the command module with --help as argument
+    const output = run_worm_help_command(command);
+    const expectedPositionalArgs = args.map((arg) => `<${arg}>`).join(" ");
+
+    expect(output).toContain(`worm ${command} ${expectedPositionalArgs}`);
+  });
 };
