bf68b41f501394bc4d933c80ff3111b28e8f5830
Aber <arblugo@gmail.com>
2023-06-19 01:04:02 +0100

    feat: update worm recover tests

--- clients/js/__tests__/recover.test.ts
+++ clients/js/__tests__/recover.test.ts
@@ -1,44 +1,28 @@
-import yargs from "yargs";
-import { describe, expect, it, jest } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
+import { run_worm_command, test_command_positional_args } from "./utils/cli";
 
 describe("worm recover", () => {
   describe("check arguments", () => {
-    const FIRST_POSITIONAL_ARGUMENT = "<digest>";
-    const SECOND_POSITIONAL_ARGUMENT = "<signature>";
+    //Args must be defined in their specific order
+    const args = ["digest", "signature"];
 
-    it(`should have correct positional arguments`, async () => {
-      const command = await yargs
-        .command(require("../src/cmds/recover"))
-        .help();
-
-      // Run the command module with --help as argument
-      const output = await new Promise((resolve) => {
-        command.parse(["recover", "--help"], (_err, _argv, output) => {
-          console.log("output", output);
-          resolve(output);
-        });
-      });
-
-      expect(output).toContain(FIRST_POSITIONAL_ARGUMENT);
-      expect(output).toContain(SECOND_POSITIONAL_ARGUMENT);
-    });
+    test_command_positional_args("recover", args);
   });
 
   describe("check functionality", () => {
-    const MOCK_DIGEST =
+    //mocks
+    const digest =
       "0x99656f88302bda18573212d4812daeea7d39f8af695db1fbc4d99fd94f552606";
-    const MOCK_SIGNATURE =
+    const signature =
       "6da03c5e56cb15aeeceadc1e17a45753ab4dc0ec7bf6a75ca03143ed4a294f6f61bc3f478a457833e43084ecd7c985bf2f55a55f168aac0e030fc49e845e497101";
 
-    const EXPECTED_ADDRESS = "0x6FbEBc898F403E4773E95feB15E80C9A99c8348d";
+    //expected output
+    const expectedAddress = "0x6FbEBc898F403E4773E95feB15E80C9A99c8348d";
 
     it(`should return correct address from mock digest and signature`, async () => {
-      const consoleSpy = jest.spyOn(console, "log");
+      const output = run_worm_command(`recover ${digest} ${signature}`);
 
-      const command = yargs.command(require("../src/cmds/recover")).help();
-      await command.parse(["recover", MOCK_DIGEST, MOCK_SIGNATURE]);
-
-      expect(consoleSpy).toBeCalledWith(EXPECTED_ADDRESS);
+      expect(output).toContain(expectedAddress);
     });
   });
 });
