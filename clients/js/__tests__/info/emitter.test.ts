bbd0db028a9fd02090a69978859b94d223aa17cf
Aber <arblugo@gmail.com>
2023-06-19 23:05:34 +0100

    feat: add worm info emitter & origin tests

---
+++ clients/js/__tests__/info/emitter.test.ts
@@ -0,0 +1,11 @@
+import { describe } from "@jest/globals";
+import { test_command_positional_args } from "../utils/cli";
+
+describe("worm info emitter", () => {
+  describe("check arguments", () => {
+    //Args must be defined in their specific order
+    const args = ["chain", "address"];
+
+    test_command_positional_args("info emitter", args);
+  });
+});
