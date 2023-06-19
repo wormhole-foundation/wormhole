352d16094e209082f0cd0c7dae58ca9d8c4525ab
Aber <arblugo@gmail.com>
2023-06-19 23:05:52 +0100

    feat: add worm info registrations tests

---
+++ clients/js/__tests__/info/registrations.test.ts
@@ -0,0 +1,21 @@
+import { describe } from "@jest/globals";
+import {
+  Flag,
+  test_command_flags,
+  test_command_positional_args,
+} from "../utils/cli";
+
+describe("worm info registrations", () => {
+  describe("check arguments", () => {
+    //Args must be defined in their specific order
+    const args = ["network", "chain", "module"];
+
+    test_command_positional_args("info registrations", args);
+  });
+
+  describe("check flags", () => {
+    const flags: Flag[] = [{ name: "--verify", alias: "-v" }];
+
+    test_command_flags("info registrations", flags);
+  });
+});
