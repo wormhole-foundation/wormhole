143f6b6e689c574662df20a4135291cee7d457f1
bruce-riley <96066700+bruce-riley@users.noreply.github.com>
2023-06-21 12:07:02 -0500

    CCQ: Messaging changes (#3093)
    * CCQ: Messaging changes
    
    * Add tests for marshaling query request
    
    * Response changes
    
    * Rework response structs, add tests
    
    * Code review rework
    
    * Make block number uint64 internally

--- node/pkg/common/query_test.go
+++ node/pkg/common/query_test.go
@@ -2,23 +2,22 @@ package common
 
 import (
 	"encoding/hex"
-	"math/big"
+	"fmt"
 	"strings"
 	"testing"
 	"time"
 
 	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
+	"github.com/wormhole-foundation/wormhole/sdk/vaa"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	ethCommon "github.com/ethereum/go-ethereum/common"
-
-	"google.golang.org/protobuf/proto"
 )
 
-func createQueryRequestForTesting() *gossipv1.QueryRequest {
+func createQueryRequestForTesting(chainId vaa.ChainID) *QueryRequest {
 	// Create a query request.
 	wethAbi, err := abi.JSON(strings.NewReader("[{\"constant\":true,\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"))
 	if err != nil {
@@ -36,7 +35,7 @@ func createQueryRequestForTesting() *gossipv1.QueryRequest {
 
 	to, _ := hex.DecodeString("0d500b1d8e8ef31e21c99d1db9a6444d3adf1270")
 	block := "0x28d9630"
-	callData := []*gossipv1.EthCallQueryRequest_EthCallData{
+	callData := []*EthCallData{
 		{
 			To:   to,
 			Data: data1,
@@ -46,21 +45,19 @@ func createQueryRequestForTesting() *gossipv1.QueryRequest {
 			Data: data2,
 		},
 	}
-	callRequest := &gossipv1.EthCallQueryRequest{
-		Block:    block,
+	callRequest := &EthCallQueryRequest{
+		BlockId:  block,
 		CallData: callData,
 	}
 
-	perChainQuery := &gossipv1.PerChainQueryRequest{
-		ChainId: 5,
-		Message: &gossipv1.PerChainQueryRequest_EthCallQueryRequest{
-			EthCallQueryRequest: callRequest,
-		},
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: chainId,
+		Query:   callRequest,
 	}
 
-	queryRequest := &gossipv1.QueryRequest{
+	queryRequest := &QueryRequest{
 		Nonce:           1,
-		PerChainQueries: []*gossipv1.PerChainQueryRequest{perChainQuery},
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
 	}
 
 	return queryRequest
@@ -71,91 +68,352 @@ func timeForTest(t time.Time) time.Time {
 	return time.UnixMicro(t.UnixMicro())
 }
 
-func TestQueryRequestProtoMarshalUnMarshal(t *testing.T) {
-	queryRequest := createQueryRequestForTesting()
-	queryRequestBytes, err := proto.Marshal(queryRequest)
+func TestQueryRequestMarshalUnmarshal(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	queryRequestBytes, err := queryRequest.Marshal()
 	require.NoError(t, err)
 
-	var queryRequest2 gossipv1.QueryRequest
-	err = proto.Unmarshal(queryRequestBytes, &queryRequest2)
+	var queryRequest2 QueryRequest
+	err = queryRequest2.Unmarshal(queryRequestBytes)
 	require.NoError(t, err)
 
-	assert.True(t, QueryRequestEqual(queryRequest, &queryRequest2))
+	assert.True(t, queryRequest.Equal(&queryRequest2))
 }
 
-func TestQueryRequestMarshalUnMarshal(t *testing.T) {
-	queryRequest := createQueryRequestForTesting()
-	queryRequestBytes, err := MarshalQueryRequest(queryRequest)
-	require.NoError(t, err)
-
-	queryRequest2, err := UnmarshalQueryRequest(queryRequestBytes)
-	require.NoError(t, err)
-
-	assert.True(t, QueryRequestEqual(queryRequest, queryRequest2))
+func TestMarshalOfQueryRequestWithNoPerChainQueriesShouldFail(t *testing.T) {
+	queryRequest := &QueryRequest{
+		Nonce: 1,
+		PerChainQueries: []*PerChainQueryRequest{
+			{
+				ChainId: vaa.ChainIDPolygon,
+				// Leave Query nil.
+			},
+		},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
 }
 
-func TestQueryResponseMarshalUnMarshal(t *testing.T) {
-	queryRequest := createQueryRequestForTesting()
-	queryRequestBytes, err := proto.Marshal(queryRequest)
-	require.NoError(t, err)
+func TestMarshalOfQueryRequestWithTooManyPerChainQueriesShouldFail(t *testing.T) {
+	perChainQueries := []*PerChainQueryRequest{}
+	for count := 0; count < 300; count++ {
+		callData := []*EthCallData{{
 
-	sig := [65]byte{}
-	signedQueryRequest := &gossipv1.SignedQueryRequest{
-		QueryRequest: queryRequestBytes,
-		Signature:    sig[:],
+			To:   []byte(fmt.Sprintf("%-20s", fmt.Sprintf("To for %d", count))),
+			Data: []byte(fmt.Sprintf("CallData for %d", count)),
+		},
+		}
+
+		perChainQueries = append(perChainQueries, &PerChainQueryRequest{
+			ChainId: vaa.ChainIDPolygon,
+			Query: &EthCallQueryRequest{
+				BlockId:  "0x28d9630",
+				CallData: callData,
+			},
+		})
 	}
 
-	results, err := hex.DecodeString("010203040506070809")
-	require.NoError(t, err)
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: perChainQueries,
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
 
-	respPub := &QueryResponsePublication{
-		Request: signedQueryRequest,
-		PerChainResponses: []PerChainQueryResponse{
-			{
-				ChainID: 5,
-				Responses: []EthCallQueryResponse{
-					{
-						Number: big.NewInt(42),
-						Hash:   ethCommon.HexToHash("0x9999bac44d09a7f69ee7941819b0a19c59ccb1969640cc513be09ef95ed2d8e2"),
-						Time:   timeForTest(time.Now()),
-						Result: results,
-					},
-					{
-						Number: big.NewInt(43),
-						Hash:   ethCommon.HexToHash("0x9999bac44d09a7f69ee7941819b0a19c59ccb1969640cc513be09ef9deadbeef"),
-						Time:   timeForTest(time.Now()),
-						Result: results,
-					},
-				},
-			},
-			{
-				ChainID: 11,
-				Responses: []EthCallQueryResponse{
-					{
-						Number: big.NewInt(44),
-						Hash:   ethCommon.HexToHash("0x9999bac44d09a7f69ee7941819b0a19c59ccb1969640cc513be09ef95ed2d8e3"),
-						Time:   timeForTest(time.Now()),
-						Result: results,
-					},
+func TestMarshalOfQueryRequestForInvalidChainIdShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDUnset)
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfQueryRequestWithInvalidBlockIdShouldFail(t *testing.T) {
+	callData := []*EthCallData{{
+		To:   []byte(fmt.Sprintf("%-20s", fmt.Sprintf("To for %d", 0))),
+		Data: []byte(fmt.Sprintf("CallData for %d", 0)),
+	}}
+
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId:  "latest",
+			CallData: callData,
+		},
+	}
+
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfQueryRequestWithNoCallDataEntriesShouldFail(t *testing.T) {
+	callData := []*EthCallData{}
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId:  "0x28d9630",
+			CallData: callData,
+		},
+	}
+
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfQueryRequestWithNilCallDataEntriesShouldFail(t *testing.T) {
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId:  "0x28d9630",
+			CallData: nil,
+		},
+	}
+
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfQueryRequestWithTooManyCallDataEntriesShouldFail(t *testing.T) {
+	callData := []*EthCallData{}
+	for count := 0; count < 300; count++ {
+		callData = append(callData, &EthCallData{
+			To:   []byte(fmt.Sprintf("%-20s", fmt.Sprintf("To for %d", count))),
+			Data: []byte(fmt.Sprintf("CallData for %d", count)),
+		})
+	}
+
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId:  "0x28d9630",
+			CallData: callData,
+		},
+	}
+
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfEthCallQueryWithNilToShouldFail(t *testing.T) {
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId: "0x28d9630",
+			CallData: []*EthCallData{
+				{
+					To:   nil,
+					Data: []byte("This can't be zero length"),
 				},
 			},
 		},
 	}
 
-	respPubBytes, err := MarshalQueryResponsePublication(respPub)
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfEthCallQueryWithEmptyToShouldFail(t *testing.T) {
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId: "0x28d9630",
+			CallData: []*EthCallData{
+				{
+					To:   []byte{},
+					Data: []byte("This can't be zero length"),
+				},
+			},
+		},
+	}
+
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfEthCallQueryWithWrongLengthToShouldFail(t *testing.T) {
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId: "0x28d9630",
+			CallData: []*EthCallData{
+				{
+					To:   []byte("TooShort"),
+					Data: []byte("This can't be zero length"),
+				},
+			},
+		},
+	}
+
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfEthCallQueryWithNilDataShouldFail(t *testing.T) {
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId: "0x28d9630",
+			CallData: []*EthCallData{
+				{
+					To:   []byte(fmt.Sprintf("%-20s", fmt.Sprintf("To for %d", 0))),
+					Data: nil,
+				},
+			},
+		},
+	}
+
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfEthCallQueryWithEmptyDataShouldFail(t *testing.T) {
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId: "0x28d9630",
+			CallData: []*EthCallData{
+				{
+					To:   []byte(fmt.Sprintf("%-20s", fmt.Sprintf("To for %d", 0))),
+					Data: []byte{},
+				},
+			},
+		},
+	}
+
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalOfEthCallQueryWithWrongToLengthShouldFail(t *testing.T) {
+	perChainQuery := &PerChainQueryRequest{
+		ChainId: vaa.ChainIDPolygon,
+		Query: &EthCallQueryRequest{
+			BlockId: "0x28d9630",
+			CallData: []*EthCallData{
+				{
+					To:   []byte("This is too short!"),
+					Data: []byte("This can't be zero length"),
+				},
+			},
+		},
+	}
+
+	queryRequest := &QueryRequest{
+		Nonce:           1,
+		PerChainQueries: []*PerChainQueryRequest{perChainQuery},
+	}
+	_, err := queryRequest.Marshal()
+	require.Error(t, err)
+}
+
+func TestPostSignedQueryRequestShouldFailIfNoOneIsListening(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	queryRequestBytes, err := queryRequest.Marshal()
 	require.NoError(t, err)
 
-	respPub2, err := UnmarshalQueryResponsePublication(respPubBytes)
+	sig := [65]byte{}
+	signedQueryRequest := &gossipv1.SignedQueryRequest{
+		QueryRequest: queryRequestBytes,
+		Signature:    sig[:],
+	}
+
+	var signedQueryReqSendC chan<- *gossipv1.SignedQueryRequest
+	assert.Error(t, PostSignedQueryRequest(signedQueryReqSendC, signedQueryRequest))
+}
+
+func createQueryResponseFromRequest(t *testing.T, queryRequest *QueryRequest) *QueryResponsePublication {
+	queryRequestBytes, err := queryRequest.Marshal()
+	require.NoError(t, err)
+
+	sig := [65]byte{}
+	signedQueryRequest := &gossipv1.SignedQueryRequest{
+		QueryRequest: queryRequestBytes,
+		Signature:    sig[:],
+	}
+
+	perChainResponses := []*PerChainQueryResponse{}
+	for idx, pcr := range queryRequest.PerChainQueries {
+		switch req := pcr.Query.(type) {
+		case *EthCallQueryRequest:
+			results := [][]byte{}
+			for idx := range req.CallData {
+				result := []byte([]byte(fmt.Sprintf("Result %d", idx)))
+				results = append(results, result[:])
+			}
+			perChainResponses = append(perChainResponses, &PerChainQueryResponse{
+				ChainId: pcr.ChainId,
+				Response: &EthCallQueryResponse{
+					BlockNumber: uint64(1000 + idx),
+					Hash:        ethCommon.HexToHash("0x9999bac44d09a7f69ee7941819b0a19c59ccb1969640cc513be09ef95ed2d8e2"),
+					Time:        timeForTest(time.Now()),
+					Results:     results,
+				},
+			})
+		default:
+			panic("invalid query type!")
+		}
+
+	}
+
+	return &QueryResponsePublication{
+		Request:           signedQueryRequest,
+		PerChainResponses: perChainResponses,
+	}
+}
+
+func TestQueryResponseMarshalUnmarshal(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	respPub := createQueryResponseFromRequest(t, queryRequest)
+
+	respPubBytes, err := respPub.Marshal()
+	require.NoError(t, err)
+
+	var respPub2 QueryResponsePublication
+	err = respPub2.Unmarshal(respPubBytes)
 	require.NoError(t, err)
 	require.NotNil(t, respPub2)
 
-	assert.True(t, respPub.Equal(respPub2))
+	assert.True(t, respPub.Equal(&respPub2))
 }
 
-/*
-func TesMarshalUnMarshalQueryResponseWithNoResults(t *testing.T) {
-	queryRequest := createQueryRequestForTesting()
-	queryRequestBytes, err := proto.Marshal(queryRequest)
+func TestMarshalUnmarshalQueryResponseWithNoPerChainResponsesShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	queryRequestBytes, err := queryRequest.Marshal()
 	require.NoError(t, err)
 
 	sig := [65]byte{}
@@ -165,17 +423,122 @@ func TesMarshalUnMarshalQueryResponseWithNoResults(t *testing.T) {
 	}
 
 	respPub := &QueryResponsePublication{
-		Request:   signedQueryRequest,
-		Responses: nil,
+		Request:           signedQueryRequest,
+		PerChainResponses: []*PerChainQueryResponse{},
 	}
 
-	respPubBytes, err := MarshalQueryResponsePublication(respPub)
-	require.NoError(t, err)
-
-	respPub2, err := UnmarshalQueryResponsePublication(respPubBytes)
-	require.NoError(t, err)
-	require.NotNil(t, respPub2)
-
-	assert.True(t, respPub.Equal(respPub2))
+	_, err = respPub.Marshal()
+	require.Error(t, err)
 }
-*/
+
+func TestMarshalUnmarshalQueryResponseWithNilPerChainResponsesShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	queryRequestBytes, err := queryRequest.Marshal()
+	require.NoError(t, err)
+
+	sig := [65]byte{}
+	signedQueryRequest := &gossipv1.SignedQueryRequest{
+		QueryRequest: queryRequestBytes,
+		Signature:    sig[:],
+	}
+
+	respPub := &QueryResponsePublication{
+		Request:           signedQueryRequest,
+		PerChainResponses: nil,
+	}
+
+	_, err = respPub.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalUnmarshalQueryResponseWithTooManyPerChainResponsesShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	respPub := createQueryResponseFromRequest(t, queryRequest)
+
+	for count := 0; count < 300; count++ {
+		respPub.PerChainResponses = append(respPub.PerChainResponses, respPub.PerChainResponses[0])
+	}
+
+	_, err := respPub.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalUnmarshalQueryResponseWithWrongNumberOfPerChainResponsesShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	respPub := createQueryResponseFromRequest(t, queryRequest)
+
+	respPub.PerChainResponses = append(respPub.PerChainResponses, respPub.PerChainResponses[0])
+
+	_, err := respPub.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalUnmarshalQueryResponseWithInvalidChainIDShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	respPub := createQueryResponseFromRequest(t, queryRequest)
+
+	respPub.PerChainResponses[0].ChainId = vaa.ChainIDUnset
+
+	_, err := respPub.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalUnmarshalQueryResponseWithNilResponseShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	respPub := createQueryResponseFromRequest(t, queryRequest)
+
+	respPub.PerChainResponses[0].Response = nil
+
+	_, err := respPub.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalUnmarshalQueryResponseWithNoResultsShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	respPub := createQueryResponseFromRequest(t, queryRequest)
+
+	switch resp := respPub.PerChainResponses[0].Response.(type) {
+	case *EthCallQueryResponse:
+		resp.Results = [][]byte{}
+	default:
+		panic("invalid query type!")
+	}
+
+	_, err := respPub.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalUnmarshalQueryResponseWithNilResultsShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	respPub := createQueryResponseFromRequest(t, queryRequest)
+
+	switch resp := respPub.PerChainResponses[0].Response.(type) {
+	case *EthCallQueryResponse:
+		resp.Results = nil
+	default:
+		panic("invalid query type!")
+	}
+
+	_, err := respPub.Marshal()
+	require.Error(t, err)
+}
+
+func TestMarshalUnmarshalQueryResponseWithTooManyResultsShouldFail(t *testing.T) {
+	queryRequest := createQueryRequestForTesting(vaa.ChainIDPolygon)
+	respPub := createQueryResponseFromRequest(t, queryRequest)
+
+	results := [][]byte{}
+	for count := 0; count < 300; count++ {
+		results = append(results, []byte{})
+	}
+
+	switch resp := respPub.PerChainResponses[0].Response.(type) {
+	case *EthCallQueryResponse:
+		resp.Results = results
+	default:
+		panic("invalid query type!")
+	}
+
+	_, err := respPub.Marshal()
+	require.Error(t, err)
+}
