0d44d8f8f05c5c0852c0f481b28db4874952735d
Aber Erre <arblugo@gmail.com>
2023-06-23 00:12:26 +0100

    feat: remove aptos case on tests after PR merge

--- clients/js/__tests__/info/contract.test.ts
+++ clients/js/__tests__/info/contract.test.ts
@@ -39,13 +39,6 @@ describe("worm info contract", () => {
           });
 
           it(`should return ${chain} NFTBridge ${network} contract correctly`, async () => {
-            //TODO: remove 'if statement' once fix on wormhole SDK is merged & published (missing aptos testnet NFTBridge contract as consts)
-            // PR source: https://github.com/wormhole-foundation/wormhole/pull/3110
-            if (chain === "aptos" && network === "testnet") {
-              expect(true).toBe(true);
-              return;
-            }
-
             try {
               const output = run_worm_command(
                 `info contract ${network} ${chain} NFTBridge`
