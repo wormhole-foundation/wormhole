3153fd12611c626ae83fdab0c753b774a44a8ad8
Aber Erre <arblugo@gmail.com>
2023-06-23 16:25:11 +0100

    fix: add missing CommandModule type

--- clients/js/src/main.ts
+++ clients/js/src/main.ts
@@ -17,21 +17,22 @@ import * as submit from "./cmds/submit";
 import * as sui from "./cmds/sui";
 import * as transfer from "./cmds/transfer";
 import * as verifyVaa from "./cmds/verifyVaa";
+import { YargsCommandModule } from "./cmds/Yargs";
 
 yargs(hideBin(process.argv))
   // https://github.com/yargs/yargs/blob/main/docs/advanced.md#commanddirdirectory-opts
   // can't use `.commandDir` because bundling + tree-shaking
   .command(aptos)
-  .command(editVaa)
+  .command(editVaa as unknown as YargsCommandModule)
   .command(evm)
   .command(generate)
   .command(info)
   .command(near)
-  .command(parse)
-  .command(recover)
-  .command(submit)
+  .command(parse as unknown as YargsCommandModule)
+  .command(recover as unknown as YargsCommandModule)
+  .command(submit as unknown as YargsCommandModule)
   .command(sui)
-  .command(transfer)
-  .command(verifyVaa)
+  .command(transfer as unknown as YargsCommandModule)
+  .command(verifyVaa as unknown as YargsCommandModule)
   .strict()
   .demandCommand().argv;
