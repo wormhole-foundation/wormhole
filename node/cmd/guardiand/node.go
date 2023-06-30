e1fd3b4bfd00c1f532a53eb56d95dac4547f253f
bruce-riley <96066700+bruce-riley@users.noreply.github.com>
2023-06-30 10:12:50 -0500

    CCQ: Make work with node refactor (#3154)
    * CCQ: Make work with node refactor
    
    Change-Id: I416db0e01156787b6ed2420598a448d23c4581ca
    
    * Tweak log message
    
    Change-Id: Ibf109cfdccf01297e0755fc849b23d42cde2b772

--- node/cmd/guardiand/node.go
+++ node/cmd/guardiand/node.go
@@ -479,7 +479,7 @@ func runNode(cmd *cobra.Command, args []string) {
 		// Deterministic ganache ETH devnet address.
 		*ethContract = unsafeDevModeEvmContractAddress(*ethContract)
 		*bscContract = unsafeDevModeEvmContractAddress(*bscContract)
-		// *polygonContract = unsafeDevModeEvmContractAddress(*polygonContract)
+		*polygonContract = unsafeDevModeEvmContractAddress(*polygonContract)
 		*avalancheContract = unsafeDevModeEvmContractAddress(*avalancheContract)
 		*oasisContract = unsafeDevModeEvmContractAddress(*oasisContract)
 		*auroraContract = unsafeDevModeEvmContractAddress(*auroraContract)
@@ -1428,6 +1428,7 @@ func runNode(cmd *cobra.Command, args []string) {
 		node.GuardianOptionAccountant(*accountantContract, *accountantWS, *accountantCheckEnabled, accountantWormchainConn),
 		node.GuardianOptionGovernor(*chainGovernorEnabled),
 		node.GuardianOptionGatewayRelayer(*gatewayRelayerContract, gatewayRelayerWormchainConn),
+		node.GuardianOptionQueryHandler(*ccqEnabled, *ccqAllowedRequesters),
 		node.GuardianOptionAdminService(*adminSocketPath, ethRPC, ethContract, rpcMap),
 		node.GuardianOptionP2P(p2pKey, *p2pNetworkID, *p2pBootstrap, *nodeName, *disableHeartbeatVerify, *p2pPort, ibc.GetFeatures),
 		node.GuardianOptionStatusServer(*statusAddr),
