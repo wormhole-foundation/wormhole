3a9eee109c026a2e17993aaff9cbbd1ef44d2ae4
tbjump <>
2023-07-05 22:53:50 +0000

    node/db: use provided logger for badgerDb

--- node/pkg/db/open.go
+++ node/pkg/db/open.go
@@ -1,21 +1,49 @@
 package db
 
 import (
+	"fmt"
 	"os"
 	"path"
 
+	"github.com/dgraph-io/badger/v3"
 	"go.uber.org/zap"
 )
+
+type badgerZapLogger struct {
+	*zap.Logger
+}
+
+func (l badgerZapLogger) Errorf(f string, v ...interface{}) {
+	l.Error(fmt.Sprintf(f, v...))
+}
+
+func (l badgerZapLogger) Warningf(f string, v ...interface{}) {
+	l.Warn(fmt.Sprintf(f, v...))
+}
+
+func (l badgerZapLogger) Infof(f string, v ...interface{}) {
+	l.Info(fmt.Sprintf(f, v...))
+}
+
+func (l badgerZapLogger) Debugf(f string, v ...interface{}) {
+	l.Debug(fmt.Sprintf(f, v...))
+}
 
 func OpenDb(logger *zap.Logger, dataDir *string) *Database {
 	dbPath := path.Join(*dataDir, "db")
 	if err := os.MkdirAll(dbPath, 0700); err != nil {
 		logger.Fatal("failed to create database directory", zap.Error(err))
 	}
-	db, err := Open(dbPath)
+
+	options := badger.DefaultOptions(dbPath)
+	options.Logger = badgerZapLogger{logger}
+
+	db, err := badger.Open(options)
 	if err != nil {
 		logger.Fatal("failed to open database", zap.Error(err))
 	}
 
-	return db
+	return &Database{
+		db: db,
+	}
 }
