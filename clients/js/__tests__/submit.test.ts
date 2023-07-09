1fb787898a528021f4cb4b4aeddf4de82bc04285
Aber Erre <arblugo@gmail.com>
2023-07-09 09:52:50 +0100

    feat: set ContractUpgrade cases & args

--- clients/js/__tests__/submit.test.ts
+++ clients/js/__tests__/submit.test.ts
@@ -1,15 +1,27 @@
 import { describe, expect, it } from "@jest/globals";
 import { run_worm_command } from "./utils/cli";
-import { getChains } from "./utils/getters";
+import { getChains, getRpcEndpoint } from "./utils/getters";
+import { ContractUpgrade } from "../src/vaa";
 
-const chains = getChains();
+const getContractUpgradeVaaByChain = (chain: string) => {
+  //TODO: use 'chain' arg to map around future vaa mocks data
+  return "sample-vaa";
+};
 
 describe("worm submit", () => {
-  describe("check functionality", () => {
+  const chains = getChains();
+
+  describe("check 'ContractUpgrade' functionality", () => {
     chains.forEach((chain) => {
-      it(`should submit correctly on ${chain}`, async () => {
-        const vaa = "mock vaa";
-        run_worm_command(`submit ${vaa}`);
+      it.skip(`should submit 'ContractUpgrade' VAA correctly on ${chain}`, async () => {
+        const contractUpgradeVAA = getContractUpgradeVaaByChain(chain);
+        // Check only 'mainnet' contracts, testnet environments may be unstable
+        const rpc = getRpcEndpoint(chain, "MAINNET");
+        const network = "mainnet";
+
+        run_worm_command(
+          `submit ${contractUpgradeVAA} --chain ${chain} --rpc ${rpc} --network ${network}`
+        );
 
         //TODO: pick up network call and verify its content against expected templates
         const capturedNetworkCall = "mock net call";
--- clients/js/__tests__/utils/getters.ts
+++ clients/js/__tests__/utils/getters.ts
@@ -1,4 +1,5 @@
-import { CHAINS } from "@certusone/wormhole-sdk/lib/esm/utils/consts";
+import { CHAINS, Network } from "@certusone/wormhole-sdk/lib/esm/utils/consts";
+import { NETWORKS as RPC_NETWORKS } from "../../src/consts/networks";
 
 export type WormholeSDKChainName = keyof typeof CHAINS;
 
@@ -10,4 +11,11 @@ export type WormholeSDKNetwork = "mainnet" | "testnet" | "devnet";
 
 export const getNetworks = () => {
   return ["mainnet", "testnet", "devnet"] as WormholeSDKNetwork[];
+};
+
+export const getRpcEndpoint = (
+  chain: WormholeSDKChainName,
+  network: Network
+) => {
+  return String(RPC_NETWORKS[network][chain].rpc);
 };
