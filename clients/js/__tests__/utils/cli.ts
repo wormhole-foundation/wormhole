df471b3b190e098b7697b6d4646c2fff6663810c
Aber Erre <arblugo@gmail.com>
2023-07-09 08:47:10 +0100

    refactor: simplify WORM_CLI_PATH

--- clients/js/__tests__/utils/cli.ts
+++ clients/js/__tests__/utils/cli.ts
@@ -1,34 +1,10 @@
 import { execSync } from "child_process";
 
-// WORM_CLI singleton, this avoids calling getWormCLI on the target machine more than once, is a syncronous costly process.
-class WormCLI {
-  static instance: WormCLI;
-  private path!: string;
-
-  constructor() {
-    if (!WormCLI.instance) {
-      this.path = this.getWormCLI();
-      WormCLI.instance = this;
-    }
-    //NOTE: guard to send the same instance in case 'new WormCLI()' is called again
-    return WormCLI.instance;
-  }
-
-  private getWormCLI = (): string => {
-    //This functions returns the machine path targeting to the generated `worm` command
-    return execSync("which worm", { encoding: "utf8" }).trim();
-  };
-
-  getPath = (): string => {
-    return this.path;
-  };
-}
-
-// Init WORM_CLI singleton
-const WORM_CLI = new WormCLI();
+// Get machine path targeting to generated `worm` CLI executable, only once
+const WORM_CLI_PATH = execSync("which worm", { encoding: "utf8" }).trim();
 
 export const run_worm_command = (commandArgs: string) => {
-  const worm = WORM_CLI.getPath();
+  const worm = WORM_CLI_PATH;
   return execSync(`${worm} ${commandArgs}`).toString();
 };
 
