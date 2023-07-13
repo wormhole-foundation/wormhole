719e106d966db1a7de7a6dbf7091818cbaf1a8d5
Nikhil Suri <nikhilsuri@comcast.net>
2023-07-13 21:50:55 +0000

    Add instantiate allowlist to codec and genesis

--- wormchain/x/wormhole/keeper/instantiate_allowlist.go
+++ wormchain/x/wormhole/keeper/instantiate_allowlist.go
@@ -8,7 +8,7 @@ import (
 	"github.com/wormhole-foundation/wormchain/x/wormhole/types"
 )
 
-func (k Keeper) SetInstantiateAllowlist(ctx sdk.Context, allowed types.WasmAllowedContractCodeId) {
+func (k Keeper) SetInstantiateAllowlist(ctx sdk.Context, allowed types.WasmInstantiateAllowedContractCodeId) {
 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.WasmInstantiateAllowlistKey))
 	b := k.cdc.MustMarshal(&allowed)
 	codeIdStr := strconv.FormatUint(allowed.CodeId, 10)
@@ -19,4 +19,19 @@ func (k Keeper) HasInstantiateAllowlist(ctx sdk.Context, contract string, codeId
 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.WasmInstantiateAllowlistKey))
 	codeIdStr := strconv.FormatUint(codeId, 10)
 	return store.Has([]byte(contract + codeIdStr))
+}
+
+func (k Keeper) GetAllWasmInstiateAllowedAddresses(ctx sdk.Context) (list []types.WasmInstantiateAllowedContractCodeId) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.ValidatorAllowlistKey))
+	iterator := sdk.KVStorePrefixIterator(store, []byte{})
+
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		var val types.WasmInstantiateAllowedContractCodeId
+		k.cdc.MustUnmarshal(iterator.Value(), &val)
+		list = append(list, val)
+	}
+
+	return
 }
