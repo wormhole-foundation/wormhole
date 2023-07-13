57a88b44c4fd2efd3a28d1fd3889d1ecb0071bfb
Aber Erre <arblugo@gmail.com>
2023-07-13 20:49:12 +0100

    feat: use simple array for networks on tests utils

--- clients/js/__tests__/utils/getters.ts
+++ clients/js/__tests__/utils/getters.ts
@@ -6,8 +6,4 @@ export const getChains = () => {
   return Object.keys(CHAINS) as WormholeSDKChainName[];
 };
 
-export type WormholeSDKNetwork = "mainnet" | "testnet" | "devnet";
-
-export const getNetworks = () => {
-  return ["mainnet", "testnet", "devnet"] as WormholeSDKNetwork[];
-};
+export const networks = ["mainnet", "testnet", "devnet"];
