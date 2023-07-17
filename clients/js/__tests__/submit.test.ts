1136d3acab564a28f00835bf7f92ead1bc622a11
Aber Erre <arblugo@gmail.com>
2023-07-17 18:12:34 +0100

    wip: tests API calls for solana

--- clients/js/__tests__/submit.test.ts
+++ clients/js/__tests__/submit.test.ts
@@ -1,18 +1,22 @@
-import { describe, expect, it } from "@jest/globals";
-import { run_worm_command } from "./utils/cli";
 import {
-  WormholeSDKChainName,
-  getChains,
-  getRpcEndpoint,
-} from "./utils/getters";
-import { ContractUpgrade } from "../src/vaa";
+  describe,
+  expect,
+  it,
+  beforeAll,
+  afterAll,
+  afterEach,
+} from "@jest/globals";
+import { WormholeSDKChainName, getRpcEndpoint } from "./utils/getters";
+import { server as mswServer, requests as mswRequests } from "./utils/msw";
+import yargs from "yargs";
+import * as submitCommand from "../src/cmds/submit";
+import { YargsCommandModule } from "../src/cmds/Yargs";
 
 const getContractUpgradeVaaByChain = (chain: string) => {
   //TODO: use 'chain' arg to map around future vaa mocks data
   if (chain === "solana") {
     const solanaUpgradeVaaTokenContract =
-      "01000000000200bca7dda78ef9fe96ef829959850fc5b02b49eeb839697657b3e2f477f75f4f204a313ab3de929ae26d95759b1be478050156747f506835c9b65a487eea0882420101de279fb9987c5c095ae960118f9c28bd122d0354119c7c9112d0410eaa93f10f6d612d87684ead5f7c6b4335ee77f550b0d93b867e14c1dee1aef90fd34db71b000000000100000001000100000000000000000000000000000000000000000000000000000000000000040000000002df553400000000000000000000000000000000000000000000546f6b656e4272696467650200010e0a589e6488147a94dcfa592b90fdd41152bb2ca77bf6016758a6f4df9d21b4";
-
+      "01000000000200c2196789ccee7ce30e300e09626b2bf594729ef17b7f6b627308527c0f5929a501cf48f10f141717b299e79cc72ab4581c0fd8e23c8fe5de40b9a13d8f6e3ce90001c2196789ccee7ce30e300e09626b2bf594729ef17b7f6b627308527c0f5929a501cf48f10f141717b299e79cc72ab4581c0fd8e23c8fe5de40b9a13d8f6e3ce90000000001000000010001000000000000000000000000000000000000000000000000000000000000000400000000025124a40000000000000000000000000000000000000000000000000000000000436f72650100012b1246c9eefa3c466792253111f35fec1ee8ee5e9debc412d2e9adadfecdcc72";
     return solanaUpgradeVaaTokenContract;
   }
 };
@@ -20,23 +24,32 @@ const getContractUpgradeVaaByChain = (chain: string) => {
 describe("worm submit", () => {
   const chains = ["solana"] as WormholeSDKChainName[];
 
+  // Listen to msw local server, network calls are captured there
+  beforeAll(() => mswServer.listen());
+  afterAll(() => mswServer.close());
+
   describe("check 'ContractUpgrade' functionality", () => {
+    // Clean server handlers and request for every test
+    afterEach(() => {
+      mswServer.resetHandlers();
+      mswRequests.length = 0;
+    });
+
     chains.forEach((chain) => {
-      it.skip(`should submit 'ContractUpgrade' VAA correctly on ${chain}`, async () => {
+      it(`should submit 'ContractUpgrade' VAA for 'TokenBridge' module correctly on ${chain}`, async () => {
         const contractUpgradeVAA = getContractUpgradeVaaByChain(chain);
         // Check only 'mainnet' contracts, testnet environments may be unstable
         const rpc = getRpcEndpoint(chain, "MAINNET");
-        const network = "mainnet";
+        const network = "testnet";
 
-        run_worm_command(
-          `submit ${contractUpgradeVAA} --chain ${chain} --rpc ${rpc} --network ${network}`
-        );
+        const argv = await yargs
+          .command(submitCommand as unknown as YargsCommandModule)
+          .parse(
+            `submit ${contractUpgradeVAA} --chain ${chain} --rpc ${rpc} --network ${network}`
+          );
 
-        //TODO: pick up network call and verify its content against expected templates
-        const capturedNetworkCall = "mock net call";
-        const template = "template of expected net call";
-
-        expect(template).toContain(capturedNetworkCall);
+        expect(mswRequests.length).toBe(1);
+        expect(mswRequests[0].url.href).toBe(rpc);
       });
     });
   });
--- clients/js/__tests__/utils/msw.ts
+++ clients/js/__tests__/utils/msw.ts
@@ -1,28 +1,50 @@
 // This module provides logic to capture network calls by using 'msw' tool
 import { setupServer } from "msw/node";
-import { rest } from "msw";
+import {
+  AsyncResponseResolverReturnType,
+  MockedRequest,
+  MockedResponse,
+  ResponseComposition,
+  RestContext,
+  rest,
+} from "msw";
 
 let requests: any[] = [];
+let responses: any[] = [];
 
-const logRequest = (req: any, res: any, ctx: any) => {
-  console.log(`Received ${req.method} request at ${req.url.href}`);
+type Body = Record<string, any>;
+type LogRequestFunction = (
+  req: MockedRequest<Body>,
+  res: ResponseComposition<Body>,
+  context: RestContext
+) => AsyncResponseResolverReturnType<MockedResponse<Body>>;
+
+const genericLogRequest: LogRequestFunction = async (req, res, ctx) => {
+  console.log(`${req.method} request to ${req.url.toString()}`);
   if (Object.keys(req.headers).length > 0) {
-    console.log("Headers:", req.headers);
+    console.log("Headers:", req.headers.all());
   }
   if (req.body) {
     console.log("Body:", req.body);
   }
+  const response = await ctx.fetch(req);
+  const responseJSON = await response.json();
+  console.log("Response:", responseJSON);
+
   requests.push(req);
-  return res(ctx.status(200), ctx.json({ message: "Success" }));
+  responses.push(responseJSON);
+
+  //Return response back to execution thread
+  return res(ctx.status(200), ctx.json(responseJSON));
 };
 
 //NOTE: Capture all network traffic
 export const handlers = [
-  rest.get("*", logRequest),
-  rest.post("*", logRequest),
-  rest.put("*", logRequest),
-  rest.delete("*", logRequest),
-  rest.patch("*", logRequest),
+  rest.get("*", genericLogRequest),
+  rest.post("*", genericLogRequest),
+  rest.put("*", genericLogRequest),
+  rest.delete("*", genericLogRequest),
+  rest.patch("*", genericLogRequest),
 ];
 
 export const server = setupServer(...handlers);
