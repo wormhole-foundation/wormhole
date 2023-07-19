e87e14f1433f363d73ce4573f9f931fd3d3dc955
Aber Erre <arblugo@gmail.com>
2023-07-19 20:16:08 +0100

    feat: handle solana request handler

--- clients/js/__tests__/utils/msw.ts
+++ clients/js/__tests__/utils/msw.ts
@@ -3,7 +3,6 @@ import { setupServer } from "msw/node";
 import {
   AsyncResponseResolverReturnType,
   MockedRequest,
-  MockedResponse,
   ResponseComposition,
   RestContext,
   rest,
@@ -17,9 +16,9 @@ type LogRequestFunction = (
   req: MockedRequest<Body>,
   res: ResponseComposition<Body>,
   context: RestContext
-) => AsyncResponseResolverReturnType<MockedResponse<Body>>;
+) => AsyncResponseResolverReturnType<any>;
 
-const genericLogRequest: LogRequestFunction = async (req, res, ctx) => {
+const logRequest = (req: MockedRequest<Body>) => {
   console.log(`${req.method} request to ${req.url.toString()}`);
   if (Object.keys(req.headers).length > 0) {
     console.log("Headers:", req.headers.all());
@@ -27,24 +26,54 @@ const genericLogRequest: LogRequestFunction = async (req, res, ctx) => {
   if (req.body) {
     console.log("Body:", req.body);
   }
+  requests.push(req);
+};
+
+const genericRequestHandler: LogRequestFunction = async (req, res, ctx) => {
+  logRequest(req);
+
   const response = await ctx.fetch(req);
   const responseJSON = await response.json();
   console.log("Response:", responseJSON);
-
-  requests.push(req);
   responses.push(responseJSON);
 
   //Return response back to execution thread
   return res(ctx.status(200), ctx.json(responseJSON));
 };
 
+const solanaRequestHandler: LogRequestFunction = async (req, res, ctx) => {
+  logRequest(req);
+
+  // Avoid sending transaction to network, send error instead (to force stop execution)
+  if (req.body && req.body.method === "sendTransaction") {
+    return res(
+      ctx.status(200),
+      // mock response with error
+      ctx.json({
+        jsonrpc: "2.0",
+        error: {
+          code: -32002,
+          message: "Transaction signature verification failed",
+        },
+        id: "9b282a84-c613-4a34-b5b8-5c6a3fd5f352",
+      })
+    );
+  } else {
+    return await genericRequestHandler(req, res, ctx);
+  }
+};
+
 //NOTE: Capture all network traffic
 export const handlers = [
-  rest.get("*", genericLogRequest),
-  rest.post("*", genericLogRequest),
-  rest.put("*", genericLogRequest),
-  rest.delete("*", genericLogRequest),
-  rest.patch("*", genericLogRequest),
+  // Interceptors
+  rest.post("https://api.devnet.solana.com/", solanaRequestHandler),
+
+  // Loggers
+  rest.get("*", genericRequestHandler),
+  rest.post("*", genericRequestHandler),
+  rest.put("*", genericRequestHandler),
+  rest.delete("*", genericRequestHandler),
+  rest.patch("*", genericRequestHandler),
 ];
 
 export const server = setupServer(...handlers);
