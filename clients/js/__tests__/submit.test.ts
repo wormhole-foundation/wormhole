10f414fe0a8269da8aebe8012f96c7a230b522ce
Aber Erre <arblugo@gmail.com>
2023-07-20 21:26:41 +0100

    wip: generate all evm chains tests dinamically

--- clients/js/__tests__/submit.test.ts
+++ clients/js/__tests__/submit.test.ts
@@ -93,57 +93,80 @@ describe("worm submit", () => {
       );
     });
 
-    describe("ethereum", () => {
-      const chain: WormholeSDKChainName = "ethereum";
-      const rpc = getRpcEndpoint(chain, "MAINNET");
-      const network = "mainnet";
+    describe("evm", () => {
+      const evmChains: WormholeSDKChainName[] = [
+        "ethereum",
+        "acala",
+        "arbitrum",
+        "aurora",
+        "avalanche",
+        "base",
+        "bsc",
+        "celo",
+        "fantom",
+        "gnosis",
+        "karura",
+        "klaytn",
+        "moonbeam",
+        "neon",
+        "oasis",
+        "optimism",
+        "polygon",
+        "sepolia",
+      ];
 
-      contractUpgradeModules.forEach((module) => {
-        it(
-          `should send transaction to ${chain} when submitting 'ContractUpgrade' VAA for '${module}' module`,
-          async () => {
-            //NOTE: use worm generate command to obtain a VAA
-            const vaa = run_worm_command(
-              `generate upgrade -c ${chain} -m ${module} -a 0xF890982f9310df57d00f659cf4fd87e65adEd8d7 -g 0xA240c0e8997D10D59690Cd6Eb36dd55B29af59ACaaa`
-            );
+      evmChains.forEach((chain) => {
+        describe(`${chain}`, () => {
+          const rpc = getRpcEndpoint(chain, "MAINNET");
+          const network = "mainnet";
 
-            //NOTE: we capture requests sent, then we force this process to fail before sending transactions
-            try {
-              await yargs
-                .command(submitCommand as unknown as YargsCommandModule)
-                .parse(
-                  `submit ${vaa} --chain ${chain} --rpc ${rpc} --network ${network}`
+          contractUpgradeModules.forEach((module) => {
+            it(
+              `should send transaction to ${chain} when submitting 'ContractUpgrade' VAA for '${module}' module`,
+              async () => {
+                //NOTE: use worm generate command to obtain a VAA
+                const vaa = run_worm_command(
+                  `generate upgrade -c ${chain} -m ${module} -a 0xF890982f9310df57d00f659cf4fd87e65adEd8d7 -g 0xA240c0e8997D10D59690Cd6Eb36dd55B29af59ACaaa`
                 );
-            } catch (e) {}
 
-            expect(requests.length).toBe(22);
-            expect(
-              requests.some(
-                (req) => req.body.method === "eth_sendRawTransaction"
-              )
-            ).toBeTruthy();
-          },
-          testTimeout
-        );
+                //NOTE: we capture requests sent, then we force this process to fail before sending transactions
+                try {
+                  await yargs
+                    .command(submitCommand as unknown as YargsCommandModule)
+                    .parse(
+                      `submit ${vaa} --chain ${chain} --rpc ${rpc} --network ${network}`
+                    );
+                } catch (e) {}
+
+                expect(
+                  requests.some(
+                    (req) => req.body.method === "eth_sendRawTransaction"
+                  )
+                ).toBeTruthy();
+              },
+              testTimeout
+            );
+          });
+
+          it(
+            `should fail to send transactions to ${chain} when submitting 'ContractUpgrade' VAA, if 'vaa' is malformed`,
+            async () => {
+              const fakeVaa = "this-is-a-fake-vaa";
+              try {
+                //NOTE: we capture requests sent, then we force this process to fail before sending transactions
+                await yargs
+                  .command(submitCommand as unknown as YargsCommandModule)
+                  .parse(
+                    `submit ${fakeVaa} --chain ${chain} --rpc ${rpc} --network ${network}`
+                  );
+              } catch (e) {}
+
+              expect(requests.length).toBe(0);
+            },
+            testTimeout
+          );
+        });
       });
-
-      it(
-        `should fail to send transactions to ${chain} when submitting 'ContractUpgrade' VAA, if 'vaa' is malformed`,
-        async () => {
-          const fakeVaa = "this-is-a-fake-vaa";
-          try {
-            //NOTE: we capture requests sent, then we force this process to fail before sending transactions
-            await yargs
-              .command(submitCommand as unknown as YargsCommandModule)
-              .parse(
-                `submit ${fakeVaa} --chain ${chain} --rpc ${rpc} --network ${network}`
-              );
-          } catch (e) {}
-
-          expect(requests.length).toBe(0);
-        },
-        testTimeout
-      );
     });
   });
 });
