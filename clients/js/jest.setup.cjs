4794f1a99fee88136549cb33c3a8015e23c583bd
Aber Erre <arblugo@gmail.com>
2023-07-23 11:20:31 +0100

    feat: rename env values to .env.mock.jest

--- clients/js/jest.setup.cjs
+++ clients/js/jest.setup.cjs
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 // Loads env variables for test environment (Jest)
-const envConfig = fs.readFileSync(".env.test", "utf8");
+const envConfig = fs.readFileSync(".env.mock.jest", "utf8");
 envConfig.split("\n").forEach((line) => {
   const [key, value] = line.split("=");
   process.env[key] = value;
