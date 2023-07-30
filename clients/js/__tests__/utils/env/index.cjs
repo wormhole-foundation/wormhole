b1f04dba0373afe8c3971cd35bb47b814ca8cdd3
Aber Erre <arblugo@gmail.com>
2023-07-30 13:56:27 +0100

    feat: handle file path & add try-catch

--- clients/js/__tests__/utils/env/index.cjs
+++ clients/js/__tests__/utils/env/index.cjs
@@ -1,9 +1,18 @@
 const fs = require("fs");
+const path = require("path");
 
-// Loads env variables for worm CLI test environment (Jest)
-// This is needed to simulate `worm submit` API calls
-const envConfig = fs.readFileSync("./cli-test-env-values", "utf8");
-envConfig.split("\n").forEach((line) => {
-  const [key, value] = line.split("=");
-  process.env[key] = value;
-});
+try {
+  const envValuesFilePath = path.resolve(__dirname, "./cli-test-env-values");
+  const envConfig = fs.readFileSync(envValuesFilePath, "utf8");
+
+  // Loads env variables for worm CLI test environment (Jest)
+  // This is needed to simulate `worm submit` API calls
+  envConfig.split("\n").forEach((line) => {
+    const [key, value] = line.split("=");
+    process.env[key] = value;
+  });
+} catch (err) {
+  console.error(
+    `Error reading Worm CLI environtment values file at ${envValuesFilePath}. Error: ${err}`
+  );
+}
