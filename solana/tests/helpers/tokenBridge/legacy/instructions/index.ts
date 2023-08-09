74ef0a6d2721b59f9d6721a9fd4f0690decaa1ac
A5 Pickle <a5-pickle@users.noreply.github.com>
2023-08-09 17:11:35 -0500

    solana: add transfer tokens with payload (native) test

--- solana/tests/helpers/tokenBridge/legacy/instructions/index.ts
+++ solana/tests/helpers/tokenBridge/legacy/instructions/index.ts
@@ -7,6 +7,7 @@ import { transferAuthorityPda } from "../state";
 export * from "./attestToken";
 export * from "./initialize";
 export * from "./transferTokens";
+export * from "./transferTokensWithPayload";
 
 export function approveTransferAuthorityIx(
   program: TokenBridgeProgram,
