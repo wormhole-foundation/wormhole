59e340b342c579ce9f157ca47834e709a047b495
A5 Pickle <a5-pickle@users.noreply.github.com>
2023-08-10 11:37:12 -0500

    add token bridge testing submodule

--- solana/tests/helpers/utils.ts
+++ solana/tests/helpers/utils.ts
@@ -235,47 +235,6 @@ export async function getTokenBalances(
   };
 }
 
-export enum TransferDirection {
-  Out,
-  In,
-}
-
-export async function expectCorrectTokenBalanceChanges(
-  connection: Connection,
-  token: PublicKey,
-  balancesBefore: TokenBalances,
-  direction: TransferDirection
-) {
-  const program = tokenBridge.getAnchorProgram(
-    connection,
-    tokenBridge.getProgramId("B6RHG3mfcckmrYN1UhmJzyS1XX3fZKbkeUcpJe9Sy3FE")
-  );
-  const forkedProgram = tokenBridge.getAnchorProgram(
-    connection,
-    tokenBridge.getProgramId("wormDTUJ6AWPNvk59vGQbDvGJmqbDTdgWgAqcLBCgUb")
-  );
-  const balancesAfter = await getTokenBalances(program, forkedProgram, token);
-
-  switch (direction) {
-    case TransferDirection.Out: {
-      const totalTokenBalanceChange = balancesBefore.token - balancesAfter.token;
-      expect(totalTokenBalanceChange % BigInt(2)).to.equal(BigInt(0));
-      const balanceChange = totalTokenBalanceChange / BigInt(2);
-      expect(balancesAfter.custodyToken - balancesBefore.custodyToken).to.equal(balanceChange);
-      expect(balancesAfter.forkCustodyToken - balancesBefore.forkCustodyToken).to.equal(
-        balanceChange
-      );
-      return;
-    }
-    case TransferDirection.In: {
-      throw new Error("not implemented yet");
-    }
-    default: {
-      throw new Error("impossible");
-    }
-  }
-}
-
 export function range(start: number, end: number): number[] {
   return Array.from({ length: end - start }, (_, i) => start + i);
 }
