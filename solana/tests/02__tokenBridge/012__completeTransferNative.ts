231cfc6eb3c66be7ba965aa50eea07f7d7311740
gator-boi <gator-boi@users.noreply.github.com>
2023-08-13 15:51:50 -0500

    solana: add relayer balance check for complete transfer test

--- solana/tests/02__tokenBridge/012__completeTransferNative.ts
+++ solana/tests/02__tokenBridge/012__completeTransferNative.ts
@@ -1,11 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import {
-  createAssociatedTokenAccount,
   getAssociatedTokenAddressSync,
   getOrCreateAssociatedTokenAccount,
-  mintTo,
 } from "@solana/spl-token";
-import { PublicKey } from "@solana/web3.js";
 import {
   ETHEREUM_TOKEN_BRIDGE_ADDRESS,
   MINT_INFO_8,
@@ -15,8 +12,8 @@ import {
   getTokenBalances,
   parallelPostVaa,
 } from "../helpers";
-import { CHAIN_ID_SOLANA, tryNativeToHexString, parseVaa } from "@certusone/wormhole-sdk";
-import { GUARDIAN_KEYS, invokeVerifySignaturesAndPostVaa } from "../helpers";
+import { CHAIN_ID_SOLANA, tryNativeToHexString, parseVaa, relayer } from "@certusone/wormhole-sdk";
+import { GUARDIAN_KEYS } from "../helpers";
 import * as tokenBridge from "../helpers/tokenBridge";
 import * as coreBridge from "../helpers/coreBridge";
 import { MockTokenBridge, MockGuardians } from "@certusone/wormhole-sdk/lib/cjs/mock";
@@ -44,7 +41,7 @@ describe("Token Bridge -- Legacy Instruction: Complete Transfer (Native)", () =>
 
   describe("Ok", () => {
     for (const { mint, decimals } of mints) {
-      it(`Invoke \`complete_transfer_native\` (${decimals} Decimals)`, async () => {
+      it(`Invoke \`complete_transfer_native\` (${decimals} Decimals (No Fee))`, async () => {
         // Create recipient token account.
         const recipient = anchor.web3.Keypair.generate();
         const recipientToken = await getOrCreateAssociatedTokenAccount(
@@ -55,13 +52,12 @@ describe("Token Bridge -- Legacy Instruction: Complete Transfer (Native)", () =>
         );
         const payerToken = getAssociatedTokenAddressSync(mint, payer.publicKey);
 
+        // Amounts.
+        const amount = BigInt(699999);
+        const fee = BigInt(0);
+
         // Create the signed transfer VAA.
-        const signedVaa = getSignedTransferVaa(
-          mint,
-          new anchor.BN(69999),
-          new anchor.BN(0),
-          recipientToken.address
-        );
+        const signedVaa = getSignedTransferVaa(mint, amount, fee, recipientToken.address);
 
         // Fetch balances before.
         const recipientBalancesBefore = await getTokenBalances(
@@ -71,23 +67,9 @@ describe("Token Bridge -- Legacy Instruction: Complete Transfer (Native)", () =>
         );
         const relayerBalancesBefore = await getTokenBalances(program, forkedProgram, payerToken);
 
-        console.log(recipientToken);
-
-        // Post the VAA.
-        await parallelPostVaa(connection, payer, signedVaa);
-
-        // Create instruction.
-        const ix = tokenBridge.legacyCompleteTransferNativeIx(
+        // Complete the transfer.
+        await parallelTxDetails(
           program,
-          {
-            payer: payer.publicKey,
-            recipientToken: recipientToken.address,
-            mint,
-            payerToken,
-          },
-          parseVaa(signedVaa)
-        );
-        const forkedIx = tokenBridge.legacyCompleteTransferNativeIx(
           forkedProgram,
           {
             payer: payer.publicKey,
@@ -95,24 +77,85 @@ describe("Token Bridge -- Legacy Instruction: Complete Transfer (Native)", () =>
             mint,
             payerToken,
           },
-          parseVaa(signedVaa)
+          signedVaa,
+          payer
         );
-        console.log(forkedIx.keys);
 
-        await expectIxOkDetails(connection, [ix, forkedIx], [payer]);
-
+        // Check recipient and relayer token balance changes.
         await tokenBridge.expectCorrectTokenBalanceChanges(
           connection,
           recipientToken.address,
           recipientBalancesBefore,
           tokenBridge.TransferDirection.In
         );
-
-        await tokenBridge.expectCorrectTokenBalanceChanges(
+        await tokenBridge.expectCorrectRelayerBalanceChanges(
           connection,
           payerToken,
           relayerBalancesBefore,
-          tokenBridge.TransferDirection.In
+          BigInt(fee.toString())
+        );
+      });
+
+      it.skip(`Invoke \`complete_transfer_native\` (${decimals} Decimals (With Fee))`, async () => {
+        // Create recipient token account.
+        const recipient = anchor.web3.Keypair.generate();
+        const recipientToken = await getOrCreateAssociatedTokenAccount(
+          connection,
+          payer,
+          mint,
+          recipient.publicKey
+        );
+        const payerToken = getAssociatedTokenAddressSync(mint, payer.publicKey);
+
+        // Amounts.
+        const amount = BigInt(699999);
+        const fee = BigInt(199999);
+
+        // Create the signed transfer VAA.
+        const signedVaa = getSignedTransferVaa(mint, amount, fee, recipientToken.address);
+
+        // Fetch balances before.
+        const recipientBalancesBefore = await getTokenBalances(
+          program,
+          forkedProgram,
+          recipientToken.address
+        );
+        const relayerBalancesBefore = await getTokenBalances(program, forkedProgram, payerToken);
+
+        // Complete the transfer.
+        await parallelTxDetails(
+          program,
+          forkedProgram,
+          {
+            payer: payer.publicKey,
+            recipientToken: recipientToken.address,
+            mint,
+            payerToken,
+          },
+          signedVaa,
+          payer
+        );
+
+        console.log(recipientBalancesBefore);
+        console.log(await getTokenBalances(program, forkedProgram, recipientToken.address));
+
+        console.log("Relayer:");
+        console.log(relayerBalancesBefore);
+        console.log(await getTokenBalances(program, forkedProgram, payerToken));
+
+        // Check recipient and relayer token balance changes.
+        await tokenBridge.expectCorrectTokenBalanceChanges(
+          connection,
+          recipientToken.address,
+          recipientBalancesBefore,
+          tokenBridge.TransferDirection.In,
+          fee
+        );
+        await tokenBridge.expectCorrectRelayerBalanceChanges(
+          connection,
+          payerToken,
+          relayerBalancesBefore,
+          fee
         );
       });
     }
@@ -121,62 +164,43 @@ describe("Token Bridge -- Legacy Instruction: Complete Transfer (Native)", () =>
 
 function getSignedTransferVaa(
   mint: anchor.web3.PublicKey,
-  amount: anchor.BN,
-  fee: anchor.BN,
+  amount: bigint,
+  fee: bigint,
   recipient: anchor.web3.PublicKey
 ): Buffer {
   const vaaBytes = dummyTokenBridge.publishTransferTokens(
     tryNativeToHexString(mint.toString(), "solana"),
     CHAIN_ID_SOLANA,
-    BigInt(amount.toString()),
+    amount,
     CHAIN_ID_SOLANA,
     recipient.toBuffer().toString("hex"),
-    BigInt(fee.toString())
+    fee
   );
   return guardians.addSignatures(vaaBytes, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
 }
 
-// async function parallelTxDetails(
-//   program: tokenBridge.TokenBridgeProgram,
-//   forkedProgram: tokenBridge.TokenBridgeProgram,
-//   accounts: { payer: PublicKey; mint: PublicKey; srcToken: PublicKey },
-//   args: tokenBridge.LegacyTransferTokensArgs,
-//   payer: anchor.web3.Keypair
-// ) {
-//   const connection = program.provider.connection;
-//   const { payer: owner, srcToken: token } = accounts;
-//   const { amount } = args;
-//   const coreMessage = anchor.web3.Keypair.generate();
-//   const approveIx = tokenBridge.approveTransferAuthorityIx(program, token, owner, amount);
-//   const ix = tokenBridge.legacyTransferTokensNativeIx(
-//     program,
-//     {
-//       coreMessage: coreMessage.publicKey,
-//       ...accounts,
-//     },
-//     args
-//   );
+async function parallelTxDetails(
+  program: tokenBridge.TokenBridgeProgram,
+  forkedProgram: tokenBridge.TokenBridgeProgram,
+  accounts: tokenBridge.LegacyCompleteTransferNativeContext,
+  signedVaa: Buffer,
+  payer: anchor.web3.Keypair
+) {
+  const connection = program.provider.connection;
 
-//   const forkCoreMessage = anchor.web3.Keypair.generate();
-//   const forkedApproveIx = tokenBridge.approveTransferAuthorityIx(
-//     forkedProgram,
-//     token,
-//     owner,
-//     amount
-//   );
-//   const forkedIx = tokenBridge.legacyTransferTokensNativeIx(
-//     forkedProgram,
-//     {
-//       coreMessage: forkCoreMessage.publicKey,
-//       ...accounts,
-//     },
-//     args
-//   );
+  // Post the VAA.
+  await parallelPostVaa(connection, payer, signedVaa);
 
-//   const [txDetails, forkTxDetails] = await Promise.all([
-//     expectIxOkDetails(connection, [approveIx, ix], [payer, coreMessage]),
-//     expectIxOkDetails(connection, [forkedApproveIx, forkedIx], [payer, forkCoreMessage]),
-//   ]);
+  // Create instruction.
+  const ix = tokenBridge.legacyCompleteTransferNativeIx(program, accounts, parseVaa(signedVaa));
+  const forkedIx = tokenBridge.legacyCompleteTransferNativeIx(
+    forkedProgram,
+    accounts,
+    parseVaa(signedVaa)
+  );
 
-//   return [coreMessage, txDetails, forkCoreMessage, forkTxDetails];
-// }
+  return await Promise.all([
+    expectIxOkDetails(connection, [ix], [payer]),
+    expectIxOkDetails(connection, [forkedIx], [payer]),
+  ]);
+}
