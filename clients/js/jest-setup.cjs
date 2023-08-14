c8cd0bdc580d8912d25a5364b34898096cbd33f5
Aber Erre <arblugo@gmail.com>
2023-08-14 15:35:24 +0100

    feat: move jest setup into command flag arg

--- clients/js/jest-setup.cjs
+++ clients/js/jest-setup.cjs
@@ -1,4 +1,6 @@
 // Inject Worm Submit CLI env variables into Jest environment, using mock env values (if missing)
 require("./__tests__/utils/env/worm-submit/wormSubmitCliEnv.cjs");
 
-// Add more setup jest configurations/plugins below..
+// Add more setup jest configurations/plugins below here..
+
+export default {};
