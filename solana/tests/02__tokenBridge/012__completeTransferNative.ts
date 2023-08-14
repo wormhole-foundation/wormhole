2b40a0b586aa6ba85fa0ae936177b60929973915
gator-boi <gator-boi@users.noreply.github.com>
2023-08-13 23:02:26 -0500

    solana: complete happy path for complete transfer tests

--- solana/tests/02__tokenBridge/012__completeTransferNative.ts
+++ solana/tests/02__tokenBridge/012__completeTransferNative.ts
@@ -10,13 +10,16 @@ import {
   MintInfo,
   expectIxOkDetails,
   getTokenBalances,
+  invokeVerifySignaturesAndPostVaa,
   parallelPostVaa,
+  expectIxErr,
 } from "../helpers";
-import { CHAIN_ID_SOLANA, tryNativeToHexString, parseVaa, relayer } from "@certusone/wormhole-sdk";
+import { CHAIN_ID_SOLANA, tryNativeToHexString, parseVaa } from "@certusone/wormhole-sdk";
 import { GUARDIAN_KEYS } from "../helpers";
 import * as tokenBridge from "../helpers/tokenBridge";
 import * as coreBridge from "../helpers/coreBridge";
 import { MockTokenBridge, MockGuardians } from "@certusone/wormhole-sdk/lib/cjs/mock";
+import { expect } from "chai";
 
 const GUARDIAN_SET_INDEX = 2;
 const dummyTokenBridge = new MockTokenBridge(
@@ -41,7 +44,7 @@ describe("Token Bridge -- Legacy Instruction: Complete Transfer (Native)", () =>
 
   describe("Ok", () => {
     for (const { mint, decimals } of mints) {
-      it(`Invoke \`complete_transfer_native\` (${decimals} Decimals (No Fee))`, async () => {
+      it(`Invoke \`complete_transfer_native\` (${decimals} Decimals (No Fee)`, async () => {
         // Create recipient token account.
         const recipient = anchor.web3.Keypair.generate();
         const recipientToken = await getOrCreateAssociatedTokenAccount(
@@ -96,7 +99,7 @@ describe("Token Bridge -- Legacy Instruction: Complete Transfer (Native)", () =>
         );
       });
 
-      it(`Invoke \`complete_transfer_native\` (${decimals} Decimals (With Fee))`, async () => {
+      it(`Invoke \`complete_transfer_native\` (${decimals} Decimals (With Fee)`, async () => {
         // Create recipient token account.
         const recipient = anchor.web3.Keypair.generate();
         const recipientToken = await getOrCreateAssociatedTokenAccount(
@@ -154,6 +157,235 @@ describe("Token Bridge -- Legacy Instruction: Complete Transfer (Native)", () =>
           fee
         );
       });
+
+      it(`Invoke \`complete_transfer_native\` (${decimals} Decimals (Self Redeemption with Fee)`, async () => {
+        // Create recipient token account.
+        const payerToken = getAssociatedTokenAddressSync(mint, payer.publicKey);
+
+        // Amounts.
+        const amount = BigInt(699999);
+        let fee = BigInt(199999);
+
+        // Create the signed transfer VAA.
+        const signedVaa = getSignedTransferVaa(mint, amount, fee, payerToken);
+
+        // Fetch balances before.
+        const recipientBalancesBefore = await getTokenBalances(program, forkedProgram, payerToken);
+
+        // Complete the transfer.
+        await parallelTxDetails(
+          program,
+          forkedProgram,
+          {
+            payer: payer.publicKey,
+            recipientToken: payerToken,
+            mint,
+            payerToken,
+          },
+          signedVaa,
+          payer
+        );
+
+        // Denormalize the fee.
+        fee = fee * BigInt(10 ** (decimals - 8));
+
+        // Check recipient and relayer token balance changes.
+        await tokenBridge.expectCorrectTokenBalanceChanges(
+          connection,
+          payerToken,
+          recipientBalancesBefore,
+          tokenBridge.TransferDirection.In,
+          BigInt(0) // No fee for self-redemption.
+        );
+      });
+
+      it(`Invoke \`complete_transfer_native\` (${decimals} Decimals (Self Redeemption no fee)`, async () => {
+        // Create recipient token account.
+        const payerToken = getAssociatedTokenAddressSync(mint, payer.publicKey);
+
+        // Amounts.
+        const amount = BigInt(699999);
+        let fee = BigInt(0);
+
+        // Create the signed transfer VAA.
+        const signedVaa = getSignedTransferVaa(mint, amount, fee, payerToken);
+
+        // Fetch balances before.
+        const recipientBalancesBefore = await getTokenBalances(program, forkedProgram, payerToken);
+
+        // Complete the transfer.
+        await parallelTxDetails(
+          program,
+          forkedProgram,
+          {
+            payer: payer.publicKey,
+            recipientToken: payerToken,
+            mint,
+            payerToken,
+          },
+          signedVaa,
+          payer
+        );
+
+        // Denormalize the fee.
+        fee = fee * BigInt(10 ** (decimals - 8));
+
+        // Check recipient and relayer token balance changes.
+        await tokenBridge.expectCorrectTokenBalanceChanges(
+          connection,
+          payerToken,
+          recipientBalancesBefore,
+          tokenBridge.TransferDirection.In,
+          BigInt(0) // No fee for self-redemption.
+        );
+      });
+
+      it(`Invoke \`complete_transfer_native\` (${decimals} Decimals (Recipient == Wallet Address)`, async () => {
+        // Create recipient token account.
+        const recipient = anchor.web3.Keypair.generate();
+        const recipientToken = await getOrCreateAssociatedTokenAccount(
+          connection,
+          payer,
+          mint,
+          recipient.publicKey
+        );
+        const payerToken = getAssociatedTokenAddressSync(mint, payer.publicKey);
+
+        // Amounts.
+        let amount = BigInt(42069);
+        let fee = BigInt(1669);
+
+        // Create the signed transfer VAA.
+        const signedVaa = getSignedTransferVaa(mint, amount, fee, recipientToken.address);
+
+        // Fetch balances before.
+        const recipientBalancesBefore = await getTokenBalances(
+          program,
+          forkedProgram,
+          recipientToken.address
+        );
+        const relayerBalancesBefore = await getTokenBalances(program, forkedProgram, payerToken);
+
+        // Post the VAA.
+        await invokeVerifySignaturesAndPostVaa(wormholeProgram, payer, signedVaa);
+
+        // Create instruction.
+        const ix = tokenBridge.legacyCompleteTransferNativeIx(
+          program,
+          {
+            payer: payer.publicKey,
+            recipientToken: recipientToken.address,
+            mint,
+            payerToken,
+          },
+          parseVaa(signedVaa)
+        );
+
+        // Complete the transfer.
+        await expectIxOkDetails(connection, [ix], [payer]);
+
+        // Denormalize the fee and amount.
+        fee = fee * BigInt(10 ** (decimals - 8));
+        amount = amount * BigInt(10 ** (decimals - 8));
+
+        // Fetch balances after.
+        const recipientBalancesAfter = await getTokenBalances(
+          program,
+          forkedProgram,
+          recipientToken.address
+        );
+        const relayerBalancesAfter = await getTokenBalances(program, forkedProgram, payerToken);
+
+        // Check recipient and relayer token balance changes.
+        expect(recipientBalancesAfter.token - recipientBalancesBefore.token).to.equal(amount - fee);
+        expect(recipientBalancesBefore.custodyToken - recipientBalancesAfter.custodyToken).to.equal(
+          amount
+        );
+        expect(relayerBalancesAfter.token - relayerBalancesBefore.token).to.equal(fee);
+      });
+
+      it(`Cannot Invoke \`complete_transfer_native\` (${decimals} Decimals (Invalid Target Chain)`, async () => {
+        // Create recipient token account.
+        const recipient = anchor.web3.Keypair.generate();
+        const recipientToken = await getOrCreateAssociatedTokenAccount(
+          connection,
+          payer,
+          mint,
+          recipient.publicKey
+        );
+        const payerToken = getAssociatedTokenAddressSync(mint, payer.publicKey);
+
+        // Amounts.
+        let amount = BigInt(42069);
+        let fee = BigInt(1669);
+
+        // Target chain.
+        const targetChain = 2;
+
+        // Create the signed transfer VAA.
+        const signedVaa = getSignedTransferVaa(
+          mint,
+          amount,
+          fee,
+          recipientToken.address,
+          targetChain
+        );
+
+        // Post the VAA.
+        await invokeVerifySignaturesAndPostVaa(wormholeProgram, payer, signedVaa);
+
+        // Create instruction.
+        const ix = tokenBridge.legacyCompleteTransferNativeIx(
+          program,
+          {
+            payer: payer.publicKey,
+            recipientToken: recipientToken.address,
+            mint,
+            payerToken,
+          },
+          parseVaa(signedVaa)
+        );
+
+        // Complete the transfer.
+        await expectIxErr(connection, [ix], [payer], "RecipientChainNotSolana");
+      });
+
+      it(`Cannot Invoke \`complete_transfer_native\` (${decimals} Decimals (Invalid Recipent ATA)`, async () => {
+        // Create recipient token account.
+        const recipient = anchor.web3.Keypair.generate();
+        const recipientToken = await getOrCreateAssociatedTokenAccount(
+          connection,
+          payer,
+          mint,
+          recipient.publicKey
+        );
+        const payerToken = getAssociatedTokenAddressSync(mint, payer.publicKey);
+
+        // Amounts.
+        let amount = BigInt(42069);
+        let fee = BigInt(1669);
+
+        // Create the signed transfer VAA.
+        const signedVaa = getSignedTransferVaa(mint, amount, fee, recipientToken.address);
+
+        // Post the VAA.
+        await invokeVerifySignaturesAndPostVaa(wormholeProgram, payer, signedVaa);
+
+        // Create instruction.
+        const ix = tokenBridge.legacyCompleteTransferNativeIx(
+          program,
+          {
+            payer: payer.publicKey,
+            recipientToken: payerToken, // Pass an invalid recipient ATA.
+            mint,
+            payerToken,
+          },
+          parseVaa(signedVaa)
+        );
+
+        // Complete the transfer.
+        await expectIxErr(connection, [ix], [payer], "ConstraintTokenOwner");
+      });
     }
   });
 });
@@ -162,13 +394,14 @@ function getSignedTransferVaa(
   mint: anchor.web3.PublicKey,
   amount: bigint,
   fee: bigint,
-  recipient: anchor.web3.PublicKey
+  recipient: anchor.web3.PublicKey,
+  targetChain?: number
 ): Buffer {
   const vaaBytes = dummyTokenBridge.publishTransferTokens(
     tryNativeToHexString(mint.toString(), "solana"),
     CHAIN_ID_SOLANA,
     amount,
-    CHAIN_ID_SOLANA,
+    targetChain ?? CHAIN_ID_SOLANA,
     recipient.toBuffer().toString("hex"),
     fee
   );
