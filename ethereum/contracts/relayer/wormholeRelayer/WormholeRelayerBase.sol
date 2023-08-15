58ccebf93b1ea98ffe4715026a6c3dc908b1d50e
Joe Howarth <josephehowarth@gmail.com>
2023-08-14 18:06:10 -0700

    AutoRelayer: remove forwards

--- ethereum/contracts/relayer/wormholeRelayer/WormholeRelayerBase.sol
+++ ethereum/contracts/relayer/wormholeRelayer/WormholeRelayerBase.sol
@@ -6,9 +6,7 @@ import {IWormhole} from "../../interfaces/IWormhole.sol";
 import {IDeliveryProvider} from "../../interfaces/relayer/IDeliveryProviderTyped.sol";
 import {toWormholeFormat, min, pay} from "../../libraries/relayer/Utils.sol";
 import {
-    NoDeliveryInProgress,
     ReentrantDelivery,
-    ForwardRequestFromWrongAddress,
     DeliveryProviderDoesNotSupportTargetChain,
     VaaKey,
     InvalidMsgValue,
@@ -16,9 +14,6 @@ import {
 } from "../../interfaces/relayer/IWormholeRelayerTyped.sol";
 import {DeliveryInstruction} from "../../libraries/relayer/RelayerInternalStructs.sol";
 import {
-    ForwardInstruction,
-    DeliveryTmpState,
-    getDeliveryTmpState,
     getDeliverySuccessState,
     getDeliveryFailureState,
     getRegisteredWormholeRelayersState
@@ -112,61 +107,5 @@ abstract contract WormholeRelayerBase is IWormholeRelayerBase {
         );
 
         emit SendEvent(sequence, deliveryQuote, paymentForExtraReceiverValue);
-    }
-
-    // ----------------------- delivery transaction temorary storage functions -----------------------
-
-    function startDelivery(address targetAddress, address deliveryProvider, uint16 refundChain, bytes32 refundAddress) internal {
-        DeliveryTmpState storage state = getDeliveryTmpState();
-        if (state.deliveryInProgress) {
-            revert ReentrantDelivery(msg.sender, state.deliveryTarget);
-        }
-
-        state.deliveryInProgress = true;
-        state.deliveryTarget = targetAddress;
-        state.deliveryProvider = deliveryProvider;
-        state.refundChain = refundChain;
-        state.refundAddress = refundAddress;
-    }
-
-    function finishDelivery() internal {
-        DeliveryTmpState storage state = getDeliveryTmpState();
-        state.deliveryInProgress = false;
-        state.deliveryTarget = address(0);
-        state.deliveryProvider = address(0);
-        state.refundChain = 0;
-        state.refundAddress = bytes32(0);
-        delete state.forwardInstructions;
-    }
-
-    function appendForwardInstruction(ForwardInstruction memory forwardInstruction) internal {
-        getDeliveryTmpState().forwardInstructions.push(forwardInstruction);
-    }
-
-    function getForwardInstructions() internal view returns (ForwardInstruction[] storage) {
-        return getDeliveryTmpState().forwardInstructions;
-    }
-
-    function getOriginalDeliveryProvider() internal view returns (address) {
-        return getDeliveryTmpState().deliveryProvider;
-    }
-
-    function getCurrentRefundChain() internal view returns (uint16) {
-        return getDeliveryTmpState().refundChain;
-    }
-
-    function getCurrentRefundAddress() internal view returns (bytes32) {
-        return getDeliveryTmpState().refundAddress;
-    }
-
-    function checkMsgSenderInDelivery() internal view {
-        DeliveryTmpState storage state = getDeliveryTmpState();
-        if (!state.deliveryInProgress) {
-            revert NoDeliveryInProgress();
-        }
-
-        if (msg.sender != state.deliveryTarget) {
-            revert ForwardRequestFromWrongAddress(msg.sender, state.deliveryTarget);
-        }
     }
 }
