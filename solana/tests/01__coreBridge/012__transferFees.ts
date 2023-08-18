c53ff75724702cfb5a0af082dd0dd8be6ae6f2f0
gator-boi <gator-boi@users.noreply.github.com>
2023-08-18 15:16:45 -0500

    solana: add negative tests to 012__transferFees.ts

--- solana/tests/01__coreBridge/012__transferFees.ts
+++ solana/tests/01__coreBridge/012__transferFees.ts
@@ -11,6 +11,7 @@ import {
   expectIxOkDetails,
   invokeVerifySignaturesAndPostVaa,
   parallelPostVaa,
+  ETHEREUM_DEADBEEF_TOKEN_ADDRESS,
 } from "../helpers";
 import * as coreBridge from "../helpers/coreBridge";
 import { GOVERNANCE_EMITTER_ADDRESS } from "../helpers/coreBridge";
@@ -141,12 +142,176 @@ describe("Core Bridge -- Legacy Instruction: Transfer Fees", () => {
         "already in use"
       );
     });
+
+    it("Cannot Invoke `transfer_fees` with Invalid Governance Emitter", async () => {
+      // Create a bad governance emitter.
+      const governance = new GovernanceEmitter(
+        Buffer.from(ETHEREUM_DEADBEEF_TOKEN_ADDRESS).toString("hex"),
+        GOVERNANCE_SEQUENCE - 1
+      );
+      const guardians = new MockGuardians(GUARDIAN_SET_INDEX, GUARDIAN_KEYS);
+
+      // Vaa info.
+      const timestamp = 12345678;
+      const chain = 1;
+      const published = governance.publishWormholeTransferFees(
+        timestamp,
+        chain,
+        BigInt(69),
+        payer.publicKey.toBuffer()
+      );
+      const signedVaa = guardians.addSignatures(
+        published,
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+      );
+
+      // Post the VAA.
+      await invokeVerifySignaturesAndPostVaa(program, payer, signedVaa);
+
+      // Parse the vaa and update the guardian set index.
+      const parsedVaa = parseVaa(signedVaa);
+
+      // Create the instruction.
+      const ix = coreBridge.legacyTransferFeesIx(
+        program,
+        { payer: payer.publicKey, recipient: payer.publicKey },
+        parsedVaa
+      );
+
+      await expectIxErr(connection, [ix], [payer], "InvalidGovernanceEmitter");
+    });
+
+    it("Cannot Invoke `transfer_fees` with Invalid Governance Action", async () => {
+      // Vaa info.
+      const timestamp = 12345678;
+      const chain = 1;
+
+      // Publish the wrong VAA type.
+      const published = governance.publishWormholeSetMessageFee(timestamp, chain, BigInt(69));
+
+      const signedVaa = guardians.addSignatures(
+        published,
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+      );
+
+      // Post the VAA.
+      await invokeVerifySignaturesAndPostVaa(program, payer, signedVaa);
+
+      // Parse the vaa and update the guardian set index.
+      const parsedVaa = parseVaa(signedVaa);
+
+      // Create the instruction.
+      const ix = coreBridge.legacyTransferFeesIx(
+        program,
+        { payer: payer.publicKey, recipient: payer.publicKey },
+        parsedVaa
+      );
+
+      await expectIxErr(connection, [ix], [payer], "InvalidGovernanceAction");
+    });
+
+    it("Cannot Invoke `transfer_fees` with Invalid Target Chain", async () => {
+      // Fetch the default VAA.
+      const invalidTargetChain = 69;
+      const signedVaa = defaultVaa(new anchor.BN(69), payer.publicKey, invalidTargetChain);
+
+      // Post the VAA.
+      await invokeVerifySignaturesAndPostVaa(program, payer, signedVaa);
+
+      // Parse the vaa and update the guardian set index.
+      const parsedVaa = parseVaa(signedVaa);
+
+      // Create the instruction.
+      const ix = coreBridge.legacyTransferFeesIx(
+        program,
+        { payer: payer.publicKey, recipient: payer.publicKey },
+        parsedVaa
+      );
+
+      await expectIxErr(connection, [ix], [payer], "GovernanceForAnotherChain");
+    });
+
+    it("Cannot Invoke `transfer_fees` with Fee Larger than Max(u64)", async () => {
+      // Fetch the default VAA.
+      const signedVaa = defaultVaa(
+        new anchor.BN(Buffer.from("010000000000000000", "hex")),
+        payer.publicKey
+      );
+
+      // Post the VAA.
+      await invokeVerifySignaturesAndPostVaa(program, payer, signedVaa);
+
+      // Parse the vaa and update the guardian set index.
+      const parsedVaa = parseVaa(signedVaa);
+
+      // Create the instruction.
+      const ix = coreBridge.legacyTransferFeesIx(
+        program,
+        { payer: payer.publicKey, recipient: payer.publicKey },
+        parsedVaa
+      );
+
+      await expectIxErr(connection, [ix], [payer], "U64Overflow");
+    });
+
+    it("Cannot Invoke `transfer_fees` with Fee Larger Than Minimum Required Rent Balance", async () => {
+      const lamportBalance = await connection
+        .getAccountInfo(coreBridge.FeeCollector.address(program.programId))
+        .then((info) => info!.lamports);
+
+      // Fetch the default VAA.
+      const signedVaa = defaultVaa(new anchor.BN(lamportBalance), payer.publicKey);
+
+      // Post the VAA.
+      await invokeVerifySignaturesAndPostVaa(program, payer, signedVaa);
+
+      // Parse the vaa and update the guardian set index.
+      const parsedVaa = parseVaa(signedVaa);
+
+      // Create the instruction.
+      const ix = coreBridge.legacyTransferFeesIx(
+        program,
+        { payer: payer.publicKey, recipient: payer.publicKey },
+        parsedVaa
+      );
+
+      await expectIxErr(connection, [ix], [payer], "NotEnoughLamports");
+    });
+
+    it.skip("Cannot Invoke `transfer_fees` With Invalid Recipient", async () => {
+      // Invalid recipient.
+      const recipient = anchor.web3.Keypair.generate();
+
+      // Create a signed vaa with the payer as an invalid recipient.
+      const signedVaa = defaultVaa(new anchor.BN(69), payer.publicKey);
+
+      // Post the VAA.
+      await invokeVerifySignaturesAndPostVaa(program, payer, signedVaa);
+
+      // Invoke the instruction.
+      await expectIxErr(
+        connection,
+        [
+          coreBridge.legacyTransferFeesIx(
+            program,
+            { payer: payer.publicKey, recipient: recipient.publicKey },
+            parseVaa(signedVaa)
+          ),
+        ],
+        [payer],
+        "InvalidFeeRecipient"
+      );
+    });
   });
 });
 
-function defaultVaa(amount: anchor.BN, recipient: anchor.web3.PublicKey): Buffer {
+function defaultVaa(
+  amount: anchor.BN,
+  recipient: anchor.web3.PublicKey,
+  targetChain?: number
+): Buffer {
   const timestamp = 12345678;
-  const chain = 1;
+  const chain = targetChain === undefined ? 1 : targetChain;
   const published = governance.publishWormholeTransferFees(
     timestamp,
     chain,
