7d42d06641340ab8e7eff7431e7c73d14302769a
gator-boi <gator-boi@users.noreply.github.com>
2023-08-21 18:04:03 -0500

    solana: add negative attest token test and rename files

--- solana/programs/token-bridge/src/utils/token.rs
+++ solana/programs/token-bridge/src/utils/token.rs
@@ -1,4 +1,5 @@
 use crate::constants::{MAX_DECIMALS, MINT_AUTHORITY_SEED_PREFIX};
+use crate::error::TokenBridgeError::InvalidMint;
 use anchor_lang::prelude::*;
 use anchor_spl::token::Mint;
 use solana_program::program_option::COption;
@@ -7,7 +8,7 @@ pub fn require_native_mint(mint: &Mint) -> Result<()> {
     if let COption::Some(mint_authority) = mint.mint_authority {
         let (token_bridge_mint_authority, _) =
             Pubkey::find_program_address(&[MINT_AUTHORITY_SEED_PREFIX], &crate::ID);
-        require_keys_neq!(mint_authority, token_bridge_mint_authority);
+        require_keys_neq!(mint_authority, token_bridge_mint_authority, InvalidMint);
     }
 
     Ok(())
