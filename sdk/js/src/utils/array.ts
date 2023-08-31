ede5c5bc1ee94460fe764dd58d93e58311d74ec4
Paul Noel <panoel007@gmail.com>
2023-08-31 10:03:08 -0500

    sdk/js: change sei array conversion

--- sdk/js/src/utils/array.ts
+++ sdk/js/src/utils/array.ts
@@ -57,7 +57,7 @@ import { isValidSuiType } from "../sui";
  */
 export const isHexNativeTerra = (h: string): boolean => h.startsWith("01");
 
-const isLikely20ByteTerra = (h: string): boolean =>
+const isLikely20ByteCosmwasm = (h: string): boolean =>
   h.startsWith("000000000000000000000000");
 
 export const nativeTerraHexToDenom = (h: string): string =>
@@ -93,23 +93,29 @@ export const tryUint8ArrayToNative = (
     if (isHexNativeTerra(h)) {
       return nativeTerraHexToDenom(h);
     } else {
-      if (chainId === CHAIN_ID_TERRA2 && !isLikely20ByteTerra(h)) {
+      if (chainId === CHAIN_ID_TERRA2 && !isLikely20ByteCosmwasm(h)) {
         // terra 2 has 32 byte addresses for contracts and 20 for wallets
         return humanAddress("terra", a);
       }
       return humanAddress("terra", a.slice(-20));
     }
   } else if (chainId === CHAIN_ID_INJECTIVE) {
-    return humanAddress("inj", a.slice(-20));
+    const h = uint8ArrayToHex(a);
+    return humanAddress("inj", isLikely20ByteCosmwasm(h) ? a.slice(-20) : a);
   } else if (chainId === CHAIN_ID_ALGORAND) {
     return uint8ArrayToNativeStringAlgorand(a);
   } else if (chainId == CHAIN_ID_WORMCHAIN) {
-    // wormchain addresses are always 20 bytes.
-    return humanAddress("wormhole", a.slice(-20));
+    const h = uint8ArrayToHex(a);
+    return humanAddress(
+      "wormhole",
+      isLikely20ByteCosmwasm(h) ? a.slice(-20) : a
+    );
   } else if (chainId === CHAIN_ID_XPLA) {
-    return humanAddress("xpla", a.slice(-20));
+    const h = uint8ArrayToHex(a);
+    return humanAddress("xpla", isLikely20ByteCosmwasm(h) ? a.slice(-20) : a);
   } else if (chainId === CHAIN_ID_SEI) {
-    return humanAddress("sei", a.slice(-20));
+    const h = uint8ArrayToHex(a);
+    return humanAddress("sei", isLikely20ByteCosmwasm(h) ? a.slice(-20) : a);
   } else if (chainId === CHAIN_ID_NEAR) {
     throw Error("uint8ArrayToNative: Use tryHexToNativeStringNear instead.");
   } else if (chainId === CHAIN_ID_OSMOSIS) {
