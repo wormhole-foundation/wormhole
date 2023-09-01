9f7f4219f9efa84e7869e9d292bbad50c330d0f4
A5 Pickle <a5-pickle@users.noreply.github.com>
2023-09-01 08:01:52 -0500

    solana: refactor zero copy

--- solana/programs/core-bridge/src/zero_copy/config.rs
+++ solana/programs/core-bridge/src/zero_copy/config.rs
@@ -1,6 +1,6 @@
 use crate::state;
 use anchor_lang::{
-    prelude::{error, require_eq, AnchorDeserialize, ErrorCode, Result},
+    prelude::{error, require_eq, ErrorCode, Result},
     Space,
 };
 
@@ -9,28 +9,24 @@ pub struct Config<'a>(&'a [u8]);
 impl<'a> Config<'a> {
     /// The current guardian set index, used to decide which signature sets to accept.
     pub fn guardian_set_index(&self) -> u32 {
-        let mut buf = &self.0[..4];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        u32::from_le_bytes(self.0[..4].try_into().unwrap())
     }
 
     /// Lamports in the collection account
     pub fn last_lamports(&self) -> u64 {
-        let mut buf = &self.0[4..12];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        u64::from_le_bytes(self.0[4..12].try_into().unwrap())
     }
 
     /// Period for how long a guardian set is valid after it has been replaced by a new one.  This
     /// guarantees that VAAs issued by that set can still be submitted for a certain period.  In
     /// this period we still trust the old guardian set.
     pub fn guardian_set_ttl(&self) -> crate::types::Duration {
-        let mut buf = &self.0[12..16];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        u32::from_le_bytes(self.0[12..16].try_into().unwrap()).into()
     }
 
     /// Amount of lamports that needs to be paid to the protocol to post a message
     pub fn fee_lamports(&self) -> u64 {
-        let mut buf = &self.0[16..24];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        u64::from_le_bytes(self.0[16..24].try_into().unwrap())
     }
 
     pub fn parse(span: &'a [u8]) -> Result<Self> {
--- solana/programs/core-bridge/src/zero_copy/posted_message_v1.rs
+++ solana/programs/core-bridge/src/zero_copy/posted_message_v1.rs
@@ -1,5 +1,5 @@
 use crate::{state, types::Timestamp};
-use anchor_lang::prelude::{require, AnchorDeserialize, ErrorCode, Pubkey, Result};
+use anchor_lang::prelude::{require, ErrorCode, Pubkey};
 
 pub struct PostedMessageV1<'a>(&'a [u8]);
 
@@ -10,50 +10,44 @@ impl<'a> PostedMessageV1<'a> {
     const DISC_LEN: usize = Self::DISCRIMINATOR.len();
 
     pub fn consistency_level(&self) -> u8 {
-        let mut buf = &self.0[..1];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        self.0[0]
     }
 
     pub fn emitter_authority(&self) -> Pubkey {
-        let mut buf = &self.0[1..33];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        Pubkey::try_from(&self.0[1..33]).unwrap()
     }
 
     pub fn status(&self) -> state::MessageStatus {
-        let mut buf = &self.0[33..34];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        anchor_lang::AnchorDeserialize::deserialize(&mut &self.0[33..34]).unwrap()
     }
 
     pub fn posted_timestamp(&self) -> Timestamp {
-        let mut buf = &self.0[37..41];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        u32::from_le_bytes(self.0[37..41].try_into().unwrap()).into()
     }
 
     pub fn nonce(&self) -> u32 {
-        let mut buf = &self.0[41..45];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        u32::from_le_bytes(self.0[41..45].try_into().unwrap())
     }
 
     pub fn sequence(&self) -> u64 {
-        let mut buf = &self.0[45..53];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        u64::from_le_bytes(self.0[45..53].try_into().unwrap())
     }
 
     pub fn emitter(&self) -> Pubkey {
-        let mut buf = &self.0[55..87];
-        AnchorDeserialize::deserialize(&mut buf).unwrap()
+        Pubkey::try_from(&self.0[55..87]).unwrap()
     }
 
     pub fn payload_size(&self) -> usize {
-        let mut buf = &self.0[87..Self::PAYLOAD_START];
-        u32::deserialize(&mut buf).unwrap().try_into().unwrap()
+        u32::from_le_bytes(self.0[87..Self::PAYLOAD_START].try_into().unwrap())
+            .try_into()
+            .unwrap()
     }
 
     pub fn payload(&self) -> &'a [u8] {
         &self.0[Self::PAYLOAD_START..]
     }
 
-    pub fn parse(span: &'a [u8]) -> Result<Self> {
+    pub fn parse(span: &'a [u8]) -> anchor_lang::Result<Self> {
         require!(
             span.len() > Self::DISC_LEN,
             ErrorCode::AccountDidNotDeserialize
