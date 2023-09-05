bdd30d43a7bc5db87efb425a097431d62f3b840c
Sebastián Claudio Nale <sebinale@gmail.com>
2023-09-05 16:57:15 -0300

    AutoRelayer: adds overrides to upgrade scripts.

--- ethereum/ts-scripts/relayer/deliveryProvider/upgradeDeliveryProvider.ts
+++ ethereum/ts-scripts/relayer/deliveryProvider/upgradeDeliveryProvider.ts
@@ -6,36 +6,72 @@ import {
   getDeliveryProvider,
   getOperatingChains,
 } from "../helpers/env";
-import { deployDeliveryProviderImplementation } from "../helpers/deployments";
+import {
+  buildOverrides,
+  deployDeliveryProviderImplementation,
+} from "../helpers/deployments";
 
 const processName = "upgradeDeliveryProvider";
 init();
-const chains = getOperatingChains();
+const operatingChains = getOperatingChains();
+
+interface DeliveryProviderUpgrade {
+  deliveryProviderImplementations: Deployment[];
+}
 
 async function run() {
   console.log("Start!");
-  const output: any = {
+  const output: DeliveryProviderUpgrade = {
     deliveryProviderImplementations: [],
   };
 
-  for (let i = 0; i < chains.length; i++) {
-    const deliveryProviderImplementation = await deployDeliveryProviderImplementation(
-      chains[i]
-    );
-    await upgradeDeliveryProvider(chains[i], deliveryProviderImplementation);
+  const tasks = await Promise.allSettled(
+    operatingChains.map(async (chain) => {
+      const implementation = await deployDeliveryProviderImplementation(chain);
+      await upgradeDeliveryProvider(chain, implementation);
 
-    output.deliveryProviderImplementations.push(deliveryProviderImplementation);
+      return implementation;
+    }),
+  );
+  for (const task of tasks) {
+    if (task.status === "rejected") {
+      console.log(
+        `DeliveryProvider upgrade failed. ${task.reason?.stack || task.reason}`,
+      );
+    } else {
+      output.deliveryProviderImplementations.push(task.value);
+    }
   }
 
   writeOutputFiles(output, processName);
 }
 
-async function upgradeDeliveryProvider(chain: ChainInfo, newImpl: Deployment) {
-  console.log("About to upgrade relay provider for chain " + chain.chainId);
-  const provider = await getDeliveryProvider(chain);
-  const tx = await provider.upgrade(chain.chainId, newImpl.address);
-  await tx.wait();
-  console.log("Successfully upgraded relay provider " + chain.chainId);
+async function upgradeDeliveryProvider(
+  operatingChain: ChainInfo,
+  newImpl: Deployment,
+) {
+  console.log(
+    "About to upgrade relay provider for chain " + operatingChain.chainId,
+  );
+  const provider = await getDeliveryProvider(operatingChain);
+
+  const overrides = await buildOverrides(
+    () => provider.estimateGas.upgrade(operatingChain.chainId, newImpl.address),
+    operatingChain,
+  );
+  const tx = await provider.upgrade(
+    operatingChain.chainId,
+    newImpl.address,
+    overrides,
+  );
+  const receipt = await tx.wait();
+
+  if (receipt.status !== 1) {
+    throw new Error(
+      `Failed to upgrade DeliveryProvider on chain ${operatingChain.chainId}, tx id: ${tx.hash}`,
+    );
+  }
+  console.log("Successfully upgraded relay provider " + operatingChain.chainId);
 }
 
 run().then(() => console.log("Done!"));
