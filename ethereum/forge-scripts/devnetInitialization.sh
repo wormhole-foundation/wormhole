da9f0ef88c7e55cf815c4d0fdba6f8a257fe0dad
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-09-29 17:44:38 -0400

    Add private key back

--- ethereum/forge-scripts/devnetInitialization.sh
+++ ethereum/forge-scripts/devnetInitialization.sh
@@ -24,7 +24,7 @@ fi
 
 PRIVATE_KEY=0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d
 
-RPC_URL=$RPC_URL NETWORK=$NETWORK npm run deploy-contracts 
+PRIVATE_KEY=$PRIVATE_KEY RPC_URL=$RPC_URL NETWORK=$NETWORK npm run deploy-contracts 
 
 forge script ./forge-scripts/DeployTestToken.s.sol:DeployTestToken --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
 
