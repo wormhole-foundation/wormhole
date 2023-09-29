3adce639faafe1a0eef8362ea84297f5feebb753
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2023-09-29 11:11:43 -0400

    aptos: tooling for mainnet upgrades (#3338)
    * aptos: update upgrade script to support mainnet
    
    Also factor out named_addresses into a separate helper script
    
    * scripts: aptos support for gov proposal generation
    
    * sdk/js: add aptos nft_bridge mainnet address

--- sdk/js/src/utils/consts.ts
+++ sdk/js/src/utils/consts.ts
@@ -396,7 +396,8 @@ const TESTNET = {
     core: "0x5bc11445584a763c1fa7ed39081f1b920954da14e04b32440cba863d03e19625",
     token_bridge:
       "0x576410486a2da45eee6c949c995670112ddf2fbeedab20350d506328eefc9d4f",
-    nft_bridge: undefined,
+    nft_bridge:
+      "0x1bdffae984043833ed7fe223f7af7a3f8902d04129b14f801823e64827da7130",
   },
   sui: {
     core: "0x31358d198147da50db32eda2562951d53973a0c0ad5ed738e9b17d88b213d790",
