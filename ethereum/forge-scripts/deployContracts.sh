e0e6d7875b71af2fb19ab54b988677a6671f4bb0
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-09-29 17:22:58 -0400

    mses with script to work

--- ethereum/forge-scripts/deployContracts.sh
+++ ethereum/forge-scripts/deployContracts.sh
@@ -1,45 +1,56 @@
 #!/bin/bash
 
 # Check if the three arguments are provided
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 RPC_URL NETWORK CHAIN_ID"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 RPC_URL NETWORK"
   exit 1
 fi
 
 # Extract the arguments into separate variables
 RPC_URL="$1"
 NETWORK="$2"
-CHAIN_ID="$3"
 
 echo 'Here is the env file you are working with!'
 cat .env
+
+# Load the environment variables from .env
+if [ -f .env ]; then
+  source .env
+else
+  echo "The .env file does not exist."
+  exit 1
+fi
+
 
 # Step 0: Deploy 4 dummy contracts to match devnet addresses in Anvil to what they originally were in Ganache 
 # (the addresses depend on the number of contracts that have been previously deployed, and the wallet address, I believe!)
 forge script ./forge-scripts/DeployDummyContract.s.sol:DeployDummyContract --sig "run(uint256)" 2 --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast
 
 # Step 1: Run 'forge script DeployCore', get the JSON output from the specified file
-returnInfo=$(forge script ./forge-scripts/DeployCore.s.sol:DeployCore --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --json | tail -n 1)
-echo 'this is the return info from the core bridge deployment'
-echo $returnInfo
+forge script ./forge-scripts/DeployCore.s.sol:DeployCore --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast
+returnInfo=$(cat ./broadcast/DeployCore.s.sol/$INIT_EVM_CHAIN_ID/run-latest.json)
 # Extract the 'WORMHOLE_ADDRESS' value from 'returnInfo'
 WORMHOLE_ADDRESS=$(jq -r '.returns.deployedAddress.value' <<< "$returnInfo")
 
 echo "Wormhole address that will be used to initialize token bridge and nft bridge: $WORMHOLE_ADDRESS"
 
 # Step 2: Replace 'WORMHOLE_ADDRESS' in the .env file with the extracted value
-sed -i "s/^WORMHOLE_ADDRESS=.*$/WORMHOLE_ADDRESS=$WORMHOLE_ADDRESS/" .env || echo "WORMHOLE_ADDRESS=$WORMHOLE_ADDRESS" >> .env
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # on macOS's sed, the -i flag needs the '' argument to not create
+  # backup files
+  sed -i '' "s/^WORMHOLE_ADDRESS=.*$/WORMHOLE_ADDRESS=$WORMHOLE_ADDRESS/" .env || echo "WORMHOLE_ADDRESS=$WORMHOLE_ADDRESS" >> .env
+else
+  sed -i "s/^WORMHOLE_ADDRESS=.*$/WORMHOLE_ADDRESS=$WORMHOLE_ADDRESS/" .env || echo "WORMHOLE_ADDRESS=$WORMHOLE_ADDRESS" >> .env
+fi
 
 # Step 3: Run 'forge script DeployTokenBridge'
-returnInfo=$(forge script ./forge-scripts/DeployTokenBridge.s.sol:DeployTokenBridge --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --json | tail -n 1)
-echo 'this is the return info from the token bridge deployment'
-echo $returnInfo
+forge script ./forge-scripts/DeployTokenBridge.s.sol:DeployTokenBridge --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast 
+returnInfo=$(cat ./broadcast/DeployTokenBridge.s.sol/$INIT_EVM_CHAIN_ID/run-latest.json)
 TOKEN_BRIDGE_ADDRESS=$(jq -r '.returns.deployedAddress.value' <<< "$returnInfo")
 
 # Step 4: Run 'forge script DeployNFTBridge'
-returnInfo=$(forge script ./forge-scripts/DeployNFTBridge.s.sol:DeployNFTBridge --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --json | tail -n 1)
-echo 'this is the return info from the ngt bridge deployment'
-echo $returnInfo
+forge script ./forge-scripts/DeployNFTBridge.s.sol:DeployNFTBridge --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast
+returnInfo=$(cat ./broadcast/DeployNFTBridge.s.sol/$INIT_EVM_CHAIN_ID/run-latest.json)
 NFT_BRIDGE_ADDRESS=$(jq -r '.returns.deployedAddress.value' <<< "$returnInfo")
 
 echo "Deployment of Core Bridge, Token Bridge, and NFT Bridge completed successfully."
@@ -50,7 +61,7 @@ json="{\"WORMHOLE_ADDRESS\":\"$WORMHOLE_ADDRESS\",\"TOKEN_BRIDGE_ADDRESS\":\"$TO
 # Get the current time as a Unix timestamp
 current_time=$(date +%s)
 
-mkdir -p "./deployment-addresses/$NETWORK/$CHAIN_ID/"
+mkdir -p "./deployment-addresses/$NETWORK/$INIT_CHAIN_ID/"
 
-echo "$json" > "./deployment-addresses/$NETWORK/$CHAIN_ID/latest.json"
-echo "$json" > "./deployment-addresses/$NETWORK/$CHAIN_ID/$current_time.json"
+echo "$json" > "./deployment-addresses/$NETWORK/$INIT_CHAIN_ID/latest.json"
+echo "$json" > "./deployment-addresses/$NETWORK/$INIT_CHAIN_ID/$current_time.json"
