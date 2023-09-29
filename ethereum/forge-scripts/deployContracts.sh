57aee5bdb19e9fc09a60269bf3c362cb5611f527
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-09-29 14:58:45 -0400

    write deployment addresses to own file

--- ethereum/forge-scripts/deployContracts.sh
+++ ethereum/forge-scripts/deployContracts.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Check if the RPC_URL argument is provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 RPC_URL"
+# Check if the three arguments are provided
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 RPC_URL NETWORK CHAIN_ID"
   exit 1
 fi
 
+# Extract the arguments into separate variables
 RPC_URL="$1"
+NETWORK="$2"
+CHAIN_ID="$3"
 
 echo 'Here is the env file you are working with!'
 cat .env
@@ -15,10 +18,8 @@ cat .env
 # (the addresses depend on the number of contracts that have been previously deployed, and the wallet address, I believe!)
 forge script ./forge-scripts/DeployDummyContract.s.sol:DeployDummyContract --sig "run(uint256)" 2 --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast
 
-# Step 1: Run 'forge script DeployCore' 
-forge script ./forge-scripts/DeployCore.s.sol:DeployCore --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast
-# Get the JSON output from the specified file
-returnInfo=$(cat ./broadcast/DeployCore.s.sol/1/run-latest.json)
+# Step 1: Run 'forge script DeployCore', get the JSON output from the specified file
+returnInfo = ${forge script ./forge-scripts/DeployCore.s.sol:DeployCore --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --json | tail -n 1}
 
 # Extract the 'WORMHOLE_ADDRESS' value from 'returnInfo'
 WORMHOLE_ADDRESS=$(jq -r '.returns.deployedAddress.value' <<< "$returnInfo")
@@ -29,9 +30,20 @@ echo "Wormhole address that will be used to initialize token bridge and nft brid
 sed -i "s/^WORMHOLE_ADDRESS=.*$/WORMHOLE_ADDRESS=$WORMHOLE_ADDRESS/" .env || echo "WORMHOLE_ADDRESS=$WORMHOLE_ADDRESS" >> .env
 
 # Step 3: Run 'forge script DeployTokenBridge'
-forge script ./forge-scripts/DeployTokenBridge.s.sol:DeployTokenBridge --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast
+returnInfo = ${forge script ./forge-scripts/DeployTokenBridge.s.sol:DeployTokenBridge --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --json | tail -n 1}
+TOKEN_BRIDGE_ADDRESS=$(jq -r '.returns.deployedAddress.value' <<< "$returnInfo")
 
 # Step 4: Run 'forge script DeployNFTBridge'
-forge script ./forge-scripts/DeployNFTBridge.s.sol:DeployNFTBridge --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast
+returnInfo = ${forge script ./forge-scripts/DeployNFTBridge.s.sol:DeployNFTBridge --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --json | tail -n 1}
+NFT_BRIDGE_ADDRESS=$(jq -r '.returns.deployedAddress.value' <<< "$returnInfo")
 
 echo "Deployment of Core Bridge, Token Bridge, and NFT Bridge completed successfully."
+
+# TODO: Add other important addresses (setup, implementation, etc)
+json="{\"WORMHOLE_ADDRESS\":\"$WORMHOLE_ADDRESS\",\"TOKEN_BRIDGE_ADDRESS\":\"$TOKEN_BRIDGE_ADDRESS\",\"NFT_BRIDGE_ADDRESS\":\"$NFT_BRIDGE_ADDRESS\"}"
+
+# Get the current time as a Unix timestamp
+current_time=$(date +%s)
+
+echo "$json" > "./deployment-addresses/$NETWORK/$CHAIN_ID/latest.json"
+echo "$json" > "./deployment-addresses/$NETWORK/$CHAIN_ID/$current_time.json"
