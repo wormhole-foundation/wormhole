db57cdd865fa9a33e081138ea2f53973c650892b
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-10-03 21:48:01 -0400

    no env variables in deploy scripts explicitly

--- ethereum/forge-scripts/prompt_functions.sh
+++ ethereum/forge-scripts/prompt_functions.sh
@@ -4,6 +4,7 @@
 prompt_for_variable() {
     local var_name="$1"
     local custom_message="$2"
+    local default_value="$3"
 
     if [ -z "${!var_name}" ]; then
         echo "$var_name is not set."
@@ -11,7 +12,13 @@ prompt_for_variable() {
         if [ -n "$custom_message" ]; then
             displayed_message="$custom_message"
         fi
+        if [ -n "$default_value" ]; then
+            displayed_message="$displayed_message (default: $default_value)"
+        fi
         read -p "$displayed_message: " var_value
+         if [ -z "$var_value" ] && [ -n "$default_value" ]; then
+            var_value="$default_value"
+        fi
         export "$var_name=$var_value"
         echo "$var_name set to: ${!var_name}"
     else
