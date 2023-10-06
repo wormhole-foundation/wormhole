0ba604f352e03c1598bd575f5cfc9e6ec63f6089
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-10-06 12:56:13 -0400

    Upgrade and shutdown scripts

--- ethereum/forge-scripts/DeployTokenBridgeImplementationOnly.s.sol
+++ ethereum/forge-scripts/DeployTokenBridgeImplementationOnly.s.sol
@@ -2,10 +2,10 @@
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-import {BridgeShutdown} from "../contracts/bridge/BridgeShutdown.sol";
+import {BridgeImplementation} from "../contracts/bridge/BridgeImplementation.sol";
 import "forge-std/Script.sol";
 
-contract DeployTokenBridgeShutdown is Script {
+contract DeployTokenBridgeImplementationOnly is Script {
     // DryRun - Deploy the system
     function dryRun() public {
         _deploy();
@@ -17,8 +17,8 @@ contract DeployTokenBridgeShutdown is Script {
         vm.stopBroadcast();
     }
     function _deploy() internal returns (address deployedAddress) {
-        BridgeShutdown shutdown = new BridgeShutdown();
+        BridgeImplementation impl = new BridgeImplementation();
 
-        return address(shutdown);
+        return address(impl);
     }
 }
