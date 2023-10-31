f582f9df171320d007d1e45e754f6694356c5fcc
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-10-31 17:59:24 -0400

    fix comment

--- ethereum/forge-scripts/incrementNonce.sh
+++ ethereum/forge-scripts/incrementNonce.sh
@@ -9,7 +9,7 @@ prompt_for_variable "PRIVATE_KEY" "Please provide your private key"
 prompt_for_variable "NONCE" "Please provide the nonce amount to increment your account by"
 
 
-# Deploy dummy contract(s) to match devnet addresses in Anvil to what they originally were in Ganache 
-# (the addresses depend on the number of contracts that have been previously deployed, and the wallet address, I believe!)
+# Increment nonce to match devnet addresses in Anvil to what they originally were in Ganache 
+# (the addresses depend on the wallet nonce and address, I believe!)
 
 forge script ./forge-scripts/SetNonce.s.sol:SetNonce --sig "incrementNonce(uint64)" $NONCE --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast
