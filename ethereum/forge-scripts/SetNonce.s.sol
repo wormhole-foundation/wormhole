50c448e0f8eaaba7de1ea5b87ee311527b13bc35
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-11-01 12:36:21 -0400

    start and stop broadcast

--- ethereum/forge-scripts/SetNonce.s.sol
+++ ethereum/forge-scripts/SetNonce.s.sol
@@ -6,10 +6,14 @@ import "forge-std/Script.sol";
 
 contract SetNonce is Script {
     function setNonce(uint64 num) public {
+        vm.startBroadcast();
         vm.setNonce(msg.sender, num);
+        vm.stopBroadcast();
     }
 
     function incrementNonce(uint64 num) public {
+        vm.startBroadcast();
         vm.setNonce(msg.sender, vm.getNonce(msg.sender) + num);
+        vm.stopBroadcast();
     }
 }
