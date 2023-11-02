682f653044538da3a3e2c4ec2c8b024b66723951
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-11-01 22:29:03 -0400

    Add NatSpec docs to State.sol

--- ethereum/contracts/State.sol
+++ ethereum/contracts/State.sol
@@ -5,12 +5,21 @@ pragma solidity ^0.8.0;
 
 import "./Structs.sol";
 
+/// @title Events for Wormhole State contract
+/// @notice Defines events related to changes in guardian sets and message publication
 contract Events {
+    /// @notice Event emitted when the guardian set changes
+    /// @param oldGuardianIndex Index of the guardian set before the change
+    /// @param newGuardianIndex Index of the guardian set after the change
     event LogGuardianSetChanged(
         uint32 oldGuardianIndex,
         uint32 newGuardianIndex
     );
 
+    /// @notice Event emitted when a message is published
+    /// @param emitter_address The address from which the message is sent
+    /// @param nonce A nonce to ensure message uniqueness
+    /// @param payload The data payload of the message
     event LogMessagePublished(
         address emitter_address,
         uint32 nonce,
@@ -18,35 +27,42 @@ contract Events {
     );
 }
 
+/// @title Storage for Wormhole State contract
+/// @notice Contains the storage states used by the Wormhole protocol
 contract Storage {
+    /// @dev Internal structure for maintaining Wormhole's state
     struct WormholeState {
         Structs.Provider provider;
 
-        // Mapping of guardian_set_index => guardian set
+        /// @dev Mapping of guardian_set_index => guardian set
         mapping(uint32 => Structs.GuardianSet) guardianSets;
 
-        // Current active guardian set index
+        /// @dev Current active guardian set index
         uint32 guardianSetIndex;
 
-        // Period for which a guardian set stays active after it has been replaced
+        /// @dev Period for which a guardian set stays active after it has been replaced
         uint32 guardianSetExpiry;
 
-        // Sequence numbers per emitter
+        /// @dev Sequence numbers per emitter
         mapping(address => uint64) sequences;
 
-        // Mapping of consumed governance actions
+        /// @dev Mapping of consumed governance actions
         mapping(bytes32 => bool) consumedGovernanceActions;
 
-        // Mapping of initialized implementations
+        /// @dev Mapping of initialized implementations
         mapping(address => bool) initializedImplementations;
 
+        /// @dev Fee required to publish a message
         uint256 messageFee;
 
-        // EIP-155 Chain ID
+        /// @dev EIP-155 Chain ID
         uint256 evmChainId;
     }
 }
 
+/// @title State contract for Wormhole
+/// @notice Manages the state variables for the Wormhole protocol
 contract State {
+    /// @dev State variable containing all Wormhole state information
     Storage.WormholeState _state;
 }
