96c9f127db4d7015c420c458b3e925a4128c153f
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-11-01 21:24:14 -0400

    Add natsec docs for Implementation.sol

--- ethereum/contracts/Implementation.sol
+++ ethereum/contracts/Implementation.sol
@@ -8,10 +8,18 @@ import "./Governance.sol";
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Upgrade.sol";
 
+/// @title Wormhole Governance Implementation Contract upgrades
+/// @notice This contract is an implementation of Wormhole's Governance contract.
 contract Implementation is Governance {
+
+    /// @notice Emitted when a message is published to the Wormhole network
     event LogMessagePublished(address indexed sender, uint64 sequence, uint32 nonce, bytes payload, uint8 consistencyLevel);
 
     /// @notice Publish a message to be attested by the Wormhole network
+    /// @param nonce The nonce to ensure message uniqueness
+    /// @param payload The content of the emitted message to be published, an arbitrary byte array
+    /// @param consistencyLevel The level of finality to reach before the guardians will observe and attest the emitted event
+    /// @return sequence A number that is unique and increments for every message for a given emitter (and implicitly chain)
     function publishMessage(
         uint32 nonce,
         bytes memory payload,
@@ -25,11 +33,15 @@ contract Implementation is Governance {
         emit LogMessagePublished(msg.sender, sequence, nonce, payload, consistencyLevel);
     }
 
+    /// @dev Update and fetch the current sequence for a given emitter
+    /// @param emitter The address that emits a message
+    /// @return sequence The next sequence for a given emitter
     function useSequence(address emitter) internal returns (uint64 sequence) {
         sequence = nextSequence(emitter);
         setNextSequence(emitter, sequence + 1);
     }
 
+    /// @notice Initializes the contract with the EVM chain ID mapping upon deployment or upgrade
     function initialize() initializer public virtual {
         // this function needs to be exposed for an upgrade to pass
         if (evmChainId() == 0) {
@@ -61,6 +73,7 @@ contract Implementation is Governance {
         }
     }
 
+    /// @dev Modifier to ensure the contract is only initialized once
     modifier initializer() {
         address implementation = ERC1967Upgrade._getImplementation();
 
@@ -74,7 +87,9 @@ contract Implementation is Governance {
         _;
     }
 
+    /// @dev Fallback function that reverts all calls
     fallback() external payable {revert("unsupported");}
 
+    /// @dev Receive function that reverts all transactions with assets
     receive() external payable {revert("the Wormhole contract does not accept assets");}
 }
