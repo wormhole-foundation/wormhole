69a4f84d11c6b38cf620668c6c55001d60292b43
Paul Noel <panoel007@gmail.com>
2023-11-03 13:06:18 -0500

    node/pkg/telemetry: prometheus remote write

--- node/cmd/ccq/status.go
+++ node/cmd/ccq/status.go
@@ -1,11 +1,13 @@
 package ccq
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/certusone/wormhole/node/pkg/common"
+	promremotew "github.com/certusone/wormhole/node/pkg/telemetry/prom_remote_write"
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
@@ -35,4 +37,26 @@ func (s *statusServer) handleHealth(w http.ResponseWriter, r *http.Request) {
 	s.logger.Debug("health check")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "ok")
+}
+
+func RunPrometheusScraper(ctx context.Context, logger *zap.Logger, info promremotew.PromTelemetryInfo) error {
+	promLogger := logger.With(zap.String("component", "prometheus_scraper"))
+	errC := make(chan error)
+	common.StartRunnable(ctx, errC, false, "prometheus_scraper", func(ctx context.Context) error {
+		t := time.NewTicker(15 * time.Second)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			case <-t.C:
+				err := promremotew.ScrapeAndSendLocalMetrics(ctx, info, promLogger)
+				if err != nil {
+					promLogger.Error("ScrapeAndSendLocalMetrics error", zap.Error(err))
+					return err
+				}
+			}
+		}
+	})
+	return nil
 }
