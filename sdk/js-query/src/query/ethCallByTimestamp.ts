58572e9eafe5e91a53a723b04baf9780cf81c827
Evan Gray <battledingo@gmail.com>
2023-11-09 22:01:44 -0500

    sdk/js-query: deserialization support

--- sdk/js-query/src/query/ethCallByTimestamp.ts
+++ sdk/js-query/src/query/ethCallByTimestamp.ts
@@ -2,7 +2,9 @@ import { Buffer } from "buffer";
 import { BinaryWriter } from "./BinaryWriter";
 import { BlockTag, EthCallData } from "./ethCall";
 import { ChainQueryType, ChainSpecificQuery } from "./request";
-import { hexToUint8Array, isValidHexString } from "./utils";
+import { coalesceUint8Array, hexToUint8Array, isValidHexString } from "./utils";
+import { BinaryReader } from "./BinaryReader";
+import { ChainSpecificResponse } from "./response";
 
 export class EthCallByTimestampQueryRequest implements ChainSpecificQuery {
   targetTimestamp: bigint;
@@ -41,6 +43,33 @@ export class EthCallByTimestampQueryRequest implements ChainSpecificQuery {
     });
     return writer.data();
   }
+
+  static from(bytes: string | Uint8Array): EthCallByTimestampQueryRequest {
+    const reader = new BinaryReader(coalesceUint8Array(bytes));
+    return this.fromReader(reader);
+  }
+
+  static fromReader(reader: BinaryReader): EthCallByTimestampQueryRequest {
+    const targetTimestamp = reader.readUint64();
+    const targetBlockHintLength = reader.readUint32();
+    const targetBlockHint = reader.readString(targetBlockHintLength);
+    const followingBlockHintLength = reader.readUint32();
+    const followingBlockHint = reader.readString(followingBlockHintLength);
+    const callDataLength = reader.readUint8();
+    const callData: EthCallData[] = [];
+    for (let idx = 0; idx < callDataLength; idx++) {
+      const to = reader.readHex(20);
+      const dataLength = reader.readUint32();
+      const data = reader.readHex(dataLength);
+      callData.push({ to, data });
+    }
+    return new EthCallByTimestampQueryRequest(
+      targetTimestamp,
+      targetBlockHint,
+      followingBlockHint,
+      callData
+    );
+  }
 }
 
 function parseBlockHint(blockHint: BlockTag): string {
@@ -61,4 +90,66 @@ function parseBlockHint(blockHint: BlockTag): string {
   }
 
   return blockHint;
+}
+
+export class EthCallByTimestampQueryResponse implements ChainSpecificResponse {
+  constructor(
+    public targetBlockNumber: bigint,
+    public targetBlockHash: string,
+    public targetBlockTime: bigint,
+    public followingBlockNumber: bigint,
+    public followingBlockHash: string,
+    public followingBlockTime: bigint,
+    public results: string[] = []
+  ) {}
+
+  type(): ChainQueryType {
+    return ChainQueryType.EthCallByTimeStamp;
+  }
+
+  serialize(): Uint8Array {
+    const writer = new BinaryWriter()
+      .writeUint64(this.targetBlockNumber)
+      .writeUint8Array(hexToUint8Array(this.targetBlockHash))
+      .writeUint64(this.targetBlockTime)
+      .writeUint64(this.followingBlockNumber)
+      .writeUint8Array(hexToUint8Array(this.followingBlockHash))
+      .writeUint64(this.followingBlockTime)
+      .writeUint8(this.results.length);
+    for (const result of this.results) {
+      const arr = hexToUint8Array(result);
+      writer.writeUint32(arr.length).writeUint8Array(arr);
+    }
+    return writer.data();
+  }
+
+  static from(bytes: string | Uint8Array): EthCallByTimestampQueryResponse {
+    const reader = new BinaryReader(coalesceUint8Array(bytes));
+    return this.fromReader(reader);
+  }
+
+  static fromReader(reader: BinaryReader): EthCallByTimestampQueryResponse {
+    const targetBlockNumber = reader.readUint64();
+    const targetBlockHash = reader.readHex(32);
+    const targetBlockTime = reader.readUint64();
+    const followingBlockNumber = reader.readUint64();
+    const followingBlockHash = reader.readHex(32);
+    const followingBlockTime = reader.readUint64();
+    const resultsLength = reader.readUint8();
+    const results: string[] = [];
+    for (let idx = 0; idx < resultsLength; idx++) {
+      const resultLength = reader.readUint32();
+      const result = reader.readHex(resultLength);
+      results.push(result);
+    }
+    return new EthCallByTimestampQueryResponse(
+      targetBlockNumber,
+      targetBlockHash,
+      targetBlockTime,
+      followingBlockNumber,
+      followingBlockHash,
+      followingBlockTime,
+      results
+    );
+  }
 }
