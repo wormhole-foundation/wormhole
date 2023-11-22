ea2795da8145b8a8bbbe5877f2b4182b52b9bdfd
solanoe <solanoepalacio@gmail.com>
2023-11-22 16:44:00 -0300

    gRelayer: rename configureDeliveryProvider -> initializeDeliveryProvider

--- ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
+++ ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
@@ -3,7 +3,7 @@
 npx tsx ./ts-scripts/relayer/config/checkNetworks.ts --set-last-run \
   && npx tsx ./ts-scripts/relayer/deliveryProvider/deployDeliveryProvider.ts \
   && npx tsx ./ts-scripts/relayer/wormholeRelayer/deployWormholeRelayer.ts \
-  && npx tsx ./ts-scripts/relayer/deliveryProvider/configureDeliveryProvider.ts \
+  && npx tsx ./ts-scripts/relayer/deliveryProvider/initializeDeliveryProvider.ts \
   && npx tsx ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
   && npx tsx ./ts-scripts/relayer/wormholeRelayer/registerChainsWormholeRelayer.ts \
   && npx tsx ./ts-scripts/relayer/config/syncContractsJson.ts
