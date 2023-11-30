e6564c2d014f9666456bcc61058bd663bca6aa96
Evan Gray <battledingo@gmail.com>
2023-11-30 11:29:02 -0500

    tilt: eth relayer pin tsx

--- ethereum/ts-scripts/relayer/shell/deployInContainer.sh
+++ ethereum/ts-scripts/relayer/shell/deployInContainer.sh
@@ -1,7 +1,7 @@
  echo "deploying generic relayer contracts" \ 
-  npx tsx ./ts-scripts/relayer/create2Factory/deployCreate2Factory.ts \
-  && npx tsx ./ts-scripts/relayer/deliveryProvider/deployDeliveryProvider.ts \
-  && npx tsx ./ts-scripts/relayer/wormholeRelayer/deployWormholeRelayer.ts \
-  && npx tsx ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
-  && npx tsx ./ts-scripts/relayer/wormholeRelayer/registerChainsWormholeRelayerSelfSign.ts \
-  && npx tsx ./ts-scripts/relayer/deliveryProvider/configureDeliveryProvider.ts \
+  npx tsx@4.5.1 ./ts-scripts/relayer/create2Factory/deployCreate2Factory.ts \
+  && npx tsx@4.5.1 ./ts-scripts/relayer/deliveryProvider/deployDeliveryProvider.ts \
+  && npx tsx@4.5.1 ./ts-scripts/relayer/wormholeRelayer/deployWormholeRelayer.ts \
+  && npx tsx@4.5.1 ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
+  && npx tsx@4.5.1 ./ts-scripts/relayer/wormholeRelayer/registerChainsWormholeRelayerSelfSign.ts \
+  && npx tsx@4.5.1 ./ts-scripts/relayer/deliveryProvider/configureDeliveryProvider.ts \
