35d9545197db1615e79d1b789a0ed758d4bec6b9
Sebastián Claudio Nale <sebinale@gmail.com>
2024-01-11 22:12:57 -0300

    evm: adds `WormholeRelayer` implementation addresses to contracts.json

--- ethereum/ts-scripts/relayer/config/syncContractsJson.ts
+++ ethereum/ts-scripts/relayer/config/syncContractsJson.ts
@@ -1,23 +1,23 @@
 import { readFileSync, writeFileSync } from "fs";
 import {
-  getWormholeRelayer,
-  getCreate2Factory,
-  getMockIntegration,
-  getDeliveryProvider,
   init,
-  loadChains,
   updateContractAddress,
   Deployment,
+  loadDeliveryProviders,
+  loadWormholeRelayers,
+  loadMockIntegrations,
+  loadCreate2Factories,
+  loadWormholeRelayerImplementations,
 } from "../helpers/env";
 
 const env = init();
-const chains = loadChains();
 
 interface ContractsJson {
   deliveryProviders: Deployment[];
   wormholeRelayers: Deployment[];
   mockIntegrations: Deployment[];
   create2Factories: Deployment[];
+  wormholeRelayerImplementations: Deployment[];
 }
 
 async function main() {
@@ -25,28 +25,12 @@ async function main() {
   const contractsFile = readFileSync(path, "utf8");
   const contracts: ContractsJson = JSON.parse(contractsFile);
   console.log(`Old:\n${contractsFile}`);
-  contracts.deliveryProviders = [];
-  contracts.wormholeRelayers = [];
-  contracts.mockIntegrations = [];
-  contracts.create2Factories = [];
-  for (const chain of chains) {
-    updateContractAddress(contracts.deliveryProviders, {
-      chainId: chain.chainId,
-      address: (await getDeliveryProvider(chain)).address,
-    });
-    updateContractAddress(contracts.wormholeRelayers, {
-      chainId: chain.chainId,
-      address: (await getWormholeRelayer(chain)).address,
-    });
-    updateContractAddress(contracts.mockIntegrations, {
-      chainId: chain.chainId,
-      address: (await getMockIntegration(chain)).address,
-    });
-    updateContractAddress(contracts.create2Factories, {
-      chainId: chain.chainId,
-      address: (await getCreate2Factory(chain)).address,
-    });
-  }
+  contracts.create2Factories = loadCreate2Factories();
+  contracts.wormholeRelayers = loadWormholeRelayers();
+  contracts.wormholeRelayerImplementations = loadWormholeRelayerImplementations();
+  contracts.deliveryProviders = loadDeliveryProviders();
+  contracts.mockIntegrations = loadMockIntegrations();
+
   const newStr = JSON.stringify(contracts, undefined, 2);
   console.log(`New:\n${newStr}`);
   writeFileSync(path, newStr);
