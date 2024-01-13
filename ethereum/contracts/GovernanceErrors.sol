8829a6e5f94d013a9a5faaecd7b14e7b2be40074
Luis Lucena <lucenaluis137@gmail.com>
2024-01-13 17:08:56 +0100

    ✨ Feat: add new GovernanceErrors proposal

---
+++ ethereum/contracts/GovernanceErrors.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+abstract contract GovernanceErrors {
+    // Base chains
+    error InvalidFork();
+    /*
+    error InvalidModule();
+    error InvalidChain();
+    
+    // Guardian
+    error NewGuardianSetIsEmpty();
+    error IndexMustIncreaseInStepsOf1();
+
+    // Not chain
+    error NotAFork();
+    error InvalidEVMChain();
+    */
+}
