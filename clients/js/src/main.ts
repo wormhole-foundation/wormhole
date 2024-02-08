2950cc626b21cbecebf33acffb981c0c40316a4e
Ilya Sukhan <126617855+kolen44@users.noreply.github.com>
2024-02-08 08:09:39 +0300

    Update main.ts
    The following changes have been made to this improved code:
    
    - Removed comments that repeat obvious information.
    - The argv constant is defined to separate it from the yargs(hideBin(process.argv)) call.
    - Code formatted to improve readability.

--- clients/js/src/main.ts
+++ clients/js/src/main.ts
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-// Side effects are here to trigger before the afflicted libraries' on-import warnings can be emitted.
-// It is also imported so that it can side-effect without being tree-shaken.
-import "./side-effects";
 import { YargsCommandModule } from "./cmds/Yargs";
 import { CLI_COMMAND_MODULES } from "./cmds";
 
-yargs(hideBin(process.argv))
-  // Build CLI commands dinamically from CLI_COMMAND_MODULES list
+// Side effects are here to trigger before the affected libraries' on-import warnings can be emitted.
+import "./side-effects";
+
+const argv = yargs(hideBin(process.argv))
+  // Build CLI commands dynamically from CLI_COMMAND_MODULES list
   // Documentation about command hierarchy can be found here: https://github.com/yargs/yargs/blob/main/docs/advanced.md#example-command-hierarchy-using-indexmjs
   .command(CLI_COMMAND_MODULES as YargsCommandModule[])
   .strict()
-  .demandCommand().argv;
+  .demandCommand()
+  .argv;
