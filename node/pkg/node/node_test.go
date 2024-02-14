a4882e12bedfa191af24b3010d08bafe827d7d5c
Jeff Schroeder <jeffschroeder@computer.org>
2024-02-14 15:19:10 -0500

    node: override the p2p advertised address
    When behind a private network such as in the case of a guardian running
    in Kubernetes, or behind a NAT, the default p2p setup doesn't work. It
    looks at all addresses that it is listening on and advertises them on p2p
    as the addresses for contacting the guardian. This patch is the first
    step towards allowing specifying a custom ip address to be advertised on
    the gossip p2p network.
    
    For example, a guardian running inside kubernetes can post a reserved ip
    address of the incoming ingress/load balancer that sends the traffic in
    to their guardian.

--- node/pkg/node/node_test.go
+++ node/pkg/node/node_test.go
@@ -190,7 +190,7 @@ func mockGuardianRunnable(t testing.TB, gs []*mockGuardian, mockGuardianIndex ui
 			GuardianOptionNoAccountant(), // disable accountant
 			GuardianOptionGovernor(true),
 			GuardianOptionGatewayRelayer("", nil), // disable gateway relayer
-			GuardianOptionP2P(gs[mockGuardianIndex].p2pKey, networkID, bootstrapPeers, nodeName, false, cfg.p2pPort, "", 0, "", func() string { return "" }),
+			GuardianOptionP2P(gs[mockGuardianIndex].p2pKey, networkID, bootstrapPeers, nodeName, false, cfg.p2pPort, "", 0, "", "", func() string { return "" }),
 			GuardianOptionPublicRpcSocket(cfg.publicSocket, publicRpcLogDetail),
 			GuardianOptionPublicrpcTcpService(cfg.publicRpc, publicRpcLogDetail),
 			GuardianOptionPublicWeb(cfg.publicWeb, cfg.publicSocket, "", false, ""),
