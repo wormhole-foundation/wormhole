c3ba200a4d681133b43d87088a77e0d33be55764
Bruce Riley <bruce@wormholelabs.xyz>
2024-03-28 11:02:53 -0500

    eth: deploy with forge

---
+++ ethereum/forge-deploy-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# The relayer won't build because it requires a newer compiler.
+rm -rf contracts/interfaces/relayer
+rm -rf contracts/mock/relayer
+rm -rf contracts/relayer
+rm -rf forge-test/relayer
+
+FOUNDRY_PROFILE=deploy forge build
+
+# To deploy the contracts, set up the .env file and do:
+# MNEMONIC=<redacted> ./forge-scripts/deployCoreBridge.sh
+# MNEMONIC=<redacted> ./forge-scripts/deployTokenBridge.sh
