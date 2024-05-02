eef7d723235fd5fcaee3187cc9e0e4664c3bd965
Sebastián Claudio Nale <sebinale@gmail.com>
2024-05-02 19:34:05 -0300

    evm: removes obsolete SR deployment steps

--- ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
+++ ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
@@ -5,5 +5,4 @@ npx tsx ./ts-scripts/relayer/config/checkNetworks.ts \
   && npx tsx ./ts-scripts/relayer/wormholeRelayer/deployWormholeRelayer.ts \
   && npx tsx ./ts-scripts/relayer/deliveryProvider/initializeDeliveryProvider.ts \
   && npx tsx ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
-  && npx tsx ./ts-scripts/relayer/wormholeRelayer/registerChainsWormholeRelayer.ts \
-  && npx tsx ./ts-scripts/relayer/config/syncContractsJson.ts
+  && npx tsx ./ts-scripts/relayer/wormholeRelayer/registerChainsWormholeRelayer.ts
--- ethereum/ts-scripts/relayer/shell/deployConfigureTest.sh
+++ ethereum/ts-scripts/relayer/shell/deployConfigureTest.sh
@@ -4,8 +4,7 @@ npx tsx ./ts-scripts/relayer/config/checkNetworks.ts \
   && npx tsx ./ts-scripts/relayer/wormholeRelayer/deployWormholeRelayer.ts \
   && npx tsx ./ts-scripts/relayer/deliveryProvider/initializeDeliveryProvider.ts \
   && npx tsx ./ts-scripts/relayer/wormholeRelayer/registerChainsWormholeRelayerSelfSign.ts \
-  && npx tsx ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
-  && npx tsx ./ts-scripts/relayer/config/syncContractsJson.ts 
+  && npx tsx ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts
   # && npx tsx ./ts-scripts/relayer/mockIntegration/messageTest.ts 
 
  # put this as 2nd script if not deployed aleady
