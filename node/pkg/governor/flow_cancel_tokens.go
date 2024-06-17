b518e760267cd067db6afd71546a1984b02a2be3
John Saigle <john@asymmetric.re>
2024-06-17 10:04:49 -0400

    node: Update flow cancel tokens
    - Add canonical Solana and Ethereum stablecoins
    - Remove Aave USDC

--- node/pkg/governor/flow_cancel_tokens.go
+++ node/pkg/governor/flow_cancel_tokens.go
@@ -11,10 +11,10 @@ package governor
 func FlowCancelTokenList() []tokenConfigEntry {
 	return []tokenConfigEntry{
 		// USDC variants
-		{chain: 2, addr: "000000000000000000000000bcca60bb61934080951369a648fb03df4f96263c", symbol: "aUSDC"},
+		{chain: 1, addr: "c6fa7af3bedbad3a3d65f36aabc97431b1bbe4c2d2f6e0e47ca60203452f5d61", symbol: "USDC"},
+		{chain: 2, addr: "000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", symbol: "USDC"},
 
 		// USDT variants
-		{chain: 1, addr: "b7db4e83eb727f1187bd7a50303f5b4e4e943503da8571ad6564a51131504792", symbol: ""},
 		{chain: 1, addr: "ce010e60afedb22717bd63192f54145a3f965a33bb82d2c7029eb2ce1e208264", symbol: "USDT"},
 		{chain: 2, addr: "000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7", symbol: "USDT"},
 
