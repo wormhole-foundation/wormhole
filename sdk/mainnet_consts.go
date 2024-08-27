6ea6c1a3997ea35dd7576cf98f218e6cd99f1101
Bruce Riley <bruce@wormholelabs.xyz>
2024-08-27 14:09:33 -0500

    Node: Fix repair_solana tool

--- sdk/mainnet_consts.go
+++ sdk/mainnet_consts.go
@@ -12,12 +12,9 @@ import (
 //
 // This list is duplicated a couple times across the codebase - make to to update all copies!
 var PublicRPCEndpoints = []string{
-	"https://wormhole-v2-mainnet-api.certus.one",
-	"https://wormhole.inotel.ro",
 	"https://wormhole-v2-mainnet-api.mcf.rocks",
 	"https://wormhole-v2-mainnet-api.chainlayer.network",
 	"https://wormhole-v2-mainnet-api.staking.fund",
-	"https://wormhole-v2-mainnet.01node.com",
 }
 
 type (
