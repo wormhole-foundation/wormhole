4f0e46f6bfbdc7964d0a9a8153a866c37e19a6d1
Joel Smith <joelsmith.2019@gmail.com>
2025-01-21 10:57:59 -0600

    chore: add cw wormhole interchain tests (#4189)
    * wormchain: cw_wormhole ict start
    
    * wormchain(interchaintest): initial test implementation
    
    * Add Additional Core Contract ICTs
    
    * Finish Core Contract VAA ICTs
    
    * Add Replay Protection Checks
    
    * Extract Helper Commands
    
    * Fix Modify Genesis
    
    * Hook into ICT Workflow
    
    * Add Missing UpgradeContract VAA Test
    
    * Add Additional Fee Checks
    
    ---------
    
    Co-authored-by: Kaku <105181329+kakucodes@users.noreply.github.com>
    Co-authored-by: Obie Kaku <obiekaku@gmail.com>

--- wormchain/interchaintest/malformed_payload_test.go
+++ wormchain/interchaintest/malformed_payload_test.go
@@ -58,7 +58,7 @@ func TestMalformedPayload(t *testing.T) {
 	fmt.Println("Core contract code id: ", coreContractCodeId)
 
 	// Instantiate wormhole core contract
-	coreInstantiateMsg := helpers.CoreContractInstantiateMsg(t, wormchainConfig, guardians)
+	coreInstantiateMsg := helpers.CoreContractInstantiateMsg(t, wormchainConfig, vaa.ChainIDWormchain, guardians)
 	coreContractAddr := helpers.InstantiateContract(t, ctx, wormchain, "faucet", coreContractCodeId, "wormhole_core", coreInstantiateMsg, guardians)
 	fmt.Println("Core contract address: ", coreContractAddr)
 
