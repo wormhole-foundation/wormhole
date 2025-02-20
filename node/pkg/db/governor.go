7aac27a7d1ee0cf12a20e036cf950259548da739
John Saigle <john@asymmetric.re>
2025-02-20 09:47:30 -0500

    Fix lint

--- node/pkg/db/governor.go
+++ node/pkg/db/governor.go
@@ -60,8 +60,8 @@ type Transfer struct {
 	MsgID          string
 	Hash           string
 	TargetAddress  vaa.Address
-	// The destination chain. 
-	TargetChain    vaa.ChainID
+	// The destination chain.
+	TargetChain vaa.ChainID
 }
 
 func (t *Transfer) Marshal() ([]byte, error) {
