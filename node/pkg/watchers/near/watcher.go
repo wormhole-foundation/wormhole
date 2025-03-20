0aaf0cb0d9f1f67171ad1e5a189fb2020f6fdf52
Bruce Riley <bruce@wormholelabs.xyz>
2025-03-20 14:29:26 -0500

    Fix a few more things

--- node/pkg/watchers/near/watcher.go
+++ node/pkg/watchers/near/watcher.go
@@ -343,7 +343,7 @@ func (e *Watcher) schedule(ctx context.Context, job *transactionProcessingJob, d
 				select {
 				case <-ctx.Done():
 					return nil
-				case e.transactionProcessingQueue <- job:
+				case e.transactionProcessingQueue <- job: //nolint:channelcheck // Only blocking this go routine.
 				}
 			}
 			return nil
