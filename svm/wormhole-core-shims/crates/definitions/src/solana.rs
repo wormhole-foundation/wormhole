31069f0dc61f185613d85252f32b5e2efd62dd94
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2025-03-27 14:53:37 +0000

    svm/definitions: use const computation to derive static PDAs
    This allows us to only have to specify the program id

--- svm/wormhole-core-shims/crates/definitions/src/solana.rs
+++ svm/wormhole-core-shims/crates/definitions/src/solana.rs
@@ -2,37 +2,86 @@
 /// devnet.
 pub const CHAIN_ID: u16 = 1;
 
-use solana_program::{pubkey, pubkey::Pubkey};
+use const_crypto::{bs58, ed25519};
 
-pub const POST_MESSAGE_SHIM_PROGRAM_ID: Pubkey =
-    pubkey!("EtZMZM22ViKMo4r5y4Anovs3wKQ2owUmDpjygnMMcdEX");
-pub const POST_MESSAGE_SHIM_EVENT_AUTHORITY: Pubkey =
-    pubkey!("HQS31aApX3DDkuXgSpV9XyDUNtFgQ31pUn5BNWHG2PSp");
-pub const POST_MESSAGE_SHIM_EVENT_AUTHORITY_BUMP: u8 = 255;
-
-pub const VERIFY_VAA_SHIM_PROGRAM_ID: Pubkey =
-    pubkey!("EFaNWErqAtVWufdNb7yofSHHfWFos843DFpu4JBw24at");
+use solana_program::pubkey::Pubkey;
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "testnet")] {
         /// Core Bridge program ID on Solana devnet.
-        pub const CORE_BRIDGE_PROGRAM_ID: Pubkey = pubkey!("3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5");
-        pub const CORE_BRIDGE_FEE_COLLECTOR: Pubkey = pubkey!("7s3a1ycs16d6SNDumaRtjcoyMaTDZPavzgsmS3uUZYWX");
-        pub const CORE_BRIDGE_CONFIG: Pubkey = pubkey!("6bi4JGDoRwUs9TYBuvoA7dUVyikTJDrJsJU1ew6KVLiu");
+        pub const CORE_BRIDGE_PROGRAM_ID_ARRAY: [u8; 32]
+            bs58::decode_pubkey("3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5");
+        pub const CORE_BRIDGE_PROGRAM_ID_ARRAY: [u8; 32] =
+
+        /// Post message shim program ID on Solana devnet.
+        pub const POST_MESSAGE_SHIM_PROGRAM_ID_ARRAY: [u8; 32] =
+            bs58::decode_pubkey("EtZMZM22ViKMo4r5y4Anovs3wKQ2owUmDpjygnMMcdEX");
+
+        /// Verify VAA shim program ID on Solana devnet.
+        pub const VERIFY_VAA_SHIM_PROGRAM_ID_ARRAY: [u8;32] =
+            bs58::decode_pubkey("EFaNWErqAtVWufdNb7yofSHHfWFos843DFpu4JBw24at");
+
+
     } else if #[cfg(feature = "localnet")] {
         /// Core Bridge program ID on Wormhole's Tilt (dev) network.
-        pub const CORE_BRIDGE_PROGRAM_ID: Pubkey = pubkey!("Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o");
-        pub const CORE_BRIDGE_FEE_COLLECTOR: Pubkey = pubkey!("GXBsgBD3LDn3vkRZF6TfY5RqgajVZ4W5bMAdiAaaUARs");
-        pub const CORE_BRIDGE_CONFIG: Pubkey = pubkey!("FKoMTctsC7vJbEqyRiiPskPnuQx2tX1kurmvWByq5uZP");
+        pub const CORE_BRIDGE_PROGRAM_ID_ARRAY: [u8; 32] =
+            bs58::decode_pubkey("Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o");
+
+        /// Post message shim program ID on Wormhole's Tilt (dev) network.
+        pub const POST_MESSAGE_SHIM_PROGRAM_ID_ARRAY: [u8; 32] =
+            bs58::decode_pubkey("EtZMZM22ViKMo4r5y4Anovs3wKQ2owUmDpjygnMMcdEX");
+
+        /// Verify VAA shim program ID on Wormhole's Tilt (dev) network.
+        pub const VERIFY_VAA_SHIM_PROGRAM_ID_ARRAY: [u8;32] =
+            bs58::decode_pubkey("EFaNWErqAtVWufdNb7yofSHHfWFos843DFpu4JBw24at");
+
     }
     // Default to mainnet.
     else {
         /// Core Bridge program ID on Solana mainnet.
-        pub const CORE_BRIDGE_PROGRAM_ID: Pubkey = pubkey!("worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth");
-        pub const CORE_BRIDGE_FEE_COLLECTOR: Pubkey = pubkey!("9bFNrXNb2WTx8fMHXCheaZqkLZ3YCCaiqTftHxeintHy");
-        pub const CORE_BRIDGE_CONFIG: Pubkey = pubkey!("2yVjuQwpsvdsrywzsJJVs9Ueh4zayyo5DYJbBNc3DDpn");
+        pub const CORE_BRIDGE_PROGRAM_ID_ARRAY: [u8; 32] =
+            bs58::decode_pubkey("worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth");
+
+        /// Post message shim program ID on Solana mainnet.
+        pub const POST_MESSAGE_SHIM_PROGRAM_ID_ARRAY: [u8; 32] =
+            bs58::decode_pubkey("EtZMZM22ViKMo4r5y4Anovs3wKQ2owUmDpjygnMMcdEX");
+
+        /// Verify VAA shim program ID on Solana mainnet.
+        pub const VERIFY_VAA_SHIM_PROGRAM_ID_ARRAY: [u8;32] =
+            bs58::decode_pubkey("EFaNWErqAtVWufdNb7yofSHHfWFos843DFpu4JBw24at");
+
     }
 }
+
+pub const CORE_BRIDGE_PROGRAM_ID: Pubkey = Pubkey::new_from_array(CORE_BRIDGE_PROGRAM_ID_ARRAY);
+
+pub const CORE_BRIDGE_FEE_COLLECTOR: Pubkey = Pubkey::new_from_array(
+    ed25519::derive_program_address(&[crate::FEE_COLLECTOR_SEED], &CORE_BRIDGE_PROGRAM_ID_ARRAY).0,
+);
+
+pub const CORE_BRIDGE_CONFIG: Pubkey = Pubkey::new_from_array(
+    ed25519::derive_program_address(
+        &[crate::CORE_BRIDGE_CONFIG_SEED],
+        &CORE_BRIDGE_PROGRAM_ID_ARRAY,
+    )
+    .0,
+);
+
+pub const POST_MESSAGE_SHIM_PROGRAM_ID: Pubkey =
+    Pubkey::new_from_array(POST_MESSAGE_SHIM_PROGRAM_ID_ARRAY);
+
+const POST_MESSAGE_SHIM_EVENT_AUTHORITY_PDA: ([u8; 32], u8) = ed25519::derive_program_address(
+    &[crate::EVENT_AUTHORITY_SEED],
+    &POST_MESSAGE_SHIM_PROGRAM_ID_ARRAY,
+);
+
+pub const POST_MESSAGE_SHIM_EVENT_AUTHORITY: Pubkey =
+    Pubkey::new_from_array(POST_MESSAGE_SHIM_EVENT_AUTHORITY_PDA.0);
+
+pub const POST_MESSAGE_SHIM_EVENT_AUTHORITY_BUMP: u8 = POST_MESSAGE_SHIM_EVENT_AUTHORITY_PDA.1;
+
+pub const VERIFY_VAA_SHIM_PROGRAM_ID: Pubkey =
+    Pubkey::new_from_array(VERIFY_VAA_SHIM_PROGRAM_ID_ARRAY);
 
 /// Finality of the message (which is when the Wormhole guardians will attest to
 /// this message's observation).
