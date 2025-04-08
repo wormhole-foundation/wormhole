afcdff238c5d0ee9e658142fe2b45f25098afe34
John Saigle <john@asymmetric.re>
2025-04-08 09:11:42 -0400

    fix violation in watcher

--- node/pkg/watchers/ibc/watcher.go
+++ node/pkg/watchers/ibc/watcher.go
@@ -778,7 +778,12 @@ func (w *Watcher) queryChannelIdToChainIdMapping() (map[string]vaa.ChainID, erro
 			return nil, fmt.Errorf("channel map entry %d contains %d items when it should contain exactly two, json: %s", idx, len(entry), string(body))
 		}
 
-		channelIdBytes, err := base64.StdEncoding.DecodeString(entry[0].(string))
+		channelIdString, ok := entry[0].(string)
+		if !ok {
+			return nil, fmt.Errorf("error converting channelIdBytes to string")
+		}
+
+		channelIdBytes, err := base64.StdEncoding.DecodeString(channelIdString)
 		if err != nil {
 			return nil, fmt.Errorf("channel ID for entry %d is invalid base64: %s, err: %s", idx, entry[0], err)
 		}
