8a615fe77e34bbe028ef95851177d1cf85373e8d
John Saigle <john@asymmetric.re>
2025-04-09 11:34:33 -0400

    fix formatting

--- node/pkg/node/node.go
+++ node/pkg/node/node.go
@@ -91,7 +91,7 @@ type G struct {
 	gst                *common.GuardianSetState
 	acct               *accountant.Accountant
 	gov                *governor.ChainGovernor
-	notary          *notary.Notary
+	notary             *notary.Notary
 	gatewayRelayer     *gwrelayer.GatewayRelayer
 	queryHandler       *query.QueryHandler
 	publicrpcServer    *grpc.Server
