c302786e281605319cc50b09ac703a7901609f0b
John Saigle <john@asymmetric.re>
2025-04-09 11:34:33 -0400

    fix formatting

--- node/cmd/guardiand/node.go
+++ node/cmd/guardiand/node.go
@@ -297,7 +297,7 @@ var (
 	txVerifierChains []vaa.ChainID
 
 	// featureFlags are additional static flags that should be published in P2P heartbeats.
-	featureFlags []string
+	featureFlags  []string
 	notaryEnabled *bool
 )
 
