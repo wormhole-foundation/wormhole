d850bdfea836a8a6d17aeff78fc5e62ccb57b6e1
John Saigle <john@asymmetric.re>
2025-04-16 14:32:29 -0400

    deps: Replace godo/fswatch with fsnotify/fsnotify
    - The fswatch repository seems to be abandoned, whereas fsnotify is
      actively supported and appears to be popular.

--- node/cmd/ccq/query_server.go
+++ node/cmd/ccq/query_server.go
@@ -281,7 +281,12 @@ func runQueryServer(cmd *cobra.Command, args []string) {
 
 	// Start watching for permissions file updates.
 	errC := make(chan error)
-	permissions.StartWatcher(ctx, logger, errC)
+	permWatcherErr := permissions.StartWatcher(ctx, logger, errC)
+	if permWatcherErr != nil {
+		// Cleanup p2p connection.
+		cancel()
+		logger.Fatal("Could not start permissions file watcher", zap.Error(err))
+	}
 
 	// Star logging cleanup process.
 	loggingMap.Start(ctx, logger, errC)
