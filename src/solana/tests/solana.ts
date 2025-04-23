44ca5a63a781fe80a57b9f6359be3793b8e2710e
Sebastián Claudio Nale <sebinale@gmail.com>
2025-04-23 13:54:41 -0300

    solana: fix import in test

--- src/solana/tests/solana.ts
+++ src/solana/tests/solana.ts
@@ -1,12 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Solana } from "../target/types/solana";
+import { VerificationV2 } from "../target/types/verification_v2.js";
 
 describe("solana", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.solana as Program<Solana>;
+  const program = anchor.workspace.solana as Program<VerificationV2>;
 
   it("Is initialized!", async () => {
     // Add your test here.
