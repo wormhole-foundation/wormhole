227e9b5de6d7e8a7f5acbe3fc20bb68e2f6d3920
Bruce Riley <bruce@wormholelabs.xyz>
2025-07-03 10:05:27 -0500

    Node: Code review rework

--- node/pkg/watchers/evm/custom_consistency_level_test.go
+++ node/pkg/watchers/evm/custom_consistency_level_test.go
@@ -1,6 +1,8 @@
 package evm
 
 import (
+	"bytes"
+	"encoding/binary"
 	"encoding/hex"
 	"testing"
 
@@ -52,4 +54,41 @@ func TestCclParseConfigSuccessNothingSpecial(t *testing.T) {
 	default:
 		panic("unsupported query type")
 	}
+}
+
+func TestCclParseAdditionalBlocksConfigWrongLength(t *testing.T) {
+	// First verify our test works by reading valid data.
+	err := testCclParseAdditionalBlocksConfig(t, "01c9002a00000000000000000000000000000000000000000000000000000000")
+	require.NoError(t, err)
+
+	// Too short (deleted the last byte).
+	err = testCclParseAdditionalBlocksConfig(t, "01c9002a000000000000000000000000000000000000000000000000000000")
+	assert.ErrorContains(t, err, "unexpected remaining unread bytes in buffer, should be 28, are 27")
+
+	// Too long (added an extra byte).
+	err = testCclParseAdditionalBlocksConfig(t, "01c9002a0000000000000000000000000000000000000000000000000000000000")
+	assert.ErrorContains(t, err, "unexpected remaining unread bytes in buffer, should be 28, are 29")
+
+	// Way too short (part of num blocks is missing).
+	err = testCclParseAdditionalBlocksConfig(t, "01c900")
+	assert.ErrorContains(t, err, "failed to read num blocks")
+
+	// Really too short (consistency level is missing).
+	err = testCclParseAdditionalBlocksConfig(t, "01")
+	assert.ErrorContains(t, err, "failed to read consistency level")
+}
+
+func testCclParseAdditionalBlocksConfig(t *testing.T, str string) error {
+	t.Helper()
+	data, err := hex.DecodeString(str)
+	require.NoError(t, err)
+	reader := bytes.NewReader(data[:])
+
+	// Skip the request type
+	reqType := CCLRequestType(0)
+	require.NoError(t, binary.Read(reader, binary.BigEndian, &reqType))
+	require.Equal(t, AdditionalBlocksType, reqType)
+
+	_, err = cclParseAdditionalBlocksConfig(reader)
+	return err
 }
