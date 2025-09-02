ea2140c7d69e4f0cb03fce0582b67c2c41680500
stavrosvl7 <stavrosvl7@gmail.com>
2025-09-02 22:41:04 +0300

    generate publicrpc.pb.go

--- node/pkg/proto/publicrpc/v1/publicrpc.pb.go
+++ node/pkg/proto/publicrpc/v1/publicrpc.pb.go
@@ -7,13 +7,12 @@
 package publicrpcv1
 
 import (
-	reflect "reflect"
-	sync "sync"
-
 	v1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
