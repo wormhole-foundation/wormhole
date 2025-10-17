093d2a1f3ca3958e7b544f8e2d715bec2a036a2c
Aaron Clark <aaronpclark@protonmail.com>
2025-10-17 11:47:18 -0500

    fogo mainnet values in governor and sdk
    fogo tokenbridge emitter wormQuCVWSSmPdjVmEzAWxAXViVyTSWnLyhff5hVYGS as hex is 289e998e357c96dbfd8490b853595e0d48639ede2d1aed4a819edcc00165904c
    
    adds Fogo TokenBridge address to mainnet_consts
    
    adds Fogo config to governor
    
    adds Fogo to ignored chains in TestGovernedChainHasGovernedAssets

--- node/pkg/governor/mainnet_chains.go
+++ node/pkg/governor/mainnet_chains.go
@@ -40,5 +40,6 @@ func ChainList() []ChainConfigEntry {
 		{EmitterChainID: vaa.ChainIDMezo, DailyLimit: 500_000, BigTransactionSize: 50_000},
 		{EmitterChainID: vaa.ChainIDXRPLEVM, DailyLimit: 500_000, BigTransactionSize: 50_000},
 		{EmitterChainID: vaa.ChainIDLinea, DailyLimit: 500_000, BigTransactionSize: 50_000},
+		{EmitterChainID: vaa.ChainIDFogo, DailyLimit: 500_000, BigTransactionSize: 50_000},
 	}
 }
