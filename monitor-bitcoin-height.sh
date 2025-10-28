5dfedb301deb703ffb95e8bc10a60e6849472d12
janniks <janniks@users.noreply.github.com>
2025-10-28 13:02:08 +0100

    chore: remove unwanted file

--- monitor-bitcoin-height.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Monitor Bitcoin block height every second
-while true; do
-  HEIGHT=$(kubectl exec bitcoin-hacknet-0 -c bitcoin-node -n wormhole -- bitcoin-cli -rpcconnect=127.0.0.1 getblockcount 2>/dev/null || echo "ERROR")
-  TIMESTAMP=$(date '+%H:%M:%S')
-  echo "[$TIMESTAMP] Block height: $HEIGHT"
-  sleep 1
-done
