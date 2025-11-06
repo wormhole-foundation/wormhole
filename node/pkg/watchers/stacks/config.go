f7034753b8ea53e4caf99cecfdfd48e811bf2a8a
jannik-stacks <237011140+jannik-stacks@users.noreply.github.com>
2025-11-06 18:45:15 +0100

    fix: remove unused l1finalizers

--- node/pkg/watchers/stacks/config.go
+++ node/pkg/watchers/stacks/config.go
@@ -32,15 +32,6 @@ func (wc *WatcherConfig) GetChainID() vaa.ChainID {
 	return wc.ChainID
 }
 
-func (wc *WatcherConfig) RequiredL1Finalizer() watchers.NetworkID {
-	return ""
-}
-
-func (wc *WatcherConfig) SetL1Finalizer(l1finalizer interfaces.L1Finalizer) {
-	// empty
-}
-
-//nolint:unparam // L1Finalizer return is always nil for Stacks as it doesn't require L1 finalization
 func (wc *WatcherConfig) Create(
 	msgC chan<- *common.MessagePublication,
 	obsvReqC <-chan *gossipv1.ObservationRequest,
@@ -48,7 +39,7 @@ func (wc *WatcherConfig) Create(
 	_ chan<- *query.PerChainQueryResponseInternal, // queryResponseC - not used for Stacks
 	_ chan<- *common.GuardianSet, // setC - not used for Stacks
 	_ common.Environment, // env - not used for Stacks
-) (interfaces.L1Finalizer, supervisor.Runnable, interfaces.Reobserver, error) {
+) (supervisor.Runnable, interfaces.Reobserver, error) {
 	watcher := NewWatcher(wc.RPCURL, wc.RPCAuthToken, wc.StateContract, wc.BitcoinBlockPollInterval, msgC, obsvReqC)
-	return nil, watcher.Run, watcher, nil // L1Finalizer is nil for Stacks (no L1 finalization needed)
+	return watcher.Run, watcher, nil
 }
