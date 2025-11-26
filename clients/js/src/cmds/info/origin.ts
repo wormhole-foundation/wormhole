4b4352ce2dcf2483efbbe6fb3f83bd501564cd1a
kinsyudev <miguel@kinsyu.dev>
2025-11-26 16:26:31 +0100

    chore: remove obsolete patch comment from origin command

--- clients/js/src/cmds/info/origin.ts
+++ clients/js/src/cmds/info/origin.ts
@@ -42,16 +42,6 @@ export const handler = async (
 		argv.address,
 		argv.rpc,
 	);
-	const chainName = toChain(res.chainId);
-
-	/**
-	 * This is a ridiculous patch for the following issue
-	 * worm info origin ethereum 0xb4b9dc1c77bdbb135ea907fd5a08094d98883a35 -n mainnet
-	 * Error: uint8ArrayToNative: Use tryHexToNativeStringNear instead.
-	 *  at c1d (/Users/kinsyu/Documents/repos/wormhole/clients/js/build/main.js:650:91470)
-	 *  at Object.I36 [as handler] (/Users/kinsyu/Documents/repos/wormhole/clients/js/build/main.js:839:40058)
-	 *  at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
-	 */
 
 	console.log({
 		...res,
