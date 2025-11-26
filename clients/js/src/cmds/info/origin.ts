4b37b92a37e14c8e782a177b294167d6aecc6833
kinsyudev <miguel@kinsyu.dev>
2025-11-26 13:20:00 +0100

    fix: add rpc overriding to `info origin` command

--- clients/js/src/cmds/info/origin.ts
+++ clients/js/src/cmds/info/origin.ts
@@ -8,42 +8,44 @@ import { toChain } from "@wormhole-foundation/sdk-base";
 export const command = "origin <chain> <address>";
 export const desc = `Print the origin chain and address of the asset that corresponds to the given chain and address.`;
 export const builder = (y: typeof yargs) =>
-  y
-    .positional("chain", {
-      describe:
-        "Chain that wrapped asset came from. To see a list of supported chains, run `worm chains`",
-      type: "string",
-      demandOption: true,
-    } as const)
-    .positional("address", {
-      describe: "Address of wrapped asset on origin chain",
-      type: "string",
-      demandOption: true,
-    })
-    .option("network", {
-      alias: "n",
-      describe: "Network of target chain",
-      choices: ["mainnet", "testnet", "devnet"],
-      default: "mainnet",
-      demandOption: false,
-    } as const)
-    .option("rpc", RPC_OPTIONS);
+	y
+		.positional("chain", {
+			describe:
+				"Chain that wrapped asset came from. To see a list of supported chains, run `worm chains`",
+			type: "string",
+			demandOption: true,
+		} as const)
+		.positional("address", {
+			describe: "Address of wrapped asset on origin chain",
+			type: "string",
+			demandOption: true,
+		})
+		.option("network", {
+			alias: "n",
+			describe: "Network of target chain",
+			choices: ["mainnet", "testnet", "devnet"],
+			default: "mainnet",
+			demandOption: false,
+		} as const)
+		.option("rpc", RPC_OPTIONS);
 export const handler = async (
-  argv: Awaited<ReturnType<typeof builder>["argv"]>
+	argv: Awaited<ReturnType<typeof builder>["argv"]>,
 ) => {
-  const consoleWarnTemp = console.warn;
-  console.warn = () => {};
+	const consoleWarnTemp = console.warn;
+	console.warn = () => {};
 
-  const network = getNetwork(argv.network);
-  const res = await getOriginalAsset(
-    chainToChain(argv.chain),
-    network,
-    argv.address
-  );
-  console.log({
-    ...res,
-    assetAddress: tryUint8ArrayToNative(res.assetAddress, toChain(res.chainId)),
-  });
+	const network = getNetwork(argv.network);
 
-  console.warn = consoleWarnTemp;
+	const res = await getOriginalAsset(
+		chainToChain(argv.chain),
+		network,
+		argv.address,
+		argv.rpc,
+	);
+	console.log({
+		...res,
+		assetAddress: tryUint8ArrayToNative(res.assetAddress, toChain(res.chainId)),
+	});
+
+	console.warn = consoleWarnTemp;
 };
