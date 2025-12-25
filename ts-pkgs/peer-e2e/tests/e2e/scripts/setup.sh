605fe5b1d87b64e4ea1be4980e5610e8bdfbde30
NoiTaTuM <noit63@gmail.com>
2025-12-25 05:56:49 -0300

    Remove error ignore during DKG E2E test setup

--- ts-pkgs/peer-e2e/tests/e2e/scripts/setup.sh
+++ ts-pkgs/peer-e2e/tests/e2e/scripts/setup.sh
@@ -3,6 +3,6 @@
 set -meuo pipefail
 
 # Create a network for the guardians to comunicate
-docker network create dkg-test || true
+docker network create dkg-test
 # Create a builder that connects directly to the dkg-test network so it can be accessed during build.
 docker buildx create --name dkg-builder --driver docker-container --driver-opt network=dkg-test
