a0b7b0896a26b4ddcbbb24fe7afa4edd02167e3e
Fernando <fergarrui@gmail.com>
2026-01-12 10:59:42 +0000

    revert, rerun 2 guardians

--- wormchain/contracts/tools/test_ntt_accountant.sh
+++ wormchain/contracts/tools/test_ntt_accountant.sh
@@ -4,6 +4,4 @@ num=${NUM_GUARDIANS:-1} # default value for NUM_GUARDIANS = 1
 for ((i=0; i<num; i++)); do
     while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' guardian-$i.guardian:6060/readyz)" != "200" ]]; do sleep 5; done
 done
-# Give guardians extra time to process any pending VAAs and sync state
-sleep 30
 CI=true npm run test-ntt-accountant
