32fdc84bd62a1f3fcf7becf0c1ef2446431c18ea
Sebastián Claudio Nale <sebinale@gmail.com>
2026-01-14 17:46:29 -0300

    ci: adjusts e2e.sh a little bit to improve its behavior

--- verification-v2/ts-pkgs/peer-e2e/tests/e2e/e2e.sh
+++ verification-v2/ts-pkgs/peer-e2e/tests/e2e/e2e.sh
@@ -6,9 +6,14 @@ rm -rf out/
 
 # These scripts are meant to be run with this directory as the working directory
 
-./scripts/clean.sh
+if [[ -z "${GITHUB_ACTIONS:-}" ]]; then
+    ./scripts/clean.sh
+fi
 ./scripts/setup.sh
 ./scripts/anvil.sh &
 ./scripts/server.sh &
 ./scripts/client.sh
 ./scripts/clean.sh
+
+# Wait for anvil and server subshells to check that their exit codes are zero
+wait
