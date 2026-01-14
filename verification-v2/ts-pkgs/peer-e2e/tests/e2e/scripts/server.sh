6e308077612c8b79c5d3a50c2942cfede25cc995
Sebastián Claudio Nale <sebinale@gmail.com>
2026-01-14 14:24:36 -0300

    e2e: fixes timeout mechanism when waiting for anvil

--- verification-v2/ts-pkgs/peer-e2e/tests/e2e/scripts/server.sh
+++ verification-v2/ts-pkgs/peer-e2e/tests/e2e/scripts/server.sh
@@ -16,9 +16,11 @@ docker build --tag peer-server \
 
 # Wait until anvil starts listening
 docker run --rm --network=dkg-test --env "ETHEREUM_RPC_URL=$ETHEREUM_RPC_URL" --env "WORMHOLE_ADDRESS=$WORMHOLE_ADDRESS" ghcr.io/foundry-rs/foundry:v1.5.1@sha256:3a70bfa9bd2c732a767bb60d12c8770b40e8f9b6cca28efc4b12b1be81c7f28e '
-  deadline=$((SECONDS+60))
+  start=$(date +%s)
+  deadline=$((start+60))
   until cast call --rpc-url "$ETHEREUM_RPC_URL" "$WORMHOLE_ADDRESS" "getCurrentGuardianSetIndex()" >/dev/null 2>&1; do
-    if [ "$SECONDS" -ge "$deadline" ]; then
+    now=$(date +%s)
+    if [ "$now" -ge "$deadline" ]; then
       echo "Timed out waiting for $ETHEREUM_RPC_URL" >&2
       exit 1
     fi
