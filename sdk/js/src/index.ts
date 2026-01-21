9e5977e2f24e589ea4f9d94d941d407e28447fba
Evan Gray <56235822+evan-gray@users.noreply.github.com>
2026-01-21 10:09:57 -0500

    relayer: rm

--- sdk/js/src/index.ts
+++ sdk/js/src/index.ts
@@ -7,7 +7,6 @@ export * from "./vaa";
 
 export * as cosmos from "./cosmos";
 export * as ethers_contracts from "./ethers-contracts";
-export * as ethers_relayer_contracts from "./ethers-relayer-contracts";
 export * as solana from "./solana";
 export * as terra from "./terra";
 export * as rpc from "./rpc";
@@ -17,6 +16,5 @@ export * as token_bridge from "./token_bridge";
 export * as nft_bridge from "./nft_bridge";
 export * as algorand from "./algorand";
 export * as sui from "./sui";
-export * as relayer from "./relayer";
 
 export { postVaaSolana, postVaaSolanaWithRetry } from "./solana";
