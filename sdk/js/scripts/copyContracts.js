0cdb92ccc3e8401516d0cf26e6e21ba73fdb7003
Evan Gray <56235822+evan-gray@users.noreply.github.com>
2026-01-21 10:09:57 -0500

    relayer: rm

--- sdk/js/scripts/copyContracts.js
+++ sdk/js/scripts/copyContracts.js
@@ -1,7 +1,3 @@
 const copydir = require("copy-dir");
 console.log("Copying from ../../ethereum/ethers-contracts");
 copydir.sync("../../ethereum/ethers-contracts", "src/ethers-contracts");
-copydir.sync(
-  "../../relayer/ethereum/ethers-contracts",
-  "src/ethers-relayer-contracts"
-);
