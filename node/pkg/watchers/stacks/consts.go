0c64319f21dfa90364c3b884ed9de7bef00e9892
jannik-stacks <237011140+jannik-stacks@users.noreply.github.com>
2026-02-24 11:13:02 -0500

    chore: fix comment alignment

--- node/pkg/watchers/stacks/consts.go
+++ node/pkg/watchers/stacks/consts.go
@@ -3,9 +3,9 @@ package stacks
 const (
 	// Size and buffer limits
 	MaxClarityValueHexSize = 2 * 1024 * 1024 // 2MB in hex encoding (1MB decoded)
-	MaxPayloadSize         = 8192             // Maximum Wormhole message payload size in bytes
-	EmitterAddressSize     = 32               // Size of emitter address buffer in bytes
-	TransactionIDSize      = 32               // Size of Stacks transaction ID in bytes (64 hex chars)
+	MaxPayloadSize         = 8192            // Maximum Wormhole message payload size in bytes
+	EmitterAddressSize     = 32              // Size of emitter address buffer in bytes
+	TransactionIDSize      = 32              // Size of Stacks transaction ID in bytes (64 hex chars)
 )
 
 const (
