3e58fca50dd85ed32e799f68b4567fe24f192a18
John Saigle <john@asymmetric.re>
2026-04-17 08:32:09 -0400

    restore failing test; should fail due to reduced coverage

--- node/pkg/watchers/solana/client_test.go
+++ node/pkg/watchers/solana/client_test.go
@@ -26,7 +26,7 @@ import (
 
 func TestVerifyConstants(t *testing.T) {
 	// If either of these ever change, message publication and reobservation will break.
-	assert.NotEqual(t, SolanaAccountLen, solana.PublicKeyLength)
+	assert.Equal(t, SolanaAccountLen, solana.PublicKeyLength)
 	assert.Equal(t, SolanaSignatureLen, len(solana.Signature{}))
 }
 
