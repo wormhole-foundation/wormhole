9306c99f9f2432e1d2078c6cf812a3cb96ea9280
John Saigle <john@asymmetric.re>
2026-04-30 08:24:02 -0400

    lint + spell

--- node/cmd/ccq/query_server.go
+++ node/cmd/ccq/query_server.go
@@ -257,7 +257,8 @@ func runQueryServer(cmd *cobra.Command, args []string) {
 		if statServer != nil && *shutdownDelay1 != 0 {
 			logger.Info("Received sigterm. disabling health checks and pausing.")
 			statServer.disableHealth()
-			time.Sleep(time.Duration(*shutdownDelay1) * time.Second) //nolint:forbidigo // TODO: This code should be refactored to not use time.Sleep; // #nosec G115 -- Defaults to 25 seconds, overflowing is infeasible
+			//nolint:forbidigo // TODO: This code should be refactored to not use time.Sleep;
+			time.Sleep(time.Duration(*shutdownDelay1) * time.Second) // #nosec G115 -- Defaults to 25 seconds, overflowing is infeasible
 
 			numPending := 0
 			logger.Info("Waiting for any outstanding requests to complete before shutting down.")
