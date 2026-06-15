a7500b66f1ea66fa1765754efe1d73486ba14b95
Douglas Galico <douglas.galico@gmail.com>
2026-06-15 22:20:29 +0200

    feat: Arc mainnet support

--- node/pkg/watchers/evm/chain_config.go
+++ node/pkg/watchers/evm/chain_config.go
@@ -103,6 +103,7 @@ var (
 		vaa.ChainIDZeroGravity: {Finalized: true, Safe: true, EvmChainID: 16661, PublicRPC: "https://evmrpc.0g.ai", ContractAddr: "0xC699482c17d43b7D5349F2D3f58d61fEFA972B8c"},
 		vaa.ChainIDNexus:       {Finalized: true, Safe: true, EvmChainID: 3946, PublicRPC: "https://mainnet.rpc.nexus.xyz", ContractAddr: "0xC8aD24fC6063c41cB5C12a8e3851AafC3b3CF027"},
 		vaa.ChainIDTempo:       {Finalized: true, Safe: true, EvmChainID: 4217, PublicRPC: "https://rpc.tempo.xyz", ContractAddr: "0xbebdb6C8ddC678FfA9f8748f85C815C556Dd8ac6"},
+		vaa.ChainIDArc:         {Finalized: true, Safe: true, EvmChainID: 5042, PublicRPC: "https://rpc.arc.network", ContractAddr: "0xC8aD24fC6063c41cB5C12a8e3851AafC3b3CF027"},
 	}
 
 	// testnetChainConfig specifies the configuration for all chains enabled in Testnet.
